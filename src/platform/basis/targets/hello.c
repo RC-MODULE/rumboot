96e054e2d7df44ea7c8ab775b568591a2fffa0fb
Andrew Andrianov <andrew@ncrmnt.org>
2017-06-28 17:38:21 +0300

    basis: Fix hello test
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/basis/targets/hello.c
+++ src/platform/basis/targets/hello.c
@@ -4,16 +4,12 @@
 int main()
 {
     rumboot_print_logo();
-    rumboot_platform_raise_event(EVENT_PERF, 0);
-
-#if 0
-    /* This shit breaks everything */
-    rumboot_printf("rumboot: spl start @0x%x\n", rumboot_platform_spl_start);
-    rumboot_printf("rumboot: spl end   @0x%x\n", rumboot_platform_spl_end);
     rumboot_printf("rumboot: spl start: 0x%x end:0x%x\n",
         rumboot_platform_spl_start,
         rumboot_platform_spl_end);
-#endif
-
+    rumboot_printf("rumboot: stack @0x%x\n",
+            rumboot_platform_stack_top);
+    rumboot_printf("rumboot: Yarr! I need moar rum!");
+    /* Return 0, test passed */
     return 0;
 }
