122dd7324adab125c42370fca707feada4e5aa81
Andrew Andrianov <andrew@ncrmnt.org>
2017-07-11 15:10:47 +0300

    io.h: More ioread/iowrite functions, improved docs
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/rumboot/io.h
+++ include/rumboot/io.h
@@ -10,9 +10,49 @@
  * #include <rumboot/io.h>
  * \endcode
  *
+ * An more complex example is provided below:
+ *
+ * \code{.c}
+ * #include <rumboot/io.h>
+ *   uint32_t temp_data;  // All the variables should be declared in stack yet
+ *   iowrite32(0x000000DA, GPIO1_BASE + GPIO_DATA);
+ *   iowrite32(0x00000000, GPIO0_BASE + GPIO_DATA);
+ *   iowrite32(0x00000009, GPIO0_BASE + GPIO_DATA);
+ *     //
+ *   temp_data = ioread32(GPIO1_BASE + GPIO_DATA);
+ *
+ *   rumboot_printf("GPIO1=0x%x\n",temp_data);
+ *
+ *   if(temp_data == 0x000000DA)
+ *     return 0;
+ *   else
+ *     return 1;
+ *
+ * \endcode
  * \addtogroup memio
  *  @{
  */
+
+ /**
+  * Read a 64-bit value from memory location base_addr
+  * @param  base_addr
+  * @return value
+  */
+  static inline uint64_t ioread64(uint32_t const base_addr)
+  {
+      return *((volatile uint64_t*)(base_addr));
+  }
+
+ /**
+  * Write a 64-bit value to memory location base_addr
+  * @param value     value
+  * @param base_addr base_addr
+  */
+  static inline void iowrite64(uint64_t const value, uint32_t const base_addr)
+  {
+      *((volatile uint64_t*)(base_addr)) = value;
+  }
+
 
 /**
  * Read a 32-bit value from memory location base_addr
@@ -52,6 +92,27 @@
  static inline void iowrite16(uint16_t const value, uint32_t const base_addr)
  {
      *((volatile uint16_t*)(base_addr)) = value;
+ }
+
+
+ /**
+  * Read a 8-bit value from memory location base_addr
+  * @param  base_addr
+  * @return value
+  */
+ static inline uint8_t ioread8(uint32_t const base_addr)
+ {
+     return *((volatile uint8_t*)(base_addr));
+ }
+
+ /**
+  * Write a 8-bit value to memory location base_addr
+  * @param value     value
+  * @param base_addr base_addr
+  */
+ static inline void iowrite8(uint8_t const value, uint32_t const base_addr)
+ {
+     *((volatile uint8_t*)(base_addr)) = value;
  }
 
 /**
