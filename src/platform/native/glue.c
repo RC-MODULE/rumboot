fd0487b62878e7b4cfd22ba1544ee8e5fd9c772a
Andrew Andrianov <andrew@ncrmnt.org>
2017-08-15 19:28:38 +0300

    native: Add missing glue functions
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/native/glue.c
+++ src/platform/native/glue.c
@@ -9,11 +9,21 @@ uint32_t rumboot_platform_get_uptime()
     return 0;
 }
 
+void rumboot_platform_panic(const char *why, ...)
+{
+    va_list ap;
+    va_start(ap, why);
+    printf("PANIC: ");
+    vprintf(why, ap);
+    va_end(ap);
+    exit(1);
+}
 
 void rumboot_platform_trace(void *pc)
 {
-
+    /* stack tracing code here */
 }
+
 void rumboot_platform_raise_event(enum rumboot_simulation_event event, uint8_t arg)
 {
     printf("[EVENT] %d arg %d\n", event, arg);
