832d3bcf3c8bd7db6cff507c7268138a747cafaa
Andrew Andrianov <andrew@ncrmnt.org>
2017-08-17 16:51:41 +0300

    rum-clang-analyze: Do not launch firefox
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- run_clang_analyze.sh
+++ run_clang_analyze.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 export CCC_CC=arm-rcm-eabihf-gcc
 export CLANG_ANALYZER_TARGET=arm-rcm-eabihf
+rm -rf build-clang-analyze
 mkdir build-clang-analyze
 cd build-clang-analyze
-cmake .. -DRUMBOOT_PLATFORM=basis -DCLANG_ANALYZE=yes
+cmake .. -DRUMBOOT_PLATFORM=basis -DCLANG_ANALYZE=Yes
+make utils
 scan-build make
