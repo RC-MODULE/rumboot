01b283e24c0e5d4fc5c221715ea6daa866cfd691
Andrew Andrianov <andrew@ncrmnt.org>
2017-08-31 15:08:02 +0300

    basis: Fix hello.c
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/native/targets/hello.c
+++ src/platform/native/targets/hello.c
@@ -1,7 +1,25 @@
+#include <stdio.h>
 #include <rumboot/printf.h>
+#include <rumboot/irq.h>
+#include <rumboot/platform.h>
+
+void handler(void *arg)
+{
+    rumboot_platform_panic("Not implemented!\n");
+}
+
 int main()
 {
-    rumboot_print_logo();
+
+    struct rumboot_irq_entry *tbl = rumboot_irq_create(NULL);
+    rumboot_irq_set_handler(tbl, 3, handler, 123);
+    rumboot_irq_activate_table(tbl);
+
+    /* IIRQ ... */
+
+    rumboot_irq_activate_table(NULL);
+    rumboot_irq_free(tbl);
+
     rumboot_printf("Hello world: %d !\n", 1);
     return 0;
 }
