798d16c8c663acfe1d5aa5ef073bf8388d330286
Andrew Andrianov <andrew@ncrmnt.org>
2017-08-31 15:45:17 +0300

    rumboot: Do not print out stack info in hello.c
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/basis/targets/simple-rom/hello.c
+++ src/platform/basis/targets/simple-rom/hello.c
@@ -7,8 +7,6 @@ int main()
     rumboot_printf("rumboot: spl start: 0x%x end:0x%x\n",
         rumboot_platform_spl_start,
         rumboot_platform_spl_end);
-    rumboot_printf("rumboot: stack @0x%x\n",
-            rumboot_platform_stack_top);
     rumboot_printf("rumboot: Yarr! I need moar rum!");
     /* Return 0, test passed */
     return 0;
