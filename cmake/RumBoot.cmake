0cc3013cdbd84b14a9baa895c3ef1e91837fb117
Andrew Andrianov <andrew@ncrmnt.org>
2017-10-04 16:27:13 +0300

    cmake: Properly handle per-configuration ldflags
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- cmake/RumBoot.cmake
+++ cmake/RumBoot.cmake
@@ -73,7 +73,7 @@ endmacro()
 function(add_rumboot_target)
   set(options )
   set(oneValueArgs SNAPSHOT LDS NAME PREFIX CONFIGURATION)
-  set(multiValueArgs FILES IRUN_FLAGS CFLAGS TESTGROUP)
+  set(multiValueArgs FILES IRUN_FLAGS CFLAGS TESTGROUP LDFLAGS)
 
   cmake_parse_arguments(TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
