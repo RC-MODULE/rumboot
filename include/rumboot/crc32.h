dc2c28fa61076f704a2b5582bf7e74d8e8469445
Stolbikova Alina <a.stolbikova@module.ru>
2017-11-07 15:32:21 +0300

    Added defenition of bootheader's validation functions.
    Signed-off-by: stolbikova <a.stolbikova@module.ru>

--- include/rumboot/crc32.h
+++ include/rumboot/crc32.h
@@ -22,7 +22,7 @@
  * Calculates a CRC32 checksum of a buffer
  * @param  crc  Initial CRC32 value (use 0 for the first block)
  * @param  buf  The pointer the the buffer
- * @param  size The size of the buffer
+ * @param  size The size of the buffer in byte
  * @return      A 32-bit cyclic redundancy check value
  */
 
