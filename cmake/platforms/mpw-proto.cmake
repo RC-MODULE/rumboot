68d4d8831bee01f36694b8ebf365fec109d55670
Andrew Andrianov <andrew@ncrmnt.org>
2017-11-21 15:23:08 +0300

    mpw-proto: It compiles!
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- cmake/platforms/mpw-proto.cmake
+++ cmake/platforms/mpw-proto.cmake
@@ -1,5 +1,12 @@
 SET(RUMBOOT_ARCH mpw-proto)
 
+find_program(PACKIMAGE rumboot-packimage REQUIRED)
+
+if (PACKIMAGE-NOTFOUND)
+  message(FATAL_ERROR "Missing rumboot-packimage in your path. This ain't gonna work")
+endif()
+
+
 file(GLOB PLATFORM_SOURCES
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/*.c
     ${CMAKE_SOURCE_DIR}/src/lib/drivers/irq-dummy.c
@@ -9,8 +16,11 @@ file(GLOB PLATFORM_SOURCES
 
 #Flags for Power PC
 macro(RUMBOOT_PLATFORM_SET_COMPILER_FLAGS)
-    SET(CMAKE_DUMP_FLAGS     "-EL")
-    SET(CMAKE_C_FLAGS     "-std=gnu99 -m32")
+    set(RUMBOOT_COMMON_FLAGS "-mcpu=476fp -gdwarf-2 -m32 -ffreestanding -O0 -mbig-endian -std=c99 -fno-zero-initialized-in-bss")
+    set(CMAKE_C_FLAGS "${RUMBOOT_COMMON_FLAGS} -mstrict-align -Wall -fdata-sections -ffunction-sections ")
+    set(CMAKE_ASM_FLAGS ${RUMBOOT_COMMON_FLAGS})
+    set(CMAKE_EXE_LINKER_FLAGS "-fno-zero-initialized-in-bss -e boot_image_entry0 -Wl,--oformat=elf32-powerpc -Ttext 0x00040018 -static -nostartfiles -Wl,--gc-sections")
+    set(CMAKE_DUMP_FLAGS     -S -EB -M476,32)
 endmacro()
 
 #Add configuration for binaries
@@ -18,7 +28,7 @@ rumboot_add_configuration(
   MPW_PROTO
   DEFAULT
   LDS mpw-proto/ram.lds
-  LDFLAGS -Wl,--start-group -lgcc -lc -lm -Wl,--end-group 
+  LDFLAGS -Wl,--start-group -lgcc -lc -lm -Wl,--end-group
   FEATURES LUA
 )
 
@@ -26,6 +36,44 @@ function(RUMBOOT_PLATFORM_PRINT_SUMMARY)
 endfunction()
 
 macro(rumboot_platform_generate_stuff_for_taget product)
+
+  add_custom_target(
+    ${product}.img ALL
+    COMMAND ${PACKIMAGE} -t bootimage -i ${product}.bin -o ${product}.img
+    COMMENT "Packing image ${product}.img"
+    DEPENDS ${product}.bin
+  )
+
+  add_custom_target(
+    ${product}.edcl ALL
+    COMMAND ${PACKIMAGE} -t hostimage -i ${product}.bin -o ${product}.img
+    COMMENT "Packing image ${product}.edcl"
+    DEPENDS ${product}.bin
+  )
+
+  add_custom_target(
+    ${product}.sd ALL
+    COMMAND dd if=/dev/zero of=${product}.sd bs=1024 count=8;
+    COMMAND dd if=${product}.img of=${product}.sd bs=1024 seek=8
+    COMMENT "Creating SD card image ${product}.sd"
+    DEPENDS ${product}.img
+  )
+
+  add_custom_target(
+    ${product}.spi ALL
+    COMMAND dd status=none if=/dev/zero of=${product}.spi bs=1K count=4096
+    COMMAND dd status=none if=${product}.img of=${product}.spi conv=notrunc
+    COMMENT "Creating SPI flash image ${product}.spi"
+    DEPENDS ${product}.img
+  )
+
+  add_dependencies(${product}.all
+        ${product}.sd
+        ${product}.spi
+        ${product}.edcl
+        ${product}.img
+    )
+
 endmacro()
 
 macro(RUMBOOT_PLATFORM_ADD_COMPONENTS)
@@ -46,11 +94,6 @@ macro(rumboot_platform_setup_configuration)
 
 endmacro()
 
-set(RUMBOOT_COMMON_FLAGS "-mcpu=476fp -gdwarf-2 -m32 -ffreestanding -O0 -mbig-endian -std=c99 -fno-zero-initialized-in-bss")
-set(CMAKE_C_FLAGS "${RUMBOOT_COMMON_FLAGS} -mstrict-align -Wall -fdata-sections -ffunction-sections ")
-set(CMAKE_ASM_FLAGS ${RUMBOOT_COMMON_FLAGS})
-set(CMAKE_EXE_LINKER_FLAGS "-fno-zero-initialized-in-bss -e boot_image_entry0 -Wl,--oformat=elf32-powerpc -Ttext 0x00040018 -static -nostartfiles -Wl,--gc-sections")
-set(CMAKE_DUMP_FLAGS     -S -EB -M476,32)
 
 if (NOT CROSS_COMPILE)
   SET(CROSS_COMPILE powerpc-rcm-elf)
