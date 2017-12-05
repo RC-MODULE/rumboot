4c7f729fbcdcf361fcd0e90021742d056086e25f
Ekaterina Shulepova <e.shulepova@module.ru>
2017-12-05 17:01:28 +0300

    Added GPIO

---
+++ include/devices/gpio.h
@@ -0,0 +1,17 @@
+//BASE 
+#define GPIO0_BASE               0x01098000
+#define GPIO1_BASE               0x01099000
+#define GPIO2_BASE               0x0109A000
+#define GPIO3_BASE               0x0109B000
+#define GPIO4_BASE               0x0109C000
+
+#define IP                       0x000
+#define VERSION                  0x004
+#define DIRECTION                0x008
+#define WRITE_APBtoPAD           0x00C
+#define READ_PADtoAPB            0x010
+#define STATUS                   0x014
+#define MASK                     0x018
+#define SET1                     0x01C
+#define SET0                     0x020
+#define SOURCE                   0x024
---
+++ src/platform/basis/targets/simple-iram/gpio.c
@@ -0,0 +1,83 @@
+#include <rumboot/printf.h>
+#include <rumboot/platform.h>
+#include <rumboot/io.h>
+#include <devices/gpio.h>
+
+#define GPIO_OK    0 
+#define GPIO_ERROR 1
+
+int GPIO( uint8_t send_data, uint8_t direction, uint32_t base_gpio_addr)
+{
+  unsigned apb_to_pad;
+  unsigned pad_to_apb;
+  unsigned a; 
+  
+  iowrite32 (0xff, base_gpio_addr + SOURCE); 
+  iowrite32 (direction,base_gpio_addr + DIRECTION);  
+  iowrite32 (send_data,base_gpio_addr + WRITE_APBtoPAD);  
+  apb_to_pad = ioread32(base_gpio_addr + WRITE_APBtoPAD); 
+  pad_to_apb = ioread32(base_gpio_addr + READ_PADtoAPB);
+  #ifdef PRINT
+  rumboot_printf("pad_to_apb = x%x\n", pad_to_apb); 
+  #endif 
+ // ("0-3 pins = outputs\n");   
+ if (direction == 0x0f){   
+    a = pad_to_apb >> 4;
+    if(apb_to_pad == a){
+      rumboot_printf("true \n");
+      return GPIO_OK;  }   
+    else                          {   
+      rumboot_printf("error \n");
+      return GPIO_ERROR;          } }
+ else
+ // ("4-7 pins = outputs\n");   
+  {if (direction == 0xf0){ 
+    a = pad_to_apb << 4;  
+    if(apb_to_pad == a)         
+      rumboot_printf("true \n");
+      return GPIO_OK;           }   
+    else                         {   
+      rumboot_printf("error \n");
+      return GPIO_ERROR;         } 
+                         } 
+  // else              {
+  // return GPIO_ERROR;}
+ // }          
+}
+
+int data_dir (uint32_t base_gpio_addr)
+{
+ //uint8_t data;
+ //uint8_t dir;
+ #if 0
+ data = 0x00;
+ dir  = 0x0f; 
+ GPIO (data,dir,base_gpio_addr); 
+ data = 0x03;
+ dir  = 0x0f;
+ GPIO (data,dir,base_gpio_addr);
+ data = 0x0f;
+ dir  = 0x0f;
+ GPIO (data,dir,base_gpio_addr);
+ data = 0x00;
+ dir  = 0xf0;
+ GPIO (data,dir,base_gpio_addr);
+ data = 0x30;
+ dir  = 0xf0;
+ GPIO (data,dir,base_gpio_addr);
+ data = 0xf0;
+ dir  = 0xf0;
+ GPIO (data,dir,base_gpio_addr);
+#endif
+return 0;
+}
+
+int main () 
+{ data_dir(GPIO0_BASE);
+  data_dir(GPIO1_BASE);
+  data_dir(GPIO2_BASE);
+  data_dir(GPIO3_BASE);
+  data_dir(GPIO4_BASE);
+   
+  return 0;
+}  
