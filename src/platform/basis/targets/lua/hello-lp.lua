ecc137b2c197e9b64c02a41f99f642f0a0290105
Andrew Andrianov <andrew@ncrmnt.org>
2018-01-16 15:41:37 +0300

    hello-lp: Improve io test
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/basis/targets/lua/hello-lp.lua
+++ src/platform/basis/targets/lua/hello-lp.lua
@@ -62,4 +62,53 @@ function ramtest()
 end
 
 
-ramtest();
+
+function dumpv(ram, accfunc, sz, size)
+  for i=0,size,sz do
+    local v = ram[accfunc](ram, i);
+    print(string.format("[%x] = %x", i, v))
+  end
+end
+
+function check_read(ram, accfunc, sz, expected)
+  print("Checking reads with "..accfunc)
+  for i,j in ipairs(expected) do
+    pos = #expected * sz;
+    print("Reading "..pos-i*sz.." el "..i);
+    if (ram[accfunc](ram, pos-i*sz) ~= j) then
+      print(string.format("[0x%x] expected 0x%x got 0x%x",
+        pos-i*sz, expected[i], ram[accfunc](ram, pos-i*sz)))
+        dumpv(ram, accfunc, sz, 8);
+      error(sz.."-byte io doesn't work as expected")
+    end
+  end
+end
+
+
+
+function iotest()
+  print("I will test all transaction types")
+  local dev=lp:open("memories")
+  local ram=dev:request_mem("IM0")
+
+  expected = { 0x1e, 0xe7, 0xde, 0xad, 0xc0, 0xbe }
+  check_read(ram, "u8", 1, expected)
+
+
+  ram:u64(0, 0x1ee7deadc0be);
+
+  if (ram:u64(0) ~= 0x1ee7deadc0be) then
+    print(string.format("%x", ram:u64(0)));
+    error("64-bit io doesn't work as expected")
+  end
+
+  expected = {  0x1ee7, 0xdeadc0be }
+  check_read(ram, "u32", 4, expected)
+
+  expected = { 0x1ee7, 0xdead, 0xc0be}
+  check_read(ram, "u16", 2, expected)
+
+  print("done")
+end
+
+iotest();
