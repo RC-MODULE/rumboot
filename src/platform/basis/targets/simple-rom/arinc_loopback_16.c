4af7140ee15d83e186f31145124a55ca2319d432
Lyudmila Alexandrova <laalexan.module.ru>
2018-01-19 10:55:43 +0300

    change stop rcv condition

--- src/platform/basis/targets/simple-rom/arinc_loopback_16.c
+++ src/platform/basis/targets/simple-rom/arinc_loopback_16.c
@@ -1,85 +1,25 @@
+//-----------------------------------------------------------------------------
+//  This program is for checking ARINC429 transactions.
+// 
+//  Test includes:
+//  - 16 ARINC429 transmitters and receivers delivery through the external loopback
+//	- 16  receivers accept short arrays awithout checking labels 
+//  - configured  frequency =100KHz by default value
+//	- use delay control for neighboring arrays switch for correct read state registers
+//	- received arrays compared with ethalon values	
+//    Test duration (RTL): < 
+//-----------------------------------------------------------------------------
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+
 #include <rumboot/printf.h>
-#include <rumboot/platform.h>
 #include <rumboot/io.h>
-#include <platform/defs.h>
+#include <devices/arinc.h>
+#include <rumboot/irq.h>
+#include <platform/devices.h>
 
-
-#define CHANNEL_EN			0x0004
-#define CHANNEL_DIS			0x0008
-#define CHANNEL_RST			0x000C
-#define PARITY_BIT			0x0010
-#define PARITY_ODD			0x0014
-#define TEST_EN				0x0018
-
-#define WAIT_SIG_RX			0x001C
-#define TMR_MASK_RX			0x0020
-#define SW_SIG_RX			0x0024
-#define WAIT_TMR_TX			0x0028
-#define SW_SIG_TX			0x002C
-#define LABEL_EN_RX			0x0030
-#define AXI_CTRL			0x0034
-#define FIFO_EN_RX			0x0038
-#define LAST_WD_TMR			0x003C
-#define INT_TX				0x0040
-#define INT_RX				0x0044
-
-#define AG_E_RX				0x1880
-#define AG_O_RX				0x1900
-#define SZ_E_RX				0x1980
-#define SZ_O_RX				0x1a00
-
-
-#define TRF_E_TX			0x1380
-#define TLF_E_TX			0x1400
-#define TRL_E_TX			0x1480
-#define TRF_O_TX			0x1500
-#define TLF_O_TX			0x1580
-#define TRL_O_TX			0x1600
-
-#define AG_E_TX				0x1080
-#define AG_O_TX				0x1100
-#define SZ_E_TX				0x1180
-#define SZ_O_TX				0x1200
-
-#define STAT_E_TX			0x1280
-#define STAT_O_TX			0x1300
-
-#define STAT_E_RX			0x1A80
-#define STAT_O_RX			0x1B00
-#define TRF_E_RX			0x1B80
-#define TLF_E_RX			0x1C00
-#define TRL_E_RX			0x1C80
-#define TRF_O_RX			0x1D00
-#define TLF_O_RX			0x1D80
-#define TRL_O_RX			0x1E00
-#define LABEL_RX			0x1E80
-#define RNUM_RX				0x1F00
-
-#define FREQ_TX				0x0440
-#define FREQ_RX				0x0c40
-#define SELF_RX				0x0C80
-
-
-#define ARINC_OK			0
-#define ARINC_FAILED		1
-
-#define TEST_OK				0
-#define TEST_ERROR			1
-
-#define arlock				0x0
-#define arprot				0x0
-#define arcache				0x0
-#define awlock				0x0
-#define awprot				0x0
-#define awcache				0x0
-//#define receiver _number    0x0
-////#define tx_mem				0x40000
-//#define rx_mem				0x50000
-#define status_success_bit  (1 << 31)
-#define ARINC_ATTEMPT			10000
 static const int32_t tx_array32[] = {
         0x80818283,
         0x40414243,
@@ -122,7 +62,7 @@ void arinc_init (uint32_t arinc_base_addr){
 	uint32_t size;
 
 	init_axi_mst =0x0;
-	size   = 0x2;//0x10;
+	size   = 0x2;
 
 	uint32_t  tx_mem = 0x40000;
 	uint32_t  rx_mem = 0x50000;
@@ -145,6 +85,7 @@ void arinc_init (uint32_t arinc_base_addr){
 	}
 	iowrite32(init_axi_mst,(arinc_base_addr + AXI_CTRL)); 			// AXI parameters
 	iowrite32(0xffff,(arinc_base_addr + WAIT_TMR_TX));   			// wait  after the first  transmitter start
+	iowrite32(0xffff,(arinc_base_addr + WAIT_SIG_RX));   			// wait delayed switch after the receiver start
 //--------------------------------------------------------------------------------------
 }
 
@@ -174,11 +115,12 @@ int main()
 	//check setting of the end of transaction delivery
 	for (i = 0; i< 16 ; i++)
 	{
-	rumboot_printf("ARINC CH=0x%x\n", i); 	
+	rumboot_printf("ARINC CH=0x%x\n", i); 
+	tmp_r = -1;
 	//while (tmp != 0x00000001) {
-       while (tmp != 0x00000002) {
+       while (tmp_r != 0x00000002) {
 	tmp = ioread32(ARINC_BASE + STAT_E_TX + i*4);
-	tmp = 0x07FFFFFF & tmp;
+	tmp_r = 0x07FFFFFF & tmp;
     //rumboot_printf("ARINC SIZE=0x%x\n", tmp); //check status
 		}   
  	//rumboot_printf("rcv_channel_num=0x%x\n", (1 << i));
@@ -189,13 +131,16 @@ int main()
 	 
 	for (i = 0; i< 16 ; i++) 
 	{
+	rumboot_printf("ARINC number=0x%x\n", i); 
+	tmp_r = -1;	
 	while (tmp_r != status_success_bit) {
 	tmp = ioread32(ARINC_BASE + STAT_E_RX +i*4);
 	//rumboot_printf("ARINC STATUS=0x%x\n", tmp); //check status
 	tmp_r = tmp & status_success_bit;
 	if (++cnt == ARINC_ATTEMPT) {
             rumboot_printf("No end exchange!\n");
-           // return ARINC_FAILED;
+ 			rumboot_printf("ARINC test ERROR!\n");
+           return TEST_ERROR;
 			}
 		}
 	}
