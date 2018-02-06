58f051bfe7fa4c897154ad361f17c8dba17c5c33
Andrew Andrianov <andrew@ncrmnt.org>
2018-02-06 13:01:47 +0300

    rumboot: Implement rumboot profiling
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/trace.c
+++ src/lib/trace.c
@@ -1,15 +1,30 @@
+#include <stdio.h> 
 #include <rumboot/platform.h>
 #include <rumboot/printf.h>
-
 /* These will be used when call tracing is enabled */
 
 void __attribute__((no_instrument_function)) __cyg_profile_func_enter(void *this_fn, void *call_site)
 {
+#ifdef RUMBOOT_TRACE
     rumboot_platform_runtime_info.nestlevel++;
     rumboot_platform_trace(this_fn);
+#endif
+
+#ifdef RUMBOOT_PROFILE
+    rumboot_platform_perf_func(this_fn);
+#endif
 }
 
-void __attribute__((no_instrument_function)) __cyg_profile_func_exit(void *this_fn, void *call_site)
+void __attribute__((no_instrument_function))
+    __cyg_profile_func_exit(void *this_fn, void *call_site)
 {
+
+#ifdef RUMBOOT_TRACE
     rumboot_platform_runtime_info.nestlevel--;
+#endif
+
+#ifdef RUMBOOT_PROFILE
+    rumboot_platform_perf_func(NULL);
+#endif
+
 }
