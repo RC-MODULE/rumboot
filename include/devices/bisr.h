1db24c404d9ce6793b2343d52889b8acd957c9c8
Leonid Shchigorev <l.shchigorev@module.ru>
2018-02-16 15:07:34 +0300

    bisr_hard and bisr_program tests were updated

--- include/devices/bisr.h
+++ include/devices/bisr.h
@@ -6,6 +6,12 @@
 #define BISR_ERROR_VECTOR_LOW_ARR0 0x08
 #define BISR_ERROR_VECTOR_HIGH_ARR0 0x0c
 
+#define FCA1 0x00
+#define CRE1 0x05
+#define FCA2 0x06
+#define CRE2 0x0b
+
+
 #include <stdbool.h>
 
 bool bisr_program_test();
