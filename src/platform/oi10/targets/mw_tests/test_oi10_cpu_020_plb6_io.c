2c576295737299461df05b73d60111d604d665b1
Victor Strukov <v.strukov@module.ru>
2018-03-31 22:50:14 +0300

    oi10_rumboot: changed .c files of tests according to test_pim

--- src/platform/oi10/targets/mw_tests/test_oi10_cpu_020_plb6_io.c
+++ src/platform/oi10/targets/mw_tests/test_oi10_cpu_020_plb6_io.c
@@ -12,7 +12,7 @@ int main()
     rumboot_printf("rumboot: spl start: 0x%x end: 0x%x\n",
                &rumboot_platform_spl_start,
                &rumboot_platform_spl_end);
-    rumboot_printf("test_oi10_cpu_020\n\n");
+    rumboot_printf("test_oi10_cpu_020_plb6_io\n\n");
 #endif
     return 0;
 }
