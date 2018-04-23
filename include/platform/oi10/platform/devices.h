8c6d5757cae36021bb40295e3f662ff508944045
Victor Strukov <v.strukov@module.ru>
2018-04-23 10:57:40 +0300

    oi10_rumboot: fixed devices.h

--- include/platform/oi10/platform/devices.h
+++ include/platform/oi10/platform/devices.h
@@ -1,14 +1,23 @@
 #ifndef OI10_DEVICES_H
 #define OI10_DEVICES_H
 
-#define BOOTROM_BASE            0xFFFC0000
-#define BOOTROM_SIZE            0x00040000
+#define BOOTROM_BASE            0xFFFF0000
+#define BOOTROM_SIZE            0x00010000
 
-#define BOOTROM_MIRROR_BASE     0x00000000
-#define BOOTROM_MIRROR_SIZE     0x00040000
+#define IM0_BASE                0xС0010000
+#define IM0_SIZE                0x00020000
 
-#define IM0_BASE                0x00040000
-#define IM0_SIZE                0x00040000
+#define IM1_BASE                0xD0000000
+#define IM1_SIZE                0x00020000
+
+#define EM0_BASE                0x80000000
+#define EM0_SIZE                0x20000000
+
+#define EM1_BASE                0xA0000000
+#define EM1_SIZE                0x20000000
+
+#define EM2_BASE                0x00000000
+#define EM2_SIZE                0x80000000
 
 //WD
 #define WD_BASE                 0x38002000
