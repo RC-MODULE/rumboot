e0ccc3bcf549176dccd8ac8983a7ca90d7330602
Victor Strukov <v.strukov@module.ru>
2018-04-26 14:10:19 +0300

    oi10_rumboot: created dcr_read/dcr_write functions, added comments regarding context-synchronizing inline assembler macros according to http://git.module.ru/verification-components/RumBoot/merge_requests/18#note_25445 and http://git.module.ru/verification-components/RumBoot/merge_requests/18#note_25503

--- include/platform/oi10/platform/arch/ppc/ppc_476fp_lib_c.h
+++ include/platform/oi10/platform/arch/ppc/ppc_476fp_lib_c.h
@@ -6,6 +6,50 @@
 #define MSR_EE              48
 #define MSR_CE              46
 
+
+/*DCR bus access*/
+static inline void dcr_write( uint32_t const addr, uint32_t const wval )
+{
+    asm volatile
+    (
+        "mtdcrx %0, %1 \n\t"
+        ::"r"(addr), "r"(wval)
+    );
+}
+
+static inline uint32_t dcr_read( uint32_t const addr )
+{
+    uint32_t rval=0;
+    asm volatile
+    (
+        "mfdcrx %0, %1 \n\t"
+        :"=r"(rval)
+        :"r"(addr)
+    );
+    return rval;
+}
+
+/*MSR access*/
+static inline void msr_write( uint32_t const wval )
+{
+    asm volatile
+    (
+        "mtmsr %0 \n\t"
+        ::"r"(wval)
+    );
+}
+
+static inline uint32_t msr_read()
+{
+    uint32_t rval=0;
+    asm volatile
+    (
+        "mfmsr %0 \n\t"
+        :"=r"(rval)
+    );
+    return rval;
+}
+
 /*DCR bus access*/
 static inline void mtdcrx( uint32_t const addr, uint32_t const wval )
 {
@@ -49,6 +93,54 @@ static inline uint32_t mfmsr()
     return rval;
 }
 
+inline static void dcbi( void* const addr )
+{
+    asm volatile
+    (
+        "dcbi 0, %0\n\t"
+        ::"r"(addr)
+    );
+}
+
+inline static void dcbz( void* const addr )
+{
+    asm volatile
+    (
+        "dcbz 0, %0\n\t"
+        ::"r"(addr)
+    );
+}
+
+inline static void dcbst( void* const addr )
+{
+    asm volatile
+    (
+        "dcbst 0, %0\n\t"
+        ::"r"(addr)
+    );
+}
+
+inline static void sc()
+{
+    asm volatile
+    (
+        "sc\n\t"
+    );
+}
+
+inline static void trap()
+{
+    asm volatile
+    (
+        "trap\n\t"
+    );
+}
+
+/* These instruction calls must be placed in the same function
+ * where called.
+ * It cannot be wrapped within an inline function, because
+ * inlining function is not a must-do but only recommendation
+ * to the compiler*/
 #define rfi()\
     asm volatile\
     (\
@@ -105,55 +197,10 @@ static inline uint32_t mfmsr()
         ::"i"(CT)\
     )
 
-inline static void dcbi( void* const addr )
-{
-    asm volatile 
-    (
-        "dcbi 0, %0\n\t"
-        ::"r"(addr)
-    );
-}
-
-inline static void dcbz( void* const addr )
-{
-    asm volatile
-    (
-        "dcbz 0, %0\n\t"
-        ::"r"(addr)
-    );
-}
-
-inline static void dcbst( void* const addr )
-{
-    asm volatile
-    (
-        "dcbst 0, %0\n\t"
-        ::"r"(addr)
-    );
-}
-
-inline static void sc() 
-{
-    asm volatile
-    (
-        "sc\n\t"
-    );
-}
-
-inline static void trap() 
-{
-    asm volatile
-    (
-        "trap\n\t"
-    );
-}
-
 #define nop()\
     asm volatile\
     (\
         "nop \n\t"\
     )
-
-
 
 #endif  /* PPC_476FP_LIB_C_H*/
