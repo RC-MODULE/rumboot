41e76552048b981c941336958f0af05664a96d15
Victor Strukov <v.strukov@module.ru>
2018-04-26 11:50:02 +0300

    oi10_rumboot: moved plb6plb4 library to src/platform/oi10/lib/drivers due to http://git.module.ru/verification-components/RumBoot/merge_requests/18#note_25497

--- cmake/platforms/oi10.cmake
+++ cmake/platforms/oi10.cmake
@@ -4,7 +4,7 @@ file(GLOB PLATFORM_SOURCES
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/*.c
     ${CMAKE_SOURCE_DIR}/src/lib/drivers/irq-dummy.c
     ${CMAKE_SOURCE_DIR}/src/lib/eventsystem-memory.c
-    ${CMAKE_SOURCE_DIR}/src/lib/drivers/p64.c
+    ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/lib/drivers/p64.c
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/ppc_mmu_impl.S
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/utlb_entries.S
 )
