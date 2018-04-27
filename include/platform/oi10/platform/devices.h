3ad60469b9d8bafc5d80cd6c7e8c0652a3409f67
Andrew Andrianov <andrew@ncrmnt.org>
2018-04-27 12:49:35 +0300

    oi10: Fix documentation in devices.h
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/platform/oi10/platform/devices.h
+++ include/platform/oi10/platform/devices.h
@@ -5,39 +5,43 @@
 
 /**
  * \defgroup platform_oi10_register_map Register Map
- *
  * \ingroup platform_oi10
  *
- * \addtogroup platform_oi10_register_map
- *
- *
- * @{
  *
  * This file contains physical addresses of devices present in SoC.
  * For developers: Please, keep the list sorted by device address
  * and formatted properly.
  *
  * WARNING: DO NOT ADD INDIVIDUAL REGISTER OFFSETS HERE
+ *
+ * \defgroup platform_oi10_register_map_virt MMIO registers
+ * \ingroup platform_oi10_register_map
+ * \defgroup platform_oi10_register_map_dcr DCR registers
+ * \ingroup platform_oi10_register_map
+ * \addtogroup platform_oi10_register_map_virt
+ * @{
+ *
+ *
  */
 
-/*PPC virtual addresses of memories, remapped in UTLB PLB6 addresses*/
 #define EM2_BASE                0x00000000
-
 #define EM0_BASE                0x80000000
-
 #define EM1_BASE                0xA0000000
-
-#define IM0_BASE                0xС0010000
-
+#define IM0_BASE                0xC0010000
 #define IM1_BASE                0xD0000000
-
 #define BOOTROM_BASE            0xFFFF0000
 
-/*DCR address space*/
+/**
+ *
+ * @}
+ * \addtogroup platform_oi10_register_map_dcr
+ * @{
+ *
+ *
+ */
+
 #define DCR_PLB6_BC_BASE            0x80000200
-
 #define DCR_PLB6PLB4_0_BASE         0x80000300
-
 /**
  * @}
  */
