99a7f01009a6cbdb4e61eee16a0f1f500595b663
Lyudmila Alexandrova <laalexan.module.ru>
2018-05-04 20:18:19 +0300

    add i2c EEPROM tests + arinc irq loopback wait rx tests

---
+++ include/devices/arinc.h
@@ -0,0 +1,79 @@
+
+#define CHANNEL_EN			0x0004
+#define CHANNEL_DIS			0x0008
+#define CHANNEL_RST			0x000C
+#define PARITY_BIT			0x0010
+#define PARITY_ODD			0x0014
+#define TEST_EN				0x0018
+
+#define WAIT_SIG_RX			0x001C
+#define TMR_MASK_RX			0x0020
+#define SW_SIG_RX			0x0024
+#define WAIT_TMR_TX			0x0028
+#define SW_SIG_TX			0x002C
+#define LABEL_EN_RX			0x0030
+#define AXI_CTRL			0x0034
+#define FIFO_EN_RX			0x0038
+#define LAST_WD_TMR			0x003C
+#define INT_TX				0x0040
+#define INT_RX				0x0044
+#define COMPL_E_RX			0x0048
+#define COMPL_O_RX			0x004c
+#define COMPL_TX			0x0050
+#define INT_DIS				0x0054
+#define INT_LONG			0x0058
+#define STUS_SW_TX			0x005C
+#define STUS_SW_RX			0x0060
+#define TEST_FRC_TS			0x0064
+
+#define AG_E_TX				0x1080
+#define AG_O_TX				0x1100
+#define SZ_E_TX				0x1180
+#define SZ_O_TX				0x1200
+
+#define STAT_E_TX			0x1280
+#define STAT_O_TX			0x1300
+
+#define TRF_E_TX			0x1380
+#define TLF_E_TX			0x1400
+#define TRL_E_TX			0x1480
+#define TRF_O_TX			0x1500
+#define TLF_O_TX			0x1580
+#define TRL_O_TX			0x1600
+
+#define AG_E_RX				0x1880
+#define AG_O_RX				0x1900
+#define SZ_E_RX				0x1980
+#define SZ_O_RX				0x1a00
+
+#define STAT_E_RX			0x1A80
+#define STAT_O_RX			0x1B00
+#define TRF_E_RX			0x1B80
+#define TLF_E_RX			0x1C00
+#define TRL_E_RX			0x1C80
+#define TRF_O_RX			0x1D00
+#define TLF_O_RX			0x1D80
+#define TRL_O_RX			0x1E00
+#define LABEL_RX			0x1E80
+#define RNUM_RX				0x1F00
+
+#define FREQ_TX				0x0440
+#define FREQ_RX				0x0c40
+#define SELF_RX				0x0C80
+
+
+#define ARINC_OK			0
+#define ARINC_FAILED		1
+
+#define TEST_OK				0
+#define TEST_ERROR			1
+
+#define arlock				0x0
+#define arprot				0x0
+#define arcache				0x0
+#define awlock				0x0
+#define awprot				0x0
+#define awcache				0x0
+
+#define status_success_bit  (1 << 31)
+#define ARINC_ATTEMPT			10000
---
+++ src/platform/basis/targets/simple-iram/arinc_irq_loopback_wait_tx.c
@@ -0,0 +1,209 @@
+//-----------------------------------------------------------------------------
+//  This program is for checking ARINC429 transactions.
+// 
+//  Test includes:
+//  - ARINC429 transmitters and receivers delivery through the external loopback
+//	-  Receiver accept short arrays awithout checking labels 
+//  - configured  frequency =100KHz
+//	- received array is compared with ethalon values	
+//    Test duration (RTL): < 
+//-----------------------------------------------------------------------------
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <rumboot/printf.h>
+#include <rumboot/io.h>
+#include <devices/arinc.h>
+#include <rumboot/irq.h>
+#include <platform/devices.h>
+#include <rumboot/io.h>
+#include <rumboot/testsuite.h>
+#include <rumboot/platform.h>
+#include <platform/interrupts.h>
+
+#define tx_mem				0x40000
+#define rx_mem				0x50000
+
+#define tx_mem_double		0x60000
+#define rx_mem_double		0x70000
+
+static const int32_t tx_array32[] = {
+        0x80818283,
+        0x40414243,
+        0x20212223,
+		0x10111213,
+        0x08090a0B,
+		0x04152637,
+        0x02132456,
+        0x01122334,
+        0x00112233,
+        0x7F8091AB,
+		0x8F90A1B2,
+		0xDF90A1B2,
+        0xE090A1B2,
+        0xEF90A1B2,
+        0xF790A1B2,
+        0xFB90A1B2,
+		0xFFFEFDFC
+};
+
+static const int32_t tx_array_double[] = {
+        0x10818283,
+        0x20414243,
+        0x40212223,
+		0x80111213,
+        0xF8090a0B,
+		0xF4152637,
+        0xF2132456,
+        0xF1122334,
+        0xF0112233,
+        0x1F8091AB,
+		0x7F90A1B2,
+		0x2F90A1B2,
+        0x1090A1B2,
+        0x1F90A1B2,
+        0x0790A1B2,
+        0x0B90A1B2,
+		0x0FFEFDFC
+};
+
+volatile uint32_t expected = 0;
+volatile uint32_t irq_arrived = 0;
+
+
+static void handler(int irq, void *arg)
+{
+	uint32_t int_status;
+	//uint32_t tmp_r;
+	uint32_t tmp;
+	
+	
+	irq_arrived++;
+	
+	//int_status = ioread32(ARINC_BASE + STAT_E_RX);
+	//rumboot_printf("ARINC STATUS=0x%x\n", int_status); //check status
+	//int_status = int_status & status_success_bit;
+	//rumboot_printf("STATUS=0x%x\n" , int_status);
+		
+//-------------------------------------------	
+	int_status = -1;
+	while (int_status != 0x00000002) {
+	tmp = ioread32(ARINC_BASE + STAT_E_TX);
+	rumboot_printf("ARINC SIZE=0x%x\n", tmp); //check status
+	int_status = 0x07FFFFFF & tmp;	
+  // rumboot_printf("ARINC SIZE=0x%x\n", tmp); //check status
+	}
+	rumboot_printf("ARINC SIZE=0x%x\n", tmp);
+//--------------------------------------------						
+	if ( int_status != expected) {
+	rumboot_platform_panic("IRQ: expected %x got %x\n", expected, int_status);
+	}
+	iowrite32(0x1,(ARINC_BASE + INT_DIS));	 //INT RESET	
+}		
+void arinc_init (uint32_t arinc_base_addr){
+	uint32_t receiver_number;
+
+	uint32_t  init_axi_mst;
+	uint32_t size;
+
+	init_axi_mst =0x0;
+	size   = 0x2;
+
+	//void *tx_mem =rumboot_malloc(8192);
+	receiver_number = 0x0;
+//-------------------------------------------------------------------------------------
+//  Set parameters for DMA exchange loopback  connected on chip pins in testbench
+//-------------------------------------------------------------------------------------
+
+	iowrite32(receiver_number,(arinc_base_addr + RNUM_RX)); //receiver ch number
+	iowrite32(tx_mem,(arinc_base_addr + AG_E_TX));    	// dma rd channel memory address
+	iowrite32(size,(arinc_base_addr + SZ_E_TX));    	// dma rd channel size
+	iowrite32(rx_mem,(arinc_base_addr + AG_E_RX));    	// dma wr channel memory address
+	iowrite32(size,(arinc_base_addr + SZ_E_RX));    	// dma wr channel size
+	
+	iowrite32(tx_mem_double,(arinc_base_addr + AG_O_TX));   // dma rd channel memory address
+	iowrite32(size,(arinc_base_addr + SZ_O_TX));    	    // dma rd channel size
+	iowrite32(rx_mem_double,(arinc_base_addr + AG_O_RX));   // dma wr channel memory address
+	iowrite32(size,(arinc_base_addr + SZ_O_RX));    	    // dma wr channel size
+	
+	iowrite32(init_axi_mst,(arinc_base_addr + AXI_CTRL)); //AXI parameters
+	iowrite32(0x1,(arinc_base_addr + WAIT_TMR_TX));   			// wait  delayed switch after the transmitter start
+	//iowrite32(0x1,(arinc_base_addr + WAIT_SIG_RX));   			// wait delayed switch after the receiver start
+	iowrite32(0x1,(arinc_base_addr + INT_TX));			 //INT TX
+	//iowrite32(0x1,(arinc_base_addr + INT_RX));			//INT RX
+	iowrite32(0x1,(arinc_base_addr + INT_LONG));		//INT level
+	
+//--------------------------------------------------------------------------------------
+}
+
+static void call_arinc_irq_even_array()
+{
+	uint32_t enable;
+	enable =0x10001;
+ 
+	arinc_init(ARINC_BASE);
+    iowrite32(enable,ARINC_BASE + CHANNEL_EN); // run transaction
+	//iowrite32(enable,ARINC_BASE + CHANNEL_DIS); // stop transmitter
+	rumboot_printf("ARINC START CH0\n");									
+}
+void wait_irq() {
+	while(!irq_arrived) { };
+	irq_arrived = 0;;
+}
+
+void arinc_stop() {
+uint32_t enable = 0x10001;	
+iowrite32(enable,ARINC_BASE + CHANNEL_DIS); // stop transmitter	
+}
+static bool test_arinc_irq_even_array(uint32_t arg)
+{
+	expected = 0x00000002;
+	call_arinc_irq_even_array();
+	wait_irq() ;
+	arinc_stop();	
+	rumboot_printf("Interrupt expected=0x%x\n" , expected);	
+	return true;
+}
+/* Declare the testsuite structure */
+TEST_SUITE_BEGIN(arinc_irq_test, "ARINC and IRQ Subsystem")
+TEST_ENTRY("ARINC_IRQ_EVEN_ARRAY", 	test_arinc_irq_even_array, 	  1000),
+TEST_SUITE_END();
+
+/* Call the whole test suite */
+int main()
+{
+	
+	uint32_t *trg 		 = (uint32_t *) 0x40000;
+	uint32_t *trg_double = (uint32_t *) 0x60000;
+
+	rumboot_printf("copy %d bytes\n", sizeof(tx_array32));  //transmi
+	memcpy(trg, tx_array32,sizeof(tx_array32));
+
+	rumboot_printf("copy %d bytes\n", sizeof(tx_array_double));  //transmi
+	memcpy(trg_double, tx_array_double,sizeof(tx_array32));
+	
+	rumboot_irq_cli(); //Disable all interrupts
+	
+	struct rumboot_irq_entry *tbl = rumboot_irq_create(NULL); //Create an IRQ table	
+	
+	rumboot_irq_set_handler(tbl, ARINC_INT, 0, handler, NULL); //Configure handler
+	
+	rumboot_irq_table_activate(tbl); //activate the table
+
+	//rumboot_irq_enable(0); //enable IRQ
+
+	rumboot_irq_sei(); //allow interrupt handling
+
+    int ret = test_suite_run(NULL, &arinc_irq_test); //run irq test suite
+    rumboot_printf("%d tests from suite failed\n", ret);
+
+	/* Deinit */
+	rumboot_irq_table_activate(NULL);
+	rumboot_irq_free(tbl);
+
+    return ret;
+}
