cacb8dc36bf1b5113e59d9a15e7f1643c35823b1
Alexander Gurov <a.gurov@modulew.ru>
2018-05-18 16:00:53 +0300

    oi10_rumboot: fix check_value macro.

--- include/platform/oi10/platform/arch/ppc/test_macro_asm.S.h
+++ include/platform/oi10/platform/arch/ppc/test_macro_asm.S.h
@@ -1,17 +1,17 @@
 #ifndef TEST_MACRO_ASM_H_
 #define TEST_MACRO_ASM_H_
 
+/* Define in test, if you need it.
 #define DEBUG_DCR_TEST
-//DCR register value checker
+*/
+/* DCR register value checker */
 .macro check_value rE,rA,rR,raddress,exp_value,reg_name
     load_const \rE, \exp_value
     load_const \rA, \raddress
     mfdcrx \rR, \rA
     cmp cr7,0, \rR,\rE
-    rumboot_putstring "\reg_name actual value: "
-#ifdef NO_TRACE_HEX
-    // trace_hex \rR
-#endif
+/* 	rumboot_putstring "\reg_name actual value: " */
+/*	trace_hex \rR */
     TEST_ASSERT(eq,cr7,"DCRE: expected \reg_name = \exp_value (\raddress) ")
 #ifdef DEBUG_DCR_TEST
     rumboot_putstring "\reg_name OK\n"
@@ -19,4 +19,4 @@
 .endm
 
 
-#endif //TEST_MACRO_ASM_H_
+#endif /* TEST_MACRO_ASM_H_ */
