78af032265afcd66b78fdb2b92687edbc844c431
Alexander Gurov <a.gurov@modulew.ru>
2018-06-13 09:17:57 +0300

    oi10_rumboot: change default (reset) reg value.

--- include/platform/oi10/platform/regs/regs_plb4arb8m.h
+++ include/platform/oi10/platform/regs/regs_plb4arb8m.h
@@ -15,8 +15,10 @@
 #define PEARL				0x6
 #define PEARH				0x7
 
+/* units/cpu/units/ppc_chip/verilog/rtl/PPC470S_TOP111323221111.v */
+#define PACR_RESET_VAL		0x9B000000
 #define PREV_RESET_VAL		0x00000301
-#define PACR_RESET_VAL		0x0B000000
+// #define PACR_RESET_VAL		0x0B000000
 #define PESR_RESET_VAL		0x00000000
 #define PEARL_RESET_VAL		0x00000000
 #define PEARH_RESET_VAL		0x00000000
