88509882229f11ef991d4c18a20611e5810131f4
Maksim Dubinkin <m.dubinkin.@modulew.ru>
2018-06-20 11:07:55 +0300

    oi10_rumboot: test_oi10_cpu_018 is corrected

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_018.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_018.c
@@ -9,19 +9,18 @@
 #include <rumboot/io.h>
 #include <rumboot/printf.h>
 
-static uint8_t res8;
-static uint16_t res16;
-static uint32_t res32;
-static uint64_t res64;
+static uint8_t res8 = 0xDD;
+static uint16_t res16 = 0x5454;
+static uint32_t res32 = 0x75757575;
+static uint64_t res64 = 0x5555555522222222;
 
-static int check_cpu_ppc_im0_0() 
+static int check_cpu_ppc_im0_0()
 {
    register uint8_t _res8;  
    register uint16_t _res16;
    register uint32_t _res32, r64_output1, r64_output2;
    register uint64_t _res64;
 
-   iowrite8(0xDD, (uint32_t)&res8);
    _res8 = ioread8((uint32_t)&res8);
    if(_res8 == 0xDD )
    {
@@ -33,7 +32,6 @@ static int check_cpu_ppc_im0_0()
       return 0;
    }
      
-   iowrite16(0x5454, (uint32_t)&res16);
    _res16 = ioread16((uint32_t)&res16);
    
    if(_res16 == 0x5454 )
@@ -46,7 +44,6 @@ static int check_cpu_ppc_im0_0()
       return 0;
    }
   
-   iowrite32(0x75757575, (uint32_t)&res32);
    _res32 = ioread32((uint32_t)&res32);
    
    if(_res32 == 0x75757575 )
@@ -59,7 +56,6 @@ static int check_cpu_ppc_im0_0()
       return 0;
    }
    
-   iowrite64(0x5555555522222222, (uint32_t)&res64);
    _res64 = ioread64((uint32_t)&res64);
    
    if(_res64 == 0x5555555522222222)
@@ -82,7 +78,6 @@ static int check_cpu_ppc_im0_1()
    register uint16_t _res16;
    register uint32_t _res32, _r64_output1, _r64_output2;
    register uint64_t _res64;
-   
    
    iowrite8(0xBB, (uint32_t)&res8);
    _res8 = ioread8((uint32_t)&res8);
