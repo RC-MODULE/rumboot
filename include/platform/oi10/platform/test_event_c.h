072ea9f481b59559ed9b4b535a87db904ddb840c
Victor Strukov <v.strukov@module.ru>
2018-06-21 13:03:42 +0300

    oi10_rumboot: oi10-31: added implementation for test_event_send_test_id

--- include/platform/oi10/platform/test_event_c.h
+++ include/platform/oi10/platform/test_event_c.h
@@ -24,4 +24,21 @@ static inline void test_event( uint32_t const event_code ) {
 }
 
 
+static inline void test_event_send_test_id( const char* test_id ) {
+    unsigned volatile test_event_opcode = EVENT_TESTEVENT;
+    unsigned volatile nop_opcode        = EVENT_NOP;
+    uint32_t event_code                 = TEST_EVENT_NEW_TEST_STARTED;
+    asm volatile(
+        "mtspr %0, %1 \n\t"
+        "mtspr %2, %3 \n\t"
+        "mtspr %4, %5 \n\t"
+        "mtspr %4, %6 \n\t"
+        ::"n"(SPR_SPRG3), "r"(test_id),
+         "n"(SPR_SPRG2), "r"(event_code),
+         "n"(SPR_SPRG1), "r"(test_event_opcode),
+         "r"(nop_opcode)
+    );
+}
+
+
 #endif /* TEST_EVENT_C_H_ */
