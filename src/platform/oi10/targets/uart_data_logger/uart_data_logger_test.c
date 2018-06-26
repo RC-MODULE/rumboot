86c123e86089f81a191f047a69d811c4cfa42746
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-06-26 09:03:00 +0300

    oi10_rumboot: oi10_oi10-32 - some bugs were fixed after code review

--- src/platform/oi10/targets/uart_data_logger/uart_data_logger_test.c
+++ src/platform/oi10/targets/uart_data_logger/uart_data_logger_test.c
@@ -8,9 +8,6 @@
 #include <platform/devices.h>
 
 
-#define MEM8(addr) *((uint8_t*)(addr))
-#define MEM32(addr) *((uint32_t*)(addr))
-
 const uint32_t UART_BASE_ADDR = 0xC0029000; 
 
 const uint32_t UARTDR       = 0x00;
@@ -36,33 +33,58 @@ typedef enum
     uart_stopbits_2
 } uart_stopbits;
 
+static inline void reg_write(uint32_t addr, uint32_t value)
+{
+    iowrite32(value, addr);
+}
+
+static inline uint32_t reg_read(uint32_t addr)
+{
+    return ioread32(addr);
+}
+
+static inline void reg_set(uint32_t addr, uint32_t value)
+{
+    iowrite32(ioread32(addr) | value, addr);
+}
+
+static inline void reg_clear(uint32_t addr, uint32_t value)
+{
+    iowrite32(ioread32(addr) & ~value, addr);
+}
+
+static inline bool reg_check(uint32_t addr, uint32_t value)
+{
+    return ((ioread32(addr) & value) == value);
+}
+
 
 static void uart_init(uint32_t freq, uint32_t baudrate)
 {
     //Set dividers according to frequency and baudrate
-    MEM32(UART_BASE_ADDR + UARTIBRD) = (uint32_t)(freq/(16*baudrate));
-    MEM32(UART_BASE_ADDR + UARTFBRD) = (uint32_t)(freq%(16*baudrate));
+    reg_write(UART_BASE_ADDR + UARTIBRD, (uint32_t)(freq/(16*baudrate)));
+    reg_write(UART_BASE_ADDR + UARTFBRD, (uint32_t)(freq%(16*baudrate)));
     
     //World length == 8bit 
-    MEM32(UART_BASE_ADDR + UARTLCR_H) |= (0x03 << 5);
+    reg_set(UART_BASE_ADDR + UARTLCR_H, (0x03 << 5));
     
     //FIFO enable
-    MEM32(UART_BASE_ADDR + UARTLCR_H) |= (0x01 << 4);
+    reg_set(UART_BASE_ADDR + UARTLCR_H, (0x01 << 4));
 }
 
 static void uart_enable()
 {
-    MEM32(UART_BASE_ADDR + UARTCR) |= 0x01;
+    reg_set(UART_BASE_ADDR + UARTCR, 0x01);
 }
 
 static void uart_write_char(uint8_t data)
 {
-    MEM8(UART_BASE_ADDR + UARTDR) = data;
+    reg_write(UART_BASE_ADDR + UARTDR, data);
 }
 
 static void uart_disable()
 {
-    MEM32(UART_BASE_ADDR + UARTCR) &= ~0x01;
+    reg_clear(UART_BASE_ADDR + UARTCR, 0x01);
 }
 
 
@@ -72,7 +94,7 @@ int main()
 {
     const char msg[] = "\x55H\nello\nworld\n"; 
     
-    size_t msg_len = sizeof(msg)/sizeof(msg[0]);
+    size_t msg_len = ARRAY_SIZE(msg);
    
     uart_disable();
     uart_init(100000000, 6250000);
@@ -80,6 +102,9 @@ int main()
     
     for (size_t i = 0; i < msg_len; i++)
     {
+        while (reg_check(UART_BASE_ADDR + UARTFR, (0x01 << 5)))
+            ;
+        
         uart_write_char((uint8_t)msg[i]);
     }    
         
