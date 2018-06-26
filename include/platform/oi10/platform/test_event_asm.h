a4486a90d773e983d098841b4cf8904def763adc
Victor Strukov <v.strukov@module.ru>
2018-06-26 14:11:03 +0300

    oi10_rumboot: oi10-31: updated assembler macros for test_event and other dependent sysstems

--- include/platform/oi10/platform/test_event_asm.h
+++ include/platform/oi10/platform/test_event_asm.h
@@ -5,44 +5,44 @@
 #include <platform/oi10/platform/test_event_codes.h>
 
 
-.macro gtube_init
-    load_const  r5, 0x00000000  // make r0 "all zeros" source
-    mtspr       SPR_SPRG0, r5
-    mtspr       SPR_SPRG1, r5
-    mtspr       SPR_SPRG2, r5
-    mtspr       SPR_SPRG3, r5
-    mtspr       SPR_SPRG4, r5
-    mtspr       SPR_SPRG5, r5
-    mtspr       SPR_SPRG6, r5
-    mtspr       SPR_SPRG7, r5
-    mtspr       SPR_SPRG8, r5
-    load_const  r6, 0x0003c0de;
-    mtspr       SPR_SPRG0, r6
+.macro gtube_init tmp_reg=r5
+    load_const  \tmp_reg, 0x00000000  // make r0 "all zeros" source
+    mtspr       SPR_SPRG0, \tmp_reg
+    mtspr       SPR_SPRG1, \tmp_reg
+    mtspr       SPR_SPRG2, \tmp_reg
+    mtspr       SPR_SPRG3, \tmp_reg
+    mtspr       SPR_SPRG4, \tmp_reg
+    mtspr       SPR_SPRG5, \tmp_reg
+    mtspr       SPR_SPRG6, \tmp_reg
+    mtspr       SPR_SPRG7, \tmp_reg
+    mtspr       SPR_SPRG8, \tmp_reg
+    load_const  \tmp_reg, 0x0003c0de;
+    mtspr       SPR_SPRG0, \tmp_reg
 .endm
 
-.macro exit code
-    test_event TEST_EVENT_NEW_TEST_FINISHED
-    load_const  r6, 0
-    load_const  r7, 1
-    load_const  r5, \code /* test stdout */
-    mtspr       SPR_SPRG2, r5
+.macro exit code tmp_reg_exit_code=r5, tmp_reg_nop=r6, tmp_reg_exit_opcode=r7
+    test_event TEST_EVENT_NEW_TEST_FINISHED, \tmp_reg_exit_code, \tmp_reg_nop, \tmp_reg_exit_opcode
+    load_const  \tmp_reg_nop, 0
+    load_const  \tmp_reg_exit_opcode, 1
+    load_const  \tmp_reg_exit_code, \code /* test stdout */
+    mtspr       SPR_SPRG2, \tmp_reg_exit_code
 
-    mtspr       SPR_SPRG1, r7
-    mtspr       SPR_SPRG1, r6
+    mtspr       SPR_SPRG1, \tmp_reg_exit_opcode
+    mtspr       SPR_SPRG1, \tmp_reg_nop
 .endm
 
 #define EVENT_TESTEVENT 0x11
-.macro test_event test_event_code
-    load_const  r5, 0
-    load_const  r6, EVENT_TESTEVENT;
-    load_const  r7, \test_event_code;
-    mtspr       SPR_SPRG2, r7
+.macro test_event test_event_code, tmp_reg_nop=r5, tmp_reg_opcode_testevent=r6, tmp_reg_event_code=r7
+    load_const  \tmp_reg_nop, 0
+    load_const  \tmp_reg_opcode_testevent, EVENT_TESTEVENT;
+    load_const  \tmp_reg_event_code, \test_event_code;
+    mtspr       SPR_SPRG2, \tmp_reg_event_code
 
-    mtspr       SPR_SPRG1, r6
-    mtspr       SPR_SPRG1, r5
+    mtspr       SPR_SPRG1, \tmp_reg_opcode_testevent
+    mtspr       SPR_SPRG1, \tmp_reg_nop
 .endm
 
-.macro test_event_send_test_id test_id
+.macro test_event_send_test_id test_id, tmp_reg_nop_or_addr=r5, tmp_reg_opcode_testevent=r6, tmp_reg_event_code=r7
     b 4f
 3:
     .ascii "\test_id"
@@ -50,9 +50,9 @@
     /*end of string*/
 .align 2, 0
 4:
-    load_addr  r5, 3b
-    mtspr       SPR_SPRG3, r5
-    test_event TEST_EVENT_NEW_TEST_STARTED
+    load_addr  \tmp_reg_nop_or_addr, 3b
+    mtspr       SPR_SPRG3, \tmp_reg_nop_or_addr
+    test_event TEST_EVENT_NEW_TEST_STARTED, \tmp_reg_nop_or_addr, \tmp_reg_opcode_testevent, \tmp_reg_event_code
 .endm
 
 #endif /* TEST_EVENT_ASM_H_ */
--- include/platform/oi10/platform/trace.S.h
+++ include/platform/oi10/platform/trace.S.h
@@ -19,7 +19,7 @@
 .endm
 
 
-.macro rumboot_putstring text
+.macro rumboot_putstring text, tmp_reg_nop_or_addr=r5, tmp_reg_opcode_testevent=r6, tmp_reg_event_code=r7
     b 2f
 1:
     .ascii "\text"
@@ -27,29 +27,38 @@
     /*end of string*/
 .align 2, 0
 2:
-    load_addr  r5, 1b
-    mtspr       SPR_SPRG3, r5
-    test_event EVENT_TRACE_MESSAGE
+    load_addr  \tmp_reg_nop_or_addr, 1b
+    mtspr       SPR_SPRG3, \tmp_reg_nop_or_addr
+    test_event EVENT_TRACE_MESSAGE, \tmp_reg_nop_or_addr, \tmp_reg_opcode_testevent, \tmp_reg_event_code
 .endm
 
-
-.macro rumboot_puthex hex_reg
+.macro rumboot_puthex hex_reg, tmp_reg_opcode_testevent=r6, tmp_reg_event_code=r7
     mtspr       SPR_SPRG3, \hex_reg
-    test_event EVENT_TRACE_HEX
+    test_event EVENT_TRACE_HEX, \hex_reg, \tmp_reg_opcode_testevent, \tmp_reg_event_code
 .endm
 
-
-.macro rumboot_putdump start_addr, length_in_bytes
-    mtspr SPR_SPRG3, \start_addr
-    mtspr SPR_SPRG4, \length_in_bytes
-    test_event EVENT_TRACE_DUMP
+.macro _rumboot_putdump tmp_reg_start_addr, tmp_reg_length_in_bytes, tmp_reg_event_code=r7
+    mtspr SPR_SPRG3, \tmp_reg_start_addr
+    mtspr SPR_SPRG4, \tmp_reg_length_in_bytes
+    test_event EVENT_TRACE_DUMP, \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_event_code
 .endm
 
+.macro rumboot_putdump start_addr, length_in_bytes, tmp_reg_start_addr=r5, tmp_reg_length_in_bytes=r6, tmp_reg_event_code=r7
+    load_const \tmp_reg_start_addr, \start_addr
+    load_const \tmp_reg_length_in_bytes, \length_in_bytes
+    _rumboot_putdump \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_event_code
+.endm
 
-.macro rumboot_issdump start_addr, length_in_bytes
-    mtspr SPR_SPRG3, \start_addr
-    mtspr SPR_SPRG4, \length_in_bytes
-    test_event TEST_EVENT_CREATE_ISS_DUMP
+.macro _rumboot_issdump tmp_reg_start_addr, tmp_reg_length_in_bytes, tmp_reg_event_code=r7
+    mtspr SPR_SPRG3, \tmp_reg_start_addr
+    mtspr SPR_SPRG4, \tmp_reg_length_in_bytes
+    test_event TEST_EVENT_CREATE_ISS_DUMP, \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_event_code
+.endm
+
+.macro rumboot_issdump start_addr, length_in_bytes, tmp_reg_start_addr=r5, tmp_reg_length_in_bytes=r6, tmp_reg_event_code=r7
+    load_const \tmp_reg_start_addr, \start_addr
+    load_const \tmp_reg_length_in_bytes, \length_in_bytes
+    _rumboot_issdump \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_event_code
 .endm
 
 #endif /* TRACE_S_H_ */
