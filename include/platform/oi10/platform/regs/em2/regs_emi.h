5c9e1e41784a0946b9cdbb3a8bf44bdc6e84a243
Stepan Chernousov <s.chernousov@modulew.ru>
2018-06-27 09:04:41 +0300

    oi10_rumboot: emi regs gefs are added

---
+++ include/platform/oi10/platform/regs/em2/regs_emi.h
@@ -0,0 +1,33 @@
+#ifndef REGS_MCLFIR_H_
+#define REGS_MCLFIR_H_
+
+#define EMI_SS0_0                    0x00     //WR 0x00000000
+#define EMI_SD0_0                    0x04     //WR 0x00000000
+#define EMI_SS1_0                    0x08     //WR 0x00000000
+#define EMI_SD1_0                    0x0C     //WR 0x00000000
+#define EMI_SS0_1                    0x10     //WR 0x00000000
+#define EMI_SD0_1                    0x14     //WR 0x00000000
+#define EMI_SS1_1                    0x18     //WR 0x00000000
+#define EMI_SD1_1                    0x1C     //WR 0x00000000
+#define EMI_SS0_2                    0x20     //WR 0x00000000
+#define EMI_SD0_2                    0x24     //WR 0x00000000
+#define EMI_SS1_2                    0x28     //WR 0x00000000
+#define EMI_SD1_2                    0x2C     //WR 0x00000000
+#define EMI_RFC                      0x30     //WR 0x00000000
+#define EMI_HSTR                     0x34     //WR 0x00000000
+#define EMI_ECNT20                   0x38     //WR 0x00000000
+#define EMI_ECNT53                   0x3C     //WR 0x00000000
+#define EMI_H1ADR                    0x40     //WR N/A
+#define EMI_H2ADR                    0x44     //WR N/A
+#define EMI_RREQADR                  0x48     //WR N/A
+#define EMI_WREQADR                  0x4C     //WR N/A
+#define EMI_WDADR                    0x50     //WR N/A
+#define EMI_BUSEN                    0x54     //WR 0x00000000
+#define EMI_WECR                     0x58     //WR 0x00000000
+#define EMI_IMR                      0x60     //WR 0x00000000
+#define EMI_IMR_SET                  0x64     //WR 0x00000000
+#define EMI_IMR_RST                  0x68     //WR 0x00000000
+#define EMI_IRR                      0x70     //WR 0x00000000
+#define EMI_IRR_RST                  0x74     //WR 0x00000000
+
+#endif /* REGS_MCLFIR_H_ */
