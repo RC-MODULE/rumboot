a77a10a3067831cc1a47713cfa7b7e331aad35e3
Stepan Chernousov <s.chernousov@modulew.ru>
2018-06-28 15:48:54 +0300

    oi10_rumboot: test cpu16 (release 1)

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_016.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_016.c
@@ -14,12 +14,16 @@
 #include <platform/regs/em2/regs_mclfir.h>
 #include <platform/regs/em2/regs_emi.h>
 
+uint8_t Error_Detect = 0;
+#ifndef __TEST_ASSERT_H__
 #define TEST_ASSERT( expr, message )\
 MACRO_BEGIN\
     if( !(expr) ) {\
         rumboot_putstring( "PROGRAM ASSERTION FAILED: \"" STRINGIZE(expr) "\"\n" __FILE__ ":" STRINGIZE(__LINE__) ": \"" message "\"\n" );\
+        Error_Detect = 1;\
     }\
 MACRO_END
+#endif
 
 static int32_t check_array32[] = {
         0xFFFFFFFF,
@@ -41,13 +45,11 @@ uint32_t check_plb6mcif2(const uint32_t base_address)
     TEST_ASSERT(dcr_read(PLB6MCIF2_BEARL + base_address) == 0x00000000,"In PLB6MCIF2_BEARL expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_BEARU + base_address) == 0x00000000,"In PLB6MCIF2_BEARU expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_BESR + base_address) == 0x00000000,"In PLB6MCIF2_BESR expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE PLB6MCIF2_INTR_EN = "); rumboot_puthex(dcr_read(PLB6MCIF2_INTR_EN + base_address));
     TEST_ASSERT(dcr_read(PLB6MCIF2_INTR_EN + base_address) == 0xFFE001C0,"In PLB6MCIF2_INTR_EN expected value 0xFFE001C0");
     TEST_ASSERT(dcr_read(PLB6MCIF2_MAP0CF + base_address) == 0x00000000,"In PLB6MCIF2_MAP0CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_MAP1CF + base_address) == 0x00000000,"In PLB6MCIF2_MAP1CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_MAP2CF + base_address) == 0x00000000,"In PLB6MCIF2_MAP2CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_MAP3CF + base_address) == 0x00000000,"In PLB6MCIF2_MAP3CF expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE PLB6MCIF2_MAXMEM = "); rumboot_puthex(dcr_read(PLB6MCIF2_MAXMEM + base_address));
     TEST_ASSERT(dcr_read(PLB6MCIF2_MAXMEM + base_address) == 0xC0000000,"In PLB6MCIF2_MAXMEM expected value 0xC0000000");//default: [0:1]= 0b11
     TEST_ASSERT(dcr_read(PLB6MCIF2_MR0CF + base_address) == 0x00000000,"In PLB6MCIF2_MR0CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_MR1CF + base_address) == 0x00000000,"In PLB6MCIF2_MR1CF expected value 0x00000000");
@@ -57,22 +59,18 @@ uint32_t check_plb6mcif2(const uint32_t base_address)
     TEST_ASSERT(dcr_read(PLB6MCIF2_P6BMTAG2 + base_address) == 0x00000000,"In PLB6MCIF2_P6BMTAG2 expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_P6BMTAG3 + base_address) == 0x00000000,"In PLB6MCIF2_P6BMTAG3 expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_PLBASYNC + base_address) == 0x00000000,"In PLB6MCIF2_PLBASYNC expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE PLB6MCIF2_PLBCFG = "); rumboot_puthex(dcr_read(PLB6MCIF2_PLBCFG + base_address));
     TEST_ASSERT(dcr_read(PLB6MCIF2_PLBCFG + base_address) == 0x200000F0,"In PLB6MCIF2_PLBCFG expected value 0x200000F0");
 
     uint32_t result = 0;
     result = dcr_read(PLB6MCIF2_PLBORD + base_address);
     TEST_ASSERT(((result == 0x01000000) || (result == 0x40000000) || (result == 0x20000000)),"In PLB6MCIF2_PLBORD expected value 0x01000000 or 0x40000000 or 0x20000000");
-    rumboot_puthex(result);
 
     TEST_ASSERT(dcr_read(PLB6MCIF2_PUABA + base_address) == 0x00000000,"In PLB6MCIF2_PUABA expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE PLB6MCIF2_RID = "); rumboot_puthex(dcr_read(PLB6MCIF2_RID + base_address));
     TEST_ASSERT(dcr_read(PLB6MCIF2_RID + base_address) == PLB6MCIF2_WATERMARK,"In PLB6MCIF2_RevID expected value 0x00_0007_07");
     TEST_ASSERT(dcr_read(PLB6MCIF2_SR0CF + base_address) == 0x00000000,"In PLB6MCIF2_SR0CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_SR1CF + base_address) == 0x00000000,"In PLB6MCIF2_SR1CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_SR2CF + base_address) == 0x00000000,"In PLB6MCIF2_SR2CF expected value 0x00000000");
     TEST_ASSERT(dcr_read(PLB6MCIF2_SR3CF + base_address) == 0x00000000,"In PLB6MCIF2_SR3CF expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE PLB6MCIF2_STATUS = "); rumboot_puthex(dcr_read(PLB6MCIF2_STATUS + base_address));
     TEST_ASSERT(dcr_read(PLB6MCIF2_STATUS + base_address) == 0x80000000,"In PLB6MCIF2_STATUS expected value 0x80000000");
 
     uint32_t i;
@@ -89,22 +87,17 @@ uint32_t check_mclfir(const uint32_t base_address)
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR0 + base_address) == 0x00000000,"In MC_ERR0 expected value 0x00000000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR1 + base_address) == 0x00000000,"In MC_ERR1 expected value 0x00000000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR_MSK0 + base_address) == 0xFFFFFFFF,"In MC_ERR_MSK0 expected value 0xFFFFFFFF");
-    rumboot_putstring("ACTUAL VALUE MCLFIR_MC_ERR_MSK1 = "); rumboot_puthex(dcr_read(MCLFIR_MC_ERR_MSK1 + base_address));
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR_MSK1 + base_address) == 0xFFFFFFC0,"In MC_ERR_MSK1 expected value 0xFFFFFFC0");
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR_ACTION0 + base_address) == 0x00000000,"In MC_ERR_ACTION0 expected value 0x00000000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR_ACTION1 + base_address) == 0x00000000,"In MC_ERR_ACTION1 expected value 0x00000000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR_WOF0 + base_address) == 0x00000000,"In MC_ERR_WOF0 expected value 0x00000000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_ERR_WOF1 + base_address) == 0x00000000,"In MC_ERR_WOF1 expected value 0x00000000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_LFIR + base_address) == 0x00000000,"In MC_LFIR expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE MCLFIR_MC_LFIR_MSK = "); rumboot_puthex(dcr_read(MCLFIR_MC_LFIR_MSK + base_address));
     TEST_ASSERT(dcr_read(MCLFIR_MC_LFIR_MSK + base_address) == 0xC0000000,"In MC_LFIR_MSK expected value 0xC0000000");
-    rumboot_putstring("ACTUAL VALUE MCLFIR_MC_CONSOL_STAT = "); rumboot_puthex(dcr_read(MCLFIR_MC_CONSOL_STAT + base_address));
     TEST_ASSERT(dcr_read(MCLFIR_MC_CONSOL_STAT + base_address) == 0x03100000,"In MC_CONSOL_STAT expected value 0x03100000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_CONSOL_STAT_TRIG + base_address) == 0x00000000,"In MC_CONSOL_STAT_TRIG expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE MCLFIR_MC_CONSOL_STAT_MSK = "); rumboot_puthex(dcr_read(MCLFIR_MC_CONSOL_STAT_MSK + base_address));
     TEST_ASSERT(dcr_read(MCLFIR_MC_CONSOL_STAT_MSK + base_address) == 0xFFF08000,"In MC_CONSOL_STAT_MSK expected value 0xFFF08000");
     TEST_ASSERT(dcr_read(MCLFIR_MC_CONSOL_STATSUM + base_address) == 0x00000000,"In MC_CONSOL_STATSUM expected value 0x00000000");
-    rumboot_putstring("ACTUAL VALUE MCLFIR_MC_CONSOL_STATSUM_MSK = "); rumboot_puthex(dcr_read(MCLFIR_MC_CONSOL_STATSUM_MSK + base_address));
     TEST_ASSERT(dcr_read(MCLFIR_MC_CONSOL_STATSUM_MSK + base_address) == 0x80000000,"In MC_CONSOL_STATSUM_MSK expected value 0x80000000");
 
     uint32_t i;
@@ -140,7 +133,6 @@ uint32_t check_emi(const uint32_t base_address)
 	TEST_ASSERT(dcr_read( EMI_IMR_SET + base_address) == 0x00000000, "In EMI_IMR_SET expected value 0x00000000");
 	TEST_ASSERT(dcr_read( EMI_IMR_RST + base_address) == 0x00000000, "In EMI_IMR_RST expected value 0x00000000");
 	TEST_ASSERT(dcr_read( EMI_IRR_RST + base_address) == 0x00000000, "In EMI_IRR_RST expected value 0x00000000");
-	//rumboot_putstring("IRR");
 	//TEST_ASSERT(dcr_read( EMI_IRR + base_address) == 0x00000000, "In EMI_IRR expected value 0x00000000");
 
 	static int32_t check_array25[] = {
@@ -161,7 +153,6 @@ uint32_t check_emi(const uint32_t base_address)
 	uint32_t i;
 	for (i = 0; i< 12; i++){
 	        dcr_write (EMI_SS0_0 + base_address, check_array25[i]);
-	        rumboot_puthex(dcr_read(EMI_SS0_0 + base_address));
 	        TEST_ASSERT(dcr_read(EMI_SS0_0  + base_address) == check_array25[i],"EMI DCR data bus check failed");
 	    }
 
@@ -190,12 +181,12 @@ uint32_t check_emi(const uint32_t base_address)
 
 int main()
 {
-	//rumboot_putstring("CHECK PLB6MCIF2\n");
-	//check_plb6mcif2 (DCR_EM2_PLB6MCIF2_BASE);
-	//rumboot_putstring("CHECK MCLFIR\n");
-	//check_mclfir (DCR_EM2_MCLFIR_BASE);
+	rumboot_putstring("CHECK PLB6MCIF2\n");
+	check_plb6mcif2 (DCR_EM2_PLB6MCIF2_BASE);
+	rumboot_putstring("CHECK MCLFIR\n");
+	check_mclfir (DCR_EM2_MCLFIR_BASE);
 	rumboot_putstring("CHECK EMI\n");
 	check_emi (DCR_EM2_EMI_BASE);
 
-	return 0;
+	return Error_Detect;
 }
