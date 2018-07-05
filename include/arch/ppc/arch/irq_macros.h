d622c23fb5ddef49b27d98ac2d20b8356545a766
Rishat Galiulin <r.galiulin@modulew.ru>
2018-07-05 18:37:43 +0300

    oi10_rumboot: irq_macros is fixed

--- include/arch/ppc/arch/irq_macros.h
+++ include/arch/ppc/arch/irq_macros.h
@@ -4,33 +4,37 @@
 /* Include doxygen.h to make sure that documentation gets updated  */
 #include "../../doxygen.h"
 
-static inline void rumboot_arch_irq_enable()
+static inline uint32_t rumboot_arch_irq_enable()
 {
-    asm volatile ("nop");
-}
-
-
-static inline int rumboot_arch_irq_disable(void)
-{
-    int result = 0;
+    uint32_t result = 0;
     asm volatile (
         "nop\n"
-        );
+    );
     return result;
 }
 
-static inline int rumboot_arch_irq_setstate(int pri_mask)
+
+static inline uint32_t rumboot_arch_irq_disable()
 {
+    uint32_t result = 0;
     asm volatile (
         "nop\n"
-        );
-    return 0;
+    );
+    return result;
+}
+
+static inline uint32_t rumboot_arch_irq_setstate(uint32_t new_state){
+    uint32_t result = 0;
+    asm volatile (
+        "nop\n"
+    );
+    return result;
 }
 
 #define RUMBOOT_ATOMIC_BLOCK() \
-     for(int mask = rumboot_arch_irq_disable(), flag = 1; \
+     for(uint32_t old_state = rumboot_arch_irq_disable(), flag = 1; \
          flag;\
-         flag = rumboot_arch_irq_setstate(mask))
+         rumboot_arch_irq_setstate(old_state), flag = 0)
 
 
 #endif /* end of include guard: PPC_IRQ_MACROS_H */
