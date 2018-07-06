35bddbdad5ff77698a303430ecc1b9f0ba3726f2
Rishat Galiulin <r.galiulin@modulew.ru>
2018-07-06 12:15:48 +0300

    oi10_rumboot: native rumboot_arch_irq_* is fixed

--- src/platform/native/glue.c
+++ src/platform/native/glue.c
@@ -47,14 +47,14 @@ int rumboot_platform_getchar(uint32_t timeout_us)
 struct rumboot_runtime_info rumboot_platform_runtime_info;
 
 
-void rumboot_arch_irq_disable()
+uint32_t rumboot_arch_irq_disable()
 {
-
+    return 0;
 }
 
-void rumboot_arch_irq_enable()
+uint32_t rumboot_arch_irq_enable()
 {
-
+    return 0;
 }
 
 void rumboot_platform_request_file(const char *plusarg, uint32_t addr)
