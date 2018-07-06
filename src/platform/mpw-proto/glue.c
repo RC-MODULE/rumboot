4abafffdf7c50e6c33d54e330c1db485b4632273
Rishat Galiulin <r.galiulin@modulew.ru>
2018-07-06 12:12:00 +0300

    oi10_rumboot: mpw-proto rumboot_arch_irq_* is fixed

--- src/platform/mpw-proto/glue.c
+++ src/platform/mpw-proto/glue.c
@@ -93,14 +93,14 @@ void rumboot_platform_irq_init()
 
 }
 
-void rumboot_arch_irq_disable()
+uint32_t rumboot_arch_irq_disable()
 {
-
+    return 0;
 }
 
-void rumboot_arch_irq_enable()
+uint32_t rumboot_arch_irq_enable()
 {
-
+    return 0;
 }
 
 void rumboot_platform_request_file(const char *plusarg, uint32_t addr)
