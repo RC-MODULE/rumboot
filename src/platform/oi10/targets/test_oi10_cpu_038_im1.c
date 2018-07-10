998d25cc86e18c549861e1d82ffa72a242bee148
Victor Strukov <v.strukov@module.ru>
2018-07-10 23:01:27 +0300

    oi10_rumboot: test_oi10_cpu_038_im1: added initial test file

---
+++ src/platform/oi10/targets/test_oi10_cpu_038_im1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <platform/common_macros/common_macros.h>
+#include <platform/test_event_codes.h>
+#include <platform/devices.h>
+#include <rumboot/platform.h>
+
+#include <rumboot/io.h>
+#include <rumboot/printf.h>
+
+int main(void) 
+{
+    rumboot_platform_request_file("IM1BIN", IM1_BASE);
+
+    int (*check_function)();
+    check_function = (void *)(IM1_BASE);
+    rumboot_putstring("starting code from IM1");
+
+      return check_function();
+
+    return 1;
+}
