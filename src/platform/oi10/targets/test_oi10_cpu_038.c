68cc50053d837188d3ad1a81c21656ab6ccffbb2
Victor Strukov <v.strukov@module.ru>
2018-07-12 14:59:53 +0300

    oi10_rumboot: test_oi10cpu_038: cleaned up warnings

--- src/platform/oi10/targets/test_oi10_cpu_038.c
+++ src/platform/oi10/targets/test_oi10_cpu_038.c
@@ -11,19 +11,20 @@
 #include <rumboot/printf.h>
 #include <platform/trace.h>
 #include <platform/arch/ppc/ppc_476fp_mmu.h>
+#ifdef TEST_OI10_CPU_038_IM0
                        //          MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
 /* TLB entries to remap 128KB pages IM0 from 0x00000010_80000000-0x00000010_8001FFFF to 0x80020000-0x8003FFFF*/
-#define TLB_ENTRY_IM0_NEW_VADDR0   MMU_TLB_ENTRY(  0x010,  0x80000,    0x80020,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_1,     0b1 )
-#define TLB_ENTRY_IM0_NEW_VADDR1   MMU_TLB_ENTRY(  0x010,  0x80010,    0x80030,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_2,     0b1 )
+#define TLB_ENTRY_IM0_NEW_VADDR0   {MMU_TLB_ENTRY(  0x010,  0x80000,    0x80020,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_1,     0b1 )}
+#define TLB_ENTRY_IM0_NEW_VADDR1   {MMU_TLB_ENTRY(  0x010,  0x80010,    0x80030,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_2,     0b1 )}
 /* TLB entries to remap 128KB pages IM1 from 0x00000020_C0000000-0x00000020_C001FFFF to 0x80000000-0x8001FFFF*/
-#define TLB_ENTRY_IM1_NEW_VADDR0   MMU_TLB_ENTRY(  0x020,  0xC0000,    0x80000,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b1 )
-#define TLB_ENTRY_IM1_NEW_VADDR1   MMU_TLB_ENTRY(  0x020,  0xC0010,    0x80010,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_IM1_NEW_VADDR0   {MMU_TLB_ENTRY(  0x020,  0xC0000,    0x80000,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b1 )}
+#define TLB_ENTRY_IM1_NEW_VADDR1   {MMU_TLB_ENTRY(  0x020,  0xC0010,    0x80010,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b1 )}
 /* TLB entries to invalidate mapping 128KB pages IM0 from 0x00000010_80000000-0x00000010_8001FFFF to 0x80000000-0x8001FFFF*/
-#define TLB_ENTRY_IM0_INVALIDATE0  MMU_TLB_ENTRY(  0x010,  0x80000,    0x80000,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_1,     0b0 )
-#define TLB_ENTRY_IM0_INVALIDATE1  MMU_TLB_ENTRY(  0x010,  0x80010,    0x80010,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_2,     0b0 )
+#define TLB_ENTRY_IM0_INVALIDATE0  {MMU_TLB_ENTRY(  0x010,  0x80000,    0x80000,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_1,     0b0 )}
+#define TLB_ENTRY_IM0_INVALIDATE1  {MMU_TLB_ENTRY(  0x010,  0x80010,    0x80010,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_2,     0b0 )}
 /* TLB entries to remap 128KB pages IM1 from 0x00000020_C0000000-0x00000020_C001FFFF to 0x80020000-0x8003FFFF*/
-#define TLB_ENTRY_IM1_INVALIDATE0  MMU_TLB_ENTRY(  0x020,  0xC0000,    0x80020,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b0 )
-#define TLB_ENTRY_IM1_INVALIDATE1  MMU_TLB_ENTRY(  0x020,  0xC0010,    0x80030,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b0 )
+#define TLB_ENTRY_IM1_INVALIDATE0  {MMU_TLB_ENTRY(  0x020,  0xC0000,    0x80020,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b0 )}
+#define TLB_ENTRY_IM1_INVALIDATE1  {MMU_TLB_ENTRY(  0x020,  0xC0010,    0x80030,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b0 )}
 
 static const tlb_entry im_switch_tlb_entries[] =   {TLB_ENTRY_IM0_INVALIDATE0,
                                                     TLB_ENTRY_IM0_INVALIDATE1,
@@ -33,6 +34,7 @@ static const tlb_entry im_switch_tlb_entries[] =   {TLB_ENTRY_IM0_INVALIDATE0,
                                                     TLB_ENTRY_IM0_NEW_VADDR1,
                                                     TLB_ENTRY_IM1_NEW_VADDR0,
                                                     TLB_ENTRY_IM1_NEW_VADDR1};
+#endif
 
 int main(void) 
 {
