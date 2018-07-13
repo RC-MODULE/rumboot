df1f1ba320ca466e2c28c1892daab63466d48061
Andrew Andrianov <andrew@ncrmnt.org>
2018-07-13 11:58:05 +0300

    scripts: Add helper script to test all platforms at once
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ scripts/test-all-builds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PLATFORMS="native mpw-proto oi10 basis"
+mkdir build-test
+cd build-test
+for p in ${PLATFORMS}; do
+    mkdir $p
+    cd $p
+    cmake ../../ -DRUMBOOT_PLATFORM=$p -DCMAKE_BUILD_TYPE=Debug
+    make || exit 1
+    cd ..
+done
