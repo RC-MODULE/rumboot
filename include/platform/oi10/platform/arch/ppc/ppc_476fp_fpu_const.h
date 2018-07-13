56c62c4c558e71387faacd0dd9bd32f42f6a5f21
Alexander Gurov <a.gurov@modulew.ru>
2018-07-13 14:33:06 +0300

    oi10_rumboot: 'ppc_476fp_fpu_const.h' - added common math constants for FPU.

---
+++ include/platform/oi10/platform/arch/ppc/ppc_476fp_fpu_const.h
@@ -0,0 +1,72 @@
+/*
+ * ppc_476fp_fpu_const.h
+ *
+ *  Created on: Jul 13, 2018
+ *      Author: d.arsentiev
+ */
+
+#ifndef PPC_476FP_FPU_CONST_H_
+#define PPC_476FP_FPU_CONST_H_
+
+/* Math constants */
+#define M_E             2.7182818284590452354
+#define M_LOG2E         1.4426950408889634074
+#define M_LOG10E        0.43429448190325182765
+#define M_LN2           0.693147180559945309417
+#define M_LN10          2.30258509299404568402
+#define M_PI            3.14159265358979323846
+#define M_PI_2          1.57079632679489661923
+#define M_PI_4          0.78539816339744830962
+#define M_1_PI          0.31830988618379067154
+#define M_2_PI          0.63661977236758134308
+#define M_SQRT2         1.41421356237309504880
+#define M_SQRT1_2       0.70710678118654752440
+#define M_TWOPI         6.283185307179586476925286
+#define M_SQRTPI        1.77245385090551602792981
+#define M_LN2LO         1.9082149292705877000E-10
+#define M_LN2HI         6.9314718036912381649E-1
+#define M_SQRT3         1.73205080756887719000
+
+/* Denormal single positive */
+#define C_S_DEN_P_MIN   0x00000001
+#define C_S_DEN_P_01    (0xFFA5A5A5 & 0x007FFFFF)
+#define C_S_DEN_P_10    (0xFF5A5A5A & 0x007FFFFF)
+#define C_S_DEN_P_MAX   0x007FFFFF
+/* Denormal single negative */
+#define C_S_DEN_N_MIN   0x807FFFFF
+#define C_S_DEN_N_01    (0xFFA5A5A5 & 0x807FFFFF)
+#define C_S_DEN_N_10    (0xFF5A5A5A & 0x807FFFFF)
+#define C_S_DEN_N_MAX   0x80000001
+/* Denormal double positive */
+#define C_D_DEN_P_MIN   0x0000000000000001
+#define C_D_DEN_P_01    0x0005A5A5A5A5A5A5
+#define C_D_DEN_P_10    0x000A5A5A5A5A5A5A
+#define C_D_DEN_P_MAX   0x000FFFFFFFFFFFFF
+/* Denormal double negative */
+#define C_D_DEN_N_MIN   0x800FFFFFFFFFFFFF
+#define C_D_DEN_N_01    0x8005A5A5A5A5A5A5
+#define C_D_DEN_N_10    0x800A5A5A5A5A5A5A
+#define C_D_DEN_N_MAX   0x8000000000000001
+
+/* +0, -0, +Inf, -Inf, +NaN, -NaN single */
+#define C_S_POS_ZERO    0x00000000
+#define C_S_NEG_ZERO    0x80000000
+#define C_S_POS_INF     0x7F800000
+#define C_S_NEG_INF     0xFF800000
+#define C_S_POS_NAN_MIN 0x7F800001
+#define C_S_POS_NAN_MAX 0x7FFFFFFF
+#define C_S_NEG_NAN_MIN 0xFF800001
+#define C_S_NEG_NAN_MAX 0xFFFFFFFF
+
+/* +0, -0, +Inf, -Inf, +NaN, -NaN double */
+#define C_D_POS_ZERO    0x0000000000000000
+#define C_D_NEG_ZERO    0x8000000000000000
+#define C_D_POS_INF     0x7FF0000000000000
+#define C_D_NEG_INF     0xFFF0000000000000
+#define C_D_POS_NAN_MIN 0x7FF0000000000001
+#define C_D_POS_NAN_MAX 0x7FFFFFFFFFFFFFFF
+#define C_D_NEG_NAN_MIN 0xFFF0000000000001
+#define C_D_NEG_NAN_MAX 0xFFFFFFFFFFFFFFFF
+
+
+#endif /* PPC_476FP_FPU_CONST_H_ */
