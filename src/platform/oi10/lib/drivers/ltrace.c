abc3467e4b59730541c7722e93dcd52aadc58d37
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-07-17 13:22:58 +0300

    oi10_rumboot: LTRACE library has been updated.

--- src/platform/oi10/lib/drivers/ltrace.c
+++ src/platform/oi10/lib/drivers/ltrace.c
@@ -1,6 +1,6 @@
+#include <arch/ppc_476fp_lib_c.h>
 #include <platform/regs/regs_ltrace.h>
-#include <platform/ltrace.h>
-#include <platform/arch/ppc/ppc_476fp_lib_c.h>
+#include <platform/devices/ltrace.h>
 #include <platform/devices.h>
 
 
@@ -23,17 +23,18 @@ static void dcr_clear(const uint32_t dcr_address, uint32_t value)
 
 
 
-void ltrace_enable()
+void ltrace_enable(const uint32_t base_address)
 {
-    dcr_set(DCR_LTRACE_BASE + LTR_TC, reg_field(0, 0x01));
+    dcr_set(base_address + LTR_TC, reg_field(0, 0x01));
 }
 
-void ltrace_disable()
+void ltrace_disable(const uint32_t base_address)
 {
-    dcr_clear(DCR_LTRACE_BASE + LTR_TC, reg_field(0, 0x00));
+    dcr_clear(base_address + LTR_TC, reg_field(0, 0x00));
 }
 
-void ltrace_init(LTRACE_CONDITION_TYPE condition_type,
+void ltrace_init(const uint32_t base_address,
+                 LTRACE_CONDITION_TYPE condition_type,
                  LTRACE_TRIGGER_MODE trigger_mode,
                  LTRACE_COMPRESSION_MODE compression_mode,
                  uint32_t cond1_or,
@@ -46,7 +47,7 @@ void ltrace_init(LTRACE_CONDITION_TYPE condition_type,
                  uint32_t complete_stop_mask,
                  uint32_t complete_enable)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_TC, (reg_field(31, (complete_enable & 0x1)) |
+    dcr_write(base_address + LTR_TC, (reg_field(31, (complete_enable & 0x1)) |
                                          reg_field(30, (complete_stop_mask & 0x1)) |
                                          reg_field(27, (cond3_invert & 0xF)) |
                                          reg_field(23, (cond2_invert & 0xF)) |
@@ -60,212 +61,212 @@ void ltrace_init(LTRACE_CONDITION_TYPE condition_type,
                                          reg_field(2, condition_type)));
 }
 
-uint32_t ltrace_get_status()
+uint32_t ltrace_get_status(const uint32_t base_address)
 {
-    return (dcr_read(DCR_LTRACE_BASE + LTR_TS) & 0xF8000000);
+    return (dcr_read(base_address + LTR_TS) & 0xF8000000);
 }
 
-void ltrace_clear_status()
+void ltrace_clear_status(const uint32_t base_address)
 {
-    dcr_clear(DCR_LTRACE_BASE + LTR_TS, 0xF8000000);
+    dcr_clear(base_address + LTR_TS, 0xF8000000);
 }
 
-uint32_t ltrace_get_last_address1()
+uint32_t ltrace_get_last_address1(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_LA1);
+    return dcr_read(base_address + LTR_LA1);
 }
 
-uint32_t ltrace_get_last_address2()
+uint32_t ltrace_get_last_address2(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_LA2);
+    return dcr_read(base_address + LTR_LA2);
 }
 
-uint32_t ltrace_get_trace_window()
+uint32_t ltrace_get_trace_window(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_WC);
+    return dcr_read(base_address + LTR_WC);
 }
 
-void ltrace_set_trace_window(const uint32_t value)
+void ltrace_set_trace_window(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_WC, value);
+    dcr_write(base_address + LTR_WC, value);
 }
 
-uint32_t ltrace_get_condition_timeout()
+uint32_t ltrace_get_condition_timeout(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CT);
+    return dcr_read(base_address + LTR_CT);
 }
 
-void ltrace_set_condition_timeout(const uint32_t value)
+void ltrace_set_condition_timeout(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CT, value);
+    dcr_write(base_address + LTR_CT, value);
 }
 
-uint32_t ltrace_get_condcomp1_polarity_h()
+uint32_t ltrace_get_condcomp1_polarity_h(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC1PH);
+    return dcr_read(base_address + LTR_CC1PH);
 }
 
-void ltrace_set_condcomp1_polarity_h(const uint32_t value)
+void ltrace_set_condcomp1_polarity_h(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC1PH, value);
+    dcr_write(base_address + LTR_CC1PH, value);
 }
 
-uint32_t ltrace_get_condcomp1_polarity_l()
+uint32_t ltrace_get_condcomp1_polarity_l(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC1PL);
+    return dcr_read(base_address + LTR_CC1PL);
 }
 
-void ltrace_set_condcomp1_polarity_l(const uint32_t value)
+void ltrace_set_condcomp1_polarity_l(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC1PL, value);
+    dcr_write(base_address + LTR_CC1PL, value);
 }
 
-uint32_t ltrace_get_condcomp1_mask_h()
+uint32_t ltrace_get_condcomp1_mask_h(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC1MH);
+    return dcr_read(base_address + LTR_CC1MH);
 }
 
-void ltrace_set_condcomp1_mask_h(const uint32_t value)
+void ltrace_set_condcomp1_mask_h(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC1MH, value);
+    dcr_write(base_address + LTR_CC1MH, value);
 }
 
-uint32_t ltrace_get_condcomp1_mask_l()
+uint32_t ltrace_get_condcomp1_mask_l(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC1ML);
+    return dcr_read(base_address + LTR_CC1ML);
 }
 
-void ltrace_set_condcomp1_mask_l(const uint32_t value)
+void ltrace_set_condcomp1_mask_l(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC1ML, value);
-}
-
-
-uint32_t ltrace_get_condcomp2_polarity_h()
-{
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC2PH);
-}
-
-void ltrace_set_condcomp2_polarity_h(const uint32_t value)
-{
-    dcr_write(DCR_LTRACE_BASE + LTR_CC2PH, value);
-}
-
-uint32_t ltrace_get_condcomp2_polarity_l()
-{
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC2PL);
-}
-
-void ltrace_set_condcomp2_polarity_l(const uint32_t value)
-{
-    dcr_write(DCR_LTRACE_BASE + LTR_CC2PL, value);
-}
-
-uint32_t ltrace_get_condcomp2_mask_h()
-{
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC2MH);
-}
-
-void ltrace_set_condcomp2_mask_h(const uint32_t value)
-{
-    dcr_write(DCR_LTRACE_BASE + LTR_CC2MH, value);
-}
-
-uint32_t ltrace_get_condcomp2_mask_l()
-{
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC2ML);
-}
-
-void ltrace_set_condcomp2_mask_l(const uint32_t value)
-{
-    dcr_write(DCR_LTRACE_BASE + LTR_CC2ML, value);
+    dcr_write(base_address + LTR_CC1ML, value);
 }
 
 
-uint32_t ltrace_get_condcomp3_polarity_h()
+uint32_t ltrace_get_condcomp2_polarity_h(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC3PH);
+    return dcr_read(base_address + LTR_CC2PH);
 }
 
-void ltrace_set_condcomp3_polarity_h(const uint32_t value)
+void ltrace_set_condcomp2_polarity_h(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC3PH, value);
+    dcr_write(base_address + LTR_CC2PH, value);
 }
 
-uint32_t ltrace_get_condcomp3_polarity_l()
+uint32_t ltrace_get_condcomp2_polarity_l(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC3PL);
+    return dcr_read(base_address + LTR_CC2PL);
 }
 
-void ltrace_set_condcomp3_polarity_l(const uint32_t value)
+void ltrace_set_condcomp2_polarity_l(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC3PL, value);
+    dcr_write(base_address + LTR_CC2PL, value);
 }
 
-uint32_t ltrace_get_condcomp3_mask_h()
+uint32_t ltrace_get_condcomp2_mask_h(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC3MH);
+    return dcr_read(base_address + LTR_CC2MH);
 }
 
-void ltrace_set_condcomp3_mask_h(const uint32_t value)
+void ltrace_set_condcomp2_mask_h(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC3MH, value);
+    dcr_write(base_address + LTR_CC2MH, value);
 }
 
-uint32_t ltrace_get_condcomp3_mask_l()
+uint32_t ltrace_get_condcomp2_mask_l(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_CC3ML);
+    return dcr_read(base_address + LTR_CC2ML);
 }
 
-void ltrace_set_condcomp3_mask_l(const uint32_t value)
+void ltrace_set_condcomp2_mask_l(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_CC3ML, value);
-}
-
-
-uint32_t ltrace_get_compression_mask_h()
-{
-    return dcr_read(DCR_LTRACE_BASE + LTR_CMH);
-}
-
-void ltrace_set_compression_mask_h(const uint32_t value)
-{
-    dcr_write(DCR_LTRACE_BASE + LTR_CMH, value);
-}
-
-uint32_t ltrace_get_compression_mask_l()
-{
-    return dcr_read(DCR_LTRACE_BASE + LTR_CML);
-}
-
-void ltrace_set_compression_mask_l(const uint32_t value)
-{
-    dcr_write(DCR_LTRACE_BASE + LTR_CML, value);
+    dcr_write(base_address + LTR_CC2ML, value);
 }
 
 
-LTRACE_ADDRESS ltrace_get_address()
+uint32_t ltrace_get_condcomp3_polarity_h(const uint32_t base_address)
+{
+    return dcr_read(base_address + LTR_CC3PH);
+}
+
+void ltrace_set_condcomp3_polarity_h(const uint32_t base_address, const uint32_t value)
+{
+    dcr_write(base_address + LTR_CC3PH, value);
+}
+
+uint32_t ltrace_get_condcomp3_polarity_l(const uint32_t base_address)
+{
+    return dcr_read(base_address + LTR_CC3PL);
+}
+
+void ltrace_set_condcomp3_polarity_l(const uint32_t base_address, const uint32_t value)
+{
+    dcr_write(base_address + LTR_CC3PL, value);
+}
+
+uint32_t ltrace_get_condcomp3_mask_h(const uint32_t base_address)
+{
+    return dcr_read(base_address + LTR_CC3MH);
+}
+
+void ltrace_set_condcomp3_mask_h(const uint32_t base_address, const uint32_t value)
+{
+    dcr_write(base_address + LTR_CC3MH, value);
+}
+
+uint32_t ltrace_get_condcomp3_mask_l(const uint32_t base_address)
+{
+    return dcr_read(base_address + LTR_CC3ML);
+}
+
+void ltrace_set_condcomp3_mask_l(const uint32_t base_address, const uint32_t value)
+{
+    dcr_write(base_address + LTR_CC3ML, value);
+}
+
+
+uint32_t ltrace_get_compression_mask_h(const uint32_t base_address)
+{
+    return dcr_read(base_address + LTR_CMH);
+}
+
+void ltrace_set_compression_mask_h(const uint32_t base_address, const uint32_t value)
+{
+    dcr_write(base_address + LTR_CMH, value);
+}
+
+uint32_t ltrace_get_compression_mask_l(const uint32_t base_address)
+{
+    return dcr_read(base_address + LTR_CML);
+}
+
+void ltrace_set_compression_mask_l(const uint32_t base_address, const uint32_t value)
+{
+    dcr_write(base_address + LTR_CML, value);
+}
+
+
+LTRACE_ADDRESS ltrace_get_address(const uint32_t base_address)
 {
     LTRACE_ADDRESS address;
 
-    uint32_t value = dcr_read(DCR_LTRACE_BASE + LTR_AI);
+    uint32_t value = dcr_read(base_address + LTR_AI);
     address.range = (value & 0xC0000000) >> 30;
     address.address = (value & 0x00000FFF);
     return address;
 }
 
-void ltrace_set_address(const LTRACE_ADDRESS address)
+void ltrace_set_address(const uint32_t base_address, const LTRACE_ADDRESS address)
 {
     uint32_t value = (address.address & 0x00000FFF) | ((address.range & 0x00000003) << 30);
-    dcr_write(DCR_LTRACE_BASE + LTR_AI, value);
+    dcr_write(base_address + LTR_AI, value);
 }
 
-uint32_t ltrace_get_data()
+uint32_t ltrace_get_data(const uint32_t base_address)
 {
-    return dcr_read(DCR_LTRACE_BASE + LTR_AD);
+    return dcr_read(base_address + LTR_AD);
 }
 
-void ltrace_set_data(const uint32_t value)
+void ltrace_set_data(const uint32_t base_address, const uint32_t value)
 {
-    dcr_write(DCR_LTRACE_BASE + LTR_AD, value);
+    dcr_write(base_address + LTR_AD, value);
 }
