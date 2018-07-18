2d6b5b95924d0cb29e8f0146e9ec064dc0cc8428
n.gorshkova <n.gorshkova@modulew.ru>
2018-07-18 16:22:41 +0300

    oi10_rumboot: spi lib - Update regs

---
+++ include/platform/oi10/platform/regs/regs_spi.h
@@ -0,0 +1,22 @@
+//Offset regs
+#define SSPCR0                  0X00
+#define SSPCR1                  0X04
+#define SSPDR                   0X08
+#define SSPSR                   0X0C
+#define SSPCPSR                 0X10
+#define SSPIMSC                 0X14
+#define SSPRIS                  0X18
+#define SSPMIS                  0X1C
+#define SSPICR                  0X20
+#define SSPDMACR                0X24
+#define SSPPeriphID0            0XFE0
+#define SSPPeriphID1            0XFE4
+#define SSPPeriphID2            0XFE8
+#define SSPPeriphID3            0XFEC
+#define SSPPCellID0             0XFF0
+#define SSPPCellID1             0XFF4
+#define SSPPCellID2             0XFF8
+#define SSPPCellID3             0XFFC
+
+//Default values
+#define DEFAULT_SSPCR0          0x0000#define DEFAULT_SSPCR1          0x0#define DEFAULT_SSPSR           0x03#define DEFAULT_SSPCPSR         0x00#define DEFAULT_SSPIMSC         0x0#define DEFAULT_SSPRIS          0x8#define DEFAULT_SSPMIS          0x0#define DEFAULT_SSPICR          0x0#define DEFAULT_SSPDMACR        0x0#define DEFAULT_SSPPeriphID0    0x22#define DEFAULT_SSPPeriphID1    0x10#define DEFAULT_SSPPeriphID2    0x34#define DEFAULT_SSPPeriphID3    0x00#define DEFAULT_SSPPCellID0     0x0D#define DEFAULT_SSPPCellID1     0xF0#define DEFAULT_SSPPCellID2     0x05#define DEFAULT_SSPPCellID3     0xB1
