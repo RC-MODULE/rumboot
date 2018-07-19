02fc53a12eba94802d3484693ae9534cb5973839
Alexander Gurov <a.gurov@modulew.ru>
2018-07-19 16:26:25 +0300

    oi10_rumboot: 'regs_lscb.h', added internal MSP regs with same index.

--- include/platform/oi10/platform/regs/regs_lscb.h
+++ include/platform/oi10/platform/regs/regs_lscb.h
@@ -14,8 +14,10 @@ BEGIN_ENUM( MSP_REG )
     DECLARE_ENUM_VAL( MSP_IMR,              0x00 )
     DECLARE_ENUM_VAL( MSP_CFG1_,            0x01 )
     DECLARE_ENUM_VAL( MSP_CFG2_,            0x02 )
+    DECLARE_ENUM_VAL( MSP_CSP,              0x03 )
     DECLARE_ENUM_VAL( MSP_SRR,              0x03 )
     DECLARE_ENUM_VAL( MSP_BC_CW,            0X04 )
+    DECLARE_ENUM_VAL( MSP_RT_CW,            0X04 )
     DECLARE_ENUM_VAL( MSP_TT_REG,           0X05 )
     DECLARE_ENUM_VAL( MSP_ISR,              0x06 )
     DECLARE_ENUM_VAL( MSP_CFG3_,            0x07 )
@@ -24,14 +26,14 @@ BEGIN_ENUM( MSP_REG )
     DECLARE_ENUM_VAL( MSP_RM_DSA,           0x0A )
     DECLARE_ENUM_VAL( MSP_BC_FTR,           0x0B )
     DECLARE_ENUM_VAL( MSP_BC_TRNM,          0x0C )
-    DECLARE_ENUM_VAL( MSP_BCFT_RTLC_MTTW,   0x0D )
+    DECLARE_ENUM_VAL( MSP_BCFT,             0x0D )
+    DECLARE_ENUM_VAL( MSP_RTLC,             0x0D )
+    DECLARE_ENUM_VAL( MSP_MTTW,             0x0D )
+    DECLARE_ENUM_VAL( MSP_BCFT_RTLC_MTTW,   0x0D ) /* from doc */
     DECLARE_ENUM_VAL( MSP_RT_SW,            0x0E )
     DECLARE_ENUM_VAL( MSP_RT_BIT_W,         0x0F )
     DECLARE_ENUM_VAL( MSP_CFG6_,            0x18 )
 END_ENUM( MSP_REG )
-
-#define MSP_CSP     MSP_SRR
-#define MSP_RT_CW   MSP_BC_CW
 
 
 #endif /* REGS_LSCB_H_ */
