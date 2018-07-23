bbcc92ac91e591a7185af3af62b2033c3133394c
Rishat Galiulin <r.galiulin@modulew.ru>
2018-07-23 19:02:14 +0300

    oi10_rumboot: ioreadX/iowriteX addr 0 access is fixed

---
+++ include/arch/ppc/arch/io.h
@@ -0,0 +1,74 @@
+/*
+ * io.h
+ *
+ *  Created on: Jul 24, 2018
+ *      Author: r.galiulin
+ */
+
+#ifndef PPC_ARCH_IO_H
+#define PPC_ARCH_IO_H
+
+
+#define RUMBOOT_ARCH_HAS_IO
+
+
+static inline uint64_t __attribute__((optimize("no-delete-null-pointer-checks"))) ioread64( uint32_t const base_addr ) {
+    return *( ( volatile uint64_t* )( base_addr ) );
+}
+
+static inline void __attribute__((optimize("no-delete-null-pointer-checks"))) iowrite64( uint64_t const value, uint32_t const base_addr ) {
+    *( ( volatile uint64_t* )( base_addr ) ) = value;
+}
+
+static inline uint32_t ioread32( uint32_t const base_addr ) {
+    uint32_t rval = 0;
+    asm volatile (
+        "lwzx %0, 0, %1 \n\t"
+        :   "=r"(rval)
+        :   "r"(base_addr)
+    );
+    return rval;
+}
+
+static inline void iowrite32( uint32_t const value, uint32_t const base_addr ) {
+    asm volatile (
+        "stwx %0, 0, %1 \n\t"
+        ::  "r"(value), "r"(base_addr)
+    );
+}
+
+static inline uint16_t ioread16( uint32_t const base_addr ) {
+    uint16_t rval = 0;
+    asm volatile (
+        "lhzx %0, 0, %1 \n\t"
+        :   "=r"(rval)
+        :   "r"(base_addr)
+    );
+    return rval;
+}
+
+static inline void iowrite16( uint16_t const value, uint32_t const base_addr ) {
+    asm volatile (
+        "sthx %0, 0, %1 \n\t"
+        ::  "r"(value), "r"(base_addr)
+    );
+}
+
+static inline uint8_t ioread8( uint32_t const base_addr ) {
+    uint8_t rval = 0;
+    asm volatile (
+        "lbzx %0, 0, %1 \n\t"
+        :   "=r"(rval)
+        :   "r"(base_addr)
+    );
+    return rval;
+}
+
+static inline void iowrite8( uint8_t const value, uint32_t const base_addr ) {
+    asm volatile (
+        "stbx %0, 0, %1 \n\t"
+        ::  "r"(value), "r"(base_addr)
+    );
+}
+
+#endif /* PPC_ARCH_IO_H */
