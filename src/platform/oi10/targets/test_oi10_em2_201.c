862b784aed252fff7669832ad3bad432fb33c4fb
Vyacheslav Gulyaev <v.gulyaev@modulew.ru>
2018-07-26 16:33:19 +0300

    oi10_rumboot: initial add of test_oi10_em2_201

---
+++ src/platform/oi10/targets/test_oi10_em2_201.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <rumboot/testsuite.h>
+
+#include <rumboot/printf.h>
+#include <rumboot/io.h>
+#include <rumboot/irq.h>
+
+#include <platform/devices.h>
+#include <regs/regs_sp804.h>
+#include <devices/sp804.h>
+#include <rumboot/platform.h>
+#include <platform/interrupts.h>
+#include <platform/regs/fields/mpic128.h>
+
+int main()
+{
+    return 0;
+}
+
