764189726eacd668e360cc775c5e3b176c22ed9e
Stepan Chernousov <s.chernousov@modulew.ru>
2018-07-27 13:20:07 +0300

    oi10_rumboot: upd em2_105 (add regpocker)

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_105.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_105.c
@@ -12,6 +12,7 @@
 #include <rumboot/platform.h>
 #include <rumboot/macros.h>
 #include <rumboot/io.h>
+#include <rumboot/regpoker.h>
 #include <platform/test_assert.h>
 #include <platform/devices.h>
 #include <platform/trace.h>
@@ -44,40 +45,33 @@ int main ()
     iowrite32_NOR ( 0xCCCCCCCC, ((uint32_t) NOR_BASE + NOR_SIZE - 0x04));
 
     rumboot_printf ("READ32 NOR\n");
-    rumboot_printf( "0x00 = %x\n",ioread32 (NOR_BASE + 0x0));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x0) == 0x00000000,"TEST ERROR: (0x00) - read value is wrong");
-    rumboot_printf( "0x04 = %x\n",ioread32 (NOR_BASE + 0x4));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x4) == 0x10101010,"TEST ERROR: (0x04) - read value is wrong");
-    rumboot_printf( "0x08 = %x\n",ioread32 (NOR_BASE + 0x8));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x8) == 0x11111111,"TEST ERROR: (0x08) - read value is wrong");
-    rumboot_printf( "0x0C = %x\n",ioread32 (NOR_BASE + 0xC));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0xC) == 0xBABADEDA,"TEST ERROR: (0x0C) - read value is wrong");
-    rumboot_printf( "0x10 = %x\n",ioread32 (NOR_BASE + 0x10));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x10) == 0xDEDABABA,"TEST ERROR: (0x10) - read value is wrong");
-    rumboot_printf( "0x14 = %x\n",ioread32 (NOR_BASE + 0x14));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x14) == 0xAAAAAAAA,"TEST ERROR: (0x15) - read value is wrong");
-    rumboot_printf( "0x18 = %x\n",ioread32 (NOR_BASE + 0x18));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x18) == 0x55555555,"TEST ERROR: (0x18) - read value is wrong");
-    rumboot_printf( "0x1C = %x\n",ioread32 (NOR_BASE + 0x1C));
-    TEST_ASSERT (ioread32 (NOR_BASE + 0x1C) == 0xCCCCCCCC,"TEST ERROR: (0x1C) - read value is wrong");
 
-    rumboot_printf( "0xFFFFFE0 = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x20));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x20) == 0x00000000,"TEST ERROR: (0xFFFFFE0) - read value is wrong");
-    rumboot_printf( "0xFFFFFE4 = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x1C));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x1C) == 0x10101010,"TEST ERROR: (0xFFFFFE4) - read value is wrong");
-    rumboot_printf( "0xFFFFFE8 = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x18));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x18) == 0x11111111,"TEST ERROR: (0xFFFFFE8) - read value is wrong");
-    rumboot_printf( "0xFFFFFEC = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x14));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x14) == 0xBABADEDA,"TEST ERROR: (0xFFFFFEC) - read value is wrong");
-    rumboot_printf( "0xFFFFFF0 = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x10));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x10) == 0xDEDABABA,"TEST ERROR: (0xFFFFFF0) - read value is wrong");
-    rumboot_printf( "0xFFFFFF4 = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x0C));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x0C) == 0xAAAAAAAA,"TEST ERROR: (0xFFFFFF4) - read value is wrong");
-    rumboot_printf( "0xFFFFFF8 = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x08));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x08) == 0x55555555,"TEST ERROR: (0xFFFFFF8) - read value is wrong");
-    rumboot_printf( "0xFFFFFFC = %x\n",ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x04));
-    TEST_ASSERT (ioread32 ((uint32_t) NOR_BASE + NOR_SIZE - 0x04) == 0xCCCCCCCC,"TEST ERROR: (0xFFFFFFC) - read value is wrong");
+    struct regpoker_checker check_array[] = {
+      { "Data0",  REGPOKER_READ32, NOR_BASE + 0x0, 0x00000000, 0xffffffff },
+      { "Data1",  REGPOKER_READ32, NOR_BASE + 0x4, 0x10101010, 0xffffffff },
+      { "Data2",  REGPOKER_READ32, NOR_BASE + 0x8, 0x11111111, 0xffffffff },
+      { "Data3",  REGPOKER_READ32, NOR_BASE + 0xC, 0xBABADEDA, 0xffffffff },
+      { "Data4",  REGPOKER_READ32, NOR_BASE + 0x10, 0xDEDABABA, 0xffffffff },
+      { "Data5",  REGPOKER_READ32, NOR_BASE + 0x14, 0xAAAAAAAA, 0xffffffff },
+      { "Data6",  REGPOKER_READ32, NOR_BASE + 0x18, 0x55555555, 0xffffffff },
+      { "Data7",  REGPOKER_READ32, NOR_BASE + 0x1C, 0xCCCCCCCC, 0xffffffff },
+      { "Data8",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x20, 0x00000000, 0xffffffff },
+      { "Data9",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x1C, 0x10101010, 0xffffffff },
+      { "Data10",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x18, 0x11111111, 0xffffffff },
+      { "Data11",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x14, 0xBABADEDA, 0xffffffff },
+      { "Data12",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x10, 0xDEDABABA, 0xffffffff },
+      { "Data13",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x0C, 0xAAAAAAAA, 0xffffffff },
+      { "Data14",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x08, 0x55555555, 0xffffffff },
+      { "Data15",  REGPOKER_READ32, (uint32_t) NOR_BASE + NOR_SIZE - 0x04, 0xCCCCCCCC, 0xffffffff },
+      { /* Sentinel */ }
+    };
 
-    rumboot_printf ("TEST OK\n");
-    return 0;
+    if (rumboot_regpoker_check_array(check_array, 0) == 0)
+    {
+        rumboot_printf ("TEST OK\n");
+        return 0;
+    }
+
+    rumboot_printf ("TEST ERROR\n");
+    return 1;
 }
