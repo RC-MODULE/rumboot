10deba548f24a56893f194a68b19b88dca38b914
Vyacheslav Gulyaev <v.gulyaev@modulew.ru>
2018-07-30 14:30:56 +0300

    oi10_rumboot: added nor timings check

--- src/platform/oi10/targets/test_oi10_em2_201.c
+++ src/platform/oi10/targets/test_oi10_em2_201.c
@@ -14,6 +14,26 @@
 #include <platform/devices/emi.h>
 #include <platform/devices/nor_1636RR4.h>
 
+
+#define EVENT_CHECK_SRAM0_TSOE_TCYC  0x0000003E
+#define EVENT_CHECK_NOR_TSOE_TCYC    0x0000003F
+
+
+void refresh_timings(int32_t bank_num)
+{
+    //TODO: have to add other banks
+    //for refreshing timings at some bank we have to make access to some other bank
+    switch (bank_num)
+    {
+        case 0:
+            ioread32(NOR_BASE);
+            break;
+        case 5:
+            ioread32(SRAM0_BASE);
+            break;
+    }
+}
+
 void emi_update_tcyc_tsoe(uint32_t bank_num, ssx_tsoe_t tsoe, ssx_tcyc_t tcyc)
 {
     emi_bank_cfg bn_cfg;
@@ -21,39 +41,53 @@ void emi_update_tcyc_tsoe(uint32_t bank_num, ssx_tsoe_t tsoe, ssx_tcyc_t tcyc)
     bn_cfg.ssx_cfg.T_SOE = tsoe;
     bn_cfg.ssx_cfg.T_CYC = tcyc;
     emi_set_bank_cfg(bank_num, DCR_EM2_EMI_BASE, &bn_cfg);
-    iowrite32_NOR(0xDEDABABA, NOR_BASE);
+    refresh_timings(bank_num);
 }
 
 void check_wrrd(uint32_t addr, uint32_t data)
 {
-    rumboot_printf("Checking store/load data 0x%x to addr 0x%x", data, addr);
-    iowrite32(data, addr);
+    rumboot_printf("Checking store/load data 0x%x to addr 0x%x\n", data, addr);
+    if (addr>=NOR_BASE)
+        nor_write32(data, addr);
+    else
+        iowrite32(data, addr);
     TEST_ASSERT(ioread32(addr)==data, "Data error!");
 }
 
-int check_sram(uint32_t base_addr)
+/*
+ * SRAM0 and NOR (2.1.1 and 2.1.2 of PPC_SRAM_SDRAM_slave0_testplan.docx)
+ */
+int check_sram_nor(uint32_t base_addr)
 {
-    #define EVENT_CHECK_SRAM0_TSOE_TCYC  0x0000003E
     #define SRAM_TSOE_SPACE  2
     #define SRAM_TCYC_SPACE  4
-    #define SRAM0_TEST_ADDR_0  (base_addr +  0x4)
-    #define SRAM0_TEST_ADDR_1  (base_addr + 0x10)
+    #define TEST_ADDR_0  (base_addr +  0x4)
+    #define TEST_ADDR_1  (base_addr + 0x10)
 
-    const ssx_tsoe_t test_tsoe_arr[SRAM_TSOE_SPACE] = {TSOE_1, TSOE_2};
-    const ssx_tcyc_t test_tcyc_arr[SRAM_TCYC_SPACE] = {TCYC_2, TCYC_3, TCYC_4, TCYC_5};
+    const ssx_tsoe_t test_tsoe_arr[SRAM_TSOE_SPACE]       = {TSOE_1, TSOE_2};
+    const ssx_tcyc_t test_tcyc_arr_sram0[SRAM_TCYC_SPACE] = {TCYC_2, TCYC_3, TCYC_4, TCYC_5};
+    const ssx_tcyc_t test_tcyc_arr_nor[SRAM_TCYC_SPACE]   = {TCYC_3, TCYC_5, TCYC_9, TCYC_12};
+    ssx_tcyc_t tcyc;
+    uint32_t event_code = (base_addr==NOR_BASE) ? EVENT_CHECK_NOR_TSOE_TCYC : EVENT_CHECK_SRAM0_TSOE_TCYC;
+    uint32_t bank       = (base_addr==NOR_BASE) ? 5 : 0;
 
-    rumboot_printf("Checking SRAM (0x%X)\n", base_addr);
+    rumboot_printf("Checking SRAM/NOR (0x%X)\n", base_addr);
+
     for (int i=0; i<SRAM_TSOE_SPACE; i++)
         for (int j=0; j<SRAM_TCYC_SPACE; j++)
         {
-            emi_update_tcyc_tsoe(0, test_tsoe_arr[i], test_tcyc_arr[j]);
-            test_event(EVENT_CHECK_SRAM0_TSOE_TCYC);
-            check_wrrd(SRAM0_TEST_ADDR_0, (i<<16) | j);
-            check_wrrd(SRAM0_TEST_ADDR_1, ~((i<<16) | j));
+            tcyc = (base_addr==NOR_BASE) ? test_tcyc_arr_nor[j] : test_tcyc_arr_sram0[j];
+            emi_update_tcyc_tsoe(bank, test_tsoe_arr[i], tcyc);
+            test_event(event_code);
+            check_wrrd(TEST_ADDR_0, (i<<16) | j);
+            check_wrrd(TEST_ADDR_1, ~((i<<16) | j));
         }
     return 0;
 }
 
+/*
+ * SDRAM
+ */
 //TODO:
 int check_sdram(uint32_t base_addr)
 {
@@ -61,6 +95,9 @@ int check_sdram(uint32_t base_addr)
     return 0;
 }
 
+/*
+ * SSRAM
+ */
 //TODO:
 int check_ssram(uint32_t base_addr)
 {
@@ -68,6 +105,9 @@ int check_ssram(uint32_t base_addr)
     return 0;
 }
 
+/*
+ * PIPELINED
+ */
 //TODO:
 int check_pipelined(uint32_t base_addr)
 {
@@ -75,13 +115,6 @@ int check_pipelined(uint32_t base_addr)
     return 0;
 }
 
-int check_nor(uint32_t base_addr)
-{
-    rumboot_printf("Checking NOR (0x%X)\n", base_addr);
-    iowrite32_NOR(0xDEDABABA, base_addr);
-    rumboot_printf("NOR[0x%x] = 0x%X\n", base_addr, ioread32(base_addr));
-    return 0;
-}
 
 
 int main()
@@ -93,7 +126,7 @@ int main()
     switch (EXT_MEM_BASE)
     {
         case SRAM0_BASE:
-            ret = check_sram(EXT_MEM_BASE);
+            ret = check_sram_nor(EXT_MEM_BASE);
             break;
         case SDRAM_BASE:
             ret = check_sdram(EXT_MEM_BASE);
@@ -105,10 +138,10 @@ int main()
             ret = check_pipelined(EXT_MEM_BASE);
             break;
         case SRAM1_BASE:
-            ret = check_sram(EXT_MEM_BASE);
+            ret = check_sram_nor(EXT_MEM_BASE);
             break;
         case NOR_BASE:
-            ret = check_nor(EXT_MEM_BASE);
+            ret = check_sram_nor(EXT_MEM_BASE);
             break;
         default:
             rumboot_printf("Wrong base address 0x%X", EXT_MEM_BASE);
