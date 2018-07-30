624e86693860f3912fa67e405b208632ea537053
Rishat Galiulin <r.galiulin@modulew.ru>
2018-07-30 16:27:05 +0300

    oi10_rumboot: UTLB page way is fixed

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_108.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_108.c
@@ -188,8 +188,9 @@ void check_sram0 (const uint32_t addr)
     rumboot_printf ("CHECK W/R SRAM0 SUCCESSFUL\n");
 }
 
-#define TLB_ENTRY_CACHE_WT   MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,  0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,     MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b1 )
-#define TLB_ENTRY_CACHE_WB   MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b0,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,  0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,     MMU_TLBWE_WAY_UND,  MMU_TLBWE_BE_UND,   0b1 )
+/*                          MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )*/
+#define TLB_ENTRY_CACHE_WT  MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,     MMU_TLBWE_WAY_3,      MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_CACHE_WB  MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b0,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,     MMU_TLBWE_WAY_3,      MMU_TLBWE_BE_UND,   0b1 )
 
 int main ()
 {
