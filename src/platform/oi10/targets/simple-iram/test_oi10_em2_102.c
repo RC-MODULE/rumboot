c95d53ce4464d1a1ab1a038d93e5ed4a821f23d3
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-07-31 15:16:47 +0300

    oi10_rumboot: test_oi10_em2_102 has been updated.

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_102.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_102.c
@@ -16,13 +16,12 @@
 #include <platform/devices/emi.h>
 
 
-
 #define TLB_ENTRY_CACHE_ON    MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b0,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,  MMU_TLBWE_BE_UND,   0b1 )
-#define TLB_ENTRY_CACHE_OFF   MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b1,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,  MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_CACHE_OFF   MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,  MMU_TLBWE_BE_UND,   0b1 )
 
 
-//const uint32_t EVENT_OI10_EM2_102_CHECK_CACHE_OFF = 0x00001000;
-//const uint32_t EVENT_OI10_EM2_102_CHECK_CACHE_ON = 0x00001001;
+const uint32_t EVENT_OI10_EM2_102_CHECK_CACHE_OFF = 0x00001000;
+const uint32_t EVENT_OI10_EM2_102_CHECK_CACHE_ON = 0x00001001;
 
 
 //uint32_t check_instruction_read_access_em2() __attribute__((section(".EM2.text")));
@@ -32,18 +31,14 @@ uint32_t check_instruction_read_access_sram0()
 
     for(i=0; i<32; i++)
     {
-//        rumboot_printf("Before: Reading instructions from SRAM0 is OK\n");
         j=i+i;
         k=2-i;
         l=i*i;
         m=10*i;
         n=13+i;
-        rumboot_printf("After: Reading instructions from SRAM0 is OK\n");
-        rumboot_printf("i = %d, j = %d, k = %d, l = %d, m = %d, n = %d\n", i, j, k, l, m, n);
-//        rumboot_printf("After: Reading instructions from SRAM0 is OK\n");
     };
 
-    rumboot_printf("Reading instructions from SRAM0 is OK\n");
+    iowrite32((j*k-l+n-m), (SRAM0_BASE + 512));
 
     return 0xBABADEDA;
 };
@@ -53,20 +48,21 @@ int main(void)
 {
     emi_init();
 
-    rumboot_printf("Copy code from IM0 to SRAM0 memory ... \n");
+    test_event_send_test_id("test_oi10_em2_102");
+
+    rumboot_printf("Copying code from IM0 to SRAM0 memory ... \n");
 
     uint32_t check_instruction_read_access_sram0_start_addr = (uint32_t)&check_instruction_read_access_sram0;
     uint32_t check_instruction_read_access_sram0_end_addr = (uint32_t)&main;
 
-    //rumboot_printf("check_instruction_read_access_sram0_start_addr = 0x%x\n", check_instruction_read_access_sram0_start_addr);
-    //rumboot_printf("check_instruction_read_access_sram0_end_addr = 0x%x\n", check_instruction_read_access_sram0_end_addr);
-
     for (uint32_t addr = check_instruction_read_access_sram0_start_addr, sram0_offset = 0;
-            addr < check_instruction_read_access_sram0_end_addr;
+            addr < check_instruction_read_access_sram0_start_addr + 256;
             addr+=4, sram0_offset+=4) {
-//        uint32_t val = ioread32(addr);
-//        rumboot_printf("source addr = 0x%x, val = 0x%x, dest addr = 0x%x\n", addr, val, (SRAM0_BASE + sram0_offset));
-        iowrite32(ioread32(addr), SRAM0_BASE + sram0_offset);
+
+        if (addr < check_instruction_read_access_sram0_end_addr)
+            iowrite32(ioread32(addr), SRAM0_BASE + sram0_offset);
+        else
+            iowrite32(0x24000000, SRAM0_BASE + sram0_offset);
     }
 
     rumboot_printf("Check SRAM0 instructions reading (CACHE OFF)\n");
@@ -77,8 +73,9 @@ int main(void)
     isync();
 
     rumboot_printf("Testing ... \n");
-    //test_event(EVENT_OI10_EM2_102_CHECK_CACHE_OFF);
+    test_event(EVENT_OI10_EM2_102_CHECK_CACHE_OFF);
     TEST_ASSERT(((uint32_t (*)())(SRAM0_BASE))() == 0xBABADEDA, "Error occurred during SRAM0 function call");
+
 
 
     //cache on
@@ -90,7 +87,7 @@ int main(void)
     isync();
 
     rumboot_printf("Testing ... \n");
-    //test_event(EVENT_OI10_EM2_102_CHECK_CACHE_ON);
+    test_event(EVENT_OI10_EM2_102_CHECK_CACHE_ON);
     TEST_ASSERT(((uint32_t (*)())(SRAM0_BASE))() == 0xBABADEDA, "Error occurred during SRAM0 function call");
 
     return 0;
