aa33ed71e5bf41f231d42fb268215bc7a8e5fe5b
Vyacheslav Gulyaev <v.gulyaev@modulew.ru>
2018-08-03 17:23:45 +0300

    oi10_rumboot: in progress

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_207.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_207.c
@@ -16,6 +16,83 @@
 #include <platform/devices/emi.h>
 #include <platform/devices/nor_1636RR4.h>
 
+void report_data_error(uint32_t addr, uint32_t exp, uint32_t act)
+{
+    rumboot_printf("Data compare error at addr 0x%X!\n Expected: 0x%X\nActual: 0x%X\n", addr, exp, act);
+}
+
+void check_wrrd8(uint32_t addr, uint8_t data)
+{
+    uint8_t rd;
+    rumboot_printf("Check Write/Read operation. Width: 1 byte\n");
+
+    iowrite8(data, addr);
+
+    rd = ioread8(addr);
+    if (rd != data)
+    {
+        report_data_error(addr, data, rd);
+        TEST_ASSERT(0, "Data error (1 byte)");
+    }
+}
+
+void check_wrrd16(uint32_t addr, uint16_t data)
+{
+    uint16_t rd;
+    rumboot_printf("Check Write/Read operation. Width: 2 bytes\n");
+
+    iowrite16(data, addr);
+
+    rd = ioread16(addr);
+    if (rd != data)
+    {
+        report_data_error(addr, data, rd);
+        TEST_ASSERT(0, "Data error (2 bytes)");
+    }
+}
+
+void check_wrrd32(uint32_t addr, uint16_t data)
+{
+    uint32_t rd;
+    rumboot_printf("Check Write/Read operation. Width: 4 bytes\n");
+
+    iowrite32(data, addr);
+
+    rd = ioread32(addr);
+    if (rd != data)
+    {
+        report_data_error(addr, data, rd);
+        TEST_ASSERT(0, "Data error (4 bytes)");
+    }
+}
+
+void check_wrrd64(uint32_t addr, uint64_t data)
+{
+    uint64_t rd;
+    rumboot_printf("Check Write/Read operation. Width: 8 bytes\n");
+
+    iowrite64(data, addr);
+
+    rd = ioread64(addr);
+    if (rd != data)
+    {
+        report_data_error(addr, data, rd);
+        TEST_ASSERT(0, "Data error (8 bytes)");
+    }
+}
+
+void check8(uint32_t addr)
+{
+
+    for (int i=0; i<16; i++)
+    {
+        if (i<8)
+            check_wrrd8(addr + i, (1 << i) );
+        else
+            check_wrrd8(addr + i, ~(1 << (i - 8) ));
+    }
+}
+
 int main()
 {
 
@@ -23,6 +100,8 @@ int main()
 
     emi_init();
     emi_set_ecc(DCR_EM2_EMI_BASE, emi_bank_all, emi_ecc_on);
+
+    check8(SRAM0_BASE);
 
     return 0;
 }
