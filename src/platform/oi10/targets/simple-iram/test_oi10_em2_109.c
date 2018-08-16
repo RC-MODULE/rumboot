8d5ba5835378f10bb4197894ca660194c5601440
Maksim Dubinkin <m.dubinkin.@modulew.ru>
2018-08-16 08:56:23 +0300

    oi10_rumboot:test em2 changes address for copy/check data

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_109.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_109.c
@@ -20,7 +20,7 @@
 #include <platform/devices.h>
 
 #define NUM_ELEM    256
-
+/*
 #define input_data_transfer_em2_1   0xC0000001 //init see in .svh
 #define input_data_transfer_em3_1   0xD0010002
 #define input_data_transfer_em2_2   0xC0003008
@@ -30,6 +30,16 @@
 #define output_data_transfer_em2_2  0xC0002006
 #define output_data_transfer_em2_3  0xC0004008
 #define output_data_transfer_em2_4  0xC0005000
+*/
+static uint32_t* em2_cs0_sram_in = (uint32_t*)(SRAM0_BASE + 0x100);    ///CHANNEL 0            //init see in .svh
+static uint32_t* em2_cs1_nor_in = (uint32_t*)(NOR_BASE + 0x105);      ///CHANNEL 1
+//static uint32_t input_data_transfer_em2_2 = 0xC0003008;
+//static uint32_t input_data_transfer_em3_2 = 0xD0011000;
+
+static uint32_t* em2_cs0_sram_out = (uint32_t*)(SRAM0_BASE + 0x103); ///CHANNEL 0
+static uint32_t* em2_cs0_sram_out_1 = (uint32_t*)(SRAM0_BASE + 0x107); ///CHANNEL 1
+//static uint32_t output_data_transfer_em2_3 = 0xC0004008;
+//static uint32_t output_data_transfer_em2_4 = 0xC0005000;
 
 uint32_t dma_single_copy(dma2plb6_setup_info * setup_info, channel_status * status)
 {
@@ -51,26 +61,27 @@ uint32_t check_data ()
     for (i = 0; i < NUM_ELEM; i++)
     {
         rumboot_printf("Num elem: "); //trace_hex(i);
-
-        if (((uint32_t*) output_data_transfer_em2_1) [i] != ((uint32_t*) input_data_transfer_em2_1)[i])
+        ///em2.cs0_in->im2.cs0_out
+        if (((uint32_t*) em2_cs0_sram_in)[i] != ((uint32_t*)em2_cs0_sram_out)[i])
         {
-            rumboot_printf ("Error in output data 1, addr = %d\n",((uint32_t*) output_data_transfer_em2_1)[i]);
+            rumboot_printf ("Error in output data 1, addr = %d\n",((uint32_t*)em2_cs0_sram_in)[i]);
 
             //trace_hex (&(((uint32_t*)output_data_transfer_em2_1) [i]));
-            rumboot_printf("Expected data = %d\n", ((uint32_t*)input_data_transfer_em2_1)[i]); //trace_hex(((uint32_t*)input_data_transfer_em2_1) [i]);
-            rumboot_printf("Actual data = %d\n", ((uint32_t*)output_data_transfer_em2_1)[i]); //trace_hex(((uint32_t*)output_data_transfer_em2_1) [i]);
+            rumboot_printf("Expected data = %d\n", ((uint32_t*)em2_cs0_sram_out)[i]); //trace_hex(((uint32_t*)input_data_transfer_em2_1) [i]);
+            rumboot_printf("Actual data = %d\n", ((uint32_t*)em2_cs0_sram_in)[i]); //trace_hex(((uint32_t*)output_data_transfer_em2_1) [i]);
             return 0;
         };
-
-        if (((uint32_t*) output_data_transfer_em2_2) [i] != ((uint32_t*) input_data_transfer_em3_1) [i])
+        ///em2.cs1.nor_in->em2.cs0_sram_out
+        if (((uint32_t*) em2_cs1_nor_in)[i] != ((uint32_t*) em2_cs0_sram_out_1)[i])
         {
-            rumboot_printf("Error in output data 2, addr = %d\n", ((uint32_t*) output_data_transfer_em2_2)[i]);
+            rumboot_printf("Error in output data 2, addr = %d\n", ((uint32_t*) em2_cs0_sram_out_1)[i]);
             //trace_hex (&(((uint32_t*)output_data_transfer_em2_2) [i]));
-            rumboot_printf("Expected data = %d\n", ((uint32_t*)input_data_transfer_em3_1)[i]); //trace_hex(((uint32_t*) input_data_transfer_em3_1) [i]);
-            rumboot_printf("Actual data = %d\n", ((uint32_t *)output_data_transfer_em2_2)[i]); //trace_hex(((uint32_t*)output_data_transfer_em2_2) [i]);
+            rumboot_printf("Expected data = %d\n", ((uint32_t*)em2_cs0_sram_out_1)[i]); //trace_hex(((uint32_t*) input_data_transfer_em3_1) [i]);
+            rumboot_printf("Actual data = %d\n", ((uint32_t *)em2_cs1_nor_in)[i]); //trace_hex(((uint32_t*)output_data_transfer_em2_2) [i]);
             return 0;
         };
-
+       /*
+        ///em2.cs2.sdram_in->em2.cs2.sdram_out
         if (((uint32_t*) output_data_transfer_em2_3) [i] != ((uint32_t*) input_data_transfer_em2_2)[i])
         {
             rumboot_printf ("Error in output data 3, addr =  %d\n", ((uint32_t*)output_data_transfer_em2_3)[i]);
@@ -79,7 +90,7 @@ uint32_t check_data ()
             rumboot_printf("Actual data = %d\n", ((uint32_t*) output_data_transfer_em2_3)[i]); //trace_hex(((uint32_t*)output_data_transfer_em2_3) [i]);
             return 0;
         };
-
+        ///em2.cs1.nor->em0.cs2.sdram
         if (((uint32_t*)output_data_transfer_em2_4) [i] != ((uint32_t*) input_data_transfer_em3_2)[i])
         {
             rumboot_printf ("Error in output data 4, addr = %d\n", ((uint32_t*)output_data_transfer_em2_4)[i]);
@@ -88,10 +99,10 @@ uint32_t check_data ()
             rumboot_printf("Actual data = %d\n", ((uint32_t*)output_data_transfer_em2_4)[i]); //trace_hex(((uint32_t*)output_data_transfer_em2_4) [i]);
             return 0;
         };
-
+*/
     }
     rumboot_printf ("Check data successful\n");
-    return 1;
+    return true;
 }
 
 void dma2plb6_trace_status(channel_status status)
@@ -118,10 +129,13 @@ void dma2plb6_trace_status(channel_status status)
     }
 }
 
-void dma2plb6_memcpy(const uint32_t base_addr, const uint64_t source, const uint64_t dest, const DmaChannel channel, const transfer_width width, const rw_transfer_size size, const uint32_t count, uint8_t wait)
+static bool dma2plb6_memcpy(const uint32_t base_addr, const uint64_t source, const uint64_t dest,
+        const DmaChannel channel, const transfer_width width,
+        const rw_transfer_size size,
+        const uint32_t count)
 {
     dma2plb6_setup_info dma_info;
-    channel_status status = {0};
+    channel_status status = {};
     dma_info.base_addr = base_addr;
     dma_info.source_adr = source;
     dma_info.dest_adr = dest;
@@ -135,38 +149,22 @@ void dma2plb6_memcpy(const uint32_t base_addr, const uint64_t source, const uint
     dma_info.rw_transfer_size = size;
     dma_info.count = count;
 
-    rumboot_printf("Call dma2plb6_memcpy\n");
+    rumboot_putstring("Call dma2plb6_memcpy\n");
 
-    rumboot_printf("Source address:\n");
-    rumboot_printf("hi= %d\n", (uint32_t)(source >> 32)); //trace_hex((uint32_t)(source >> 32));
-    rumboot_printf("lo= %d\n", (uint32_t)(source)); //trace_hex((uint32_t)(source));
+    rumboot_putstring("Source address:\n");
+    rumboot_printf("hi = 0x%x\n", (uint32_t)(source >> 32));
+    rumboot_printf("lo = 0x%x\n", (uint32_t)(source));
 
-    rumboot_printf("Destination address\n");
-    rumboot_printf("hi= %d\n", (uint32_t)(dest >> 32)); //trace_hex((uint32_t)(dest >> 32));
-    rumboot_printf("lo= %d\n", (uint32_t)dest); //trace_hex((uint32_t)dest);
+    rumboot_putstring("Destination address\n");
+    rumboot_printf("hi = 0x%x\n", (uint32_t)(dest >> 32));
+    rumboot_printf("lo = 0x%x\n", (uint32_t)dest);
 
-
-    if (wait == 0){ dma2plb6_mcpy(&dma_info);}
-    else
+    if(dma2plb6_single_copy(&dma_info,&status) == false)
     {
-        if(dma_single_copy(&dma_info,&status) == false)
-        {
-            dma2plb6_trace_status(status);
-        }
+        dma2plb6_trace_status(status);
+        return false;
     }
-
-}
-
-uint64_t get_physical_addr_em2 (uint32_t ea)
-{
-    //return ea + 0x400000000 - 0xc0000000;
-    return ea + EM2_BANK0_BASE - 0xc0000000;
-}
-
-uint64_t get_physical_addr_em3 (uint32_t ea)
-{
-//    return ea + 0x600000000 - 0xd0000000;
-    return ea + EM2_BANK0_BASE - 0xd0000000;
+    return true;
 }
 
 int main (void)
@@ -177,23 +175,31 @@ int main (void)
     emi_init();
 
     //init_data see in .svh
+    /*
+    static bool dma2plb6_memcpy(const uint32_t base_addr,
+            const uint64_t source,
+            const uint64_t dest,
+            const DmaChannel channel,
+            const transfer_width width,
+            const rw_transfer_size size,
+            const uint32_t count);
+    */
+    src = rumboot_virt_to_dma((uint32_t*)em2_cs0_sram_in);
+    dst = rumboot_virt_to_dma((uint32_t*)em2_cs0_sram_out);
+    dma2plb6_memcpy (EM2_BASE, src, dst, channel0, tr_width_byte, rw_tr_size_1q, size_for_dma);
 
-    src = get_physical_addr_em2((uint32_t)input_data_transfer_em2_1);
-    dst = get_physical_addr_em2((uint32_t)output_data_transfer_em2_1);
-    dma2plb6_memcpy (EM2_BASE, src, dst, channel0, tr_width_byte, rw_tr_size_1q, size_for_dma, 0);
+    src = rumboot_virt_to_dma((uint32_t*)em2_cs1_nor_in);
+    dst = rumboot_virt_to_dma((uint32_t*) em2_cs0_sram_out_1);
+    dma2plb6_memcpy (EM2_BASE, src, dst, channel1, tr_width_halfword, rw_tr_size_2q, size_for_dma/2);
+/*
+    src = rumboot_virt_to_dma((uint32_t*)input_data_transfer_em2_2);
+    dst = rumboot_virt_to_dma((uint32_t*)output_data_transfer_em2_3);
+    dma2plb6_memcpy (EM2_BASE, src, dst, channel2, tr_width_doubleword, rw_tr_size_4q, size_for_dma/8);
 
-    src = get_physical_addr_em3((uint32_t)input_data_transfer_em3_1);
-    dst = get_physical_addr_em2((uint32_t)output_data_transfer_em2_2);
-    dma2plb6_memcpy (EM2_BASE, src, dst, channel1, tr_width_halfword, rw_tr_size_2q, size_for_dma/2, 0);
-
-    src = get_physical_addr_em2((uint32_t)input_data_transfer_em2_2);
-    dst = get_physical_addr_em2((uint32_t)output_data_transfer_em2_3);
-    dma2plb6_memcpy (EM2_BASE, src, dst, channel2, tr_width_doubleword, rw_tr_size_4q, size_for_dma/8, 0);
-
-    src = get_physical_addr_em3((uint32_t)input_data_transfer_em3_2);
-    dst = get_physical_addr_em2((uint32_t)output_data_transfer_em2_4);
-    dma2plb6_memcpy (EM2_BASE, src, dst, channel3, tr_width_quadword, rw_tr_size_8q, size_for_dma/16, 1);
-
+    src = rumboot_virt_to_dma((uint32_t*)input_data_transfer_em3_2);
+    dst = rumboot_virt_to_dma((uint32_t*)output_data_transfer_em2_4);
+    dma2plb6_memcpy (EM2_BASE, src, dst, channel3, tr_width_quadword, rw_tr_size_8q, size_for_dma/16);
+*/
     if (check_data())
         rumboot_putstring("TEST_OK\n");
         return 0;
