4f7dea0078c3abfda0f7dcda6fc498c2df163d18
Vyacheslav Gulyaev <v.gulyaev@modulew.ru>
2018-08-20 13:44:08 +0300

    oi10_rumboot: fix test_oi10_em2_207 due to emi_init dunction changing

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_207.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_207.c
@@ -151,7 +151,7 @@ int main()
 {
     rumboot_printf("Start test_oi10_em2_207\n");
 
-    emi_init();
+    emi_init(DCR_EM2_EMI_BASE);
 
     emi_set_ecc(DCR_EM2_EMI_BASE, emi_bank_all, emi_ecc_on);
     run_check();
