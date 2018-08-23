6e143978c478984320a81e3591f40f522c2e1688
Maksim Dubinkin <m.dubinkin.@modulew.ru>
2018-08-23 08:32:41 +0300

    oi10_rumboot:test does not work with nor read/write access data

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_109.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_109.c
@@ -22,14 +22,15 @@
 #define NUM_ELEM 16
 ///src
 static uint32_t em2_cs0_sram_out = (SRAM0_BASE);
-static uint32_t em2_cs1_nor_out = (NOR_BASE);
+//static uint32_t em2_cs1_nor_out = (NOR_BASE);
 static uint32_t em2_cs2_sdram_out = (SDRAM_BASE);
-static uint32_t em2_cs1_nor_out_3000 = (NOR_BASE) + 4 * NUM_ELEM;
+//static uint32_t em2_cs1_nor_out_3000 = (NOR_BASE) + 4 * NUM_ELEM;
+
 ///dst
 static uint32_t em2_cs0_sram_in = (SRAM0_BASE) + 4 * NUM_ELEM;
-static uint32_t em2_cs0_sram_in_2000 = (SRAM0_BASE) + 2 * 4 * NUM_ELEM;
+//static uint32_t em2_cs0_sram_in_2000 = (SRAM0_BASE) + 2 * 4 * NUM_ELEM;
 static uint32_t em2_cs2_sdram_in_4000 = (SDRAM_BASE) + 4 * NUM_ELEM;
-static uint32_t em2_cs2_sdram_in_8000 = (SDRAM_BASE) + 2 * 4 * NUM_ELEM;
+//static uint32_t em2_cs2_sdram_in_8000 = (SDRAM_BASE) + 2 * 4 * NUM_ELEM;
 
 static void init_data(uint32_t addr, uint32_t value)
 {
@@ -39,16 +40,17 @@ static void init_data(uint32_t addr, uint32_t value)
         iowrite32(value, (uint32_t)(addr + 4*j));
     }
 }
-
+/*
 static void init_nor_data(uint32_t addr, uint32_t value)
 {
     uint32_t j = 0;
-    for(j = 0; j < NUM_ELEM; j++ )
+    for(j = 0; j < NUM_ELEM; j++)
     {
         nor_write32(value, (uint32_t)(addr + 4 * j));
     }
 }
-
+*/
+/*
 static bool check_data ()
 {
     rumboot_printf("Check data...\n");
@@ -57,55 +59,79 @@ static bool check_data ()
     for (i = 0; i < NUM_ELEM; i++)
     {
         ///em2.cs0_in->im2.cs0_out
-        if ( ioread32(em2_cs0_sram_in + 4* i) != ioread32 (em2_cs0_sram_out + 4 * i))
+        if ( ioread32(em2_cs0_sram_in + 4* i) != ioread32(em2_cs0_sram_out + 4 * i))
         {
-            rumboot_printf ("Error in output data 1, addr = %d\n", ioread32(em2_cs0_sram_out[i]));
-            rumboot_printf("Expected data = %d\n", ioread32(em2_cs0_sram_out[i]));
-            rumboot_printf("Actual data = %d\n", ioread32(em2_cs0_sram_in[i]));
+//          rumboot_printf ("Error in output data 1, addr = %d\n", ioread32(em2_cs0_sram_out[i]));
+//          rumboot_printf("Expected data = %d\n", ioread32(em2_cs0_sram_out[i]));
+//          rumboot_printf("Actual data = %d\n", ioread32(em2_cs0_sram_in[i]));
             return false;
         };
         ///em2.cs1.nor_in->em2.cs0_sram_out
+
         if (ioread32(em2_cs0_sram_in_2000 + 4 * i) != ioread32(em2_cs1_nor_out + 4 * i))
         {
-            rumboot_printf("Error in output data 2, addr = %d\n", ioread32(em2_cs1_nor_out[i]));
-            rumboot_printf("Expected data = %d\n", ioread32(em2_cs1_nor_out[i]));
-            rumboot_printf("Actual data = %d\n", ioread32(em2_cs0_sram_in_2000[i]));
+        //    rumboot_printf("Error in output data 2, addr = %d\n", ioread32(em2_cs1_nor_out[i]));
+        //    rumboot_printf("Expected data = %d\n", ioread32(em2_cs1_nor_out[i]));
+       //     rumboot_printf("Actual data = %d\n", ioread32(em2_cs0_sram_in_2000[i]));
             return false;
         };
+
         ///em2.cs2.sdram_in->em2.cs2.sdram_out
         if (ioread32(em2_cs2_sdram_in_4000 + 4*i) != ioread32(em2_cs2_sdram_out + 4*i))
         {
-            rumboot_printf ("Error in output data 3, addr =  %d\n", ioread32(em2_cs2_sdram_out[i]));
-            rumboot_printf("Expected data = %d\n",ioread32(em2_cs2_sdram_out[i]));
-            rumboot_printf("Actual data = %d\n", ioread32(em2_cs2_sdram_in_4000[i]));
+       //     rumboot_printf ("Error in output data 3, addr =  %d\n", ioread32(em2_cs2_sdram_out[i]));
+       //     rumboot_printf("Expected data = %d\n",ioread32(em2_cs2_sdram_out[i]));
+       //     rumboot_printf("Actual data = %d\n", ioread32(em2_cs2_sdram_in_4000[i]));
             return false;
         };
         ///em2.cs1.nor->em0.cs2.sdram
        if (ioread32(em2_cs2_sdram_in_8000 + 4*i) != ioread32(em2_cs1_nor_out_3000 + 4 *i))
         {
-            rumboot_printf ("Error in output data 4, addr = %d\n", ioread(em2_cs1_nor_out_3000[i]));
-            rumboot_printf("Expected data = %d\n", ioread32(em2_cs1_nor_out_3000[i]));
-            rumboot_printf("Actual data = %d\n", ioread32(em2_cs2_sdram_in_8000[i]));
+         //   rumboot_printf ("Error in output data 4, addr = %d\n", ioread(em2_cs1_nor_out_3000[i]));
+        //    rumboot_printf("Expected data = %d\n", ioread32(em2_cs1_nor_out_3000[i]));
+        //    rumboot_printf("Actual data = %d\n", ioread32(em2_cs2_sdram_in_8000[i]));
             return false;
         };
+
     }
     rumboot_printf ("Check data successful\n");
+    return true;
+}
+*/
+
+static bool check_data1(uint32_t addr_dst, uint32_t addr_src)
+{
+    rumboot_printf("Check data...\n");
+    rumboot_printf("Count of data elements: %d \n", NUM_ELEM);
+    register uint32_t i = 0;
+    for (i = 0; i < NUM_ELEM; i++)
+    {
+        ///em2.cs0_in->im2.cs0_out
+        if (ioread32(addr_dst + 4* i) != ioread32(addr_src + 4 * i))
+        {
+          //  rumboot_printf ("Error in output data 1, addr = %d\n", ioread32(addr_src[i]));
+          //  rumboot_printf("Expected data = %d\n", ioread32(addr_src[i]));
+          //  rumboot_printf("Actual data = %d\n", ioread32(addr_dst[i]));
+            return false;
+        }
+    }
     return true;
 }
 
 int main (void)
 {
     emi_init(DCR_EM2_EMI_BASE);
-    uint32_t j = 0;
 
     rumboot_printf ("Init data0\n") ;
     init_data(em2_cs0_sram_out, 0xaaaaaaaa);
-    rumboot_printf("Init data1\n");
-    init_nor_data(em2_cs1_nor_out, 0xbbbbbbbb);
+//    rumboot_printf("Init data1\n");
+//    init_nor_data(em2_cs1_nor_out, 0xbbbbbbbb);
     rumboot_printf ("Init data2\n") ;
     init_data(em2_cs2_sdram_out, 0xcccccccc);
-    rumboot_printf("Init data3 \n");
-    init_nor_data(em2_cs1_nor_out, 0xdddddddd);
+//    rumboot_printf("Init data3 \n");
+//    init_nor_data(em2_cs1_nor_out_3000, 0xdddddddd);
+
+    rumboot_printf ("End init data\n");
 
     uint32_t size_for_dma = NUM_ELEM * sizeof(uint32_t);
 
@@ -123,7 +149,7 @@ int main (void)
     dma_info0.transfer_width = tr_width_byte;
     dma_info0.rw_transfer_size = rw_tr_size_1q;
     dma_info0.count = size_for_dma;
-
+/*
     dma2plb6_setup_info dma_info1;
     channel_status status1 = {};
     dma_info1.base_addr = DCR_DMAPLB6_BASE;
@@ -138,6 +164,7 @@ int main (void)
     dma_info1.transfer_width = tr_width_halfword;
     dma_info1.rw_transfer_size = rw_tr_size_2q;
     dma_info1.count = size_for_dma/2;
+*/
 
     dma2plb6_setup_info dma_info2;
     channel_status status2 = {};
@@ -153,7 +180,7 @@ int main (void)
     dma_info2.transfer_width = tr_width_doubleword;
     dma_info2.rw_transfer_size = rw_tr_size_4q;
     dma_info2.count = size_for_dma/8;
-
+/*
     dma2plb6_setup_info dma_info3;
     channel_status status3 = {};
     dma_info3.base_addr = DCR_DMAPLB6_BASE;
@@ -168,26 +195,50 @@ int main (void)
     dma_info3.transfer_width = tr_width_quadword;
     dma_info3.rw_transfer_size = rw_tr_size_8q;
     dma_info3.count = size_for_dma/16;
-
+*/
     rumboot_printf ("Start dma0\n");
     dma2plb6_mcpy(&dma_info0);
-    rumboot_printf ("Start dma1\n");
-    dma2plb6_mcpy(&dma_info1);
+//    rumboot_printf ("Start dma1\n");
+//    dma2plb6_mcpy(&dma_info1);
     rumboot_printf ("Start dma2\n");
     dma2plb6_mcpy(&dma_info2);
-    rumboot_printf ("Start dma3\n");
-    dma2plb6_mcpy(&dma_info3);
+//    rumboot_printf ("Start dma3\n");
+//    dma2plb6_mcpy(&dma_info3);
+//  rumboot_printf ("Start wait...\n");
 
-    rumboot_printf ("Start wait...\n");
-    if (wait_dma2plb6_mcpy(&dma_info0, &status0) && wait_dma2plb6_mcpy(&dma_info1, &status1) &&
-            wait_dma2plb6_mcpy(&dma_info2, &status2) && wait_dma2plb6_mcpy(&dma_info3, &status3) == false) return 1; // == TEST_EROR
-
-    if (check_data())
+   // if (wait_dma2plb6_mcpy(&dma_info0, &status0) && wait_dma2plb6_mcpy(&dma_info1, &status1) &&
+    //        wait_dma2plb6_mcpy(&dma_info2, &status2) && wait_dma2plb6_mcpy(&dma_info3, &status3) == false) return 1; // == TEST_EROR
+    /*
+    if((wait_dma2plb6_mcpy(&dma_info0, &status0) == false && check_data() == false) && (wait_dma2plb6_mcpy(&dma_info1, &status1) == false && check_data() == false)
+            && (wait_dma2plb6_mcpy(&dma_info2, &status2) == false && check_data() == false)
+             && (wait_dma2plb6_mcpy(&dma_info3, &status3) == false && check_data() == false))
     {
-       rumboot_printf ("TEST_OK\n") ;
-       return 0;
+        rumboot_printf("WAIT2PLB6_MCPY IS FAIL!\n");
+        return 1;
+    }
+    */
+
+    if( !wait_dma2plb6_mcpy(&dma_info0, &status0) )// &&
+      // (wait_dma2plb6_mcpy(&dma_info1, &status1) == false) && (check_data1(em2_cs0_sram_in_2000, em2_cs1_nor_out) == false) &&
+     //  (wait_dma2plb6_mcpy(&dma_info2, &status2) == false) && (check_data1(em2_cs2_sdram_in_4000, em2_cs2_sdram_out) == false) &&
+     //  (wait_dma2plb6_mcpy(&dma_info3, &status3) == false) && (check_data1(em2_cs2_sdram_in_8000, em2_cs1_nor_out_3000) == false))
+    {
+        rumboot_printf("WAIT2PLB6_MCPY IS FAIL!\n");
+        return 1;
+    }
+    if( !check_data1(em2_cs0_sram_in, em2_cs0_sram_out))
+        return 1;
+
+    if( !wait_dma2plb6_mcpy(&dma_info2, &status2) )
+    {
+        rumboot_printf("WAIT2PLB6_MCPY IS FAIL!\n");
+                return 1;
     }
 
-    rumboot_printf ("TEST_ERROR\n") ;
-    return 1;
+    if( !check_data1(em2_cs2_sdram_in_4000, em2_cs2_sdram_out))
+            return 1;
+
+
+
+    return 0;
 }
