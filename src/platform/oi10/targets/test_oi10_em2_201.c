3eec296d6ef48fa8432cb05054702e0da3d87184
Vyacheslav Gulyaev <v.gulyaev@modulew.ru>
2018-09-05 08:42:11 +0300

    oi10_rumboot: test_oi10_em2_201 in progress

--- src/platform/oi10/targets/test_oi10_em2_201.c
+++ src/platform/oi10/targets/test_oi10_em2_201.c
@@ -14,9 +14,9 @@
 #include <platform/devices/emi.h>
 #include <platform/devices/nor_1636RR4.h>
 
-#define EVENT_CHECK_SRAM0_TSOE_TCYC      0x0000003E
-#define EVENT_CHECK_NOR_TSOE_TCYC        0x0000003F
-#define EVENT_CHECK_SRAM1_TSOE_TCYC      0x00000040
+#define EVENT_CHECK_SRAM0_TSOE_TCYC       0x0000003E
+#define EVENT_CHECK_NOR_TSOE_TCYC         0x0000003F
+#define EVENT_CHECK_SRAM1_TSOE_TCYC       0x00000040
 #define EVENT_CHECK_SSRAM_SST_TSSOE_FT    0x00000041
 #define EVENT_CHECK_SSRAM_SST_TSSOE_PIPE  0x00000042
 
@@ -74,35 +74,49 @@ void check_wrrd(uint32_t addr, uint32_t data)
 }
 
 /*
- * SRAM0 and NOR (2.1.1 and 2.1.2 of PPC_SRAM_SDRAM_slave0_testplan.docx)
+ * SRAM0 (2.1.1 of PPC_SRAM_SDRAM_slave0_testplan.docx)
  */
-int check_sram_nor(uint32_t base_addr)
+int check_sram0(uint32_t base_addr)
 {
-    #define SRAM_TSOE_SPACE  2
-    #define SRAM_TCYC_SPACE  4
+    #define SRAM0_TSOE_SPACE  2
+    #define SRAM0_TCYC_SPACE  4
 
-    const ssx_tsoe_t test_tsoe_arr[SRAM_TSOE_SPACE]       = {TSOE_1, TSOE_2};
-    const ssx_tcyc_t test_tcyc_arr_sram0[SRAM_TCYC_SPACE] = {TCYC_7, TCYC_9, TCYC_11, TCYC_15};
-    const ssx_tcyc_t test_tcyc_arr_nor[SRAM_TCYC_SPACE]   = {TCYC_6, TCYC_8, TCYC_10, TCYC_12};
-    ssx_tcyc_t tcyc;
+    const ssx_tsoe_t test_tsoe_arr[SRAM0_TSOE_SPACE] = {TSOE_1, TSOE_2};
+    const ssx_tcyc_t test_tcyc_arr[SRAM0_TCYC_SPACE] = {TCYC_2, TCYC_3, TCYC_4, TCYC_5};
 
-    uint32_t event_code = (base_addr==NOR_BASE)   ? EVENT_CHECK_NOR_TSOE_TCYC   :
-                          (base_addr==SRAM1_BASE) ? EVENT_CHECK_SRAM1_TSOE_TCYC :
-                                                    EVENT_CHECK_SRAM0_TSOE_TCYC;
+    rumboot_printf("Checking SRAM0 (0x%X)\n", base_addr);
 
-    emi_bank_num bank   = (base_addr==NOR_BASE)   ? emi_b5_nor   :
-                          (base_addr==SRAM1_BASE) ? emi_b4_sram1 :
-                                                    0;
-
-    rumboot_printf("Checking SRAM/NOR (0x%X)\n", base_addr);
-
-    for (int i=0; i<SRAM_TSOE_SPACE; i++)
-        for (int j=0; j<SRAM_TCYC_SPACE; j++)
+    for (int i=0; i<SRAM0_TSOE_SPACE; i++)
+        for (int j=0; j<SRAM0_TCYC_SPACE; j++)
         {
-            tcyc = (base_addr==NOR_BASE) ? test_tcyc_arr_nor[j] : test_tcyc_arr_sram0[j];
-            emi_update_tcyc_tsoe(bank, test_tsoe_arr[i], tcyc);
-            rumboot_printf("Setting tcyc / tsoe: %d / %d\n", tcyc, test_tsoe_arr[i]);
-            test_event(event_code);
+            emi_update_tcyc_tsoe(emi_b0_sram0, test_tsoe_arr[i], test_tcyc_arr[j]);
+            rumboot_printf("Setting tcyc / tsoe: %d / %d\n", test_tcyc_arr[j], test_tsoe_arr[i]);
+            test_event(EVENT_CHECK_SRAM0_TSOE_TCYC);
+            check_wrrd(TEST_ADDR_0, (i<<16) | j);
+            check_wrrd(TEST_ADDR_1, ~((i<<16) | j));
+        }
+    return 0;
+}
+
+/*
+ * NOR (2.1.2 of PPC_SRAM_SDRAM_slave0_testplan.docx)
+ */
+int check_nor(uint32_t base_addr)
+{
+    #define NOR_TSOE_SPACE  2
+    #define NOR_TCYC_SPACE  3
+
+    const ssx_tsoe_t test_tsoe_arr[NOR_TSOE_SPACE] = {TSOE_1, TSOE_2};
+    const ssx_tcyc_t test_tcyc_arr[NOR_TCYC_SPACE] = {TCYC_5, TCYC_9, TCYC_13};
+
+    rumboot_printf("Checking NOR (0x%X)\n", base_addr);
+
+    for (int i=0; i<NOR_TSOE_SPACE; i++)
+        for (int j=0; j<NOR_TCYC_SPACE; j++)
+        {
+            emi_update_tcyc_tsoe(emi_b5_nor, test_tsoe_arr[i], test_tcyc_arr[j]);
+            rumboot_printf("Setting tcyc / tsoe: %d / %d\n", test_tcyc_arr[j], test_tsoe_arr[i]);
+            test_event(EVENT_CHECK_NOR_TSOE_TCYC);
             check_wrrd(TEST_ADDR_0, (i<<16) | j);
             check_wrrd(TEST_ADDR_1, ~((i<<16) | j));
         }
@@ -166,7 +180,7 @@ int main()
     switch (EXT_MEM_BASE)
     {
         case SRAM0_BASE:
-            ret = check_sram_nor(EXT_MEM_BASE);
+            ret = check_sram0(EXT_MEM_BASE);
             break;
         case SDRAM_BASE:
             ret = check_sdram(EXT_MEM_BASE);
@@ -178,10 +192,10 @@ int main()
             ret = check_pipelined(EXT_MEM_BASE);
             break;
         case SRAM1_BASE:
-            ret = check_sram_nor(EXT_MEM_BASE);
+            ret = check_sram0(EXT_MEM_BASE);
             break;
         case NOR_BASE:
-            ret = check_sram_nor(EXT_MEM_BASE);
+            ret = check_nor(EXT_MEM_BASE);
             break;
         default:
             rumboot_printf("Wrong base address 0x%X", EXT_MEM_BASE);
