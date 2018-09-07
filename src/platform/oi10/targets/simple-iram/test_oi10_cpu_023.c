2ada3817172c1a431de9085d2e1412bb2ef92238
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-09-07 12:04:40 +0300

    oi10_rumboot: test_oi10_cpu_023 has been updated.

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_023.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_023.c
@@ -17,8 +17,8 @@
 
 
 //size in bytes
-#define TEST_DATA_SIZE 16
-//#define TEST_DATA_SIZE 128
+#define TEST_DATA_SIZE 128
+
 //test data content
 #define TEST_DATA_CONTENT 0x0123456789ABCDEF
 
@@ -105,21 +105,25 @@ static uint32_t check_dma2plb6_0_mem_to_mem(uint32_t source_ea, uint32_t dest_ea
     dma_info.err_int_enable = false;
     dma_info.int_mode = int_mode_level_wait_clr;
     dma_info.channel = channel0;
-    for (dma_info.rw_transfer_size = rw_tr_size_8q; dma_info.rw_transfer_size <= rw_tr_size_8q; dma_info.rw_transfer_size++)
+    for (dma_info.rw_transfer_size = rw_tr_size_1q; dma_info.rw_transfer_size <= rw_tr_size_8q; dma_info.rw_transfer_size++)
     {
         rumboot_printf("rw_transfer_size = 0x%x\n", dma_info.rw_transfer_size);
-        for(dma_info.transfer_width = tr_width_quadword; dma_info.transfer_width <= tr_width_quadword; dma_info.transfer_width++)
+        for(dma_info.transfer_width = tr_width_byte; dma_info.transfer_width <= tr_width_quadword; dma_info.transfer_width++)
         {
+            rumboot_printf("transfer_width = 0x%x\n", dma_info.transfer_width);
+
             fill((uint64_t *)source_ea, fill_word, TEST_DATA_SIZE);
             fill((uint64_t *)dest_ea, 0x0, TEST_DATA_SIZE);
+
             dma_info.count = TEST_DATA_SIZE/dma2plb6_get_bytesize(dma_info.transfer_width);
+
             if(dma2plb6_single_copy(&dma_info, &status) == false){
                 SHOW_STATUS(status.spec_error_status);
                 return false;
             } else {
                 TEST_ASSERT(compare(source_ea, dest_ea, TEST_DATA_SIZE) == true, "Memory comparison failed");
                 fill_word = ~fill_word;
-                rumboot_printf("done: tr_width = 0x%x\n", dma_info.transfer_width);
+//                rumboot_printf("done: tr_width = 0x%x\n", dma_info.transfer_width);
             }
         }
     }
@@ -129,21 +133,35 @@ static uint32_t check_dma2plb6_0_mem_to_mem(uint32_t source_ea, uint32_t dest_ea
 
 static uint32_t check_single_channel()
 {
+    uint32_t src_im0, dst_im0;
+    uint32_t src_im1, dst_im1;
+    uint32_t src_em2, dst_em2;
+
     volatile uint64_t src_im0_physical, dst_im0_physical;
     volatile uint64_t src_im1_physical, dst_im1_physical;
     volatile uint64_t src_em2_physical, dst_em2_physical;
 
+    src_im0 = (uint32_t)source_area_im0;
+    dst_im0 = (uint32_t)dest_area_im0;
+
+    src_im1 = (IM1_BASE);
+    dst_im1 = (IM1_BASE + 0x80);
+
+    src_em2 = (EM2_BASE);
+    dst_em2 = (EM2_BASE + 0x80);
+
+
     //prepare physical address
     set_mem_window(MEM_WINDOW_SHARED);//WORKAROUND
-    src_im0_physical = rumboot_virt_to_dma((uint32_t*)source_area_im0);
-    dst_im0_physical = rumboot_virt_to_dma((uint32_t*)dest_area_im0);
+    src_im0_physical = rumboot_virt_to_phys((uint32_t*)src_im0);
+    dst_im0_physical = rumboot_virt_to_phys((uint32_t*)dst_im0);
 
     set_mem_window(MEM_WINDOW_0);//WORKAROUND
-    src_im1_physical = rumboot_virt_to_dma((uint32_t*)(IM1_BASE));
-    dst_im1_physical = rumboot_virt_to_dma((uint32_t*)(IM1_BASE + 0x80));
+    src_im1_physical = rumboot_virt_to_phys((uint32_t*)src_im1);
+    dst_im1_physical = rumboot_virt_to_phys((uint32_t*)dst_im1);
 
-    src_em2_physical = rumboot_virt_to_dma((uint32_t*)(EM2_BASE));
-    dst_em2_physical = rumboot_virt_to_dma((uint32_t*)(EM2_BASE + 0x80));
+    src_em2_physical = rumboot_virt_to_phys((uint32_t*)src_em2);
+    dst_em2_physical = rumboot_virt_to_phys((uint32_t*)dst_em2);
 
     TEST_ASSERT(src_im0_physical >=0,"IM0 src addr is not presented in UTLB");
     TEST_ASSERT(dst_im0_physical >=0,"IM0 dst addr is not presented in UTLB");
@@ -154,57 +172,57 @@ static uint32_t check_single_channel()
 
 
     rumboot_printf("im0->im0\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem((uint32_t)source_area_im0,
-                                           (uint32_t)dest_area_im0,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_im0,
+                                           dst_im0,
                                            src_im0_physical,
                                            dst_im0_physical) == true, "IM0-to-IM0 failed");
 
     rumboot_printf("im0->im1\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem((uint32_t)source_area_im0,
-                                           IM1_BASE,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_im0,
+                                           dst_im1,
                                            src_im0_physical,
                                            dst_im1_physical) == true, "IM0-to-IM1 failed");
 
     rumboot_printf("im1->im0\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(IM1_BASE,
-                                           (uint32_t)dest_area_im0,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_im1,
+                                           dst_im0,
                                            src_im1_physical,
                                            dst_im0_physical) == true, "IM1-to-IM0 failed");
 
     rumboot_printf("im1->im1\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(IM1_BASE,
-                                          IM1_BASE + 0x80,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_im1,
+                                          dst_im1,
                                           src_im1_physical,
                                           dst_im1_physical) == true, "IM1-to-IM1 failed");
 
     rumboot_printf("em2->im0\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(EM2_BASE,
-                                           (uint32_t)dest_area_im0,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_em2,
+                                           dst_im0,
                                            src_em2_physical,
                                            dst_im0_physical) == true, "EM2-to-IM0 failed");
 
 
     rumboot_printf("im0->em2\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem((uint32_t)source_area_im0,
-                                           EM2_BASE,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_im0,
+                                           dst_em2,
                                            src_im0_physical,
                                            dst_em2_physical) == true, "IM0-to-EM2 failed");
 
     rumboot_printf("em2->im1\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(EM2_BASE,
-                                           IM1_BASE,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_em2,
+                                           dst_im1,
                                            src_em2_physical,
                                            dst_im1_physical) == true, "EM2-to-IM1 failed");
 
     rumboot_printf("im1->em2\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(IM1_BASE,
-                                           EM2_BASE,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_im1,
+                                           dst_em2,
                                            src_im1_physical,
                                            dst_em2_physical) == true, "IM1-to-EM2 failed");
 
     rumboot_printf("em2->em2\n");
-    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(EM2_BASE,
-                                          EM2_BASE + 0x80,
+    TEST_ASSERT(check_dma2plb6_0_mem_to_mem(src_em2,
+                                          dst_em2,
                                           src_em2_physical,
                                           dst_em2_physical) == true, "EM2-to-EM2 failed");
 
@@ -218,8 +236,8 @@ static dma2plb6_setup_info get_default_dma_info(DmaChannel channel, uint32_t* ch
     dma2plb6_setup_info dma_info;
 
     dma_info.base_addr = DCR_DMAPLB6_BASE;
-    dma_info.source_adr = rumboot_virt_to_dma(ch_src);
-    dma_info.dest_adr = rumboot_virt_to_dma(ch_dst);
+    dma_info.source_adr = rumboot_virt_to_phys(ch_src);
+    dma_info.dest_adr = rumboot_virt_to_phys(ch_dst);
     dma_info.priority = priority_medium_low;
     dma_info.striding_info.striding = striding_none;
     dma_info.tc_int_enable = false;
@@ -399,10 +417,10 @@ int main(void)
 
     emi_init(DCR_EM2_EMI_BASE);
 
-    result += check_multiple_channels_2();
+    result += check_single_channel();
+
     return result;
 
-    result += check_single_channel();
     result += check_multiple_channels_4();
     result += check_multiple_channels_2();
 
