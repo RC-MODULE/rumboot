b3443a61a8a4f9d6ed6229472275e6b8486b2a9b
m.dubikin <m.dubikin@modulew.ru>
2018-09-19 10:25:55 +0300

    oi10_rumboot: added a new read-only periph registers for sp805 watchdog

---
+++ include/regs/regs_sp805.h
@@ -0,0 +1,27 @@
+#ifndef DEVICES_WD_H
+#define DEVICES_WD_H
+
+/**
+ * \defgroup devices_sp805_regs Registers
+ * \ingroup devices_sp805
+ *
+ * \addtogroup devices_sp805_regs
+ *
+ * @{
+ */
+
+
+#define WD_REG_PERIPHID0       0xFE0
+#define WD_REG_PERIPHID1       0xFE4
+#define WD_REG_PERIPHID2       0xFE8
+#define WD_REG_PERIPHID3       0xFEC
+#define WD_REG_PCELLID0        0xFF0
+#define WD_REG_PCELLID1        0xFF4
+#define WD_REG_PCELLID2        0xFF8
+#define WD_REG_PCELLID3        0xFFC
+
+/**
+* @}
+*/
+
+#endif /* end of include guard: DEVICES_WD_H */
