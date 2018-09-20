44e1557257b6850fa9b5db9071dcb65cb3ec00eb
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-09-20 15:56:06 +0300

    oi10_rumboot: uart_data_logger test code has been updated.

--- src/platform/oi10/targets/uart_data_logger/uart_data_logger_test.c
+++ src/platform/oi10/targets/uart_data_logger/uart_data_logger_test.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+
 #include <rumboot/printf.h>
 #include <rumboot/platform.h>
 #include <rumboot/macros.h>
 #include <rumboot/io.h>
+
 #include <platform/devices.h>
+#include <platform/test_assert.h>
+#include <platform/test_event_c.h>
 
 
 const uint32_t UART_BASE_ADDR = 0xC0029000; 
@@ -74,6 +78,9 @@ static void uart_init(uint32_t freq, uint32_t baudrate)
 
 static void uart_enable()
 {
+    reg_set(UART_BASE_ADDR + UARTCR, (0x01 << 8));
+    reg_set(UART_BASE_ADDR + UARTCR, (0x01 << 9));
+
     reg_set(UART_BASE_ADDR + UARTCR, 0x01);
 }
 
@@ -88,13 +95,18 @@ static void uart_write_char(uint8_t data)
 static void uart_disable()
 {
     reg_clear(UART_BASE_ADDR + UARTCR, 0x01);
+
+    reg_clear(UART_BASE_ADDR + UARTCR, (0x01 << 8));
+    reg_clear(UART_BASE_ADDR + UARTCR, (0x01 << 9));
 }
 
 
-
+const uint32_t TEST_EVENT_UART_DATA_LOGGER_TX = TEST_EVENT_CODE_MIN + 0x100;
 
 int main()
 {
+    test_event_send_test_id("test_oi10_uart_data_logger");
+
     const char msg[] = "\x55H\nello\nworld\n"; 
     
     size_t msg_len = ARRAY_SIZE(msg);
@@ -110,8 +122,26 @@ int main()
     
     //Waiting TFIFO to be empty.
     while (!reg_check(UART_BASE_ADDR + UARTFR, (0x01 << 7)))
-        ;    
+        ;
         
+
+    test_event(TEST_EVENT_UART_DATA_LOGGER_TX);
+
+    uint32_t data = 0;
+
+    //Waiting RFIFO to be full.
+    for (int i = 0; i < 256; i++)
+    {
+        while (reg_check(UART_BASE_ADDR + UARTFR, (0x01 << 4)))
+            ;
+
+        data = reg_read(UART_BASE_ADDR + UARTDR);
+
+        rumboot_printf("UART data received: 0x%x\n", data);
+
+        //TEST_ASSERT(data == i, "Incorrect data received.");
+    }
+
     uart_disable();
 
 	return 0;
