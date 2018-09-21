ed142a450cbb7aabb02cf7107c115bf6e42370c0
m.dubikin <m.dubikin@modulew.ru>
2018-09-21 09:01:40 +0300

    oi10_rumboot: Added watchdog regs

--- include/regs/regs_sp805.h
+++ include/regs/regs_sp805.h
@@ -9,16 +9,24 @@
  *
  * @{
  */
+#define WD_REG_LOAD             0x00
+#define WD_REG_VALUE            0x04
+#define WD_REG_CONTROL          0x08
+#define WD_REG_INTCLR           0x0C
+#define WD_REG_RIS              0x10
+#define WD_REG_MIS              0x14
+#define WD_REG_LOCK             0xC0
+#define WD_REG_ITCR             0xF0
+#define WD_REG_ITOP             0xF04
 
-
-#define WD_REG_PERIPHID0       0xFE0
-#define WD_REG_PERIPHID1       0xFE4
-#define WD_REG_PERIPHID2       0xFE8
-#define WD_REG_PERIPHID3       0xFEC
-#define WD_REG_PCELLID0        0xFF0
-#define WD_REG_PCELLID1        0xFF4
-#define WD_REG_PCELLID2        0xFF8
-#define WD_REG_PCELLID3        0xFFC
+#define WD_REG_PERIPHID0        0xFE0
+#define WD_REG_PERIPHID1        0xFE4
+#define WD_REG_PERIPHID2        0xFE8
+#define WD_REG_PERIPHID3        0xFEC
+#define WD_REG_PCELLID0         0xFF0
+#define WD_REG_PCELLID1         0xFF4
+#define WD_REG_PCELLID2         0xFF8
+#define WD_REG_PCELLID3         0xFFC
 
 /**
 * @}
