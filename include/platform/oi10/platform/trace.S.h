74aa431fa60daa9d1f65fe5070a6a939868236c5
Rishat Galiulin <r.galiulin@modulew.ru>
2018-09-25 01:50:48 +0300

    oi10_rumboot: Make event sending by SPRG atomic

--- include/platform/oi10/platform/trace.S.h
+++ include/platform/oi10/platform/trace.S.h
@@ -1,60 +1,101 @@
 #ifndef TRACE_S_H_
 #define TRACE_S_H_
 
+
 #include <platform/arch/ppc/ppc_476fp_asm.h>
 #include <platform/test_event_asm.h>
 #include <platform/test_event_codes.h>
 
-.macro putchar char, reg_data=r5, reg_ctrl=r6
-    load_const  \reg_data, \char; /* test stdout */
-    load_const  \reg_ctrl, 2;
-    mtspr       SPR_SPRG2, \reg_data
-    load_const  \reg_data, 0
-    mtspr       SPR_SPRG1, \reg_ctrl
-    mtspr       SPR_SPRG1, \reg_data
+
+.macro putchar char, tmp_reg=r5, tmp_reg_msr=r6
+    disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    load_const          \tmp_reg, \char
+    rumboot_event_arg1_ EVENT_STDOUT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
 
 
-.macro rumboot_putstring text, tmp_reg_nop_or_addr=r5, tmp_reg_opcode_testevent=r6, tmp_reg_event_code=r7
+.macro rumboot_putstring text, tmp_reg=r5, tmp_reg_msr=r6
     b 2f
-1:
-    .ascii "\text"
-    .byte  0x0
-    /*end of string*/
+
+1:  .ascii "\text"
+    .byte  0x0 /*end of string*/
+
 .align 2, 0
-2:
-    load_addr  \tmp_reg_nop_or_addr, 1b
-    mtspr       SPR_SPRG3, \tmp_reg_nop_or_addr
-    test_event EVENT_TRACE_MESSAGE, \tmp_reg_nop_or_addr, \tmp_reg_opcode_testevent, \tmp_reg_event_code
+2:  disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    load_addr           \tmp_reg, 1b
+    mtspr               SPR_SPRG3, \tmp_reg
+    load_const          \tmp_reg, EVENT_TRACE_MESSAGE
+    rumboot_event_arg1_ EVENT_TESTEVENT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
 
-.macro rumboot_puthex hex_reg, tmp_reg_nop=r5, tmp_reg_opcode_testevent=r6, tmp_reg_event_code=r7
-    mtspr       SPR_SPRG3, \hex_reg
-    test_event EVENT_TRACE_HEX, \tmp_reg_nop, \tmp_reg_opcode_testevent, \tmp_reg_event_code
+
+.macro rumboot_puthex hex_reg, tmp_reg=r5, tmp_reg_msr=r6
+    disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    mtspr               SPR_SPRG3, \hex_reg
+    load_const          \tmp_reg, EVENT_TRACE_HEX
+    rumboot_event_arg1_ EVENT_TESTEVENT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
 
-.macro _rumboot_putdump tmp_reg_start_addr, tmp_reg_length_in_bytes, tmp_reg_nop=r6, tmp_reg_event_code=r7
-    mtspr SPR_SPRG3, \tmp_reg_start_addr
-    mtspr SPR_SPRG4, \tmp_reg_length_in_bytes
-    test_event EVENT_TRACE_DUMP, \tmp_reg_nop, \tmp_reg_length_in_bytes, \tmp_reg_event_code
+
+.macro _rumboot_putdump tmp_reg_start_addr, tmp_reg_length_in_bytes, tmp_reg=r5, tmp_reg_msr=r6
+    disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    mtspr               SPR_SPRG3, \tmp_reg_start_addr
+    mtspr               SPR_SPRG4, \tmp_reg_length_in_bytes
+    load_const          \tmp_reg, EVENT_TRACE_DUMP
+    rumboot_event_arg1_ EVENT_TESTEVENT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
 
-.macro rumboot_putdump start_addr, length_in_bytes, tmp_reg_start_addr=r5, tmp_reg_length_in_bytes=r6, tmp_reg_event_code=r7
-    load_const \tmp_reg_start_addr, \start_addr
-    load_const \tmp_reg_length_in_bytes, \length_in_bytes
-    _rumboot_putdump \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_start_addr, \tmp_reg_event_code
+
+.macro rumboot_putdump start_addr, length_in_bytes, tmp_reg=r5, tmp_reg_msr=r6
+    disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    load_const          \tmp_reg, \start_addr
+    mtspr               SPR_SPRG3, \tmp_reg
+    load_const          \tmp_reg, \length_in_bytes
+    mtspr               SPR_SPRG4, \tmp_reg
+    load_const          \tmp_reg, EVENT_TRACE_DUMP
+    rumboot_event_arg1_ EVENT_TESTEVENT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
 
-.macro _rumboot_issdump tmp_reg_start_addr, tmp_reg_length_in_bytes, tmp_reg_event_code=r7
-    mtspr SPR_SPRG3, \tmp_reg_start_addr
-    mtspr SPR_SPRG4, \tmp_reg_length_in_bytes
-    test_event TEST_EVENT_CREATE_ISS_DUMP, \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_event_code
+
+.macro _rumboot_issdump tmp_reg_start_addr, tmp_reg_length_in_bytes, tmp_reg=r5, tmp_reg_msr=r6
+    disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    mtspr               SPR_SPRG3, \tmp_reg_start_addr
+    mtspr               SPR_SPRG4, \tmp_reg_length_in_bytes
+    load_const          \tmp_reg, TEST_EVENT_CREATE_ISS_DUMP
+    rumboot_event_arg1_ EVENT_TESTEVENT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
 
-.macro rumboot_issdump start_addr, length_in_bytes, tmp_reg_start_addr=r5, tmp_reg_length_in_bytes=r6, tmp_reg_event_code=r7
-    load_const \tmp_reg_start_addr, \start_addr
-    load_const \tmp_reg_length_in_bytes, \length_in_bytes
-    _rumboot_issdump \tmp_reg_start_addr, \tmp_reg_length_in_bytes, \tmp_reg_event_code
+
+.macro rumboot_issdump start_addr, length_in_bytes, tmp_reg=r5, tmp_reg_msr=r6
+    disable_ext_irq     \tmp_reg, \tmp_reg_msr
+
+    load_const          \tmp_reg, \start_addr
+    mtspr               SPR_SPRG3, \tmp_reg
+    load_const          \tmp_reg, \length_in_bytes
+    mtspr               SPR_SPRG4, \tmp_reg
+    load_const          \tmp_reg, TEST_EVENT_CREATE_ISS_DUMP
+    rumboot_event_arg1_ EVENT_TESTEVENT, \tmp_reg
+
+    mtmsr               \tmp_reg_msr
 .endm
+
 
 #endif /* TRACE_S_H_ */
