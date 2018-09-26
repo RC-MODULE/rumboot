b91432a679b19b92bb4b2654be60709469c5f9be
Chernousov Stepan <s.chernousov@modulew.ru>
2018-09-26 15:13:41 +0300

    oi10_rumboot: regs_emi update

--- include/platform/oi10/platform/regs/regs_emi.h
+++ include/platform/oi10/platform/regs/regs_emi.h
@@ -27,7 +27,7 @@ DECLARE_ENUM_VAL( EMI_WREQADR,                  0x4C )    //WR N/A
 DECLARE_ENUM_VAL( EMI_WDADR,                    0x50 )    //WR N/A
 DECLARE_ENUM_VAL( EMI_BUSEN,                    0x54 )    //WR 0x00000000
 DECLARE_ENUM_VAL( EMI_WECR,                     0x58 )    //WR 0x00000000
-DECLARE_ENUM_VAL( EMI_FLCNTRL,                  0x5C )    //WR 0x00000000
+DECLARE_ENUM_VAL( EMI_FLCNTRL,                  0x5C )    //WR 0x0000001C
 DECLARE_ENUM_VAL( EMI_IMR,                      0x60 )    //WR 0x00000000
 DECLARE_ENUM_VAL( EMI_IMR_SET,                  0x64 )    //WR 0x00000000
 DECLARE_ENUM_VAL( EMI_IMR_RST,                  0x68 )    //WR 0x00000000
@@ -35,6 +35,11 @@ DECLARE_ENUM_VAL( EMI_IRR,                      0x70 )    //WR 0x00000000
 DECLARE_ENUM_VAL( EMI_IRR_RST,                  0x74 )    //WR 0x00000000
 DECLARE_ENUM_VAL( EMI_ECCWRR,                   0x78 )    //WR 0x00000000
 DECLARE_ENUM_VAL( EMI_ECCRDR,                   0x7C )    //WR 0x00000000
+DECLARE_ENUM_VAL( EMI_H1CMR,                    0xC0 )    //WR 0x00000000
+DECLARE_ENUM_VAL( EMI_H2CMR,                    0xC4 )    //WR 0x00000000
+DECLARE_ENUM_VAL( EMI_RREQCMR,                  0xC8 )    //WR 0x00000000
+DECLARE_ENUM_VAL( EMI_WREQCMR,                  0xCC )    //WR 0x00000000
+DECLARE_ENUM_VAL( EMI_WDCMR,                    0xD0 )    //WR 0x00000000
 END_ENUM( EMI_REG )
 
 #endif /* REGS_EMI_H_ */
