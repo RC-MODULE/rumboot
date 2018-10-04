df474773687ad38422857ba5fdfeb7a5f41a0255
n.gorshkova <n.gorshkova@modulew.ru>
2018-10-04 10:41:08 +0300

    oi10_rumboot: test_oi10_sys_005 has been updated

--- src/platform/oi10/targets/simple-iram/test_oi10_sys_005.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_sys_005.c
@@ -11,17 +11,10 @@
 #include <platform/interrupts.h>
 #include <platform/test_assert.h>
 #include <platform/regs/fields/mpic128.h>
+#include <platform/regs/regs_plb4ahb.h>
 
-#define NON_EXISTENT_ADDR   0xC0403000
+#define NON_EXISTENT_ADDR   0xC0025000
 #define TEST_VALUE          0xBABADEDA
-#define SEAR_upper          0x1
-#define SEAR_lower          0x2
-#define SESR                0x3
-
-//REG_WRITE_DCR( PLB4AHB, SEAR_upper, 32 )
-//REG_WRITE_DCR( PLB4AHB, SEAR_lower, 32 )
-//REG_WRITE_DCR( PLB4AHB, SESR, 32 )
-
 
 typedef enum test_event_code
 {
@@ -35,27 +28,24 @@ int main()
     test_event_send_test_id( "test_oi10_sys_005" );
 
     rumboot_printf("Checking access to existing devices in AXI\n");
-    iowrite32(TEST_VALUE, IM1_BASE);
-    rumboot_printf("Read value = 0x%\n", ioread32(IM1_BASE));
     test_event(TEC_START_CHECK_CELL1);
+    iowrite32(TEST_VALUE, IM1_BASE);
     rumboot_printf("Checking: OK\n");
 
-//    GMII_write_MAC_LSB(GMII1_BASE, 0x0);
 
     rumboot_printf("Checking access to non-existent devices in AXI\n");
-    iowrite32(TEST_VALUE, NON_EXISTENT_ADDR);
     test_event(TEC_START_CHECK_HRESP);
+    iowrite32(TEST_VALUE, NON_EXISTENT_ADDR);
     rumboot_printf("Checking: OK\n");
 
     rumboot_printf("Cleaning of registers SEAR, SESR\n");
-//    PLB4AHB_dcr_write_SEAR_upper(PLB4AHB_BASE,0xF);
-//    PLB4AHB_dcr_write_SEAR_lower(PLB4AHB_BASE,0xF);
-//    PLB4AHB_dcr_write_SESR(PLB4AHB_BASE,0xF);
+    dcr_write(DCR_PLB4AHB_0_BASE + SEAR_ADDRUPPER, 0xF);
+    dcr_write(DCR_PLB4AHB_0_BASE + SEAR_ADDRLOWER, 0xF);
+    dcr_write(DCR_PLB4AHB_0_BASE + SESR, 0xF);
 
     rumboot_printf("Checking access to existing devices in AXI\n");
-    iowrite32(TEST_VALUE, IM1_BASE + 0x20);
-    rumboot_printf("Read value = 0x%\n", ioread32(IM1_BASE + 0x20));
     test_event(TEC_START_CHECK_CELL2);
+    ioread32(IM1_BASE + 0x20);
     rumboot_printf("Checking: OK\n");
 
     return 0;
