02a2ff0d9304ba3312784c114a2477b8e6a1d13f
Andrew Andrianov <andrew@ncrmnt.org>
2018-10-04 17:26:03 +0300

    bootrom: physmap: Fix image loading logic
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/bootsrc/physmap.c
+++ src/lib/bootsrc/physmap.c
@@ -17,12 +17,21 @@ static void physmap_deinit(const struct rumboot_bootsource *src, void *pdata)
 
 static size_t physmap_read(const struct rumboot_bootsource *src, void *pdata, void *to, size_t offset, size_t length)
 {
+#if 1
+        uint64_t *dest = to;
+        volatile uint64_t *from = (void *)src->base;
+        size_t i;
+        for (i = offset; i < length; i = i + 8)
+                *dest++ = *from++;
+
+#else
+        /* WTF? */
         uint64_t *dest = to;
         size_t i;
-        for (i = offset; i < length; i=i+8) {
+        for (i = offset; i < length; i = i + 8)
                 *dest++ = ioread64(src->base + i);
-                /* TODO: Exception handling */
-        }
+
+#endif
         return length;
 }
 
@@ -30,5 +39,6 @@ const struct rumboot_bootmodule g_bootmodule_physmap = {
         .init		= physmap_init,
         .deinit		= physmap_deinit,
         .read		= physmap_read,
+        .align		= 8,
         .privdatalen	= sizeof(struct physmap_private_data)
 };
