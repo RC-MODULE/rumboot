f8858c41113c17b0e72f1e5cc74d6ab3d586ad1f
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-10-08 09:02:07 +0300

    oi10_rumboot: test_oi10_em2_205 MCLFIR and interrupt checks has been added.

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_205.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_205.c
@@ -3,18 +3,22 @@
 #include <string.h>
 
 #include <rumboot/io.h>
+#include <rumboot/irq.h>
 #include <rumboot/printf.h>
 #include <rumboot/timer.h>
 
 #include <platform/common_macros/common_macros.h>
 #include <platform/devices.h>
 #include <platform/devices/emi.h>
+#include <platform/interrupts.h>
 
 #define RUMBOOT_ASSERT_WARN_ONLY
 #include <platform/devices/nor_1636RR4.h>
 #undef RUMBOOT_ASSERT_WARN_ONLY
 
+#include <platform/regs/regs_mclfir.h>
 #include <platform/regs/regs_emi.h>
+#include <platform/regs/fields/mpic128.h>
 #include <platform/test_event_c.h>
 #include <platform/test_event_codes.h>
 
@@ -45,6 +49,84 @@
 #define ADDR_NOR_DE                             NOR_BASE + 0x10100
 
 #define BABA0001_ECC                            0x7A
+
+
+static void irq_handler( int irq, void *arg ) {
+
+    switch (irq) {
+        case EMI_CNTR_INT_0:
+            rumboot_putstring("EMI_CNTR_INT_0 occured.\n");
+
+            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+
+            break;
+
+        case EMI_CNTR_INT_1:
+            rumboot_putstring("EMI_CNTR_INT_1 occured.\n");
+
+            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+
+            break;
+
+        case EMI_CNTR_INT_2:
+            rumboot_putstring("EMI_CNTR_INT_2 occured.\n");
+
+            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+
+            break;
+
+        case EMI_CNTR_INT_3:
+            rumboot_putstring("EMI_CNTR_INT_3 occured.\n");
+
+            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+
+            break;
+
+        case MCLFIR_REC_INT:
+            rumboot_putstring("MCLFIR_REC_INT occured.\n");
+
+            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+            rumboot_printf("MCLFIR_MC_LFIR = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR));
+
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+            break;
+
+        case MCLFIR_UNREC_INT:
+            rumboot_putstring("MCLFIR_UNREC_INT occured.\n");
+
+            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+            rumboot_printf("MCLFIR_MC_LFIR = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR));
+            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+            break;
+
+        default:
+            rumboot_putstring("Unexpected interrupt occured!\n");
+            break;
+    }
+}
 
 
 bool check_data()
@@ -168,10 +250,44 @@ bool check_data()
 
 int main(void) 
 {
+    uint32_t test_result = 0;
+
     test_event_send_test_id("test_oi10_em2_205");
 
     emi_init(DCR_EM2_EMI_BASE);
     emi_set_ecc(DCR_EM2_EMI_BASE, emi_bank_all, emi_ecc_on);
+
+
+    rumboot_irq_cli();
+    struct rumboot_irq_entry *tbl = rumboot_irq_create( NULL );
+
+    rumboot_irq_set_handler( tbl, EMI_CNTR_INT_0, RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, irq_handler, ( void* )0 );
+    rumboot_irq_set_handler( tbl, EMI_CNTR_INT_1, RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, irq_handler, ( void* )0 );
+    rumboot_irq_set_handler( tbl, EMI_CNTR_INT_2, RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, irq_handler, ( void* )0 );
+    rumboot_irq_set_handler( tbl, EMI_CNTR_INT_3, RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, irq_handler, ( void* )0 );
+
+    rumboot_irq_set_handler( tbl, MCLFIR_REC_INT,   RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, irq_handler, ( void* )0 );
+    rumboot_irq_set_handler( tbl, MCLFIR_UNREC_INT, RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, irq_handler, ( void* )0 );
+
+    /* Activate the table */
+    rumboot_irq_table_activate( tbl );
+    rumboot_irq_enable( EMI_CNTR_INT_0 );
+    rumboot_irq_enable( EMI_CNTR_INT_1 );
+    rumboot_irq_enable( EMI_CNTR_INT_2 );
+    rumboot_irq_enable( EMI_CNTR_INT_3 );
+    rumboot_irq_enable( MCLFIR_REC_INT );
+    rumboot_irq_enable( MCLFIR_UNREC_INT );
+    rumboot_irq_sei();
+
+
+    //Unmask EMI memory error interrupts
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IMR, 0xFFF);
+
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_MSK_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_MSK_AND1, 0x0100003F);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_MSK_AND, 0x3FFFFFFF);
+
 
     rumboot_putstring("WRITE SRAM0\n");
     test_event(EVENT_INJECT_SRAM0_0); //DATA_SE = 0xBABA0000
@@ -203,24 +319,29 @@ int main(void)
     isync();
 
 
+    //Software error injection for NOR
     dcr_write(DCR_EM2_EMI_BASE + EMI_ECCWRR, BABA0001_ECC);
     dcr_write(DCR_EM2_EMI_BASE + EMI_FLCNTRL,  (dcr_read(DCR_EM2_EMI_BASE + EMI_FLCNTRL) & 0x1C)  | ECC_CTRL_CNT_ECCWRR);
 
     rumboot_putstring("WRITE NOR\n");
     rumboot_putstring("Next assertion is expected behaviour!");
-    nor_write32(0xBABA0001, ADDR_NOR_SE);
+    nor_write32(0xBABA0000, ADDR_NOR_SE);
     isync();
 
-    nor_write32(0xBABA0001, ADDR_NOR_DE);
+    nor_write32(0xBABA0007, ADDR_NOR_DE);
     isync();
 
     udelay(5);
 
     rumboot_putstring("CHECK\n");
     if (!check_data())
-        return 1;
+        test_result = 1;
 
-    rumboot_putstring("Test has been finished successfully.");
+    rumboot_irq_table_activate( NULL );
+    rumboot_irq_free( tbl );
 
-    return 0;
+    if (!test_result)
+        rumboot_putstring("Test has been finished successfully.");
+
+    return test_result;
 }
