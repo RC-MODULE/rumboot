daa4f4cfcbf05a87e3adaa3b06456d10202ea28e
n.gorshkova <n.gorshkova@modulew.ru>
2018-10-10 15:53:19 +0300

    oi10_rumboot: pl022_flash - deleted macros

--- src/lib/drivers/pl022_flash.c
+++ src/lib/drivers/pl022_flash.c
@@ -4,8 +4,6 @@
 #include <rumboot/macros.h>
 #include <regs/regs_spi.h>
 #include <devices/pl022_flash.h>
-#include <platform/common_macros/common_macros.h>
-#include <platform/trace.h>
 
 #define SPIFLASH_PAGEPROG      0x02
 #define SPIFLASH_READDATA      0x03
@@ -36,20 +34,18 @@ static uint32_t read_flash_status (uint32_t base_addr)
 
     return (ioread32(addr)); //read data from rx fifo
 }
-#define WRITE_ADDR_SPI(i, dummy)  iowrite8(0x00, addr);//write data to SPI - write address
-#define READ_DATA_STAFF(i, dummy)  (void)ioread8(addr); //read data from SPI - staff
-#define WRITE_ADDR_SPI(i, dummy)  iowrite8(0x00, addr);//write data to SPI - write address
-#define WRITE_DATA_STAFF(i, dummy)  iowrite8(0xFF, addr);//write data to SPI - write address
 
 void pl022_flash_erase(uint32_t base_addr)
 {
     uint32_t addr = base_addr+GSPI_SSPDR;
 
     iowrite8(SPIFLASH_BLOCK64ERASE, addr); //sector erase command
-    REPEAT(3, WRITE_ADDR_SPI, dummy)
+    for (int i =0; i<3; i++)
+        iowrite8(0x00, addr);//write data to SPI - write address
     while((ioread32(base_addr+GSPI_SSPSR) & 0x1) == 0) //wait tx fifo empty
         {}
-    REPEAT(4, READ_DATA_STAFF, dummy)
+    for (int i =0; i<4; i++)
+        (void)ioread8(addr); //read data from SPI - staff
     while ((read_flash_status(base_addr) & 0x1) == 0x1) //wait write complete
         {}
 }
@@ -58,7 +54,8 @@ void pl022_flash_write_data(uint32_t const base_addr, uint32_t data)
 {
     uint32_t addr =  base_addr+GSPI_SSPDR;
     iowrite8(SPIFLASH_PAGEPROG, addr); //write data to SPI - command write
-    REPEAT(3, WRITE_ADDR_SPI, dummy)
+    for (int i =0; i<3; i++)
+        iowrite8(0x00, addr);//write data to SPI - write address
 
     iowrite8((data & 0x000000FF), base_addr+GSPI_SSPDR); //write data to SPI - write data 1
     iowrite8((data & 0x0000FF00) >> 8, base_addr+GSPI_SSPDR); //write data to SPI - write data 2
@@ -67,7 +64,8 @@ void pl022_flash_write_data(uint32_t const base_addr, uint32_t data)
 
     while((ioread8(base_addr+GSPI_SSPSR) & 0x1) == 0) //wait tx fifo empty
         {}
-    REPEAT(8, READ_DATA_STAFF, dummy)
+    for (int i =0; i<8; i++)
+        (void)ioread8(addr); //read data from SPI - staff
     while ((read_flash_status(base_addr) & 0x1) == 0x1) //wait write complete
         {}
 }
@@ -78,12 +76,16 @@ uint32_t pl022_flash_read_data(uint32_t const base_addr)
     uint32_t addr =  base_addr+GSPI_SSPDR;
 
     iowrite8(SPIFLASH_READDATA, addr); //write data to SPI - command read
-    REPEAT(3, WRITE_ADDR_SPI, dummy)
+    for (int i =0; i<3; i++)
+        iowrite8(0x00, addr);//write data to SPI - write address
 
-    REPEAT(4, WRITE_DATA_STAFF, dummy)
+    for (int i =0; i<4; i++)
+        iowrite8(0xFF, addr);//write data to SPI - write address
     while((ioread8(base_addr+GSPI_SSPSR) & 0x1) == 0) //wait tx fifo empty
         {}
-    REPEAT(4, READ_DATA_STAFF, dummy)
+
+    for (int i =0; i<4; i++)
+        (void)ioread8(addr); //read data from SPI - staff
 
     data = ioread8(addr);
     data = data | (ioread8(addr) << 8);
