aaf1960022f43a4881bdbf78c1bc9dd06d2a3d1f
Mikhail Afanasyev <m.afanasyev@modulew.ru>
2018-10-18 11:29:03 +0300

    oi10_rumboot: test_oi10_em2_205 branch on CTR register error.

--- src/platform/oi10/targets/simple-iram/test_oi10_em2_205.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_em2_205.c
@@ -51,75 +51,53 @@
 #define BABA0001_ECC                            0x7A
 
 
+volatile bool emi_single_irq_raised                 = false;
+volatile bool emi_double_irq_raised                 = false;
+volatile bool mclfir_recoverable_error_irq_raised   = false;
+volatile bool mclfir_unrecoverable_error_irq_raised = false;
+
+const uint32_t ERROR_WAIT_TIMEOUT = 1000;
+
+
 static void irq_handler( int irq, void *arg ) {
 
     switch (irq) {
         case EMI_CNTR_INT_0:
             rumboot_putstring("EMI_CNTR_INT_0 occured.\n");
 
-            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
-            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
-
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+            emi_single_irq_raised = true;
 
             break;
 
         case EMI_CNTR_INT_1:
             rumboot_putstring("EMI_CNTR_INT_1 occured.\n");
 
-            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
-            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
-
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+            emi_double_irq_raised = true;
 
             break;
 
         case EMI_CNTR_INT_2:
             rumboot_putstring("EMI_CNTR_INT_2 occured.\n");
 
-            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
-            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
-
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
-
             break;
 
         case EMI_CNTR_INT_3:
             rumboot_putstring("EMI_CNTR_INT_3 occured.\n");
-
-            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
-            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
-
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
 
             break;
 
         case MCLFIR_REC_INT:
             rumboot_putstring("MCLFIR_REC_INT occured.\n");
 
-            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
-            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
-            rumboot_printf("MCLFIR_MC_LFIR = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR));
+            mclfir_recoverable_error_irq_raised = true;
 
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
             break;
 
         case MCLFIR_UNREC_INT:
             rumboot_putstring("MCLFIR_UNREC_INT occured.\n");
 
-            rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
-            rumboot_printf("MCLFIR_MC_LFIR = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR));
-            rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+            mclfir_unrecoverable_error_irq_raised = true;
 
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
-            dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
             break;
 
         default:
@@ -137,8 +115,37 @@ bool check_data()
     dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT53, 0x00); //clear reg
 
 
+
     rumboot_putstring("Checking SRAM0 ... \n");
     reg = ioread32(ADDR_SRAM0_SE);
+
+
+    TEST_WAIT_ASSERT(emi_single_irq_raised == true, ERROR_WAIT_TIMEOUT, "SRAM0: waiting single error timeout.");
+
+    emi_single_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000001, "SRAM0: expected single error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0001);
+
+
+    TEST_WAIT_ASSERT(mclfir_recoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "SRAM0: waiting recoverable error timeout.");
+
+    mclfir_recoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000001, "SRAM0: expected recoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
+    TEST_ASSERT(stat == 0x000001, "SRAM0: invalid value in STATUS_IRQ");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT20, 0x00);
+
+
     if (reg != 0xBABA0001)
     {
         rumboot_putstring("ECC ERROR SRAM0 (Single error)\n");
@@ -147,11 +154,37 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n",reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
-    TEST_ASSERT(stat == 0x000001, "Invalid value in STATUS_IRQ (SRAM0)");
-    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT20, 0x00);
+
+
 
     reg = ioread32(ADDR_SRAM0_DE);
+
+
+    TEST_WAIT_ASSERT(emi_double_irq_raised == true, ERROR_WAIT_TIMEOUT, "SRAM0: waiting double error timeout.");
+
+    emi_double_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000002, "SRAM0: expected double error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0002);
+
+
+    TEST_WAIT_ASSERT(mclfir_unrecoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "SRAM0: waiting unrecoverable error timeout.");
+
+    mclfir_unrecoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000002, "SRAM0: Expected unrecoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
+    TEST_ASSERT(stat == 0x000000, "SRAM0: invalid value in STATUS_IRQ.");
+
+
     if (reg != 0xBABA0007) //see .svh
     {
         rumboot_putstring("ECC ERROR SRAM0 (Double error)\n");
@@ -160,12 +193,39 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n", reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
-    TEST_ASSERT(stat == 0x000000, "Invalid value in STATUS_IRQ (SRAM)");
+
 
 
     rumboot_putstring("Checking SSRAM ... \n");
     reg = ioread32(ADDR_SSRAM_SE);
+
+
+    TEST_WAIT_ASSERT(emi_single_irq_raised == true, ERROR_WAIT_TIMEOUT, "SSRAM: waiting single error timeout.");
+
+    emi_single_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000004, "SSRAM: expected single error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0004);
+
+
+    TEST_WAIT_ASSERT(mclfir_recoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "SSRAM: waiting recoverable error timeout.");
+
+    mclfir_recoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000001, "SSRAM: expected recoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
+    TEST_ASSERT(stat == 0x10000, "SSRAM: invalid value in STATUS_IRQ");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT20, 0x00);
+
+
     if (reg != 0xBABA0001)
     {
         rumboot_putstring("ECC ERROR SSRAM (Single error)\n");
@@ -174,11 +234,37 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n",reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
-    TEST_ASSERT(stat == 0x10000, "Invalid value in STATUS_IRQ (SSRAM)");
-    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT20, 0x00);
+
+
 
     reg = ioread32(ADDR_SSRAM_DE);
+
+
+    TEST_WAIT_ASSERT(emi_double_irq_raised == true, ERROR_WAIT_TIMEOUT, "SSRAM: waiting double error timeout.");
+
+    emi_double_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000008, "SSRAM: Expected double error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0008);
+
+
+    TEST_WAIT_ASSERT(mclfir_unrecoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "SSRAM: waiting unrecoverable error timeout.");
+
+    mclfir_unrecoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000002, "SSRAM: expected unrecoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
+    TEST_ASSERT(stat == 0x00, "SSRAM: invalid value in STATUS_IRQ.");
+
+
     if (reg != 0xBABA0007) //see .svh
     {
         rumboot_putstring("ECC ERROR SSRAM (Double error)\n");
@@ -187,12 +273,39 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n", reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
-    TEST_ASSERT(stat == 0x00, "Invalid value in STATUS_IRQ (SSRAM)");
+
 
 
     rumboot_putstring("Checking SDRAM ... \n");
     reg = ioread32(ADDR_SDRAM_SE);
+
+
+    TEST_WAIT_ASSERT(emi_single_irq_raised == true, ERROR_WAIT_TIMEOUT, "SDRAM: waiting single error timeout.");
+
+    emi_single_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000010, "SDRAM: expected single error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0010);
+
+
+    TEST_WAIT_ASSERT(mclfir_recoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "SDRAM: waiting recoverable error timeout.");
+
+    mclfir_recoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000001, "SDRAM: expected recoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
+    TEST_ASSERT(stat == 0x100, "SDRAM: invalid value in STATUS_IRQ");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT20, 0x00);
+
+
     if (reg != 0xBABA0001)
     {
         rumboot_putstring("ECC ERROR SDRAM (Single error)\n");
@@ -201,11 +314,37 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n",reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
-    TEST_ASSERT(stat == 0x100, "Invalid value in STATUS_IRQ (SDRAM)");
-    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT20, 0x00);
+
+
 
     reg = ioread32(ADDR_SDRAM_DE);
+
+
+    TEST_WAIT_ASSERT(emi_double_irq_raised == true, ERROR_WAIT_TIMEOUT, "SDRAM: waiting double error timeout.");
+
+    emi_double_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000020, "SDRAM: expected double error");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0020);
+
+
+    TEST_WAIT_ASSERT(mclfir_unrecoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "SDRAM: waiting unrecoverable error timeout.");
+
+    mclfir_unrecoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000002, "SDRAM: expected unrecoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
+    TEST_ASSERT(stat == 0x00, "SDRAM: invalid value in STATUS_IRQ.");
+
+
     if (reg != 0xBABA0007) //see .svh
     {
         rumboot_putstring("ECC ERROR SDRAM (Double error)\n");
@@ -214,12 +353,39 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n", reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT20);
-    TEST_ASSERT(stat == 0x00, "Invalid value in STATUS_IRQ (SDRAM)");
+
 
 
     rumboot_putstring("Checking NOR ... \n");
     reg = ioread32(ADDR_NOR_SE);
+
+
+    TEST_WAIT_ASSERT(emi_single_irq_raised == true, ERROR_WAIT_TIMEOUT, "NOR: waiting single error timeout.");
+
+    emi_single_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000040, "NOR: expected single error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0040);
+
+
+    TEST_WAIT_ASSERT(mclfir_recoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "NOR: waiting recoverable error timeout.");
+
+    mclfir_recoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000001, "NOR: expected recoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT53);
+    TEST_ASSERT(stat == 0x010000, "NOR: invalid value in STATUS_IRQ.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT53, 0x00);
+
+
     if (reg != 0xBABA0001)
     {
         rumboot_putstring("ECC ERROR NOR (Single error)\n");
@@ -228,11 +394,37 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n", reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT53);
-    TEST_ASSERT(stat == 0x010000, "Invalid value in STATUS_IRQ (NOR)");
-    dcr_write(DCR_EM2_EMI_BASE + EMI_ECNT53, 0x00);
+
+
 
     reg = ioread32(ADDR_NOR_DE);
+
+
+    TEST_WAIT_ASSERT(emi_double_irq_raised == true, ERROR_WAIT_TIMEOUT, "NOR: waiting double error timeout.");
+
+    emi_double_irq_raised = false;
+
+    TEST_ASSERT(dcr_read(DCR_EM2_EMI_BASE + EMI_IRR) == 0x000200, "NOR: expected double error.");
+    dcr_write(DCR_EM2_EMI_BASE + EMI_IRR_RST, 0x0200);
+
+
+    TEST_WAIT_ASSERT(mclfir_unrecoverable_error_irq_raised == true, ERROR_WAIT_TIMEOUT, "NOR: waiting unrecoverable error timeout.");
+
+    mclfir_unrecoverable_error_irq_raised = false;
+
+    rumboot_printf("MCLFIR_MC_ERR0 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR0));
+    rumboot_printf("MCLFIR_MC_ERR1 = 0x%x\n", dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR1));
+    TEST_ASSERT(dcr_read(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR) == 0x00000002, "NOR: expected unrecoverable error.");
+
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND0, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_ERR_AND1, 0x00000000);
+    dcr_write(DCR_EM2_MCLFIR_BASE + MCLFIR_MC_LFIR_AND, 0x00000000);
+
+
+    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT53);
+    TEST_ASSERT(stat == 0x00, "NOR: invalid value in STATUS_IRQ.");
+
+
     if (reg != 0xBABA0007) //see .svh
     {
         rumboot_putstring("ECC ERROR NOR (Double error)\n");
@@ -241,8 +433,7 @@ bool check_data()
         rumboot_printf("Actual = 0x%x\n", reg);
         return false;
     }
-    stat = dcr_read(DCR_EM2_EMI_BASE + EMI_ECNT53);
-    TEST_ASSERT(stat == 0x00, "Invalid value in STATUS_IRQ (NOR)");
+
 
     return true;
 }
@@ -256,6 +447,13 @@ int main(void)
 
     emi_init(DCR_EM2_EMI_BASE);
     emi_set_ecc(DCR_EM2_EMI_BASE, emi_bank_all, emi_ecc_on);
+
+    {
+        //Initialize SRAM0 memory
+        for( int i = 0; i < 8; i++ ) {
+            iowrite32( 0x00000000, SRAM0_BASE + (i*4) );
+        }
+    }
 
     rumboot_irq_cli();
     struct rumboot_irq_entry *tbl = rumboot_irq_create( NULL );
@@ -340,7 +538,6 @@ int main(void)
     dcr_write(DCR_EM2_EMI_BASE + EMI_FLCNTRL,  (dcr_read(DCR_EM2_EMI_BASE + EMI_FLCNTRL) & 0x1C)  | ECC_CTRL_CNT_ECCWRR);
 
     rumboot_putstring("WRITE NOR\n");
-    rumboot_putstring("Next assertion is expected behaviour!");
     nor_write32(0xBABA0000, ADDR_NOR_SE);
     msync();
 
