b7f4f015bb614464274a4ee2e787abaf45591386
Andrew Andrianov <andrew@ncrmnt.org>
2018-10-22 15:08:52 +0300

    bootrom: unit: Always call rumboot_platform_init_loader(&conf);
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/common/bootrom/unit.c
+++ src/platform/common/bootrom/unit.c
@@ -11,16 +11,17 @@
 int main()
 {
 	char pdata[512];
-	#ifndef RUMBOOT_NATIVE
+#ifndef RUMBOOT_NATIVE
     	struct rumboot_bootheader *hdr = (struct rumboot_bootheader *)&rumboot_platform_spl_start;
-	#else
-		struct rumboot_config conf;
-		rumboot_platform_read_config(&conf);
+#else
+	struct rumboot_config conf;
+	rumboot_platform_read_config(&conf);
     	struct rumboot_bootheader *hdr = malloc(SIZE);
-	#endif
+	rumboot_platform_init_loader(&conf);
+#endif
 	rumboot_printf("Hello, I'm bootsource unit test for %d\n", SOURCE);
 	rumboot_printf("I'm expecting boot to complete with error code %d\n", EXPECTED);
-    int ret = bootsource_try_by_id(SOURCE, pdata, hdr, SIZE);
+        int ret = bootsource_try_by_id(SOURCE, pdata, hdr, SIZE);
 	rumboot_printf("bootsource_try_by_id returned %d, expecting %d\n", ret, EXPECTED);
 	if (ret == EXPECTED) {
 		return 0;
