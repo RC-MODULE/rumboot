b08d3da5f26c8b685744fd9714f6d36e602474fd
Andrew Andrianov <andrew@ncrmnt.org>
2018-10-24 13:33:36 +0300

    scripts: Update clang analyzer script
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- scripts/run_clang_analyze.sh
+++ scripts/run_clang_analyze.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-export CCC_CC=arm-rcm-eabihf-gcc
-export CLANG_ANALYZER_TARGET=arm-rcm-eabihf
+
+case "$1" in
+    "oi10")
+        export CCC_CC=powerpc-rcm-elf-gcc
+        export CLANG_ANALYZER_TARGET=powerpc-rcm-elf-gcc
+    ;;
+    "basis")
+        export CCC_CC=arm-rcm-eabihf-gcc
+        export CLANG_ANALYZER_TARGET=arm-rcm-eabihf
+    ;;
+    native)
+        export CCC_CC=gcc
+    ;;
+esac
+
 rm -rf build-clang-analyze
 mkdir build-clang-analyze
 cd build-clang-analyze
-cmake .. -DRUMBOOT_DISABLE_CCACHE=Yes -DRUMBOOT_PLATFORM=basis -DCLANG_ANALYZE=Yes
+cmake .. -DCMAKE_BUILD_TYPE=Production -DRUMBOOT_DISABLE_CCACHE=Yes -DRUMBOOT_PLATFORM=$1 -DCLANG_ANALYZE=Yes
 make utils
 scan-build make
