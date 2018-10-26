11fd4da0efa4ac940b17aaeaf50336176b8464da
Chernousov Stepan <s.chernousov@modulew.ru>
2018-10-26 09:34:20 +0300

    oi10_rumboot: mem_008 fix comments

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_008.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_008.c
@@ -20,7 +20,7 @@
 #include <platform/regs/regs_l2c_l2.h>
 #include <platform/devices/dma2plb6.h>
 
-//                                      MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
+//                                        MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
 #define TLB_ENTRY_CACHE_VALID_0           MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 #define TLB_ENTRY_CACHE_VALID_1           MMU_TLB_ENTRY(  0x000,  0x40000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 
@@ -44,8 +44,6 @@ static volatile uint64_t test_array[]={
         isync(); \
         TEST_ASSERT( (ADDRESS >> 13) == (spr_read(SPR_ICDBTRH) >> 13), "Wrong value in SPR_ICDBTRH" ); \
     }while (0);
-
-//rumboot_printf("SPR = %x\n",spr_read(SPR_ICDBTRH)) ;
 
 void write_blr_l2_line_by_addr(uint32_t volatile address)
 {
@@ -72,7 +70,6 @@ void cache_icu_tag(uint32_t erpn_tag, uint32_t index)
     rumboot_printf("ERPN_TAG = %x\n", erpn_tag);
     uint32_t tag = get_EA_tag(erpn_tag);
     rumboot_printf("TAG = %x\n", tag);
-    //set_mem_window(GET_WINDOW(erpn_tag));
 
     CODE_COPY_RUN(GET_ADDR_BY_TAG_INDEX_WORD(tag,index,0));
     CODE_COPY_RUN(GET_ADDR_BY_TAG_INDEX_WORD(tag+1,index,0));
@@ -96,7 +93,6 @@ bool test_icu_tag_array()
     cache_icu_tag((0b110011001100110011 << 2) | 0b00,0x12);//array 0
     cache_icu_tag((0b101010101010101010 << 2) | 0b00,0x14);//array 0
     cache_icu_tag((0b010101010101010101 << 2) | 0b00,0x16);//array 0
-    //test_event(EVENT_CACHING_DONE);
 //                   E                 TAG   +   WAY
     cache_icu_tag((0b111111111111111111 << 2) | 0b00,0x80);//array 0
     cache_icu_tag((0b000000000000000000 << 2) | 0b00,0x82);//array 0
@@ -110,7 +106,6 @@ bool test_icu_tag_array()
     cache_icu_tag((0b110011001100110011 << 2) | 0b00,0x92);//array 0
     cache_icu_tag((0b101010101010101010 << 2) | 0b00,0x94);//array 0
     cache_icu_tag((0b010101010101010101 << 2) | 0b00,0x96);//array 0
-    //test_event(EVENT_CACHING_DONE);
 
 //                   E                 TAG   +   WAY
     cache_icu_tag((0b111111111111111111 << 2) | 0b00,0x0);//array 0
@@ -125,7 +120,6 @@ bool test_icu_tag_array()
     cache_icu_tag((0b110011001100110011 << 2) | 0b10,0x12);//array 0
     cache_icu_tag((0b101010101010101010 << 2) | 0b10,0x14);//array 0
     cache_icu_tag((0b010101010101010101 << 2) | 0b10,0x16);//array 0
-    //test_event(EVENT_CACHING_DONE);
 //                   E                 TAG   +   WAY
     cache_icu_tag((0b111111111111111111 << 2) | 0b10,0x80);//array 0
     cache_icu_tag((0b000000000000000000 << 2) | 0b10,0x82);//array 0
@@ -139,7 +133,6 @@ bool test_icu_tag_array()
     cache_icu_tag((0b110011001100110011 << 2) | 0b10,0x92);//array 0
     cache_icu_tag((0b101010101010101010 << 2) | 0b10,0x94);//array 0
     cache_icu_tag((0b010101010101010101 << 2) | 0b10,0x96);//array 0
-    //test_event(EVENT_CACHING_DONE);
 
     return true;
 }
@@ -155,7 +148,6 @@ int main(void)
     static const tlb_entry tlb_entry_cacheable_valid_1 = {TLB_ENTRY_CACHE_VALID_1};
     write_tlb_entries(&tlb_entry_cacheable_valid_1, 1);
 
-    //START TEST (call func in test_oi10_cpu_mem_008.c)
     bool result = test_icu_tag_array();
     if (!result)
     {
