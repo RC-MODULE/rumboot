af1dc959dfbfbbae05af4d29f804a2357de43590
Andrew Andrianov <andrew@ncrmnt.org>
2018-10-28 14:36:10 +0300

    eventchannel-memory: Revert broken eventsystem changes
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/eventsystem/eventchannel-memory.c
+++ src/lib/eventsystem/eventchannel-memory.c
@@ -13,13 +13,13 @@
 __attribute__((no_instrument_function)) void rumboot_platform_event_raise(enum rumboot_simulation_event const event,
 				  uint32_t const * const data, uint32_t const len)
 {
-    uint32_t const max_event_params_n = ARRAY_SIZE( rumboot_platform_runtime_info->out.data );
-    if( len <= max_event_params_n ) rumboot_platform_panic( "Too many event params. Max: %d, set: %d", max_event_params_n, len );
+       int i;
+       RUMBOOT_ATOMIC_BLOCK() {
+               for (i=0; i<len; i++)
+                       rumboot_platform_runtime_info->out.data[i] = data[i];
+               rumboot_platform_runtime_info->out.opcode = event;
+       }
 
-    RUMBOOT_ATOMIC_BLOCK() {
-        memcpy( (void *)(rumboot_platform_runtime_info->out.data), data, len*sizeof(data[0]));
-        rumboot_platform_runtime_info->out.opcode = event;
-    }
 }
 
  __attribute__((no_instrument_function)) enum rumboot_simulation_event
