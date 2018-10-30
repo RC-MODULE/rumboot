39ad23ffa7eeb164bcb155b4e7f28cd55cacf711
Rishat Galiulin <r.galiulin@modulew.ru>
2018-10-30 11:13:37 +0300

    oi10_rumboot: test_oi10_spi - Refactor

--- src/platform/oi10/targets/test_oi10_spi.c
+++ src/platform/oi10/targets/test_oi10_spi.c
@@ -22,7 +22,7 @@
 #include <devices/gpio_pl061.h>
 #include <platform/devices/emi.h>
 
-#define BYTE_NUMBER             16
+#define BYTE_NUMBER             64
 #define DATA_SIZE               (BYTE_NUMBER+4)
 #define PAGE_PROGRAM_COMMAND    0x02000000
 #define TEST_DATA               0x99
@@ -145,6 +145,7 @@ static uint8_t __attribute__((section(".data.data_dst"))) data_dst[BYTE_NUMBER];
 
 ssp_params ssp_param = {0x6, 1, 1, 7, master_mode, false, ssp_motorola_fr_form};
 dma_params dma_param = {lsb, 8, lsb, 8};
+struct pl022_config conf;
 
 static uint32_t gspi_dma_axi(uint32_t base_addr, uint32_t* r_mem_addr, uint32_t* w_mem_addr, uint32_t test_data)
 {
@@ -155,7 +156,7 @@ static uint32_t gspi_dma_axi(uint32_t base_addr, uint32_t* r_mem_addr, uint32_t*
 
     gspi_dma_reset(base_addr);
 
-    gspi_init(base_addr, ssp_param); //turn on SSP controller
+    gspi_init(base_addr, ssp_param, &conf); //turn on SSP controller
     gspi_set_int_mask(base_addr, 0x02); //interrupt masks - unmask rx_fifo not empty
 
     pl022_flash_write_enable(base_addr);
@@ -224,7 +225,7 @@ static uint32_t gspi_ssp_flash(uint32_t base_addr)
 
     //Check SPI interrupt with loop operation
     ssp_param.loopback = true;
-    gspi_init(base_addr, ssp_param); //turn on SSP controller, loop operation
+    gspi_init(base_addr, ssp_param, &conf); //turn on SSP controller, loop operation
     gspi_set_int_mask(base_addr, 0x02); //interrupt masks - unmask rx_fifo not empty
 
 
@@ -247,7 +248,7 @@ static uint32_t gspi_ssp_flash(uint32_t base_addr)
 
     //Write data to spi flash
     ssp_param.loopback = false;
-    gspi_init(base_addr, ssp_param); //turn on SSP controller, loop operation
+    gspi_init(base_addr, ssp_param, &conf);  //turn on SSP controller, loop operation
     gspi_set_int_mask(base_addr, 0x0); //mask all interrupts
 
 
@@ -306,7 +307,7 @@ int main(void)
 
     // Data for check IM0
     for (int i=4; i<DATA_SIZE; i++)
-        data_src[i] = TEST_DATA_IM0;
+        data_src[i] = i;
 //    rumboot_putdump( (uint32_t)data_src, sizeof(data_src));
     iowrite32(0x0, (uint32_t)data_src + DATA_SIZE);
 
