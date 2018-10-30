cd8b47d9b817bd1359f0ef6d5250222f603c7f44
Rishat Galiulin <r.galiulin@modulew.ru>
2018-10-30 19:23:42 +0300

    oi10_rumboot: test_oi10_spi - Move pl022_flash read/write to test

--- src/platform/oi10/targets/test_oi10_spi.c
+++ src/platform/oi10/targets/test_oi10_spi.c
@@ -213,6 +213,50 @@ static uint32_t gspi_dma_axi(uint32_t const base_addr, uint32_t const r_mem_addr
     return 0;
 }
 
+static void pl022_flash_write_data(uint32_t const base_addr, uint32_t const data)
+{
+    uint32_t addr =  base_addr+GSPI_SSPDR;
+    iowrite8(SPIFLASH_PAGEPROG, addr); //write data to SPI - command write
+    for (int i =0; i<3; i++)
+        iowrite8(0x00, addr);//write data to SPI - write address
+
+    iowrite8((data & 0x000000FF), base_addr+GSPI_SSPDR); //write data to SPI - write data 1
+    iowrite8((data & 0x0000FF00) >> 8, base_addr+GSPI_SSPDR); //write data to SPI - write data 2
+    iowrite8((data & 0x00FF0000) >> 16, base_addr+GSPI_SSPDR); //write data to SPI - write data 3
+    iowrite8((data & 0xFF000000) >> 24, base_addr+GSPI_SSPDR); //write data to SPI - write data 4
+
+    while((ioread8(base_addr+GSPI_SSPSR) & 0x1) == 0) //wait tx fifo empty
+        {}
+    for (int i =0; i<8; i++)
+        (void)ioread8(addr); //read data from SPI - staff
+    while ((read_flash_status(base_addr) & 0x1) == 0x1) //wait write complete
+        {}
+}
+
+static uint32_t pl022_flash_read_data(uint32_t const base_addr)
+{
+    uint32_t data = 0x00000000;
+    uint32_t addr =  base_addr+GSPI_SSPDR;
+
+    iowrite8(SPIFLASH_READDATA, addr); //write data to SPI - command read
+    for (int i =0; i<3; i++)
+        iowrite8(0x00, addr);//write data to SPI - write address
+
+    for (int i =0; i<4; i++)
+        iowrite8(0xFF, addr);//write data to SPI - write address
+    while((ioread8(base_addr+GSPI_SSPSR) & 0x1) == 0) //wait tx fifo empty
+        {}
+
+    for (int i =0; i<4; i++)
+        (void)ioread8(addr); //read data from SPI - staff
+
+    data = ioread8(addr);
+    data = data | (ioread8(addr) << 8);
+    data = data | (ioread8(addr) << 16);
+    data = data | (ioread8(addr) << 24);
+    return data;
+}
+
 static uint32_t gspi_ssp_flash(uint32_t const base_addr)
 {
     //Read and check ID
