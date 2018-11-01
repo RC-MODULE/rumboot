8bc679c1319f31ad698f95e808f603c90ab76d97
Victor Strukov <v.strukov@module.ru>
2018-11-01 15:50:11 +0300

    oi10_rumboot: memfill.h: added memcmp function declaration

--- include/rumboot/memfill.h
+++ include/rumboot/memfill.h
@@ -1,4 +1,17 @@
 
 int rumboot_memfill32(void *addr, size_t sz, int val, int incr);
 int rumboot_memcheck32(void *src, void *dst, size_t sz);
+/**
+ * Returns value like memcmp, comparing bytewise memory
+ * regions from addresses src and dst of size sz.
+ * Unlike memcmp it compares everything up to the end of
+ * specified memory regions, provides tracing and
+ * can be accelerated in SystemVerilog while modelling
+ *
+ * @param  src  first memory area address
+ * @param  dst  second memory area address
+ * @param  sz   count of bytes to be compared
+ * @return      like in memcmp
+ */
+int rumboot_memcmp(void *src, void *dst, size_t sz);
 
