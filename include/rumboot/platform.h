90b4ba1a7372b4cff0702e4738242620bd5c5c60
Victor Strukov <v.strukov@module.ru>
2018-11-06 10:42:41 +0300

    oi10_rumboot: platform.h: moved here rumboot_memcmp function header from memfill.h

--- include/rumboot/platform.h
+++ include/rumboot/platform.h
@@ -98,6 +98,19 @@ void rumboot_platform_setup();
  */
 uint32_t rumboot_platform_get_uptime();
 
+/**
+ * Returns value like memcmp, comparing bytewise memory
+ * regions from addresses src and dst of size sz.
+ * Unlike memcmp it compares everything up to the end of
+ * specified memory regions, provides tracing and
+ * can be accelerated in SystemVerilog while modelling
+ *
+ * @param  src  first memory area address
+ * @param  dst  second memory area address
+ * @param  sz   count of bytes to be compared
+ * @return      like in memcmp
+ */
+int rumboot_memcmp(void *src, void *dst, size_t sz);
 
 /**
  * This function is called to trace a function call with a pc value
