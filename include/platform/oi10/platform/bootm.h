75a25555a9be753b8e869d535366597d1da13004
Andrew Andrianov <andrew@ncrmnt.org>
2018-11-07 18:57:28 +0300

    oi10: bootm.h: Swap SLOWUART and SELFTEST pins to match docs
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/platform/oi10/platform/bootm.h
+++ include/platform/oi10/platform/bootm.h
@@ -2,12 +2,12 @@
 #define BOOTM_H_
 
 
-#define BOOTM_SLOWUART     (1 << 0)
+#define BOOTM_SELFTEST     (1 << 0)
 #define BOOTM_HOST         (1 << 1)
 #define BOOTM_FASTUART     (1 << 2)
 #define BOOTM_SDIO_CD      (1 << 3)
+#define BOOTM_SLOWUART     (1 << 4)
 //#define BOOTM_SPI_CS       (1 << 4)
-#define BOOTM_SELFTEST     (1 << 4)
 #define BOOTM_CPU_ECC      (1 << 5)
 #define BOOTM_EMI_ECC      (1 << 6)
 #define BOOTM_NOR_BOOT     (1 << 7)
