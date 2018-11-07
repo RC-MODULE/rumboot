242ad9b12738f3be4580ccb611c2532332967875
Chernousov Stepan <s.chernousov@modulew.ru>
2018-11-07 16:11:45 +0300

    oi10_rumboot: test cpu_mem_010 upd

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_010.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_010.c
@@ -32,21 +32,14 @@
 #define TLB_ENTRY_CACHE_VALID_0           MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 #define TLB_ENTRY_CACHE_VALID_1           MMU_TLB_ENTRY(  0x000,  0x40000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 
-#define ddr_init_array  SRAM0_BASE;
-
-
-#define CACHE_DATA_LINE_BY_TAG_INDEX(TAG,INDEX) \
-    rumboot_printf("Address = %x\n", (0x00 | ((TAG)<<13) | ((INDEX)<<5) | ((0)<<2)) );\
-    msync(); \
-    /*Minimal caching size is 1 line = 32 bytes = 8 words = 4 doublewords*/ \
-    iowrite64(0x00 | ((TAG)<<13) | ((INDEX)<<5) | ((0)<<2)), test_10_data_array64[0]; \
-    iowrite64(0x00 | ((TAG)<<13) | ((INDEX)<<5) | ((2)<<2)), test_10_data_array64[1]; \
-    iowrite64(0x00 | ((TAG)<<13) | ((INDEX)<<5) | ((4)<<2)), test_10_data_array64[2]; \
-    iowrite64(0x00 | ((TAG)<<13) | ((INDEX)<<5) | ((6)<<2)), test_10_data_array64[3]; \
-    msync(); \
-    /*1st read initiates a caching*/ \
-    TEST_ASSERT(ioread32( 0x00 | ((TAG)<<13) | ((INDEX)<<5) | ((0)<<2) ) == (uint32_t*) *test_10_data_array64, "Compare failed");\
-    msync();
+//data array
+uint64_t test_data_array[] =
+{
+        0xFFFFFFFFFFFFFFFF,
+        0xFFFFFFFF00000000,
+        0x00000000FFFFFFFF,
+        0xF0F0F0F0F0F0F0F0
+};
 
 #define REVERSE8(X) \
     (((X & 0x01) << 7) | \
@@ -61,15 +54,6 @@
 #define MOVE1TO6(X) (((X&(1<<1))<<5) |  ((X&0x7C)>>1) | (X & 1))
 #define TAG_A_TO_INDEX(A)  (REVERSE8(MOVE1TO6(A) << 1))
 
-//data array
-uint64_t test_10_data_array64[4] =
-{
-        0xFFFFFFFFFFFFFFFF,
-        0xFFFFFFFF00000000,
-        0x00000000FFFFFFFF,
-        0xF0F0F0F0F0F0F0F0
-};
-
 /*
  * erpn - 0 or 1 for em0
  * tag - 0..0x7FFFF
@@ -79,76 +63,92 @@ uint64_t test_10_data_array64[4] =
  * [31:29] = 0
  */
 
+void CACHE_DATA_LINE_BY_TAG_INDEX (uint32_t TAG, uint32_t INDEX)
+{
+    uint32_t addr = (0x00 | ((TAG)<<14) | ((INDEX)<<5) | (0<<2));
+    rumboot_printf("Address = 0x%x (tag = 0x%x, index = 0x%x)\n", addr, TAG, INDEX );
+    msync();
+    /*Minimal caching size is 1 line = 32 bytes = 8 words = 4 doublewords*/
+    for (int i = 0; i < 4; i++)
+        iowrite64(test_data_array[i], addr + i*8 );
+    msync();
+    /*1st read initiates a caching*/
+    TEST_ASSERT(ioread32(addr) == ioread32((uint32_t)test_data_array), "Compare failed");
+    msync();
+}
+
 void cache_data(uint32_t tag, uint32_t addr)
 {
     uint32_t index = TAG_A_TO_INDEX(addr);
-    rumboot_printf("index = %x\n", index);
     if(index & 1) //ODD
     {
         if(index >=128)
         {
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index))
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index))
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index));
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index));
         }
         else
         {
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index+128))
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index+128))
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index+128));
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index+128));
         }
     }
     else//EVEN
     {
         if(index >=128)
         {
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index-128))
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index-128))
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index-128));
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index-128));
         }
         else
         {
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index))
-            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index))
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index));
+            CACHE_DATA_LINE_BY_TAG_INDEX(tag+1,(index));
         }
     }
 }
 
 void check_dcu_tag()
 {
-     rumboot_printf("SRAM0\n");
-     cache_data((0x3F), 0x00);
+
+     rumboot_printf("\nSRAM0\n");
+     cache_data((0x3E), 0x00);
      cache_data((0x00), 0x04);
      cache_data((0x15), 0x08);
-     cache_data((0x2A), 0x0B);
+     cache_data((0x2A), 0x0C);
 
-     rumboot_printf("SDRAM\n");
-     cache_data((0x8FFF), 0x00);
+     rumboot_printf("\nSDRAM\n");
+     cache_data((0x8FFE), 0x00);
      cache_data((0x8000), 0x04);
      cache_data((0x8AAA), 0x08);
-     cache_data((0x8555), 0x0B);
+     cache_data((0x8555), 0x0C);
 
-     rumboot_printf("SSRAM\n");
-     cache_data((0x100FF), 0x00);
+     rumboot_printf("\nSSRAM\n");
+     cache_data((0x100FE), 0x00);
      cache_data((0x10000), 0x04);
      cache_data((0x100AA), 0x08);
-     cache_data((0x10055), 0x0B);
+     cache_data((0x10055), 0x0C);
 
-     rumboot_printf("PIPELINED\n");
-     cache_data((0x140FF), 0x00);
+     rumboot_printf("\nPIPELINED\n");
+     cache_data((0x140FE), 0x00);
      cache_data((0x14000), 0x04);
      cache_data((0x140AA), 0x08);
-     cache_data((0x14055), 0x0B);
+     cache_data((0x14055), 0x0C);
 
-     rumboot_printf("SRAM1\n");
-     cache_data((0x180FF), 0x00);
-     cache_data((0x18000), 0x01);
-     cache_data((0x180AA), 0x02);
-     cache_data((0x18055), 0x03);
+     rumboot_printf("\nSRAM1\n");
+     cache_data((0x180FE), 0x00);
+     cache_data((0x18000), 0x04);
+     cache_data((0x180AA), 0x08);
+     cache_data((0x18055), 0x0C);
 }
 
 int main()
 {
+     rumboot_printf("Start test\n");
+     rumboot_printf("EMI init\n");
      emi_init(DCR_EM2_EMI_BASE);
 
-     //cache on
+     rumboot_printf("cache on\n");
      static const tlb_entry tlb_entry_cacheable_valid_0 = {TLB_ENTRY_CACHE_VALID_0};
      write_tlb_entries(&tlb_entry_cacheable_valid_0, 1);
 
@@ -156,7 +156,7 @@ int main()
      write_tlb_entries(&tlb_entry_cacheable_valid_1, 1);
 
      check_dcu_tag();
-
+     rumboot_printf("TEST OK\n");
      return 0;
 }
 
