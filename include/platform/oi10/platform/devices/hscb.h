144ebcc1e8c5dcf2e2d6db75804a486b3624df85
Victor Strukov <v.strukov@module.ru>
2018-11-12 08:53:40 +0300

    oi10_rumboot: hscb.h: moved to platform/oi10 folder, requires adjusting includes in tests

--- include/platform/oi10/platform/devices/hscb.h
+++ include/platform/oi10/platform/devices/hscb.h
@@ -18,7 +18,7 @@
 #include <rumboot/printf.h>
 #include <platform/devices.h>
 #include <platform/trace.h>
-#include <regs/regs_hscb.h>
+#include <platform/regs/regs_hscb.h>
 
 /**
  * \defgroup devices_hscb HSCB
@@ -189,38 +189,6 @@ uint32_t hscb_change_endian (uint32_t data_in);
  * \param[in]   len         length of data to be processed in bytes
  */
 void hscb_convert_to_bytes (uint32_t* data_in, uint8_t* data_out, uint32_t len);
-
-/**
- * \brief Set descriptor data function, DEPRECATED!!! Use hscb_set_descr_in_mem instead
- * \param[in]   sys_addr    address of descriptor
- * \param[in]   len         length of data array
- * \param[in]   desc_addr   address of data array (address in descriptor)
- * \param[in]   data_in     byte-data array for write to memory pointer
- * \param[in]   act0        descriptors flag pointer
- * \param[in]   interrupt   descriptors flag pointer
- * \param[in]   end         descriptors flag pointer
- * \param[in]   valid       descriptors flag pointer
- * \return                  Function returns address of next descriptor
- */
-uint32_t hscb_set_desc (uint32_t sys_addr, uint32_t len, uint32_t desc_addr, uint8_t* data_in, bool act0, bool interrupt, bool valid, bool endian);
-
-/**
- * \brief Get descriptor data function, DEPRECATED!!! Use hscb_get_descr_from_mem instead
- * \param[in]   sys_addr    address of descriptor
- * \param[in]   data_out    accepted byte-data array pointer
- * \param[in]   len         length of data array pointer
- * \param[in]   act0        descriptors flag pointer
- * \param[in]   interrupt   descriptors flag pointer
- * \param[in]   end         descriptors flag pointer
- * \param[in]   valid       descriptors flag pointer
- * \return                  In case COMPL descriptor
- *
- *      function returns address of next descriptor
- *
- *      function returns 0xffffffff as impossible address
- */
-uint32_t hscb_get_desc (uint32_t sys_addr, uint8_t* data_out, uint32_t* len,  bool* act0, bool* interrupt, bool* end, bool* valid, bool endian);
-
 
 /**
  * \brief Set descriptor in memory
