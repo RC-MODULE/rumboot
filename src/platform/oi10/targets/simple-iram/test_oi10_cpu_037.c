e799ae93f21244f6a1b21b69ea6350b472dde69f
Margarita Smolina <m.smolina@modulew.ru>
2018-11-28 15:52:18 +0300

    oi10_rumboot: add test_oi10_cpu_037

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_037.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_037.c
@@ -34,34 +34,120 @@
 #include <platform/devices/plb6mcif2.h>
 #include <devices/uart_pl011.h>
 #include <rumboot/irq.h>
-
+#include <platform/arch/ppc/ppc_476fp_fpu_fields.h>
+#include <platform/arch/ppc/ppc_476fp_fpu_const.h>
 
 #define ITRPT_XSR_FP_i IBM_BIT_INDEX(64, ITRPT_XSR_FP_e)
 
-double FPSCR_OX_1 = 1.1111e64;
-double FPSCR_OX_2 = 1.1111e64;
+float FPSCR_1 = 9.989e-37;
+float FPSCR_2 = 9.979e-37;
+float FPSCR_3 = 1.123455e-34;
+float FPSCR_4 = 1.123456e-34;
+float FPSCR_5 = 0.0e-37;
+float FPSCR_6 = 0.121e+37;
+float result_sum;
 
-double result_sum;
 
 static void enable_fpu()
 {
     msr_write(msr_read() | (1 << ITRPT_XSR_FP_i));
 }
 
-int main ()
+uint64_t  value;
+
+void read_fpu ()
 {
-
-    uint32_t value;
-    enable_fpu();
-
-    result_sum = FPSCR_OX_1 + FPSCR_OX_2;
-    rumboot_printf("Result sum: %d\n", result_sum);
     asm volatile
             (
                     "mffs %0 \n\t"
                     : "=r" (value)
             );
-    rumboot_printf("Result: %b\n", value);
+}
+
+void check_exception ()
+{
+    if ((value >> FPU_FPSCR_OX_e)     & 1) rumboot_printf("A floating-point overflow exception occurred\n");
+    if ((value >> FPU_FPSCR_UX_e)     & 1) rumboot_printf("A floating-point underflow exception occurred\n");
+    if ((value >> FPU_FPSCR_ZX_e)     & 1) rumboot_printf("A floating-point zero divide exception occurred\n");
+    if ((value >> FPU_FPSCR_XX_e)     & 1) rumboot_printf("A floating-point inexact exception occurred\n");
+    if ((value >> FPU_FPSCR_VXSNAN_e) & 1) rumboot_printf("A floating-point invalid operation exception (VXSNAN) occurred\n");
+    if ((value >> FPU_FPSCR_VXISI_e)  & 1) rumboot_printf("A floating-point invalid operation exception (VXISI) occurred\n");
+    if ((value >> FPU_FPSCR_VXIDI_e)  & 1) rumboot_printf("A floating-point invalid operation exception (VXIDI) occurred\n");
+    if ((value >> FPU_FPSCR_VXZDZ_e)  & 1) rumboot_printf("A floating-point invalid operation exception (VXZDZ) occurred\n");
+    if ((value >> FPU_FPSCR_VXSOFT_e) & 1) rumboot_printf("A floating-point invalid operation exception (software request) occurred\n");
+    if ((value >> FPU_FPSCR_VXSQRT_e) & 1) rumboot_printf("A floating-point invalid operation exception (invalid square root) occurred\n");
+    if ((value >> FPU_FPSCR_FI_e)     & 1) rumboot_printf("Floating-point fraction inexact \n");
+    if ((value >> FPU_FPSCR_FR_e)     & 1) rumboot_printf("Floating-point fraction rounded \n");
+    //if ((value >> FPU_FPSCR_FE_e)     & 1) rumboot_printf("Floating-point equal to zero \n");
+    if ((value >> FPU_FPSCR_FU_e)     & 1) rumboot_printf("Floating-point unordered or not-a-number (NaN). \n");
+
+
+
+}
+
+void check_OX ()
+{
+
+    rumboot_printf("TEST OX\n");
+    result_sum = FPSCR_1 + FPSCR_2;
+    read_fpu ();
+    check_exception ();
+
+    result_sum = FPSCR_1 + FPSCR_2 - 192;
+
+    read_fpu ();
+    check_exception ();
+
+}
+
+void check_UX ()
+{
+    rumboot_printf("TEST UX\n");
+    result_sum = FPSCR_3 - FPSCR_4;
+    if (result_sum == 0.0e-264 ) rumboot_printf("result_sum = 0 \n");
+    read_fpu ();
+    check_exception ();
+}
+
+void check_ZX ()
+{
+    rumboot_printf("TEST ZX\n");
+    result_sum = FPSCR_1/FPSCR_5;
+    read_fpu ();
+    check_exception ();
+}
+
+
+int main ()
+{
+    check_exception ();
+    //Overflow Exception(OX)
+    enable_fpu();
+    rumboot_printf("TEST START\n");
+
+    read_fpu ();
+    check_exception ();
+
+    //rumboot_printf("TEST OX\n");
+    //result_sum = FPSCR_1 + FPSCR_2;
+
+   // rumboot_printf("TEST UX\n");
+    //result_sum = FPSCR_3 - FPSCR_4;
+    //if (result_sum == 0.0e-264 ) rumboot_printf("result_sum = 0 \n");
+    //rumboot_printf("TEST ZX\n");
+    //result_sum = FPSCR_1/FPSCR_5;
+    //rumboot_printf("result_sum = %d\n", result_sum);
+    //rumboot_printf("TEST XX\n");
+    //result_sum = FPSCR_3 + FPSCR_6;
+    //rumboot_printf("result_sum = %d\n", result_sum);
+    //read_fpu ();
+    //check_exception ();
+
+    //check_OX ();
+    //check_UX ();
+    //check_ZX ();
+
+
     rumboot_printf("TEST OK\n");
     return 0;
 }
