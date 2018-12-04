b35dc523e6ccbf5a185d685f8e1ddcedbf1951d6
Chernousov Stepan <s.chernousov@modulew.ru>
2018-12-04 12:43:58 +0300

    oi10_rumboot: cpu_021 added log msgs

--- src/platform/oi10/targets/test_oi10_cpu_021.c
+++ src/platform/oi10/targets/test_oi10_cpu_021.c
@@ -8,10 +8,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+
 #include <rumboot/printf.h>
 #include <rumboot/platform.h>
 #include <rumboot/macros.h>
 #include <rumboot/io.h>
+
 #include <platform/test_assert.h>
 #include <platform/devices.h>
 #include <platform/trace.h>
@@ -30,33 +32,40 @@
 #define TLB_ENTRY_CACHE_VALID      MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      L2C_IL1I_BIT,    L2C_IL1D_BIT,    L2C_WRITE_MODE_BIT,    L2C_INHIBIT_BIT, L2C_MEMORY_COHERENCE_BIT,   0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 
 #define BASE_INIT_VALUE  0x00
-uint32_t __attribute__((section(".data"),aligned(16))) volatile sram0_data[64] = { BASE_INIT_VALUE };
+uint32_t __attribute__((section(".data"),aligned(32))) volatile sram0_data[64] = { BASE_INIT_VALUE };
 
-uint8_t __attribute__((section(".text.test_oi10_cpu_021"))) cache_testing_function()
+uint8_t __attribute__((section(".text.test"))) cache_testing_function()
 {
     static const tlb_entry sram0_tlb_entry_cacheable_valid = {TLB_ENTRY_CACHE_VALID};
     static const tlb_entry sram0_tlb_entry_non_cacheable_valid = {TLB_ENTRY_NOCACHE_VALID};
 
     uint32_t addr = (uint32_t) sram0_data;
+    rumboot_printf("1. ADDR = 0x%x, INIT VALUE = 0x%x\n", addr, BASE_INIT_VALUE);
 
     //2
+    rumboot_printf("2. Cache on\n");
     write_tlb_entries(&sram0_tlb_entry_cacheable_valid,1); //cache on
 
     //3
     const uint32_t const_A = 0xAAAAAAAA;
+    rumboot_printf("3. A = 0x%x\n", const_A);
 
     //4
+    rumboot_printf("4. Write A in 0x%x\n", addr);
     iowrite32(const_A, addr);
     msync();
 
     //5
+    rumboot_printf("5. Cache off\n");
     write_tlb_entries(&sram0_tlb_entry_non_cacheable_valid,1); //cache off
 
     //6
+    rumboot_printf("6. Read from 0x%x\n", addr);
     uint32_t read_data = ioread32 (addr);
     msync();
 
     //7
+    rumboot_printf("7. Check read data\n");
     if (L2C_INHIBIT_BIT)
     {
       if (read_data != const_A) return 1;
@@ -71,17 +80,21 @@ uint8_t __attribute__((section(".text.test_oi10_cpu_021"))) cache_testing_functi
 
     //8
     const uint32_t const_B = 0xBBBBBBBB;
+    rumboot_printf("8. B = 0x%x\n", const_B);
 
     //9
+    rumboot_printf("9. Wrte B in 0x%x\n", addr);
     iowrite32(const_B, addr);
     msync();
 
     //10
+    rumboot_printf("10. Cache off. Read from 0x%x\n", addr);
     write_tlb_entries(&sram0_tlb_entry_cacheable_valid,1); //cache on
     read_data = ioread32 (addr);
     msync();
 
     //11
+    rumboot_printf("11. Check read data\n");
     if (L2C_INHIBIT_BIT)
     {
       if (read_data != const_B) return 2;
@@ -92,27 +105,33 @@ uint8_t __attribute__((section(".text.test_oi10_cpu_021"))) cache_testing_functi
     }
 
     //12
+    rumboot_printf("12. Write A in 0x%x\n", addr);
     iowrite32(const_A, addr);
     msync();
 
     //13
+    rumboot_printf("13. Flush (0x%x)\n", addr);
     dcbf((uint32_t *) addr);
     msync();
 
     //14
+    rumboot_printf("14. Cache off. Read from 0x%x\n", addr);
     write_tlb_entries(&sram0_tlb_entry_cacheable_valid,1); //cache off
     read_data = ioread32 (addr);
     msync();
 
     //15
+    rumboot_printf("15. Check read data\n");
     if (read_data != const_A) return 3;
 
     //16
+    rumboot_printf("16. Cache on. Read from 0x%x\n", addr);
     write_tlb_entries(&sram0_tlb_entry_cacheable_valid,1); //cache on
     read_data = ioread32 (addr);
     msync();
 
     //17
+    rumboot_printf("17. Check read data\n");
     if (read_data != const_A) return 4;
 
     return 0;
