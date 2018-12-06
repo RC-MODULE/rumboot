12e8a68ab18c9f239e49ef52f2f1e02ffebd4dc2
Maksim Dubinkin <m.dubinkin@modulew.ru>
2018-12-06 08:46:47 +0300

    oi10_rumboot:test_oi10_ctrl_000.c - Added check ro/rw registers

--- src/platform/oi10/targets/simple-iram/test_oi10_ctrl_000.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_ctrl_000.c
@@ -11,148 +11,39 @@
 #include <rumboot/printf.h>
 #include <rumboot/io.h>
 #include <platform/devices.h>
-#include <platform/regs/sctl.h>
+#include <platform/oi10/platform/regs/regs_sctl.h>
 
-static uint8_t APB_check_ctrl_regs_ro(uint32_t base_addr)
+static uint8_t dcr_check_ctrl_regs_ro(uint32_t base_addr)
 {   
-    rumboot_printf("Checking registers default value...\n");
-      struct regpoker_checker check_array[] = {
-      { "SYS_BOOT_CFG_BOOTM7", REGPOKER_READ32, SYS_BOOT_CFG, 0x0, 0xf },
-      { "SYS_BOOT_CFG_BOOTM6", REGPOKER_READ32, SYS_BOOT_CFG << 1, 0x0, 0xf },
-      { "SYS_BOOT_CFG_BOOTM2", REGPOKER_READ32, SYS_BOOT_CFG << 2, 0x0, 0xf },
-      { "SYS_BOOT_CFG_BOOTM1", REGPOKER_READ32, SYS_BOOT_CFG << 3, 0x0, 0xf },
-      { "SYS_BOOT_CFG_BOOTM0", REGPOKER_READ32, SYS_BOOT_CFG, 0x0, 0xf },
-      {  }
+    rumboot_printf("Checking sctl registers default value...\n");
+
+     struct regpoker_checker check_array[] = {
+     { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0 << 6, 0b0, 0b1 },
+     { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0 << 5, 0b0, 0b1 },
+     { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF << 3 , 0b0, 0b1 },
+     {  }
     };
-      if( rumboot_regpoker_check_array( check_array, base_addr ) == 0 ) {
-              rumboot_printf( "OK\n" );
-              return 0;
-          }
-          rumboot_printf( "ERROR\n" );
-          return 1;
+      if( rumboot_regpoker_check_array( check_array, base_addr ) == 0 )
+      {
+          rumboot_printf( "OK\n" );
+          return 0;
+      }
+      rumboot_printf( "ERROR\n" );
+      return 1;
 }
 
-static int AXI_check_regs32(uint32_t base_addr)
+
+static int dcr_check_ctrl_regs_rw(uint32_t base_addr)
 {
-    rumboot_printf("Checking ID registers random value...\n");
-    rumboot_printf("Checking ID registers random value regs32...\n");
+    rumboot_printf("Checking sctl registers for rw value...\n");
 
     struct regpoker_checker check_array[] = {
-    { "NIC400_CPU_S_RQOS", REGPOKER_READ32, NIC400_CPU_S_RQOS, 0x0, 0b1111 },
-    { "NIC400_CPU_S_RQOS", REGPOKER_WRITE32, NIC400_CPU_S_RQOS, 0x0, 0b1111 },
-
-    { "NIC400_CPU_S_WQOS", REGPOKER_READ32, NIC400_CPU_S_WQOS, 0x0, 0b1111 },
-    { "NIC400_CPU_S_WQOS", REGPOKER_WRITE32, NIC400_CPU_S_RQOS, 0x0, 0b1111 },
-
-    { "NIC400_CPU_S_FN_MOD", REGPOKER_READ32, NIC400_CPU_S_FN_MOD, 0x0, 0b11 },
-    { "NIC400_CPU_S_FN_MOD", REGPOKER_WRITE32, NIC400_CPU_S_FN_MOD, 0x0, 0b11 },
-
-    { "NIC400_GSPI_1_S_RQOS", REGPOKER_READ32, NIC400_GSPI_1_S_RQOS, 0x0, 0b1111
-},
-    { "NIC400_GSPI_1_S_RQOS", REGPOKER_WRITE32, NIC400_GSPI_1_S_RQOS, 0x0,0b1111
-},
-
-    { "NIC400_GSPI_1_S_WQOS", REGPOKER_READ32, NIC400_GSPI_1_S_WQOS, 0x0, 0b1111
-},
-    { "NIC400_GSPI_1_S_WQOS", REGPOKER_WRITE32, NIC400_GSPI_1_S_WQOS, 0x0,
-0b1111 },
-
-    { "NIC400_GSPI_1_S_FN_MOD", REGPOKER_READ32, NIC400_GSPI_1_S_FN_MOD, 0x0,
-0b11 },
-    { "NIC400_GSPI_1_S_FN_MOD", REGPOKER_WRITE32, NIC400_GSPI_1_S_FN_MOD, 0x0,
-0b11 },
-
-    { "NIC400_ETH_1_S_RQOS", REGPOKER_READ32, NIC400_ETH_1_S_RQOS, 0x0, 0b1111
-},
-    { "NIC400_ETH_1_S_RQOS", REGPOKER_WRITE32, NIC400_ETH_1_S_RQOS, 0x0, 0b1111
-},
-
-    { "NIC400_ETH_1_S_WQOS", REGPOKER_READ32, NIC400_ETH_1_S_WQOS, 0x0, 0b1111
-},
-    { "NIC400_ETH_1_S_WQOS", REGPOKER_WRITE32, NIC400_ETH_1_S_WQOS, 0x0, 0b1111
-},
-
-    { "NIC400_ETH_1_S_FN_MOD", REGPOKER_READ32, NIC400_ETH_1_S_FN_MOD, 0x0, 0b11
-},
-    { "NIC400_ETH_1_S_FN_MOD", REGPOKER_WRITE32, NIC400_ETH_1_S_FN_MOD, 0x0,
-0b11 },
-
-    { "NIC400_SW_0_3_S_RQOS", REGPOKER_READ32, NIC400_SW_0_3_S_RQOS, 0x4, 0b1111
-},
-    { "NIC400_SW_0_3_S_RQOS", REGPOKER_WRITE32, NIC400_SW_0_3_S_RQOS, 0x4,
-0b1111 },
-
-    { "NIC400_SW_0_3_S_WQOS", REGPOKER_READ32, NIC400_SW_0_3_S_WQOS, 0x4, 0b1111
-},
-    { "NIC400_SW_0_3_S_WQOS", REGPOKER_WRITE32, NIC400_SW_0_3_S_WQOS, 0x4,
-0b1111 },
-
-    { "NIC400_SW_0_3_S_FN_MOD", REGPOKER_READ32, NIC400_SW_0_3_S_FN_MOD, 0x0,
-0b11 },
-    { "NIC400_SW_0_3_S_FN_MOD", REGPOKER_WRITE32, NIC400_SW_0_3_S_FN_MOD, 0x0,
-0b11 },
-
-    { "NIC400_GSPI_0_S_RQOS", REGPOKER_READ32, NIC400_GSPI_0_S_RQOS, 0x0, 0b1111
-},
-    { "NIC400_GSPI_0_S_RQOS", REGPOKER_WRITE32, NIC400_GSPI_0_S_RQOS, 0x0,
-0b1111 },
-
-    { "NIC400_GSPI_0_S_WQOS", REGPOKER_READ32, NIC400_GSPI_0_S_WQOS, 0x0, 0b1111
-},
-    { "NIC400_GSPI_0_S_WQOS", REGPOKER_WRITE32, NIC400_GSPI_0_S_WQOS, 0x0,
-0b1111 },
-
-    { "NIC400_GSPI_0_S_FN_MOD", REGPOKER_READ32, NIC400_GSPI_0_S_FN_MOD, 0x0,
-0b11 },
-    { "NIC400_GSPI_0_S_FN_MOD", REGPOKER_WRITE32, NIC400_GSPI_0_S_FN_MOD, 0x0,
-0b11 },
-
-    { "NIC400_ETH_0_S_RQOS", REGPOKER_READ32, NIC400_ETH_0_S_RQOS, 0x0, 0b1111
-},
-    { "NIC400_ETH_0_S_RQOS", REGPOKER_WRITE32, NIC400_ETH_0_S_RQOS, 0x0, 0b1111
-},
-
-    { "NIC400_ETH_0_S_WQOS", REGPOKER_READ32, NIC400_ETH_0_S_WQOS, 0x0, 0b1111
-},
-    { "NIC400_ETH_0_S_WQOS", REGPOKER_WRITE32, NIC400_ETH_0_S_WQOS, 0x0, 0b1111
-},
-
-    { "NIC400_ETH_0_S_FN_MOD", REGPOKER_READ32, NIC400_ETH_0_S_FN_MOD, 0x0, 0b11
-},
-    { "NIC400_ETH_0_S_FN_MOD", REGPOKER_WRITE32, NIC400_ETH_0_S_FN_MOD, 0x0,
-0b11 },
-
-    { "NIC400_MKIO_0_S_RQOS", REGPOKER_READ32, NIC400_MKIO_0_S_RQOS, 0x0, 0b1111
-},
-    { "NIC400_MKIO_0_S_RQOS", REGPOKER_WRITE32, NIC400_MKIO_0_S_RQOS, 0x0,
-0b1111 },
-
-    { "NIC400_MKIO_0_S_WQOS", REGPOKER_READ32, NIC400_MKIO_0_S_WQOS, 0x0, 0b1111
-},
-    { "NIC400_MKIO_0_S_WQOS", REGPOKER_WRITE32, NIC400_MKIO_0_S_WQOS, 0x0,
-0b1111 },
-
-    { "NIC400_MKIO_0_S_FN_MOD", REGPOKER_READ32, NIC400_MKIO_0_S_FN_MOD, 0x0,
-0b11 },
-    { "NIC400_MKIO_0_S_FN_MOD", REGPOKER_WRITE32, NIC400_MKIO_0_S_FN_MOD, 0x0,
-0b11 },
-
-    { "NIC400_MKIO_1_S_RQOS", REGPOKER_READ32, NIC400_MKIO_1_S_RQOS, 0x0, 0b1111
-},
-    { "NIC400_MKIO_1_S_RQOS", REGPOKER_WRITE32, NIC400_MKIO_1_S_RQOS, 0x0,
-0b1111 },
-
-    { "NIC400_MKIO_1_S_WQOS", REGPOKER_READ32, NIC400_MKIO_1_S_WQOS, 0x0, 0b1111
-},
-    { "NIC400_MKIO_1_S_WQOS", REGPOKER_WRITE32, NIC400_MKIO_1_S_WQOS, 0x0,
-0b1111 },
-
-    { "NIC400_MKIO_1_S_FN_MOD", REGPOKER_READ32, NIC400_MKIO_1_S_FN_MOD, 0x0,
-0b11 },
-    { "NIC400_MKIO_1_S_FN_MOD", REGPOKER_WRITE32, NIC400_MKIO_1_S_FN_MOD, 0x0,
-0b11 },
-
-    {}
+    { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0 << 3, 0b0, 0b1 },
+    { "KMBIST_CHAIN_SF_i", REGPOKER_WRITE_DCR, KMBIST_CHAIN_SF_0 << 3, 0b0, 0b1 },
+    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF << 4, 0b0, 0b1},
+    { "PPC_SYS_CONF", REGPOKER_WRITE_DCR, PPC_SYS_CONF << 4, 0b1, 0b1},
+    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF << 5, 0b0, 0b1},
+    { "PPC_SYS_CONF", REGPOKER_WRITE_DCR, PPC_SYS_CONF << 5, 0b1, 0b1},
     };
     if( rumboot_regpoker_check_array( check_array, base_addr ) == 0 ) {
            rumboot_printf( "OK\n" );
@@ -161,7 +52,7 @@ static int AXI_check_regs32(uint32_t base_addr)
     rumboot_printf( "ERROR\n" );
     return 1;
 }
-
+/*
 static int AXI_check_regs64(uint32_t base_addr)
 {
     rumboot_printf("Checking ID registers random value...\n");
@@ -224,21 +115,26 @@ static int AXI_check_regs64(uint32_t base_addr)
     rumboot_printf( "ERROR\n" );
     return 1;
 }
-
+*/
 int main(void)
 {
-   register int result;
+   register int result = 0;
    rumboot_printf("Check write/read ctrl register values\n");
 
-   result = AXIx_check_ID_regs(DCR_CTRL_BASE) ||
-           AXIx_check_ID_regs(AXI64HSIFS_CTRL_BASE) ||
-           AXI_check_regs32(AXI32HSIFS_CTRL_BASE) ||
-           AXI_check_regs64(AXI64HSIFS_CTRL_BASE);
+   result = dcr_check_ctrl_regs_ro(DCR_SCTL_BASE) ||
+   dcr_check_ctrl_regs_rw(DCR_SCTL_BASE);
+
+       //AXIx_check_ID_regs(DCR_CTRL_BASE) ||
+       //      AXIx_check_ID_regs(AXI64HSIFS_CTRL_BASE) ||
+       //      AXI_check_regs32(AXI32HSIFS_CTRL_BASE) ||
+       //      AXI_check_regs64(AXI64HSIFS_CTRL_BASE);
+
    if(!result)
    {
        rumboot_printf("Checked TEST_OK\n");
        return 0;
    }
+
    rumboot_printf("Checked TEST_ERROR\n");
    return result;
 }
