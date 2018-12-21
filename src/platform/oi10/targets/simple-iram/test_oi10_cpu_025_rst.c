a16510b28584c3258de02a69d7e25c01e8524d1e
Margarita Smolina <m.smolina@modulew.ru>
2018-12-21 14:10:42 +0300

    oi10_rumboot: fix test_oi10_cpu_025_rst.c

---
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_rst.c
@@ -0,0 +1,17 @@
+/*
+ * test_oi10_cpu_025_rst.c
+ *
+ *  Created on: Dec 21, 2018
+ *      Author: m.smolina
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <rumboot/printf.h>
+#include <rumboot/platform.h>
+#include <rumboot/macros.h>
+#include <platform/test_event_c.h>
+#include <rumboot/irq.h>
+#include <platform/arch/ppc/ppc_476fp_debug_fields.h>
+
