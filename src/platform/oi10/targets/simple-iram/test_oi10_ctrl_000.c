206e37451bdb2bbc95710285872880a38cdb5f60
Maksim Dubinkin <m.dubinkin@modulew.ru>
2018-12-21 09:29:30 +0300

    oi10_rumboot:test_oi10_ctrl_000.c - Fixed check ro/rw registers

--- src/platform/oi10/targets/simple-iram/test_oi10_ctrl_000.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_ctrl_000.c
@@ -4,13 +4,11 @@
 #include <string.h>
 #include <rumboot/printf.h>
 #include <rumboot/regpoker.h>
-#include <rumboot/testsuite.h>
 #include <rumboot/irq.h>
 #include <platform/interrupts.h>
 #include <platform/oi10/platform/devices.h>
 #include <platform/regs/regs_sctl.h>
 
-#ifdef CHECK_REGS
 static uint8_t check_ctrl_regs_ro(uint32_t base_addr)
 {   
     rumboot_printf("Checking sctl registers default value...\n");
@@ -39,27 +37,13 @@ static int check_ctrl_regs_rw(uint32_t base_addr)
     { "KMBIST_CHAIN_SF_I", REGPOKER_WRITE_DCR, KMBIST_CHAIN_SF_0, 0b000,   0b1 },
     { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0, 0b0000, 0b1111 },
     { "KMBIST_CHAIN_SF_i", REGPOKER_WRITE_DCR, KMBIST_CHAIN_SF_0, 0b000,   0b111 },
-
-    { "PPC_SYS_CONF", REGPOKER_WRITE_DCR, PPC_SYS_CONF, 0b00000, 0b11000 },
-    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0b1000, 0b1111 },
-    { "PPC_SYS_CONF", REGPOKER_READ_DCR,0b0000 & (4 << PPC_SYS_CONF), 0b0, 0b1 },
-    { "PPC_SYS_CONF", REGPOKER_WRITE_DCR, 0b0000 & (4 << PPC_SYS_CONF), 0b0, 0b1},
-    { "PPC_SYS_CONF", REGPOKER_READ_DCR, 0b00000 & (5 << PPC_SYS_CONF), 0b0, 0b1 },
-    { "PPC_SYS_CONF", REGPOKER_WRITE_DCR, 0b00000 &(5 << PPC_SYS_CONF), 0b0, 0b1},
-    { "PPC_SLP_CTRL", REGPOKER_READ_DCR, 0b00 & (2 << PPC_SLP_CTRL), 0b00, 0b11 },
-    { "PPC_SLP_CTRL", REGPOKER_WRITE_DCR, 0b00 & (2 << PPC_SLP_CTRL), 0b00, 0b11},
-    { "PPC_SLP_CTRL", REGPOKER_READ_DCR, 0b0 & (1 << PPC_SLP_CTRL), 0b00, 0b11 },
-    { "PPC_SLP_CTRL", REGPOKER_WRITE_DCR, 0b0 & (1 << PPC_SLP_CTRL), 0b00, 0b11 },
-    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, 0b0000 & (4 << PPC_SLP_CTRL), 0b0, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_WRITE_DCR, 0b0000 & (4 << PPC_SLP_CTRL), 0b1, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, 0b000 & (3 << PPC_SLP_CTRL), 0b0, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_WRITE_DCR, 0b000 & (3 << PPC_SLP_CTRL), 0b1, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, 0b00 & (2 << PPC_SLP_CTRL), 0b0, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_WRITE_DCR, 0b00 & (2 << PPC_SLP_CTRL), 0b1, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, 0b0 & (1 << PPC_SLP_CTRL), 0b0, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_WRITE_DCR, 0b0 & (1 << PPC_SLP_CTRL), 0b1, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, 0 << PPC_SLP_CTRL, 0b0, 0b1 },
-    { "PPC_PMU_CTRL", REGPOKER_WRITE_DCR, 0 << PPC_SLP_CTRL, 0b0, 0b1 },
+    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0b11000, 0b11111 },
+    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0b0, 0b1 },
+    { "PPC_SLP_CTRL", REGPOKER_READ_DCR, PPC_SLP_CTRL, 0b0000, 0b1111 },
+    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, PPC_PMU_CTRL, 0b00000, 0b11111 },
+    { "IFSYS_ETH_CTRL", REGPOKER_READ_DCR, IFSYS_ETH_CTRL, 0b0000, 0b1111 },
+    { "IFSYS_ETH_CTRL", REGPOKER_READ_DCR, IFSYS_ETH_CTRL, 0x10000, 0x11111 },
+    { "IFSYS_ETH_HPROT", REGPOKER_READ_DCR, IFSYS_ETH_HPROT, 0x33333333, 0xffffffff },
     {  }
     };
     if( rumboot_regpoker_check_array( check_array, base_addr ) == 0 ) {
@@ -69,19 +53,13 @@ static int check_ctrl_regs_rw(uint32_t base_addr)
     rumboot_printf( "ERROR\n" );
     return 1;
 }
-#endif
-
-TEST_SUITE_BEGIN(sctl_testlist, "SCTL TEST")
-#ifdef CHECK_REGS
-TEST_ENTRY("sctl ro", check_ctrl_regs_ro, (uint32_t, DCR_SCTL_BASE)),
-TEST_ENTRY("sctl rw", check_ctrl_regs_rw, (uint32_t, DCR_SCTL_BASE)),
-#endif
-TEST_SUITE_END();
 
 int main(void)
 {
    register int result;
    rumboot_printf("Check write/read ctrl register values\n");
-   result = test_suite_run(NULL, &sctl_testlist);
+
+   result = check_ctrl_regs_ro(DCR_SCTL_BASE)
+           || check_ctrl_regs_rw(DCR_SCTL_BASE);
    return (!result) ? rumboot_printf("CHECKED TEST_OK\n"), result: rumboot_printf("CHECKED TEST_ERROR\n"), result;
 }
