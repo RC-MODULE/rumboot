f5d9b388659b8e6651f239df07098e3211bcb00c
Chernousov Stepan <s.chernousov@modulew.ru>
2018-12-25 08:10:14 +0300

    oi10_rumgoot: cpu_mem_19 completed

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_019.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_019.c
@@ -36,8 +36,6 @@
 #include <platform/regs/regs_emi.h>
 #include <platform/regs/fields/emi.h>
 
-#define PRINT_READ_WRITE_L2ARRACC_INFO
-
 //0x00000000_00000000ULL - 0x00000001_FFFFFFFFULL - SRAM_SDRAM EM2
 
 #define L2C_SIZE        (256*1024)
@@ -54,9 +52,9 @@ const uint64_t test_phys_addr[ADDR_ARRAY_LEN] = {
     0x00FF00F80ULL,
     0x0CCCCCC80ULL,
     0x0AAAAAA80ULL,
-    0x000F0F080ULL,
+    0x010F0F080ULL,
     0x10F0F0F00ULL,
-    0x1000FF000ULL,
+    0x1B00FF000ULL,
     0x133333300ULL,
     0x155555500ULL,
     0x10FF00000ULL,
@@ -79,24 +77,23 @@ const uint32_t test_addr[ADDR_ARRAY_LEN] = {
 };
 
 /*                           MMU_TLB_ENTRY( ERPN, RPN,    EPN,    DSIZ,              IL1I,IL1D,W,I,M,G,E,                 UX,UW,UR,SX,SW,SR DULXE,IULXE,TS,TID,              WAY,              BID,             V )*/
-#define TLB_ENTRY0_CACHE_WB  MMU_TLB_ENTRY( 0x000,0x00000,0x00010,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY1_CACHE_WB  MMU_TLB_ENTRY( 0x000,0x000F0,0x00020,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY2_CACHE_WB  MMU_TLB_ENTRY( 0x000,0x0FF00,0x00030,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY3_CACHE_WB  MMU_TLB_ENTRY( 0x000,0xCCCC0,0x00040,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY4_CACHE_WB  MMU_TLB_ENTRY( 0x000,0xAAAA0,0x00050,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY5_CACHE_WB  MMU_TLB_ENTRY( 0x000,0x00F00,0x00060,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY6_CACHE_WB  MMU_TLB_ENTRY( 0x001,0x0F0F0,0x00070,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY7_CACHE_WB  MMU_TLB_ENTRY( 0x001,0x000F0,0x00080,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY8_CACHE_WB  MMU_TLB_ENTRY( 0x001,0x33330,0x00090,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY9_CACHE_WB  MMU_TLB_ENTRY( 0x001,0x55550,0x000A0,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY10_CACHE_WB MMU_TLB_ENTRY( 0x001,0x0FF00,0x000B0,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
-#define TLB_ENTRY11_CACHE_WB MMU_TLB_ENTRY( 0x001,0x0FFF0,0x000C0,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY0_CACHE_WT  MMU_TLB_ENTRY( 0x000,0x00000,0x00010,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY1_CACHE_WT  MMU_TLB_ENTRY( 0x000,0x000F0,0x00020,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY2_CACHE_WT  MMU_TLB_ENTRY( 0x000,0x0FF00,0x00030,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY3_CACHE_WT  MMU_TLB_ENTRY( 0x000,0xCCCC0,0x00040,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY4_CACHE_WT  MMU_TLB_ENTRY( 0x000,0xAAAA0,0x00050,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY5_CACHE_WT  MMU_TLB_ENTRY( 0x000,0x10F00,0x00060,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY6_CACHE_WT  MMU_TLB_ENTRY( 0x001,0x0F0F0,0x00070,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY7_CACHE_WT  MMU_TLB_ENTRY( 0x001,0xB00F0,0x00080,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY8_CACHE_WT  MMU_TLB_ENTRY( 0x001,0x33330,0x00090,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY9_CACHE_WT  MMU_TLB_ENTRY( 0x001,0x55550,0x000A0,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY10_CACHE_WT MMU_TLB_ENTRY( 0x001,0x0FF00,0x000B0,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
+#define TLB_ENTRY11_CACHE_WT MMU_TLB_ENTRY( 0x001,0x0FFF0,0x000C0,MMU_TLBE_DSIZ_64KB,1,   1,   1,0,1,1,MMU_TLBE_E_BIG_END,0, 0, 0, 1, 1, 1, 0,    0,    0, MEM_WINDOW_SHARED,MMU_TLBWE_WAY_UND,MMU_TLBWE_BE_UND,1 )
 
 #define CREATE_DWORD(addr,i) (((addr << 12) + i) | ( ( 0x000000000000BEEFULL + ( i << 16 ) ) << 32 ))
 
 volatile uint64_t *cashed_data;
-volatile uint64_t ref_data[DATA_LEN];
-volatile uint64_t l2c_data[DATA_LEN];
+volatile uint64_t ref_data[DATA_LEN] = {0};
 
 void check_read_write_via_l2carracc(int indx)
 {
@@ -105,9 +102,8 @@ void check_read_write_via_l2carracc(int indx)
 
     ext_phys_addr = (uint32_t)(test_phys_addr[indx] >> 32);
     phys_addr = (uint32_t)(test_phys_addr[indx] & 0xFFFFFFFF);
-    rumboot_printf("Check read/write via L2ARRACC* started (ext_phys_addr = 0x%x; phys_addr = 0x%x)\n",ext_phys_addr,phys_addr);
 
-    rumboot_printf("  Init data... ");
+    rumboot_printf("Init data... ");
 
     cashed_data = (volatile uint64_t *)test_addr[indx];
 
@@ -119,39 +115,61 @@ void check_read_write_via_l2carracc(int indx)
     msync();
     rumboot_printf("done\n");
 
-    rumboot_printf("  Start reading... ");
+    rumboot_printf("Start reading... ");
     for (i = 0; i < DATA_LEN; i++ )
     {
         ref_data[i] = cashed_data[i];
     }
     msync();
-    rumboot_printf("done\n");
+    rumboot_printf("done\n\n");
 
-    rumboot_printf("  Start reading via L2ARRACC*\n");
+    int32_t cache_way = -1;
+    rumboot_printf("l2c get way\n");
+    if (l2c_arracc_get_way_by_address( DCR_L2C_BASE, ext_phys_addr, phys_addr, &cache_way ) == false)
+    {
+        rumboot_printf("==> Error reading (way) via L2ARRACC* <==\n");
+        TEST_ASSERT(0, "TEST_ERROR");
+    }
+
+    rumboot_printf("Start reading via L2ARRACC*\n\n");
     for (i = 0; i < DATA_LEN; i++ )
     {
-        #ifdef PRINT_READ_WRITE_L2ARRACC_INFO
-            if(l2c_arracc_get_data_by_address(DCR_L2C_BASE,ext_phys_addr,phys_addr+(i<<3),&l2c_data[i]) == false)
-            {
-                rumboot_printf("==> Error reading via L2ARRACC* <==\n");
-                TEST_ASSERT(0, "TEST_ERROR");
-            }
-        #else
-            l2c_data[i] = l2c_arracc_data_read(DCR_L2C_BASE, ext_phys_addr,phys_addr+(i<<3));
-            if(i & 0x1F) rumboot_printf(".");
-            else rumboot_printf("\n  .");
-        #endif
-        rumboot_printf("\n");
-    }
-    msync();
-    rumboot_printf(" done\n\n");
+        uint32_t tag_data = 0;
+        uint64_t l2c_data = 0;
 
-    rumboot_printf("  Check data... ");
-    for (i = 0; i < DATA_LEN; i++)
-    {
-        TEST_ASSERT(l2c_data[i] == ref_data[i],"Data not equal!");
+        rumboot_printf("addr = %x_%x (i = %x)\n", ext_phys_addr, phys_addr+(i<<3), i);
+        rumboot_printf("cache way == %x\n", cache_way);
+
+        //**********************************************************************************
+
+        rumboot_printf("l2c get tag info\n");
+        if (l2c_arracc_tag_info_read_by_way (DCR_L2C_BASE, ext_phys_addr, phys_addr+(i<<3), cache_way, &tag_data) == false)
+        {
+            rumboot_printf("==> Error reading (tag) via L2ARRACC* <==\n");
+            TEST_ASSERT(0, "TEST_ERROR");
+        }
+        rumboot_printf("tag info == %x\n", tag_data);
+
+        rumboot_printf ("tag validation...");
+        uint32_t tmp_tag = (1 << 31) + (((ext_phys_addr << 16) + ((phys_addr) >> 16)) << 3);
+        TEST_ASSERT(tag_data == tmp_tag,"TEST ERROR: invalid tag");
+        rumboot_printf("done\n");
+
+        //**********************************************************************************
+        rumboot_printf("l2c get data\n");
+        if(l2c_arracc_data_read_by_way(DCR_L2C_BASE, ext_phys_addr, phys_addr+(i<<3), cache_way, &l2c_data) == false)
+        {
+            rumboot_printf("==> Error reading (data) via L2ARRACC* <==\n");
+            TEST_ASSERT(0, "TEST_ERROR");
+        }
+        msync();
+        rumboot_printf("data_h == %x\n", (uint32_t) (l2c_data >> 32));
+        rumboot_printf("data_l == %x\n", (uint32_t) l2c_data);
+
+        rumboot_printf ("data validation...");
+        TEST_ASSERT(l2c_data == ref_data[i],"TEST ERROR: invalid tag");
+        rumboot_printf("done\n\n");
     }
-    rumboot_printf("done\n");
 }
 
 int main(void)
@@ -160,9 +178,9 @@ int main(void)
     volatile uint32_t data;
 
     static const tlb_entry tlb_entry[ADDR_ARRAY_LEN] =
-       {{TLB_ENTRY0_CACHE_WB}, {TLB_ENTRY1_CACHE_WB}, {TLB_ENTRY2_CACHE_WB}, {TLB_ENTRY3_CACHE_WB},
-        {TLB_ENTRY4_CACHE_WB}, {TLB_ENTRY5_CACHE_WB}, {TLB_ENTRY6_CACHE_WB}, {TLB_ENTRY7_CACHE_WB},
-        {TLB_ENTRY8_CACHE_WB}, {TLB_ENTRY9_CACHE_WB}, {TLB_ENTRY10_CACHE_WB},{TLB_ENTRY11_CACHE_WB}};
+       {{TLB_ENTRY0_CACHE_WT}, {TLB_ENTRY1_CACHE_WT}, {TLB_ENTRY2_CACHE_WT}, {TLB_ENTRY3_CACHE_WT},
+        {TLB_ENTRY4_CACHE_WT}, {TLB_ENTRY5_CACHE_WT}, {TLB_ENTRY6_CACHE_WT}, {TLB_ENTRY7_CACHE_WT},
+        {TLB_ENTRY8_CACHE_WT}, {TLB_ENTRY9_CACHE_WT}, {TLB_ENTRY10_CACHE_WT},{TLB_ENTRY11_CACHE_WT}};
 
     emi_init(DCR_EM2_EMI_BASE);
 
