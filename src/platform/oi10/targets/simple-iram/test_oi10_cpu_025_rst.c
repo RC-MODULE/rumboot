80311261f8887bc136c89ff9f624bbcdcb5a378a
Margarita Smolina <m.smolina@modulew.ru>
2019-01-14 14:06:18 +0300

    oi10_rumboot:update test_oi10_cpu_025_rst.c

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_rst.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_rst.c
@@ -13,32 +13,22 @@
 #include <rumboot/macros.h>
 #include <platform/test_event_c.h>
 #include <rumboot/irq.h>
-#include <platform/arch/ppc/ppc_476fp_debug_fields.h>
 #include <platform/trace.h>
 #include <platform/test_assert.h>
 #include <platform/devices.h>
 #include <platform/interrupts.h>
+#include <platform/arch/ppc/ppc_476fp_config.h>
+#include <platform/arch/ppc/ppc_476fp_lib_c.h>
 #include <platform/arch/ppc/ppc_476fp_debug_fields.h>
 #include <platform/arch/ppc/ppc_476fp_itrpt_fields.h>
 #include <platform/arch/ppc/ppc_476fp_ctrl_fields.h>
 
-enum {
-DECLARE_ENUM_VAL( MCSR_L2_e,         42 )
-DECLARE_ENUM_VAL( MCSR_L2_n,         1 )
-DECLARE_ENUM_VAL( MCSR_L2_i,         IBM_BIT_INDEX( 64, MCSR_L2_e ) )
-} MCSR_FIELD;
-
-enum {
+BEGIN_ENUM( RST_REQ_TYPE )
 DECLARE_ENUM_VAL (RESET_REQ_TYPE_NONE,   0b00)
 DECLARE_ENUM_VAL (RESET_REQ_TYPE_CORE,   0b01)
 DECLARE_ENUM_VAL (RESET_REQ_TYPE_CHIP,   0b10)
 DECLARE_ENUM_VAL (RESET_REQ_TYPE_SYSTEM, 0b11)
-} RST_REQ_TYPE;
-
-static volatile uint32_t RESET_REQ_TYPE __attribute__((section(".data"))) = RESET_REQ_TYPE_CORE;
-static volatile uint32_t NXT_RESET_REQ_TYPE __attribute__((section(".data"))) = RESET_REQ_TYPE_CORE;
-static volatile bool MC_HANDLED;
-
+END_ENUM( RST_REQ_TYPE );
 
 
 enum {
@@ -48,6 +38,20 @@ enum {
 enum {
     TEST_DATA_UNDEF     = 0x8E591794
 } test_data;
+
+
+#define EVENT_RESET                        TEST_EVENT_CODE_MIN + 0
+#define EVENT_FINISHED                     TEST_EVENT_CODE_MIN + 1
+
+static volatile uint32_t RESET_REQ_TYPE __attribute__((section(".data"))) = RESET_REQ_TYPE_CORE;
+static volatile uint32_t NXT_RESET_REQ_TYPE __attribute__((section(".data"))) = RESET_REQ_TYPE_CORE;
+static volatile bool MC_HANDLED;
+
+//enum {
+//DECLARE_ENUM_VAL( MCSR_L2_e,         42 )
+//DECLARE_ENUM_VAL( MCSR_L2_n,         1 )
+//DECLARE_ENUM_VAL( MCSR_L2_i,         IBM_BIT_INDEX( 64, MCSR_L2_e ) )
+//} MCSR_FIELD;
 
 volatile static uint32_t mc_cnt = 0;
 
@@ -76,16 +80,22 @@ void update_reset_req_type ()
 //    trace_hex(NXT_RESET_REQ_TYPE);
 }
 
+static void reset_system (RST_REQ_TYPE dbcr_rst_type)
+{
+    uint32_t dbcr0 =  spr_read (SPR_DBCR0);
+    CLEAR_BITS_BY_MASK(dbcr0, 0b11 << DEBUG_DBCR0_RST_i);
+    spr_write(SPR_DBCR0, SET_BITS_BY_MASK(dbcr0, dbcr_rst_type << DEBUG_DBCR0_RST_i));
+}
+
 static void exception_handler(int id, const char *name)
 {
     rumboot_printf("\nWE GOT AN EXCEPTION: %d: %s\n", id, name);
     if(id == RUMBOOT_IRQ_MACHINE_CHECK)
     {
         rumboot_printf("It is OK!\n");
-        rumboot_printf("MSR:  0x%x\n", msr_read());
-        rumboot_printf("MCSR: 0x%x\n", spr_read(SPR_MCSR_C));
-        rumboot_printf("MCSRR0: 0x%x\n", spr_read(SPR_MCSRR0));
-        rumboot_printf("MCSRR1: 0x%x\n", spr_read(SPR_MCSRR1));
+        test_event(EVENT_RESET);
+        update_reset_req_type ();
+        reset_system(RESET_REQ_TYPE);
 	    mc_cnt += 1;
 	    spr_write(SPR_MCSR_C,0xFFFFFFFF);
     }
@@ -122,7 +132,7 @@ int check_machinecheck ()
     rumboot_irq_set_exception_handler(exception_handler);
 
 	rumboot_printf("send TEC_CHECK_DEBUG_MACHINECHECK\n");
-	test_event(TEC_CHECK_DEBUG_MACHINECHECK );
+	//test_event( );
 
 	//Check C470S_DBGMACHINECHECK via ESR[ISMC]
 	spr_write(SPR_ESR,0x80000000);
@@ -176,6 +186,8 @@ int check_machinecheck ()
 
 int main ()
 {
+    test_event_send_test_id( "test_oi10_cpu_025_rst");
+
     rumboot_printf("TEST START\n");
 
     MC_HANDLED = false;
@@ -183,13 +195,13 @@ int main ()
     if(RESET_REQ_TYPE == RESET_REQ_TYPE_NONE)
     {
         rumboot_printf("Reset sequence done\n");
-        return 1;
+        return 0;
     }
     check_machinecheck ();
 
 
 
-    rumboot_printf("TEST OK\n");
+    rumboot_printf("TEST ERROR\n");
     return 0;
 }
 
