443ef77dd6bba8124070fe481cf22ca5033715cb
Chernousov Stepan <s.chernousov@modulew.ru>
2019-01-22 09:34:37 +0300

    oi10_rumboot: test 021 wb cache size upd

--- src/platform/oi10/targets/test_oi10_cpu_021_wb_cache_size.c
+++ src/platform/oi10/targets/test_oi10_cpu_021_wb_cache_size.c
@@ -30,12 +30,70 @@
 #define PTRN_INC_1 0x1
 #define PTRN_INC_2 0x2
 
-#define START_ADDR          (SRAM0_BASE + 0x10000)
+#define START_ADDR          (SRAM0_BASE + 0x8000)
 #define DATA_SIZE           0x40000  //256KB
 #define NUM_WORDS           (DATA_SIZE/4)
-#define NUM_DWORDS          (NUM_WORDS/2)
 #define L2C_LINE_SIZE       128
-#define GET_EXP_DATA(a, i)  (((a >> 2) + 1) * i)
+#define GET_EXP_DATA(addr, inc)  ( ((addr - START_ADDR) >> 2)  * inc )
+
+                                    //MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,  IL1D,   W,     I,     M,     G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,               V   )
+#define TLB_ENTRY_CACHE_WB_VALID      MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,   0b0,    0b0,   0b0,   0b1,   0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,  0b1 )
+
+static bool read_and_check (uint32_t base_addr, uint32_t size_b, uint32_t data_inc)
+{
+    for (uint32_t i = 0; i < (size_b/sizeof(uint64_t)); i++)
+    {
+        rumboot_printf ("i = %x, ", i);
+        uint32_t addr = base_addr + 8*i;
+        uint64_t rdata = ioread64(addr);
+        rumboot_printf ("addr = %x\n", addr);
+
+        if ( (((uint32_t)(rdata >> 32)) != GET_EXP_DATA(addr, data_inc)) || ( ((uint32_t)(rdata & 0xFFFFFFFF)) != GET_EXP_DATA((addr+0x4), data_inc)) )
+        {
+            rumboot_printf ("ERROR: invalid read data\n");
+            rumboot_printf("expected:\n");
+            rumboot_printf("data_h = %x\n", (uint32_t) GET_EXP_DATA(addr, data_inc));
+            rumboot_printf("data_l = %x\n", (uint32_t) GET_EXP_DATA((addr+0x4), data_inc) );
+            rumboot_printf("actual:\n");
+            rumboot_printf("data_h = %x\n", (uint32_t)(rdata >> 32));
+            rumboot_printf("data_l = %x\n\n", (uint32_t) rdata );
+
+            return false;
+        }
+        msync(); isync();
+
+        //l2c get way
+        int32_t cache_way = -1;
+        uint64_t phys_addr = rumboot_virt_to_phys((void *) addr);
+        if (l2c_arracc_get_way_by_address( DCR_L2C_BASE, (uint32_t) (phys_addr >> 32), (uint32_t) (phys_addr), &cache_way ) == false)
+        {
+            rumboot_printf("==> Error reading (way) via L2ARRACC* <==\n");
+            return false;
+        }
+
+        //l2c get data
+        uint64_t l2c_data = 0;
+        if(l2c_arracc_data_read_by_way(DCR_L2C_BASE, (uint32_t) (phys_addr >> 32), (uint32_t) (phys_addr), cache_way, &l2c_data) == false)
+        {
+            rumboot_printf("==> Error reading (data) via L2ARRACC* <==\n");
+            return false;
+        }
+
+        if (l2c_data != rdata)
+        {
+            rumboot_printf("ERROR: invalid cache data\n");
+            rumboot_printf("expected:\n");
+            rumboot_printf("data_h = %x\n", (uint32_t)(rdata >> 32));
+            rumboot_printf("data_l = %x\n", (uint32_t) rdata );
+            rumboot_printf("actual:\n");
+            rumboot_printf("l2c_data_h = %x\n", (uint32_t)(l2c_data >> 32));
+            rumboot_printf("l2c_data_l = %x\n\n", (uint32_t) l2c_data );
+
+            //return false;
+        }
+    }
+    return true;
+}
 
 bool __attribute__((section(".text.test"))) cache_testing_function()
 {
@@ -45,45 +103,65 @@ bool __attribute__((section(".text.test"))) cache_testing_function()
     rumboot_memfill32( (void*)START_ADDR, NUM_WORDS + (L2C_LINE_SIZE/4), 0x00, PTRN_INC_1);
     msync();
 
-    rumboot_printf ("2. Read and check first 16 dwords\n");
-    for (uint8_t i = 0; i < (L2C_LINE_SIZE/sizeof(uint64_t)); i++) //L2C_LINE_SIZE/sizeof(uint64_t) == 16
-    {
-        rumboot_printf ("i = %x\n", i);
-        uint32_t addr = START_ADDR + 8*i;
-        uint64_t rdata = ioread64(addr);
-        msync();
+    rumboot_printf ("Set tlb (WB)\n");
+    static const tlb_entry sram0_tlb_entry_cacheable_valid = {TLB_ENTRY_CACHE_WB_VALID};
+    write_tlb_entries(&sram0_tlb_entry_cacheable_valid, 1);
+    dci(2); dci(0);
 
-        if ( ((rdata << 32) == GET_EXP_DATA(addr, PTRN_INC_1)) && ((rdata & 0xFFFFFFFF) == GET_EXP_DATA((addr+0x4), PTRN_INC_1)) )
-        {
-            rumboot_printf ("ERROR: invalid read data");
-            return false;
-        }
+    rumboot_printf ("2. Read and check L2C Line Size\n");
+    if ( !read_and_check (START_ADDR, L2C_LINE_SIZE, PTRN_INC_1)) return false;
 
-        //l2c get way
-        int32_t cache_way = -1;
-        uint64_t phys_addr = rumboot_virt_to_phys((void *) addr);
-        if (l2c_arracc_get_way_by_address( DCR_L2C_BASE, phys_addr >> 32, phys_addr & 0xFFFFFFFF, &cache_way ) == false)
-        {
-            rumboot_printf("==> Error reading (way) via L2ARRACC* <==\n");
-            return false;
-        }
+    rumboot_printf ("3. Read and check L2C Line Size (check L2C.O_M_ADDR)\n");
+    //test_event(EVENT_START_CHECK_O_M_ADDR);
+    if ( !read_and_check (START_ADDR, L2C_LINE_SIZE, PTRN_INC_1)) return false;
+    //test_event(EVENT_END_CHECK_O_M_ADDR);
 
-        //l2c get data
-        uint64_t l2c_data = 0;
-        if(l2c_arracc_data_read_by_way(DCR_L2C_BASE, phys_addr >> 32, phys_addr & 0xFFFFFFFF, cache_way, &l2c_data) == false)
-        {
-            rumboot_printf("==> Error reading (data) via L2ARRACC* <==\n");
-            return false;
-        }
+    rumboot_printf ("4. Rewrite L2C Line Size (pattern 2)\n");
+    for (uint32_t i = 0, d = 0x00; i < (L2C_LINE_SIZE/4); i++, d += PTRN_INC_2)
+        iowrite32(d, START_ADDR + 4*i);
+    msync();
 
-        if (l2c_data != rdata)
-        {
-            rumboot_printf("ERROR: invalid cache data\n");
-            rumboot_printf("l2c_data_h = %x\n", l2c_data << 32);
-            rumboot_printf("l2c_data_l = %x\n", l2c_data & 0xFFFFFFFF);
-            return false;
-        }
-    }
+    rumboot_printf ("5. Read and check L2C Line Size (check L2C.O_M_ADDR)\n");
+    //test_event(EVENT_START_CHECK_O_M_ADDR);
+    if ( !read_and_check (START_ADDR, L2C_LINE_SIZE, PTRN_INC_2)) return false;
+    //test_event(EVENT_END_CHECK_O_M_ADDR);
+
+    rumboot_printf ("6. Rewrite L2C Line Size (pattern 1)\n");
+    for (uint32_t i = 0, d = 0x00; i < (L2C_LINE_SIZE/4); i++, d += PTRN_INC_1)
+        iowrite32(d, START_ADDR + 4*i);
+    msync();
+
+    rumboot_printf ("7. Read and check mem, size = 0x%x\n", DATA_SIZE);
+    if ( !read_and_check (START_ADDR, DATA_SIZE, PTRN_INC_1)) return false;
+
+    rumboot_printf ("8. Read and check mem, size = 0x%x (check L2C.O_M_ADDR)\n", DATA_SIZE);
+    //test_event(EVENT_START_CHECK_O_M_ADDR);
+    if ( !read_and_check (START_ADDR, DATA_SIZE, PTRN_INC_1)) return false;
+    //test_event(EVENT_END_CHECK_O_M_ADDR);
+
+    rumboot_printf ("9. Rewrite mem (pattern 2)\n");
+    for (uint32_t i = 0, d = 0x00; i < NUM_WORDS; i++, d += PTRN_INC_2)
+        iowrite32(d, START_ADDR + 4*i);
+    msync();
+
+    rumboot_printf ("10. Read and check mem, size = 0x%x (check L2C.O_M_ADDR)\n", DATA_SIZE);
+    //test_event(EVENT_START_CHECK_O_M_ADDR);
+    if ( !read_and_check (START_ADDR, DATA_SIZE, PTRN_INC_2)) return false;
+    //test_event(EVENT_END_CHECK_O_M_ADDR);
+
+    rumboot_printf ("11. Read and check new L2C Line Size\n");
+    if ( !read_and_check ((START_ADDR + DATA_SIZE), L2C_LINE_SIZE, PTRN_INC_1)) return false;
+
+    rumboot_printf ("12. Rewrite new L2C Line Size\n");
+    for (uint32_t i = 0, d = GET_EXP_DATA((START_ADDR + DATA_SIZE), PTRN_INC_2); i < (L2C_LINE_SIZE/4); i++, d += PTRN_INC_2)
+        iowrite32(d, (START_ADDR + DATA_SIZE) + 4*i);
+    msync();
+
+    rumboot_printf ("13. Read and check new L2C Line Size\n");
+    //test_event(EVENT_START_CHECK_O_M_ADDR);
+    if ( !read_and_check ((START_ADDR + DATA_SIZE), L2C_LINE_SIZE, PTRN_INC_2)) return false;
+    //test_event(EVENT_END_CHECK_O_M_ADDR);
+
 
     rumboot_printf ("Testing function completed\n");
     return true;
--- src/platform/oi10/targets/test_oi10_cpu_021_wb_cache_size_base.c
+++ src/platform/oi10/targets/test_oi10_cpu_021_wb_cache_size_base.c
@@ -14,8 +14,6 @@
 #include <platform/devices/emi.h>
 #include <rumboot/memfill.h>
 
-                                    //MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,  IL1D,   W,     I,     M,     G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,               V   )
-#define TLB_ENTRY_CACHE_WB_VALID      MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,   0b0,    0b0,   0b0,   0b1,   0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,  0b1 )
 #define M_BASE SRAM0_BASE
 
 typedef bool func();
@@ -25,14 +23,10 @@ int main(void)
     rumboot_printf ("EMI init\n");
     emi_init(DCR_EM2_EMI_BASE);
 
-    rumboot_memfill8_modelling( (void*)M_BASE, 0x10000, 0x00, 0x00 ); //anti X fix
+    rumboot_memfill8_modelling( (void*)M_BASE, 0x8000, 0x00, 0x00 ); //anti X fix
 
     rumboot_printf ("Load cache test function in 0x%x\n", M_BASE);
     rumboot_platform_request_file("MBIN", M_BASE); //M_BASE defined in cmake
-
-    rumboot_printf ("Set tlb (WB)\n");
-    static const tlb_entry sram0_tlb_entry_cacheable_valid = {TLB_ENTRY_CACHE_WB_VALID};
-    write_tlb_entries(&sram0_tlb_entry_cacheable_valid, 1);
 
     rumboot_printf("Call cache test function\n");
     func* test_func = (func*)(M_BASE);
