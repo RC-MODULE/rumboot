8945320f5ad99c5859119f8f9d022f21f6c3e518
Andrew Andrianov <andrew@ncrmnt.org>
2019-01-23 15:41:49 +0300

    bootrom: unit: Properly init uart for production tests (exception.c)
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/common/bootrom/exception.c
+++ src/platform/common/bootrom/exception.c
@@ -11,11 +11,11 @@
 
 int main()
 {
-        rumboot_printf("Hello, I will check exception handling.\n");
         rumboot_irq_set_exception_handler(rumboot_arch_exception);
         struct rumboot_config conf;
         rumboot_platform_read_config(&conf);
         rumboot_platform_init_loader(&conf);
+        rumboot_printf("Hello, I will check exception handling.\n");
 
 #if defined(__PPC__)
         asm ("sc");
