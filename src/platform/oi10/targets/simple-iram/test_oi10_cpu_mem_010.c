990fb25430307425898478c8c2f076f00fb5c2ec
Chernousov Stepan <s.chernousov@modulew.ru>
2019-01-25 08:31:13 +0300

    oi10_rumboot: test cpu mem 010 upd

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_010.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_010.c
@@ -27,46 +27,36 @@
 #include <platform/devices/emi.h>
 #include <platform/regs/regs_emi.h>
 #include <platform/regs/fields/emi.h>
+#include <platform/devices/nor_1636RR4.h>
 
-//                                        MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
-#define TLB_ENTRY_CACHE_VALID_0           MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
-#define TLB_ENTRY_CACHE_VALID_1           MMU_TLB_ENTRY(  0x000,  0x40000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+//                                  MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
+#define TLB_ENTRY_CACHE_VALID_0     MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_CACHE_VALID_1     MMU_TLB_ENTRY(  0x000,  0x40000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_CACHE_VALID_2     MMU_TLB_ENTRY(  0x001,  0x80000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_3,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_CACHE_VALID_3     MMU_TLB_ENTRY(  0x001,  0xC0000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_3,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 
+#define TLB_ENTRY_VALID_0           MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_VALID_1           MMU_TLB_ENTRY(  0x000,  0x40000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_VALID_2           MMU_TLB_ENTRY(  0x001,  0x80000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_3,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+#define TLB_ENTRY_VALID_3           MMU_TLB_ENTRY(  0x001,  0xC0000,    0x40000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_3,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
+
+#define DATA_CACHE_VAL 0x1234ABCD
 
 //tag array
 uint32_t test_tag_array[] =
 {
-    //sram
-    0x00,
-    0x04,
-    0x08,
-    0x10,
-    0x20,
-    //sdram
-    0x10000,
-    0x10040,
-    0x10080,
-    0x10100,
-    0x10200,
-    0x10400,
-    0x10800,
-    0x10FFC,
-    //ssram
-    0x10000,
-    //pipe
-    0x28000,
-    //sram1
-    0x30000,
-};
-uint32_t LEN_TAG_ARR = sizeof(test_tag_array) / sizeof (uint32_t);
-
-//data array
-uint64_t test_data_array[] =
-{
-    0x1234ABCDFFFFFFFF,
-    0x1111111122222222,
-    0x3333333344444444,
-    0x5555555566666666
+    0b1111111111111111 << 2,
+    0b0000000000000000 << 2,
+    0b0011111111111111 << 2,
+    0b1100000000000000 << 2,
+    0b0011111111000000 << 2,
+    0b1100000000111111 << 2,
+    0b0011110000111100 << 2,
+    0b1100001111000011 << 2,
+    0b0011001100110011 << 2,
+    0b1100110011001100 << 2,
+    0b1010101010101010 << 2,
+    0b0101010101010101 << 2
 };
 
 #define REVERSE8(X) \
@@ -86,72 +76,124 @@ uint64_t test_data_array[] =
  * erpn - 0 or 1 for em0
  * tag - 0..0x7FFFF
  * erpn_tag:
- * [28:19] = ERPN, (may be 0 or 1 only for EM0, see tlb entries)
+ * [28:19] = ERPN, (may be 0 or 1 only for EM2, see tlb entries)
  * [18:0] = TAG
  * [31:29] = 0
  */
-uint32_t calc_addr (uint32_t tag, uint32_t index)
+static uint32_t calc_addr (uint32_t tag, uint32_t index)
 {
     return (0x00 | ((tag)<<13) | ((index)<<5) | (0<<2));
 }
 
-void CACHE_DATA_LINE_BY_TAG_INDEX (uint32_t TAG, uint32_t INDEX)
+static void init_block(uint8_t way, uint32_t base_ind)
 {
-    uint32_t addr = calc_addr(TAG, INDEX);
-    rumboot_printf("Address = 0x%x (tag = 0x%x)\n", addr, TAG );
-    msync();
-    /*Minimal caching size is 1 line = 32 bytes = 8 words = 4 doublewords*/
-    for (int i = 0; i < 4; i++)
-        iowrite64(test_data_array[i], addr + i*8 );
-    msync();
-    /*1st read initiates a caching*/
-    TEST_ASSERT(ioread32(addr) == ioread32((uint32_t)test_data_array), "Compare failed");
-
-
-    rumboot_printf("dcread...\n");
-    msync();
-    uint32_t dcread_data = dcread (addr & 0x7FFC);
-    isync();
-    TEST_ASSERT(dcread_data == ioread32((uint32_t)test_data_array), "Compare failed");
-    uint32_t reg = spr_read(SPR_DCDBTRH);
-    rumboot_printf("SPR_DCDBTRH = %x, TAG = %x\n\n",reg,  reg >> 13);
-    TEST_ASSERT((reg >> 13) == TAG, "TEST_ERROR: wrong tag in SPR_DCDBTRH");
-}
-
-void cache_data(uint32_t tag, uint32_t addr)
-{
-    uint32_t index = TAG_A_TO_INDEX(addr);
-    if((index & 1) && (index < 128)) index+=128;
-    if( (!(index & 1)) && (index >=128) ) index -= 128;
-
-    CACHE_DATA_LINE_BY_TAG_INDEX(tag,(index));
-}
-
-void check_dcu_tag()
-{
-    rumboot_printf("\nCheck_dcu_tag\n");
-    uint32_t i = 0;
-    for (i = 0; i < LEN_TAG_ARR; i++)
+    for (uint32_t i = 0, ind = base_ind; i < ARRAY_SIZE(test_tag_array); i++, ind++ )
     {
-        cache_data(test_tag_array[i], i);
+        uint32_t tag = test_tag_array[i] | way;
+        uint32_t index = TAG_A_TO_INDEX(ind);
+
+        uint32_t addr = calc_addr(tag, index);
+
+        if (addr >= NOR_BASE)
+        {
+            nor_write32(DATA_CACHE_VAL, addr);
+            for (uint32_t j = 1; j < 32; j++)
+                nor_write32(0x00, addr + 4*j);
+        }
+        else
+        {
+            iowrite32(DATA_CACHE_VAL, addr);
+            for (uint32_t j = 1; j < 32; j++)
+                iowrite32(0x00, addr + 4*j);
+
+        }
+        msync();
     }
+}
+
+static void mem_init(void) {
+    init_block(0b00, 0x00);
+    init_block(0b01, 0x80);
+    set_mem_window(MEM_WINDOW_3);
+    init_block(0b10, 0x00);
+    init_block(0b11, 0x80);
+    set_mem_window(MEM_WINDOW_0);
+}
+
+
+void cache_dcu_tag(uint8_t way, uint32_t base_ind)
+{
+    for (uint32_t i = 0, ind = base_ind; i < ARRAY_SIZE(test_tag_array); i++, ind++)
+    {
+        uint32_t tag = test_tag_array[i] | way;
+        uint32_t index = TAG_A_TO_INDEX(ind);
+
+        uint32_t addr = calc_addr(tag, index);
+        uint64_t phys_addr = rumboot_virt_to_phys((void*)addr);
+
+        rumboot_printf("TAG = %x\n", ((uint32_t)phys_addr) >> 13);
+        rumboot_printf("address = %x_%x\n", (uint32_t)(phys_addr >> 32), (uint32_t)phys_addr);
+
+        /*1st read initiates a caching*/
+        rumboot_printf("read...\n");
+        uint32_t read_data = ioread32(addr);
+        msync();
+
+        TEST_ASSERT(read_data == DATA_CACHE_VAL, "Compare failed");
+
+        rumboot_printf("dcread...\n");
+        uint32_t dcread_data = dcread ((uint32_t) phys_addr);
+        isync();
+        TEST_ASSERT(dcread_data == DATA_CACHE_VAL, "Compare failed");
+        uint32_t reg = spr_read(SPR_DCDBTRH);
+        rumboot_printf("SPR_DCDBTRH = %x, TAG = %x\n\n", reg,  reg >> 13);
+        TEST_ASSERT((reg >> 13) == (((uint32_t)phys_addr) >> 13), "TEST_ERROR: wrong tag in SPR_DCDBTRH");
+        TEST_ASSERT( ((uint32_t) (phys_addr >> 32)) == (reg & 1), "ERROR: Ext addr value in SPR_DCDBTRH" );
+    }
+}
+
+static bool test_dcu_tag_array()
+{
+    rumboot_printf("Start test dcu tag array\n");
+    cache_dcu_tag(0b00, 0x00);
+    cache_dcu_tag(0b01, 0x80);
+    set_mem_window(MEM_WINDOW_3);
+    cache_dcu_tag(0b10, 0x00);
+    cache_dcu_tag(0b11, 0x80);
+    set_mem_window(MEM_WINDOW_0);
+
+    return true;
 }
 
 int main()
 {
-     rumboot_printf("Start test\n");
-     rumboot_printf("EMI init\n");
-     emi_init(DCR_EM2_EMI_BASE);
+    static const tlb_entry tlb_entry_valid[] = {
+                    {TLB_ENTRY_VALID_0}, {TLB_ENTRY_VALID_1}, {TLB_ENTRY_VALID_2}, {TLB_ENTRY_VALID_3}
+        };
+    write_tlb_entries(tlb_entry_valid, ARRAY_SIZE(tlb_entry_valid));
 
-     rumboot_printf("cache on\n");
-     static const tlb_entry tlb_entry_cacheable_valid_0 = {TLB_ENTRY_CACHE_VALID_0};
-     write_tlb_entries(&tlb_entry_cacheable_valid_0, 1);
+    emi_init(DCR_EM2_EMI_BASE);
 
-     static const tlb_entry tlb_entry_cacheable_valid_1 = {TLB_ENTRY_CACHE_VALID_1};
-     write_tlb_entries(&tlb_entry_cacheable_valid_1, 1);
+    rumboot_memfill8_modelling ((void*)0x00, 0x10000, 0x00, 0x00); //anti x
 
-     check_dcu_tag();
-     rumboot_printf("TEST OK\n");
-     return 0;
+    rumboot_printf("Init mem\n");
+    mem_init();
+    rumboot_printf("Init mem done\n");
+
+    rumboot_printf("Set tlb\n");
+    static const tlb_entry tlb_entry_cacheable_valid[] = {
+            {TLB_ENTRY_CACHE_VALID_0}, {TLB_ENTRY_CACHE_VALID_1}, {TLB_ENTRY_CACHE_VALID_2}, {TLB_ENTRY_CACHE_VALID_3}
+    };
+    write_tlb_entries(tlb_entry_cacheable_valid, ARRAY_SIZE(tlb_entry_cacheable_valid));
+
+    if (!test_dcu_tag_array())
+     {
+         rumboot_printf("TEST_ERROR\n");
+         return 1;
+     }
+
+    rumboot_printf("TEST_OK\n");
+    return 0;
+
 }
 
