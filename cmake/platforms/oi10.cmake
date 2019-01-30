0a7ec54e65c768eaca0c604a70fa13ee582fc8c7
Chernousov Stepan <s.chernousov@modulew.ru>
2019-01-30 11:32:24 +0300

    oi10_rumboot: added ppc_476fp_power_modes lib

--- cmake/platforms/oi10.cmake
+++ cmake/platforms/oi10.cmake
@@ -27,6 +27,7 @@ file(GLOB PLATFORM_SOURCES
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/lib/drivers/sp805.c
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/ppc_mmu_impl.S
     ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/utlb_entries.S
+    ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/ppc_476fp_power_modes.S
 )
 
 #Flags for Power PC
