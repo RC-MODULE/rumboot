715684f6969f12f50f1445f8f5a1eec37a70f889
Rishat Galiulin <r.galiulin@modulew.ru>
2019-02-05 03:47:25 +0300

    oi10_rumboot: test_oi10_cpu_021_wb_cache_size - Fix includes

--- src/platform/oi10/targets/test_oi10_cpu_021_wb_cache_size.c
+++ src/platform/oi10/targets/test_oi10_cpu_021_wb_cache_size.c
@@ -19,7 +19,7 @@
 #include <platform/devices/l2c.h>
 #include <platform/trace.h>
 #include <platform/test_event_c.h>
-#include <platform/arch/ppc/ppc_476fp_lib_c.h>
+#include <arch/ppc_476fp_lib_c.h>
 #include <platform/arch/ppc/ppc_476fp_mmu_fields.h>
 #include <platform/arch/ppc/ppc_476fp_mmu.h>
 #include <platform/ppc470s/mmu/mem_window.h>
