69098bc570b840ba01b63abec5782e3099e545ee
Margarita Smolina <m.smolina@modulew.ru>
2019-02-11 13:20:52 +0300

    oi10_rumboot: test_oi10_cpu_025_ppc.c update check

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_ppc.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_ppc.c
@@ -8,11 +8,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+
 #include <rumboot/printf.h>
 #include <rumboot/platform.h>
 #include <rumboot/macros.h>
-#include <platform/test_event_c.h>
 #include <rumboot/irq.h>
+#include <rumboot/rumboot.h>
+
+#include <platform/test_event_c.h>
 #include <platform/trace.h>
 #include <platform/test_assert.h>
 #include <platform/devices.h>
@@ -30,7 +33,7 @@
 #define TLB_ENTRY_EM0_0_NOCACHE_VALID       MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,  MMU_TLBWE_BE_UND,   0b1 )
 #define TLB_ENTRY_EM0_0_NOCACHE_INVALID     MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,  MMU_TLBWE_BE_UND,   0b0 )
 #define TLB_ENTRY_EM0_0_CACHE_VALID         MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b1,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,  MMU_TLBWE_BE_UND,   0b1 )
-#define TLB_ENTRY_BOOTROM_MIRROR_0          MMU_TLB_ENTRY(  0x01F,  0xFFFF0,    0xFFFF0,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b0,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_0,  MMU_TLBWE_BE_0,     0b1 )
+#define TLB_ENTRY_BOOTROM_MIRROR_0          MMU_TLB_ENTRY(  0x01F,  0xFFFF0,    0xFFFF0,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b0,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_0,  MMU_TLBWE_BE_0,     0b1 )
 //                                  .long   MMU_TLB_ENTRY(  0x01F,  0xFFFF0,    0xFFFF0,    MMU_TLBE_DSIZ_64KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b0,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_0,    MMU_TLBWE_BE_0,     0b1 )
 //                                  .long   MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )
 
@@ -38,41 +41,87 @@
 #define UNEXIST_DCR_ADDR                0x00
 #define TEST_MPW_CPU_025_PPC_TIMEOUT    100
 
+
 static const tlb_entry bootrom_mirror           = {TLB_ENTRY_BOOTROM_MIRROR_0};
 static const tlb_entry em0_0_cache_on_valid     = {TLB_ENTRY_EM0_0_CACHE_VALID};
-//static const tlb_entry em0_0_cache_off_valid    = {TLB_ENTRY_EM0_0_NOCACHE_VALID};
-//static const tlb_entry em0_0_cache_off_invalid  = {TLB_ENTRY_EM0_0_NOCACHE_INVALID};
 static const tlb_entry em0_0_cache_off_valid    = {TLB_ENTRY_EM0_0_NOCACHE_VALID};
 
-
-volatile uint32_t dcu_cached_test_data __attribute__((section(".data"), aligned(0x100))) = 0xAAAA5555;
 
 volatile uint32_t mem_test_data __attribute__((section(".data"))) = 0x5555AAAA;
 
 volatile uint32_t MC_HANDLED __attribute__((section(".data")))   = 0;
 
-void __attribute__((section(".text"), aligned(0x100))) cached_func_from_EM0()
+uint32_t value = 0;
+
+//void __attribute__((section(".text"), aligned(0x100))) cached_func_from_EM0()
+//{
+//    rumboot_printf("execute cached_func_from_EM0\n");
+//}
+typedef void func();
+extern uint32_t cached_func[], func_size;
+
+asm  (
+
+  "   .global cached_func           \n\t"
+  "   .global func_size             \n\t"
+  ".align 8                         \n\t"
+  "cached_func:                     \n\t"
+  "   lis 3,  value@h               \n\t"
+  "   ori 3, 3,  value@l            \n\t"
+  "   lwz 5, 0(3)                   \n\t"
+  "   li  4, 0                      \n\t"
+  "   orc 5, 5, 4                   \n\t"
+  "   stw 5, 0(3)                   \n\t"
+  "   blr                           \n\t"
+  ".align 8                         \n\t"
+  "func_size:                       \n\t"
+  "   .long (.-cached_func)         \n\t"
+);
+
+void __attribute__((section(".text"))) cached_func_from_EM0()
 {
     rumboot_printf("execute cached_func_from_EM0\n");
 }
 
-
-void __attribute__((section(".text"), aligned(0x100))) gen_IC_exception_asm()
+static void gen_exception()
 {
-    rumboot_printf("generate IC exception\n");
+    rumboot_printf("generate L2C exception\n");
     asm volatile   (
         "icbi 0,%0\n\t"
         "msync\n\t"
-        "mtspr 888, %1\n\t"
+        "mtspr %3, %1\n\t"
         "isync\n\t"
         "icbt 0,0,%0\n\t"
         "msync\n\t"
-        "mtspr 888, %2\n\t"
+        "mtspr %3, %2\n\t"
         "isync\n\t"
-        ::"r"((uint32_t) cached_func_from_EM0), "r"(2 << CTRL_CCR1_ICDPEI_i), "r"(0)
+        ::"r"((uint32_t) cached_func_from_EM0), "r"(2 << CTRL_CCR1_ICDPEI_i), "r"(0), "i"(SPR_CCR1)
         :
     );
     cached_func_from_EM0();
+}
+
+static void gen_IC_exception_asm()
+{
+    rumboot_printf("generate IC exception\n");
+    func* func_test = (func*)rumboot_malloc_from_named_heap_aligned( "SRAM0", func_size, 8 );
+    memcpy( func_test, cached_func, func_size );
+    msync();
+    asm volatile   (
+        "icbi 0,%0\n\t"
+        "msync\n\t"
+        "mtspr %3, %1\n\t"
+        "isync\n\t"
+        "icbt 0,0,%0\n\t"
+        "msync\n\t"
+        "mtspr %3, %2\n\t"
+        "isync\n\t"
+        ::"r"((uint32_t) func_test), "r"(0b10 << CTRL_CCR1_ICDPEI_i), "r"(0), "i"(SPR_CCR1)
+        :
+    );
+    rumboot_printf("Check value\n");
+    func_test ();
+    rumboot_printf("value = %x\n", value);
 }
 
 static void load_code_in_cache(uint32_t start_addr, uint32_t code_size)
@@ -86,7 +135,6 @@ static void load_code_in_cache(uint32_t start_addr, uint32_t code_size)
     }
 }
 
-
 inline static void enable_machine_check()
 {
     msr_write(msr_read() | (1<<ITRPT_XSR_ME_i)); /* MSR[ME] - Machine check enable.     */
@@ -94,17 +142,14 @@ inline static void enable_machine_check()
 
 static void exit_from_machine_check_interrupt(uint32_t MCSR_value)
 {
-    //SPR_MCSRR0_write((uint32_t) exit_from_test);
-    //SPR_MCSRR1_write(msr_read());
-    //msr_write(0x0);
-
     spr_write(SPR_MCSR_C,MCSR_value);
 
-    //SPR_MCSR_C_write(MCSR_value);
 }
 
 static void check_mc_status_CCR1(uint32_t mc_interrupt_status)
 {
+    rumboot_printf("mc_interrupt_status = %x\n",mc_interrupt_status);
+
     TEST_ASSERT(mc_interrupt_status & (1 << ITRPT_MCSR_MCS_i), "Failed to set machine check interrupt");
     if (mc_interrupt_status & (1<<ITRPT_MCSR_TLB_i))
     {
@@ -142,13 +187,13 @@ static void check_mc_status_CCR1(uint32_t mc_interrupt_status)
         SET_BIT(MC_HANDLED, ITRPT_MCSR_FPR_i);
         rumboot_printf("detected 'FPR parity error' interrupt\n");
     }
-/*
-    else if (mc_interrupt_status & (1<<MCSR_IMP_i))
-    {
-        SET_BIT(MC_HANDLED, MCSR_IMP_i);
-        rumboot_printf("detected 'Imprecise machine check' interrupt\n");
-    }
-*/
+
+//    else if (mc_interrupt_status & (1<<ITRPT_MCSR_IMP_i))
+//    {
+//        SET_BIT(MC_HANDLED, ITRPT_MCSR_IMP_i);
+//        rumboot_printf("detected 'Imprecise machine check' interrupt\n");
+//    }
+
     else if (mc_interrupt_status & (1<<ITRPT_MCSR_L2_i))
     {
         SET_BIT(MC_HANDLED, ITRPT_MCSR_L2_i);
@@ -165,6 +210,7 @@ static void check_mc_status_CCR1(uint32_t mc_interrupt_status)
         rumboot_printf("MCSR = %x\n",mc_interrupt_status);
         //test_event(EVENT_ERROR);
     }
+    msync();
 }
 
 static void machine_check_interrupt_handler_CCR1(int id, const char *name)
@@ -189,61 +235,22 @@ static void machine_check_interrupt_handler_CCR1(int id, const char *name)
     }
 }
 
-//static void exception_handler(int id, const char *name)
-//{
-//    rumboot_printf("\nWE GOT AN EXCEPTION: %d: %s\n", id, name);
-//    if(id == RUMBOOT_IRQ_MACHINE_CHECK)
-//    {
-//        rumboot_printf("It is OK!\n");
-//        spr_write(SPR_MCSR_C,0xFFFFFFFF);
-//    }
-//    else
-//    {
-//        rumboot_printf("--- Guru Meditation ---\n");
-//        rumboot_printf("MSR:  0x%x\n", msr_read());
-//        rumboot_printf("SRR0: 0x%x\n", spr_read(SPR_SRR0));
-//        rumboot_printf("SRR1: 0x%x\n", spr_read(SPR_SRR1));
-//        rumboot_printf("CSRR0: 0x%x\n", spr_read(SPR_CSRR0));
-//        rumboot_printf("CSRR1: 0x%x\n", spr_read(SPR_CSRR1));
-//        rumboot_printf("MCSRR0: 0x%x\n", spr_read(SPR_MCSRR0));
-//        rumboot_printf("MCSRR1: 0x%x\n", spr_read(SPR_MCSRR1));
-//        rumboot_printf("---       ---       ---\n");
-//        rumboot_platform_panic("Please reset or power-cycle the board\n");
-//    }
-//}
-
 static void setup_machine_check_interrupt(void (*handler)(int id, const char *name))
 {
     rumboot_irq_set_exception_handler(handler);
-
-//    irq_init_cpu_int_vec();
-//    irq_set_handler(RUMBOOT_IRQ_MACHINE_CHECK, interrupt_handled);
-
 }
 
 void test_setup_CCR1()
 {
     rumboot_printf("Check interrupts created by CCR1\n");
-    MC_HANDLED = 0;
 
-    //SPR_CCR2_write(SPR_CCR2_read() | (1 << CTRL_CCR2_MCDTO_i));
-
-    spr_write(SPR_CCR2 , spr_read(SPR_CCR2) | (1 << CTRL_CCR2_MCDTO_i));
+    spr_write(SPR_CCR2 , spr_read(SPR_CCR2) | (1 << CTRL_CCR2_MCDTO_i)); //enable_machine_check_on_dcr_timeout
 
     setup_machine_check_interrupt(machine_check_interrupt_handler_CCR1);
     write_tlb_entries(&em0_0_cache_off_valid,1);//set em0_0 cache_off tlb_entry
-/*
-    ddr_init_impl(
-            DdrHlbId_Default,
-            DdrInitMode_Default,
-            DdrEccMode_Default,
-            DdrPmMode_Default,
-            DdrBurstLength_4,
-            DdrPartialWriteMode_ReadModifyWrite);
-*/
+
     emi_init(DCR_EM2_EMI_BASE);
 
-//    write_tlb_entries(&em0_0_cache_off_invalid,1);//invalidate em0_0 cache_off tlb_entry
     write_tlb_entries(&em0_0_cache_on_valid,1);//set em0_0 cache_on tlb_entry
 }
 
@@ -252,11 +259,17 @@ static void generate_TLB_mc()
 {
     rumboot_printf("Check 'UTLB parity error' generation with CCR1_MMUPEI field\n");
 
-    spr_write(SPR_CCR1 , spr_read(SPR_CCR1) | (1 << CTRL_CCR1_MMUDPEI_i)
-                                            | (1 << CTRL_CCR1_MMUTPEI_i));
+    uint32_t test_tlb_addr;
+
+    spr_write(SPR_CCR1 , spr_read(SPR_CCR1) | (0b1 << CTRL_CCR1_MMUDPEI_i)
+                                            | (0b1 << CTRL_CCR1_MMUTPEI_i)); //enable_mmu_parity_error_insert
     isync();
 
     write_tlb_entries(&bootrom_mirror,1);//set tlb with errors
+    test_tlb_addr = ioread32 (BOOTROM_BASE);
+    rumboot_printf("test_tlb_addr = %d\n",test_tlb_addr);
+
+
 }
 
 static void generate_IC_mc()
@@ -268,63 +281,74 @@ static void generate_IC_mc()
 
 static void generate_DC_mc()
 {
-    uint8_t tmp;
     rumboot_printf("Check 'D-cache error' generation with CCR1_DCPEI field\n");
+    uint8_t* data_addr = (uint8_t*)rumboot_malloc_from_named_heap_aligned( "SRAM0", 256, 8 );
+    rumboot_memfill8(data_addr, 256, 0x00, 1);
+    msync();
+
     asm volatile   (
-        "dcbt 0, %1 \n\t"
+        "dcbi 0,%0\n\t"
         "msync\n\t"
-        "mtspr 888, %2 \n\t"
+        "mtspr %3, %1\n\t"
         "isync\n\t"
-        "stb %3, 0(%1) \n\t"
+        "dcbt 0,0,%0\n\t"
         "msync\n\t"
-        "mtspr 888, %4 \n\t"
+        "mtspr %3, %2\n\t"
         "isync\n\t"
-        "lbz %0, 0(%1) \n\t"
-        :"=r"(tmp)
-        :"r"((uint32_t) (&dcu_cached_test_data)), "r"((3 << CTRL_CCR1_DCDPEI_i) | (3 << CTRL_CCR1_DCLPEI_i) | (3 << CTRL_CCR1_DCTPEI_i)) ,"r"(0x77), "r"(0)
+        ::"r"((uint32_t) data_addr), "r"(0b11 << CTRL_CCR1_DCDPEI_i), "r"(0), "i"(SPR_CCR1)
         :
     );
-    rumboot_printf("tmp = %x\n", tmp);
+
+    rumboot_printf("Check value\n");
+    rumboot_printf("value = %x\n", ioread32((uint32_t) data_addr));
+
 }
 
-/*
 static void generate_GPR_mc()
 {
     rumboot_printf("Check 'GPR parity error' generation with CCR1_GPRPEI field\n");
+    uint32_t data = 0x7E57DA7A;
     asm volatile   (
         "lwz 3, 0(%0)\n\t"
         "lwz 4, 0(%0)\n\t"
-        "mtspr 888, %1 \n\t"
+        "msync\n\t"
+        "mtspr %3, %1 \n\t"
         "isync\n\t"
         "lwz 3, 0(%0)\n\t"
         "lwz 4, 0(%0)\n\t"
-        "isync\n\t"
+        "msync\n\t"
+        "mtspr %3, %2 \n\t"
         "isync\n\t"
         "cmp 0, 0, 3, 4\n\t"
-        ::"r"(&mem_test_data), "r"(3 << CTRL_CCR1_GPRPEI_i), "r"(0)
+        ::"r"((uint32_t) &data), "r"(0b11 << CTRL_CCR1_GPRPEI_i), "r"(0), "i"(SPR_CCR1)
         :"3","4"
     );
+    rumboot_printf("data = %d\n",data);
 }
-*/
 
 static void generate_FPR_mc()
 {
     rumboot_printf("Check 'FPR parity error' generation with CCR1_FPRPEI field\n");
-    msr_write(msr_read() | (1<<ITRPT_XSR_FP_i));
+    uint32_t const msr_old_value = msr_read();
+    msr_write(msr_old_value | (1<<ITRPT_XSR_FP_i));
+
     asm volatile   (
         "lfs 1, 0(%0)\n\t"
         "lfs 2, 0(%0)\n\t"
-        "mtspr 888, %1 \n\t"
+        "msync\n\t"
+        "mtspr %3, %1 \n\t"
         "isync\n\t"
         "lfs 1, 0(%0)\n\t"
         "lfs 2, 0(%0)\n\t"
-        "isync\n\t"
-        "mtspr 888, %2 \n\t"
+        "msync\n\t"
+        "mtspr %3, %2 \n\t"
         "isync\n\t"
         "fcmpu 0, 1, 2\n\t"
-        ::"r"(&mem_test_data), "r"(3 << CTRL_CCR1_FPRPEI_i), "r"(0)
+        ::"r"(&mem_test_data), "r"(0b11 << CTRL_CCR1_FPRPEI_i), "r"(0), "i"(SPR_CCR1)
         :"1","2"
     );
+    msr_write(msr_old_value);
+    ici (0); dci(0); dci(2);
 }
 
 static void generate_DCR_mc()
@@ -333,17 +357,26 @@ static void generate_DCR_mc()
     dcr_read(UNEXIST_DCR_ADDR);
 }
 
+static void generate_L2C_mc()
+{
+    rumboot_printf("Check 'L2C error' generation with CCR1  field\n");
+    load_code_in_cache((uint32_t)gen_exception, 0x40);
+    gen_exception();
+}
+
 static void wait_interrupt(ITRPT_MCSR_FIELD MCSR_bit)
 {
     uint32_t t = 0;
     while (!(MC_HANDLED & (1 << MCSR_bit)) && t++<TEST_MPW_CPU_025_PPC_TIMEOUT);
     rumboot_printf("MC_HANDLED = %x\n",MC_HANDLED);
-    rumboot_printf("MCSR_bit = %x\n",MCSR_bit);
-    //TEST_WAIT_ASSERT ((MC_HANDLED & (1 << MCSR_bit)), 100, "Interrupt handle timeout!");
+    rumboot_printf("MCSR_bit = %d\n",MCSR_bit);
+    TEST_WAIT_ASSERT ((MC_HANDLED & (1 << MCSR_bit)), 100, "Interrupt handle timeout!");
 }
 
 void check_mc_with_CCR1(ITRPT_MCSR_FIELD MCSR_bit)
 {
+    MC_HANDLED = 0;
+    msync();
     enable_machine_check();
     switch (MCSR_bit)
     {
@@ -353,21 +386,20 @@ void check_mc_with_CCR1(ITRPT_MCSR_FIELD MCSR_bit)
         case ITRPT_MCSR_IC_i:     generate_IC_mc();
                             rumboot_printf("wait interrupt from MCSR_IC\n");
                             break;
-        case ITRPT_MCSR_DC_i:
-                            //enable_L1_parity_checking();
-                            generate_DC_mc();
+        case ITRPT_MCSR_DC_i:     generate_DC_mc();
                             rumboot_printf("wait interrupt from MCSR_DC\n");
                             break;
-/*
         case ITRPT_MCSR_GPR_i:    generate_GPR_mc();
-                            trace_msg("wait interrupt from MCSR_GPR\n");
+                                  rumboot_printf("wait interrupt from MCSR_GPR\n");
                             break;
-*/
         case ITRPT_MCSR_FPR_i:    generate_FPR_mc();
                             rumboot_printf("wait interrupt from MCSR_FPR\n");
                             break;
         case ITRPT_MCSR_DCR_i:    generate_DCR_mc();
                             rumboot_printf("wait interrupt from MCSR_DCR\n");
+                            break;
+        case ITRPT_MCSR_L2_i:    generate_L2C_mc();
+                            rumboot_printf("wait interrupt from MCSR_L2C\n");
                             break;
         default: break;
     }
@@ -378,16 +410,19 @@ int main ()
 {
     //test_event_send_test_id( "test_oi10_cpu_025_rst");
 
+    rumboot_memfill8_modelling((void*)SRAM0_BASE, 0x1000, 0x00, 0x00); //workaround (init 4KB SRAM0)
+
     rumboot_printf("TEST START\n");
 
     test_setup_CCR1();
 
-    //check_mc_with_CCR1(ITRPT_MCSR_TLB_i);  //not mc
-    //check_mc_with_CCR1(ITRPT_MCSR_IC_i); // mc l2c
-    //check_mc_with_CCR1(ITRPT_MCSR_DC_i); // not mc
-    //check_mc_with_CCR1(ITRPT_MCSR_FPR_i); // not mc
-//    check_mc_with_CCR1(ITRPT_MCSR_GPR_i);
-    check_mc_with_CCR1(ITRPT_MCSR_DCR_i);
+    //check_mc_with_CCR1(ITRPT_MCSR_IC_i);    // mc ic
+    //check_mc_with_CCR1(ITRPT_MCSR_DCR_i);   // mc.
+    check_mc_with_CCR1(ITRPT_MCSR_FPR_i);   // mc
+    //check_mc_with_CCR1(ITRPT_MCSR_DC_i);      // mc
+    //check_mc_with_CCR1(ITRPT_MCSR_TLB_i);   //not mc
+    check_mc_with_CCR1(ITRPT_MCSR_GPR_i);   // not mc
+    //check_mc_with_CCR1(ITRPT_MCSR_L2_i);   // not mc
 
 
     rumboot_printf("TEST OK\n");
