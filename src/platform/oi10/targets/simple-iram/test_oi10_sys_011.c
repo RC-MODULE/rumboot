261de98a406d5538d252191f9c50fa16b0609eca
Vladidslav Fufaev <v.fufaev@modulew.ru>
2019-02-11 13:12:58 +0300

    oi10_rumboot: test_oi10_sys_011 (systemcontrol test was added by request)

--- src/platform/oi10/targets/simple-iram/test_oi10_sys_011.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_sys_011.c
@@ -31,7 +31,8 @@ enum {
        TEC_CHECK_DEBUG_HALT,
        TEC_CHECK_DEBUG_UNCONDEVENT,
        TEC_CHECK_DEBUG_SYSTEMSTATUS,
-       TEC_CHECK_DEBUG_MASKOUT
+       TEC_CHECK_DEBUG_MASKOUT,
+       TEC_CHECK_DEBUG_SYSTEMCONTROL
 } test_event_code;
 
 enum {
@@ -225,6 +226,22 @@ int check_dbomask ()
     return 0;
 }
 
+int check_systemcontrol ()
+{
+    rumboot_printf("SYSTEMCONTROL check start!\n");
+    spr_write(SPR_DBDR, TEST_DATA_ERROR);
+
+    rumboot_platform_event_clear();
+    rumboot_printf("send TEC_CHECK_DEBUG_SYSTEMCONTROL\n");
+    test_event(TEC_CHECK_DEBUG_SYSTEMCONTROL );
+    if(event_get()) return 1;
+
+    spr_write(SPR_DBSR_RC,0xFFFFFFFF); //clear dbsr
+
+    rumboot_printf("SYSTEMCONTROL check done!\n");
+    return 0;
+}
+
 int main()
 {
 	test_event_send_test_id("test_oi10_sys_011");
@@ -247,6 +264,10 @@ int main()
 	}
     if(check_dbomask()) {
         rumboot_printf("DBOMask test failed!");
+        return 1;
+    }
+    if(check_systemcontrol()) {
+        rumboot_printf("SYSTEMCONTROL test failed!");
         return 1;
     }
 
