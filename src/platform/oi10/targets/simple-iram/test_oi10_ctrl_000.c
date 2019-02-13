3652f339d00f4c50a408ab0cbe56a905f809adfe
Maksim Dubinkin <m.dubinkin@modulew.ru>
2019-02-13 12:03:17 +0300

    oi10_rumboot:test_oi10_ctrl_000.c - Replaced wrong path include header and fixed sctl regs

--- src/platform/oi10/targets/simple-iram/test_oi10_ctrl_000.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_ctrl_000.c
@@ -7,17 +7,17 @@
 #include <rumboot/irq.h>
 #include <platform/interrupts.h>
 #include <platform/oi10/platform/devices.h>
-#include <platform/regs/regs_sctl.h>
+#include <platform/regs/sctl.h>
 
 static uint8_t check_ctrl_regs_ro(uint32_t base_addr)
 {   
     rumboot_printf("Checking sctl registers default value...\n");
     struct regpoker_checker check_array[] =
      {
-     { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0x0, 0x1 },
+     { "PPC_SYS_CONF", REGPOKER_READ_DCR, SCTL_PPC_SYS_CONF, 0x0, 0x1 },
      { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0, 0b00000, 0b11111 },
      { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0, 0b000000, 0b111111 },
-     { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0b000, 0b111 },
+     { "PPC_SYS_CONF", REGPOKER_READ_DCR, SCTL_PPC_SYS_CONF, 0b000, 0b111 },
      {  }
     };
       if( rumboot_regpoker_check_array( check_array, base_addr ) == 0 )
@@ -37,13 +37,13 @@ static int check_ctrl_regs_rw(uint32_t base_addr)
     { "KMBIST_CHAIN_SF_I", REGPOKER_WRITE_DCR, KMBIST_CHAIN_SF_0, 0b000,   0b1 },
     { "KMBIST_CHAIN_SF_i", REGPOKER_READ_DCR, KMBIST_CHAIN_SF_0, 0b0000, 0b1111 },
     { "KMBIST_CHAIN_SF_i", REGPOKER_WRITE_DCR, KMBIST_CHAIN_SF_0, 0b000,   0b111 },
-    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0b11000, 0b11111 },
-    { "PPC_SYS_CONF", REGPOKER_READ_DCR, PPC_SYS_CONF, 0b0, 0b1 },
-    { "PPC_SLP_CTRL", REGPOKER_READ_DCR, PPC_SLP_CTRL, 0b0000, 0b1111 },
-    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, PPC_PMU_CTRL, 0b00000, 0b11111 },
-    { "IFSYS_ETH_CTRL", REGPOKER_READ_DCR, IFSYS_ETH_CTRL, 0b0000, 0b1111 },
-    { "IFSYS_ETH_CTRL", REGPOKER_READ_DCR, IFSYS_ETH_CTRL, 0x10000, 0x11111 },
-    { "IFSYS_ETH_HPROT", REGPOKER_READ_DCR, IFSYS_ETH_HPROT, 0x33333333, 0xffffffff },
+    { "PPC_SYS_CONF", REGPOKER_READ_DCR, SCTL_PPC_SYS_CONF, 0b11000, 0b11111 },
+    { "PPC_SYS_CONF", REGPOKER_READ_DCR, SCTL_PPC_SYS_CONF, 0b0, 0b1 },
+    { "PPC_SLP_CTRL", REGPOKER_READ_DCR, SCTL_PPC_SLP_CTRL, 0b0000, 0b1111 },
+    { "PPC_PMU_CTRL", REGPOKER_READ_DCR, SCTL_PPC_PMU_CTRL, 0b00000, 0b11111 },
+    { "IFSYS_ETH_CTRL", REGPOKER_READ_DCR, SCTL_IFSYS_ETH_CTRL, 0b0000, 0b1111 },
+    { "IFSYS_ETH_CTRL", REGPOKER_READ_DCR, SCTL_IFSYS_ETH_CTRL, 0x10000, 0x11111 },
+    { "IFSYS_ETH_HPROT", REGPOKER_READ_DCR, SCTL_IFSYS_ETH_HPROT, 0x33333333, 0xffffffff },
     {  }
     };
     if( rumboot_regpoker_check_array( check_array, base_addr ) == 0 ) {
