afd01de3e4ff2627482ec006b7b24cf760228ebf
Rishat Galiulin <r.galiulin@modulew.ru>
2019-02-16 01:38:20 +0300

    oi10_rumboot: test_oi10_cpu_mem_013.c - Fix includes

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_013.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_mem_013.c
@@ -11,7 +11,7 @@
 #include <platform/devices/emi.h>
 #include <platform/trace.h>
 #include <platform/test_event_c.h>
-#include <platform/arch/ppc/ppc_476fp_lib_c.h>
+#include <arch/ppc_476fp_lib_c.h>
 #include <platform/regs/regs_plb6mcif2.h>
 #include <platform/regs/regs_mclfir.h>
 #include <platform/regs/regs_emi.h>
