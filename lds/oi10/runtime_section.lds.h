b28ba1285160f724e463277ca76245010d7785f0
Andrew Andrianov <andrew@ncrmnt.org>
2019-02-19 17:17:41 +0300

    oi10: Move runtime to 8001fd00
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- lds/oi10/runtime_section.lds.h
+++ lds/oi10/runtime_section.lds.h
@@ -1,4 +1,4 @@
-    .runtime 0x8001fe00 (NOLOAD) : {
+    .runtime 0x8001fd00 (NOLOAD) : {
         /* Runtime info is at the very top of iram */
         rumboot_platform_runtime_info = .;
         *(.rumboot_platform_runtime_info);
