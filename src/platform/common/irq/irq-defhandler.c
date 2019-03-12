7211a4b668296f64ba8055232032afad72f69857
Andrew Andrianov <andrew@ncrmnt.org>
2019-03-12 18:05:56 +0300

    irq: Rewrite irq-defhandler using new API
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/common/irq/irq-defhandler.c
+++ src/platform/common/irq/irq-defhandler.c
@@ -14,24 +14,40 @@
 void handler(int irq, void *arg)
 {
     int *gotit = arg;
-    *gotit=1;
-    rumboot_printf("IRQ default handler called, arg %x\n", arg);    
+    rumboot_printf("IRQ default handler called, arg %x (%d)\n", arg, *gotit);    
+    (*gotit)++; 
 }
 
 int main()
 {
-    rumboot_irq_sei();
-    int gotit = 0;
-    rumboot_printf("Setting default handler\n");
-    rumboot_irq_set_default_handler(handler, &gotit);
+    volatile uint32_t gotit = 0;
 
-    struct rumboot_irq_entry *tbl = rumboot_irq_create(NULL);
-    rumboot_irq_table_activate(tbl);
-    rumboot_irq_enable(USE_SWINT);
+    rumboot_printf("Creating a table\n");
+	struct rumboot_irq_entry *tbl = rumboot_irq_create(NULL);
+	rumboot_irq_table_activate(tbl);
+	rumboot_irq_enable(USE_SWINT);
+	rumboot_irq_sei();
+
+    rumboot_printf("Setting default handler\n");
+    rumboot_irq_set_default_handler(handler, (void *) &gotit);
+
+	rumboot_irq_table_activate(NULL);
+    rumboot_printf("Firing IRQ without a table\n");
+	rumboot_irq_swint(USE_SWINT);
+
+	rumboot_irq_table_activate(tbl);
 
     rumboot_printf("Firing IRQ\n");
-	rumboot_irq_swint(USE_SWINT);;
+	rumboot_irq_swint(USE_SWINT);
     udelay(1);
-    rumboot_printf("IRQ Received: %d", gotit);
-    return (gotit > 0) ? 0 : 1;
+    rumboot_printf("Total IRQ Received: %d (must be 2)\n", gotit);
+    rumboot_printf("IRQ Accouting count: %d (must be 1)\n", rumboot_irq_get_count(USE_SWINT));
+
+    if (gotit != 2 )
+        return 1;
+
+    if (rumboot_irq_get_count(USE_SWINT) != 1)
+        return 1;
+
+    return 0;
 }
