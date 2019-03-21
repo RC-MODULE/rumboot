73bf7bb29ddb7b7d1366cc6a0e71df57245ac851
Mikhail Chelyshev <mch87@module.ru>
2019-03-21 18:17:28 +0300

    Correct HSCB test.
    Signed-off-by: Mikhail Chelyshev <mch87@module.ru>

--- src/platform/oi10/targets/test_oi10_hscb.c
+++ src/platform/oi10/targets/test_oi10_hscb.c
@@ -848,14 +848,17 @@ int main() {
     rumboot_printf( "Checking regs of HSCB (0x%x) \n", HSCB_UNDER_TEST_BASE );
     result += check_hscb_default_val( HSCB_UNDER_TEST_BASE );
     result += check_hscb_regs( HSCB_UNDER_TEST_BASE );
+    hscb_sw_rst(HSCB_UNDER_TEST_BASE);
+    hscb_adma_sw_rst(HSCB_UNDER_TEST_BASE);
 #endif
+#ifdef HSCB_FUNC_TEST
     rumboot_printf( "Checking base functions of HSCB (0x%x) and HSCB (0x%x)\n",
             HSCB_UNDER_TEST_BASE, HSCB_SUPPLEMENTARY_BASE);
     hscb_sw_rst(HSCB_UNDER_TEST_BASE);
     hscb_adma_sw_rst(HSCB_UNDER_TEST_BASE);
     hscb_sw_rst(HSCB_SUPPLEMENTARY_BASE);
     hscb_adma_sw_rst(HSCB_SUPPLEMENTARY_BASE);
-#ifdef HSCB_FUNC_TEST
+    
     struct rumboot_irq_entry *tbl;
     uint32_t count_of_memory_areas;
     uint8_t** data_areas = NULL;
