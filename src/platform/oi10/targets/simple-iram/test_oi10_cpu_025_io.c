07397ad8f9430f6027d826e57459bb42496d6628
Alexander Gurov <a.gurov@modulew.ru>
2019-04-04 16:52:13 +0300

    oi10_rumboot: test_oi10_cpu_025_io - make hardware part able to be turned off.

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_io.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_025_io.c
@@ -5,6 +5,8 @@
  *      Author: m.smolina
  */
 
+// #define USE_HARDWARE_PARTS
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -77,26 +79,6 @@ static void exception_handler(int const id, char const * const name ) {
     msync();
 }
 
-
-void generate_P6BC_O_SYSTEM_HUNG(uint32_t l2c_dcr_base)
-{
-    rumboot_printf("Check O_SYSTEM_HUNG generation\n");
-    l2c_pmu_write(l2c_dcr_base, L2C_L2PLBMCKEN1, 0x0);
-    l2c_pmu_write(l2c_dcr_base, L2C_L2CPUMCKEN, 0x0);
-    l2c_l2_write(l2c_dcr_base, L2C_L2RACCFG,l2c_l2_read(l2c_dcr_base, L2C_L2RACCFG) | (1 << L2C_L2RACCFG_DisablePlbErrScrub_i));
-    l2c_l2_write(l2c_dcr_base, L2C_L2WACCFG,l2c_l2_read(l2c_dcr_base, L2C_L2WACCFG) | (1 << L2C_L2WACCFG_DisablePlbErrScrub_i));
-    dcr_write(DCR_PLB6_BC_BASE + PLB6BC_HCPP, PLB6BC_HCPP_4K);
-
-    static const tlb_entry tlb_entry_fake[] = {
-//       MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
-        {MMU_TLB_ENTRY(  0x040,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )}
-    };
-    write_tlb_entries( tlb_entry_fake, ARRAY_SIZE(tlb_entry_fake) );
-
-    iowrite32 (0x00, 0x00000000);
-    ioread32(0x00000000);
-}
-
 void generate_BR6TO4_INTR(uint32_t base_address)
 {
     rumboot_printf("Check BR6TO4_INTR generation\n");
@@ -123,13 +105,6 @@ void DMA_O_S_SLV_ERR_INT_inj_int_handler()
     dcr_write(DCR_DMAPLB6_BASE + PLB6_DMA_PSE, 1 << DMA_PSE_PE_i);
     SET_BIT(MC_HANDLED, DMA2PLB6_SLV_ERR_INT);
     test_event(EVENT_CLEAR_INT_DMA_O_S_SLV_ERR_INT);
-}
-
-void P6BC_O_SYSTEM_HUNG_inj_int_handler()
-{
-    rumboot_printf("P6BC_O_SYSTEM_HUNG_inj_int_handler\n");
-    SET_BIT(MC_HANDLED, O_SYSTEM_HUNG);
-    test_event(EVENT_CLEAR_INT_P6BC_O_SYSTEM_HUNG);
 }
 
 void P6P4_0_BR6TO4_INTR_inj_int_handler()
@@ -159,15 +134,6 @@ void ARB_SYSDCRERR_inj_int_handler()
     TEST_ASSERT((daesr_reg & (0b1 << DCRARB_DAESR_TE_i)) && (daesr_reg & (0b1 << DCRARB_DAESR_EV_i)), "Failed to set DCRARB DAESR[TE] and DAESR[EV] bit");
     test_event(EVENT_CLEAR_INT_ARB_SYSDCRERR);
     dcr_write(DCR_ARB_BASE + DCRARB_DAESR, 0b1<<DCRARB_DAESR_EV_i); //dcrarb_clear_daesr
-}
-
-void P6BC_O_SYSTEM_HUNG_L2C_reg_int_handler()
-{
-    rumboot_printf("P6BC_O_SYSTEM_HUNG_L2C1_reg_int_handler\n");
-    SET_BIT(MC_HANDLED, O_SYSTEM_HUNG);
-    l2c_l2_write(DCR_L2C_BASE, L2C_L2RACCFG, l2c_l2_read(DCR_L2C_BASE, L2C_L2RACCFG) & ~(1 << L2C_L2RACCFG_DisablePlbErrScrub_i));
-    l2c_l2_write(DCR_L2C_BASE, L2C_L2WACCFG, l2c_l2_read(DCR_L2C_BASE, L2C_L2WACCFG) & ~(1 << L2C_L2WACCFG_DisablePlbErrScrub_i));
-    l2c_l2_write(DCR_L2C_BASE, L2C_L2PLBFRC0,(1 << L2C_L2PLBFRC0_IntvnDataPE0_i) );//generate interrupt for reset slave core
 }
 
 void P6P4_0_BR6TO4_INTR_reg_int_handler()
@@ -201,17 +167,6 @@ void test_setup( uint32_t interrupt_number )
 
     switch (interrupt_number)
     {
-    case O_SYSTEM_HUNG:
-                        rumboot_printf("Set handler for O_SYSTEM_HUNG\n");
-                        rumboot_irq_cli();
-                        tbl = rumboot_irq_create( NULL );
-                        rumboot_irq_set_handler( tbl, O_SYSTEM_HUNG, RUMBOOT_IRQ_LEVEL | RUMBOOT_IRQ_HIGH, P6BC_O_SYSTEM_HUNG_L2C_reg_int_handler, NULL );
-                        rumboot_irq_table_activate( tbl );
-                        rumboot_irq_enable( O_SYSTEM_HUNG );
-                        rumboot_irq_sei();
-
-                        MC_HANDLED = 0;
-    break;
     case PLB6PLB40_O_0_BR6TO4_INTR:
                         rumboot_printf("Set handler for PLB6PLB40_O_0_BR6TO4_INTR \n");
                         rumboot_irq_cli();
@@ -248,22 +203,12 @@ void test_setup( uint32_t interrupt_number )
     }
 }
 
+#ifdef USE_HARDWARE_PARTS
 void test_setup_inj( uint32_t interrupt_number )
 {
 
     switch (interrupt_number)
     {
-    case O_SYSTEM_HUNG:
-                        rumboot_printf("Set handler for O_SYSTEM_HUNG\n");
-                        rumboot_irq_cli();
-                        tbl = rumboot_irq_create( NULL );
-                        rumboot_irq_set_handler( tbl, O_SYSTEM_HUNG, RUMBOOT_IRQ_EDGE | RUMBOOT_IRQ_POS, P6BC_O_SYSTEM_HUNG_inj_int_handler, NULL );
-                        rumboot_irq_table_activate( tbl );
-                        rumboot_irq_enable( O_SYSTEM_HUNG );
-                        rumboot_irq_sei();
-
-                        MC_HANDLED = 0;
-    break;
     case PLB6PLB40_O_0_BR6TO4_INTR:
                         rumboot_printf("Set handler for PLB6PLB40_O_0_BR6TO4_INTR \n");
                         rumboot_irq_cli();
@@ -310,6 +255,7 @@ void test_setup_inj( uint32_t interrupt_number )
     break;
     }
 }
+#endif
 
 void check_interrupt_from_io_dev_reg(uint32_t interrupt_number)
 {
@@ -317,13 +263,6 @@ void check_interrupt_from_io_dev_reg(uint32_t interrupt_number)
 
     switch (interrupt_number)
     {
-        case O_SYSTEM_HUNG:
-                                            rumboot_printf("Check software generation P6BC_O_SYSTEM_HUNG\n");
-                                            //setup_interrupt(interrupt_number, P6BC_O_SYSTEM_HUNG_L2C_reg_int_handler);
-                                            generate_P6BC_O_SYSTEM_HUNG(DCR_L2C_BASE);
-                                            //START_SLAVE_CORE_TEST_PART = 1;
-
-                                            break;
         case PLB6PLB40_O_0_BR6TO4_INTR:
                                             rumboot_printf("Check software generation P6P4_0_BR6TO4_INTR\n");
                                             generate_BR6TO4_INTR(DCR_PLB6PLB4_0_BASE);
@@ -346,6 +285,7 @@ void check_interrupt_from_io_dev_reg(uint32_t interrupt_number)
     wait_interrupt(interrupt_number);
 }
 
+#ifdef USE_HARDWARE_PARTS
 void check_interrupt_from_io_dev_inj(uint32_t interrupt_number)
 {
     test_setup_inj( interrupt_number );
@@ -354,10 +294,6 @@ void check_interrupt_from_io_dev_inj(uint32_t interrupt_number)
     {
         case DMA2PLB6_SLV_ERR_INT:
                                             test_event(EVENT_GENERATE_INT_DMA_O_S_SLV_ERR_INT);
-                                            break;
-
-        case O_SYSTEM_HUNG:
-                                            test_event(EVENT_GENERATE_INT_P6BC_O_SYSTEM_HUNG);
                                             break;
 
         case PLB6PLB40_O_0_BR6TO4_INTR:
@@ -381,27 +317,31 @@ void check_interrupt_from_io_dev_inj(uint32_t interrupt_number)
 
     wait_interrupt(interrupt_number);
 }
+#endif
 
 int main()
 {
-    test_event_send_test_id( "test_oi10_cpu_025_io");
+
+#ifdef USE_HARDWARE_PARTS
+    test_event_send_test_id( "test_oi10_cpu_025_io" );
+#endif
 
     rumboot_irq_set_exception_handler(exception_handler);
 
     MC_HANDLED = 0;
 
+#ifdef USE_HARDWARE_PARTS
     //check_interrupt_from_io_dev_inj(DMA2PLB6_SLV_ERR_INT);
-    check_interrupt_from_io_dev_inj(O_SYSTEM_HUNG);
     check_interrupt_from_io_dev_inj(PLB6PLB40_O_0_BR6TO4_INTR);
     //check_interrupt_from_io_dev_inj(PLB6PLB41_O_BR6TO4_INTR);
     check_interrupt_from_io_dev_inj(ARB_SYSDCRERR);
+#endif
 
 
     check_interrupt_from_io_dev_reg(PLB6PLB40_O_0_BR6TO4_INTR);
     check_interrupt_from_io_dev_reg(PLB6PLB41_O_BR6TO4_INTR);
     check_interrupt_from_io_dev_reg(ARB_SYSDCRERR);
-    check_interrupt_from_io_dev_reg(O_SYSTEM_HUNG);
-
 
     return 0;
 }
+
