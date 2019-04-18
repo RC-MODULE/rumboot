8e7cb754a9318efc947aa444d5cc7f7037dfac19
Rishat Galiulin <r.galiulin@modulew.ru>
2019-04-18 13:03:06 +0300

    oi10_rumboot: test_oi10_cpu_025_wd - Remove interrupts masking. Fix reset status saving.

--- src/platform/oi10/targets/test_oi10_cpu_025_wd.c
+++ src/platform/oi10/targets/test_oi10_cpu_025_wd.c
@@ -24,16 +24,14 @@
 #include <platform/arch/ppc/ppc_476fp_ctrl_fields.h>
 #include <platform/arch/ppc/ppc_476fp_timer_fields.h>
 
-typedef enum test_event_code
-{
+
+typedef enum test_event_code {
     TEC_CHECK_WD_CORE_RESET     = TEST_EVENT_CODE_MIN,
     TEC_CHECK_WD_CHIP_RESET,
     TEC_CHECK_WD_SYSTEM_RESET
 } test_event_code;
 
-
-typedef enum test_check
-{
+typedef enum test_check {
     TC_NONE,
     TC_CORE_RESET,
     TC_CHIP_RESET,
@@ -51,25 +49,23 @@ static void generate_wd_reset( TIMER_TCR_WRC const reset_type )
 {
     rumboot_platform_perf("generate_wd_reset");
 
+    spr_write( SPR_TCR, (TIMER_TCR_WP_2pow21_clocks << TIMER_TCR_WP_i)
+                      | (reset_type                 << TIMER_TCR_WRC_i)
+                      | (0b0                        << TIMER_TCR_WIE_i)
+                      | (0b0                        << TIMER_TCR_DIE_i)
+                      | (0b0                        << TIMER_TCR_FIE_i) );
 
-    spr_write(SPR_TCR,    (TIMER_TCR_WP_2pow21_clocks  << TIMER_TCR_WP_i)
-                        | (reset_type                  << TIMER_TCR_WRC_i)
-                        | (0b0                         << TIMER_TCR_WIE_i)
-                        | (0b0                         << TIMER_TCR_DIE_i)
-                        | (0b0                         << TIMER_TCR_FIE_i) );
+    spr_write( SPR_TBL_W, 0 );                          //reset TBL
+    spr_write( SPR_TSR_RC, (0b1     << TIMER_TSR_ENW_i)    // clear WD next action
+                         | (0b1     << TIMER_TSR_WIS_i)    // clear WD exception
+                         | (0b11    << TIMER_TSR_WRS_i) ); // clear WD reset status
 
-
-    spr_write(SPR_TBL_W,0); //reset TBL
-    spr_write(SPR_TSR_RC,  (0b1  << TIMER_TSR_ENW_i )     // clear WD next action
-                         | (0b1  << TIMER_TSR_WIS_i)     // clear WD exception
-                         | (0b11 << TIMER_TSR_WRS_i) );  // clear WD reset status
-
-    spr_write( SPR_TBL_W,(0b1 << 20) ); // set TBL value that triggers TSR[ENW] to 1
-    spr_write( SPR_TBL_W,(0b0 << 20) ); // prepare for next trigger
-    spr_write( SPR_TBL_W,(0b1 << 20) ); // set TBL value that triggers TSR[WIS] to 1
-    spr_write( SPR_TBL_W,(0b0 << 20) ); // prepare for next trigger
-    spr_write( SPR_TBL_W,(0b1 << 20) ); // set TBL value that triggers reset when TSR[ENW] == 1 and TSR[WIS] == 1
-
+    spr_write( SPR_TBL_W, FIELD_MASK32(8, (20-8)) );    // set TBL value that triggers TSR[ENW] to 1
+    while( !(spr_read(SPR_TBL_R) & (0b1 << 20)) );      // wait trigger
+    spr_write( SPR_TBL_W, FIELD_MASK32(8, (20-8)) );    // set TBL value that triggers TSR[WIS] to 1
+    while( !(spr_read(SPR_TBL_R) & (0b1 << 20)) );      // wait trigger
+    spr_write( SPR_TBL_W, FIELD_MASK32(8, (20-8)) );    // set TBL value that triggers reset when TSR[ENW] == 1 and TSR[WIS] == 1
+    while( !(spr_read(SPR_TBL_R) & (0b1 << 20)) );      // wait trigger
 }
 
 
@@ -100,27 +96,15 @@ static void check_wd_system_reset() {
     generate_wd_reset( TIMER_TCR_WRC_System_reset );
 }
 
-
-int main()
-{
+int main() {
     test_event_send_test_id( "test_oi10_cpu_025_wd");
-
-    msr_write( msr_read() & ~( (0b1 << ITRPT_XSR_WE_i)
-                             | (0b1 << ITRPT_XSR_CE_i)
-                             | (0b1 << ITRPT_XSR_EE_i)
-                             | (0b1 << ITRPT_XSR_ME_i)
-                             | (0b1 << ITRPT_XSR_FE0_i)
-                             | (0b1 << ITRPT_XSR_DWE_i)
-                             | (0b1 << ITRPT_XSR_DE_i)
-                             | (0b1 << ITRPT_XSR_FE1_i)) );
 
     uint32_t reg = 0;
 
-    if(rumboot_platform_runtime_info->persistent[test_check_magic] != MAGIC )
-        {
-        rumboot_printf("MAGIC\n");
+    if(rumboot_platform_runtime_info->persistent[test_check_magic] != MAGIC ) {
+        rumboot_platform_runtime_info->persistent[test_check_magic] = MAGIC;
         rumboot_platform_runtime_info->persistent[last_test_check] = TC_NONE;
-        }
+    }
 
 
     switch( rumboot_platform_runtime_info->persistent[last_test_check] ) {
@@ -141,8 +125,7 @@ int main()
             check_wd_system_reset();
             break;
         }
-        case TC_SYSTEM_RESET:
-        {
+        case TC_SYSTEM_RESET: {
             reg = spr_read (SPR_DBSR_RC);
             TEST_ASSERT((reg & (1<<28)) && (reg & (1<<29)), "Error. Invalid value in DBSR[MRR]");
             rumboot_platform_runtime_info->persistent[test_check_magic] = ~MAGIC;
