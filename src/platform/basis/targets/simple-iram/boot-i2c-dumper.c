5fcb462d10225d77992127bf27b335471511880a
Andrew Andrianov <andrew@ncrmnt.org>
2019-04-22 15:35:43 +0300

    basis: i2c-dumper: Use internal hexdump
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/basis/targets/simple-iram/boot-i2c-dumper.c
+++ src/platform/basis/targets/simple-iram/boot-i2c-dumper.c
@@ -44,57 +44,8 @@
 #include <platform/devices.h>
 #include <regs/regs_sdio.h>
 #include <rumboot/io.h>
+#include <rumboot/hexdump.h>
 
-
-
-#ifndef HEXDUMP_COLS
-#define HEXDUMP_COLS 16
-#endif
- 
-void hexdump(void *mem, unsigned int len)
-{
-        unsigned int i, j;
-        
-        for(i = 0; i < len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0); i++)
-        {
-                /* print offset */
-                if(i % HEXDUMP_COLS == 0)
-                {
-                        rumboot_printf("0x%06x: ", i);
-                }
- 
-                /* print hex data */
-                if(i < len)
-                {
-                        rumboot_printf("%02x ", 0xFF & ((char*)mem)[i]);
-                }
-                else /* end of block, just aligning for ASCII dump */
-                {
-                        rumboot_printf("   ");
-                }
-                
-                /* print ASCII dump */
-                if(i % HEXDUMP_COLS == (HEXDUMP_COLS - 1))
-                {
-                        for(j = i - (HEXDUMP_COLS - 1); j <= i; j++)
-                        {
-                                if(j >= len) /* end of block, not really printing */
-                                {
-                                        rumboot_platform_putchar(' ');
-                                }
-                                else if(isprint(((char*)mem)[j])) /* printable char */
-                                {
-                                        rumboot_platform_putchar(0xFF & ((char*)mem)[j]);        
-                                }
-                                else /* other char */
-                                {
-                                        rumboot_platform_putchar('.');
-                                }
-                        }
-                        rumboot_platform_putchar('\n');
-                }
-        }
-}
 
 static const struct rumboot_bootsource src[] = {
         {
@@ -116,7 +67,7 @@ int main()
 //        while(1) {
 	        int ret = bootsource_read(src, (void *) pdata, buf, 0, 512);
 	        rumboot_printf("Read %d bytes\n", ret);
-	        hexdump(buf, 512);
+	        rumboot_hexdump(buf, 512);
 //        }
 
 	bootsource_deinit(src, (void *) pdata);        
