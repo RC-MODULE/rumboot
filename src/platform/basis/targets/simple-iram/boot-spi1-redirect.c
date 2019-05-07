a962c189e92284a0c1790eb3b30f89f453e778b0
Andrew Andrianov <andrew@ncrmnt.org>
2019-05-07 18:50:50 +0300

    basis: spi1-redirect: Keep calm, clean up and use rumboot_putchar
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/basis/targets/simple-iram/boot-spi1-redirect.c
+++ src/platform/basis/targets/simple-iram/boot-spi1-redirect.c
@@ -1,37 +1,3 @@
-/**
- * \defgroup test_hello_iram Hello World
- * \ingroup tests_basis
- *
- * Description
- * ===========
- *
- * Prints hello world via gtube and exits with 0 exit code
- *
- * Verification environment/plusargs
- * ==============================================
- * None
- *
- * CFLAGS
- * ======
- * None
- *
- * Expected output
- * ===============
- *
- *
- * How to compile
- * ==============
- *
- * \code{cmake}
- *    add_rumboot_target(
- *       CONFIGURATION IRAM
- *       FILES common/tools/drom-check.c
- *       NAME "hello-world"
- *   )
- * \endcode
- *
- */
-
 #include <stdio.h>
 #include <rumboot/printf.h>
 #include <rumboot/irq.h>
@@ -44,61 +10,6 @@
 #include <platform/devices.h>
 #include <regs/regs_sdio.h>
 #include <rumboot/io.h>
-
-
-
-#ifndef HEXDUMP_COLS
-#define HEXDUMP_COLS 16
-#endif
- 
-void hexdump(void *mem, unsigned int len)
-{
-        unsigned int i, j;
-        
-        for(i = 0; i < len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0); i++)
-        {
-                /* print offset */
-                if(i % HEXDUMP_COLS == 0)
-                {
-                        rumboot_printf("0x%06x: ", i);
-                }
- 
-                /* print hex data */
-                if(i < len)
-                {
-                        rumboot_printf("%02x ", 0xFF & ((char*)mem)[i]);
-                }
-                else /* end of block, just aligning for ASCII dump */
-                {
-                        rumboot_printf("   ");
-                }
-                
-                /* print ASCII dump */
-                if(i % HEXDUMP_COLS == (HEXDUMP_COLS - 1))
-                {
-                        for(j = i - (HEXDUMP_COLS - 1); j <= i; j++)
-                        {
-                                if(j >= len) /* end of block, not really printing */
-                                {
-                                        rumboot_platform_putchar(' ');
-                                }
-                                else if(isprint(((char*)mem)[j])) /* printable char */
-                                {
-                                        rumboot_platform_putchar(0xFF & ((char*)mem)[j]);        
-                                }
-                                else /* other char */
-                                {
-                                        rumboot_platform_putchar('.');
-                                }
-                        }
-                        rumboot_platform_putchar('\n');
-                }
-        }
-}
-
-
-
-
 
 int (*rom_bootsource_try_single)(const struct rumboot_bootsource *src, void *pdata, struct rumboot_bootheader *dst, size_t maxsize, size_t *offset);
 const struct rumboot_bootsource *(*rom_rumboot_platform_get_bootsources)();
