161fe21dc233daf1febd0a324715b435685c2d6e
Lyudmila Alexandrova <laalexan.module.ru>
2019-05-08 10:04:12 +0300

    add PostProduction delays

--- src/platform/basis/targets/i2c/multimaster/i2c_multimst_write_random_read.c
+++ src/platform/basis/targets/i2c/multimaster/i2c_multimst_write_random_read.c
@@ -3,8 +3,8 @@
 // 
 //  Test includes:
 //  - 2 I2c masters working as transmitters and receivers with 24FC1025 EEPROM simulation model
-//  - configured  frequency =1MHz
-//	- use I2c arbitration lost signal generation and checking for both controllers
+//  - configured  frequency =1MHz ( or 400kHZ PostProduction)
+//	- use I2c arbitration lost signal generation or timeout and checking for both controllers
 //	- received array is  compared with ethalon values	
 //    Test duration (RTL): < 
 //-----------------------------------------------------------------------------
@@ -58,13 +58,26 @@ int main()
 {
 	int tmp =-1;
 	uint32_t  len = 0x4;
+	uint32_t  delay_trn = 0;
 	int i = 0;
 	int sda, scl;
+
+	i2c_stat_rst(I2C_BASE);
+	i2c_reset(I2C_BASE);
+  	tmp = ioread32(I2C_BASE + I2C_STATUS);
+	scl = (tmp & (1 << 23)) ? 1 : 0;
+	sda = (tmp & (1 << 25)) ? 1 : 0;
+	if ((!sda) || (!scl)) {
+		rumboot_printf("Invalid I2C line state: SDA: %d SCL: %d \n", sda, scl);
+		goto bailout;
+	}
 	
+	rumboot_printf("START  \n");
+	//rumboot_printf("I2C line state: SDA: %d SCL: %d \n", sda, scl);
    //init
  	iowrite32((0x31),( I2C_BASE + I2C_PRESCALE));//I2C_PRESCALE
 	iowrite32(len, I2C_BASE + I2C_NUMBER);
-	iowrite32((0x00010001),( I2C_BASE + I2C_FIFOFIL));//I2C_FIFOFIL
+	iowrite32((0x00040001),( I2C_BASE + I2C_FIFOFIL));//I2C_FIFOFIL
 
 //-------------------------- I2C0 write 4 bytes into EEPROM-------
 	iowrite32((0xA0),( I2C_BASE + I2C_TRANSMIT));//I2C_TRANSMIT  ee -sensor dev. address
@@ -103,34 +116,45 @@ int main()
 	}
 	//-------------------------------I2C1 write---------------------------------
 	  //init
- 	iowrite32((0x31),( I2C_BASE +0x1000 + I2C_PRESCALE));//I2C_PRESCALE
+ 	//delay_cycle  (2000);
+		#ifdef CMAKE_BUILD_TYPE_POSTPRODUCTION
+		delay_cycle (2000);
+		#else
+		delay_cycle (100);	
+		#endif
+	
+	iowrite32((0x31),( I2C_BASE +0x1000 + I2C_PRESCALE));//I2C_PRESCALE
 	iowrite32(len, I2C_BASE  +0x1000+ I2C_NUMBER);
-	iowrite32((0x00010001),( I2C_BASE +0x1000+ I2C_FIFOFIL));//I2C_FIFOFIL
-
+	iowrite32((0x00040001),( I2C_BASE +0x1000+ I2C_FIFOFIL));//I2C_FIFOFIL
+   
 //-------------------------- write 4 byte into EEPROM-------
+	
 	iowrite32((0xA0),( I2C_BASE +0x1000 + I2C_TRANSMIT));//I2C_TRANSMIT  ee -sensor dev. address
 	iowrite32((0x00),( I2C_BASE +0x1000 + I2C_TRANSMIT));//first address, I2C_TRANSMIT
 	iowrite32((0x10),( I2C_BASE +0x1000 + I2C_TRANSMIT));//second address, I2C_TRANSMIT
 	iowrite32((0x63),( I2C_BASE +0x1000 + I2C_TRANSMIT)); //data
 	iowrite32((0x62),( I2C_BASE +0x1000	+ I2C_TRANSMIT)); //data
 	iowrite32((0x61),( I2C_BASE +0x1000 + I2C_TRANSMIT)); //data
-	iowrite32((0x60),( I2C_BASE +0x1000 + I2C_TRANSMIT)); //data
+	iowrite32((0x60),( I2C_BASE +0x1000 + I2C_TRANSMIT)); //data		
 	iowrite32((0x13),( I2C_BASE +0x1000 + I2C_CTRL));//I2c_CTRL   start, en, write
 	
-	
-			bool time_out2 = true;
 
-		  tmp =-1;
+			bool time_out2 = true;
+			
+			tmp =-1;		
         while (tmp != 0x10) {
-		tmp = ioread32( I2C_BASE +0x1000 + I2C_STATUS);
-		//rumboot_printf( "check trn_empty for WRITE:  \n");
+		if (delay_trn < 0x1000) delay_trn++;	
+		tmp = ioread32( I2C_BASE +0x1000 + I2C_STATUS);		
         tmp = 0x10 & tmp;        // if trn_empty
+		//rumboot_printf("delay_trn =0x%x\n",delay_trn);
 		 if (tmp ==0x10) time_out2 = false;
-		}   // while (tmp != 0x10	
-  
+		 if ( (delay_trn == 0x1000) & (tmp != 0x10)) goto next; 
+			}   // while (tmp != 0x10
+		
+	next:
 		if (i2c_is_nack(I2C_BASE +0x1000)) {
 			rumboot_printf( "error: WRITE operation nack_i2c1 while writing eeprom offset \n");
-			//goto first_crach;
+			goto bailout;
 		}
 		if (i2c_is_arblost(I2C_BASE +0x1000)) {
 			rumboot_printf( "error: WRITE operation arbitration lost while writing eeprom offset \n");
@@ -139,8 +163,9 @@ int main()
 
 	
 	if (time_out2) {
-		rumboot_printf("error: time_out while writing eeprom offset  \n");
-		goto bailout;
+		rumboot_printf("error: time_out while writing eeprom offset i2c1 \n");
+		 time_out2 = false;
+		//goto bailout;
 	}
 
 	//----------- intr check -----------------------------------
@@ -160,12 +185,13 @@ int main()
   }   // while (tmp != 0x400)
 //----------------------- - begin write_read instruction---------------
 
-        delay_cycle (2000);
-
-  iowrite32((0x0 ),(I2C_BASE + I2C_STAT_RST)); //I2C_STAT_RST
-  iowrite32((0x1 ),(I2C_BASE + I2C_STAT_RST)); //I2C_STAT_RST
-  iowrite32((0x0 ),(I2C_BASE + I2C_STAT_RST)); //I2C_STAT_RST
-
+    //delay_cycle (2000);
+	#ifdef CMAKE_BUILD_TYPE_POSTPRODUCTION
+	delay_cycle (2000);
+	//#else
+	//delay_cycle (500);	
+	#endif
+	i2c_stat_rst(I2C_BASE);
   	tmp = ioread32(I2C_BASE + I2C_STATUS);
 	scl = (tmp & (1 << 23)) ? 1 : 0;
 	sda = (tmp & (1 << 25)) ? 1 : 0;
@@ -180,8 +206,10 @@ int main()
  	iowrite32((0x31),( I2C_BASE + I2C_PRESCALE));//I2C_PRESCALE
 	iowrite32(len, I2C_BASE + I2C_NUMBER);
 	iowrite32((0x00010001),( I2C_BASE + I2C_FIFOFIL));//I2C_FIFOFIL
-   delay_cycle (20000); 
-  
+  // delay_cycle (20000); 
+    #ifdef CMAKE_BUILD_TYPE_POSTPRODUCTION
+	delay_cycle (20000);
+	#endif
 //----------------------- - begin write_read instruction---------------
 //data write instruction
  	iowrite32((0x00),( I2C_BASE + I2C_STATUS));//0x02C	reset I2C_STAT_RST
@@ -198,16 +226,17 @@ int main()
 //----------- intr check -----------------------------------
 
 	bool timeout = true;
-
 	tmp =-1;
         while (tmp != 0x10) {
 		tmp = ioread32( I2C_BASE + I2C_STATUS);
-		//rumboot_printf( "check trn_empty for READ:  \n");
         tmp = 0x10 & tmp;        // if trn_empty
 		timeout = false;
 		}
 		// rumboot_printf(" I2C line state after write: SDA: %d SCL: %d \n", sda, scl);
-		 delay_cycle (20000);
+		 //delay_cycle (20000);
+		#ifdef CMAKE_BUILD_TYPE_POSTPRODUCTION
+		delay_cycle (20000);			
+		#endif
 		if (i2c_is_nack(I2C_BASE)) {
 			rumboot_printf( "error: first nack2 while writing eeprom offset \n");		
 			goto bailout;
@@ -224,9 +253,7 @@ int main()
 	}
 	
 //----------------------------reset status before next start------
-	 iowrite32((0x0),( I2C_BASE + I2C_STATUS));  //0x02C	reset I2C_STAT_RST
-	 iowrite32((0x1 ),(I2C_BASE + I2C_STAT_RST)); //I2C_STAT_RST
-     iowrite32((0x0 ),(I2C_BASE + I2C_STAT_RST)); //I2C_STAT_RST
+	i2c_stat_rst(I2C_BASE);
 //---------------------------------------------------------				
 		
 //--data read instruction-------------------
@@ -245,13 +272,17 @@ int main()
 			if (have_bytes)
 				break;
 		}
+		#ifdef CMAKE_BUILD_TYPE_POSTPRODUCTION
 		delay_cycle (20000);
-		if (len &&  ((i2c_is_nack(I2C_BASE)) == 0)) {
+		#endif
+		if (len &&  (i2c_is_nack(I2C_BASE) ==0)) {
 				rumboot_printf( "error: unexpected nack at byte \n");
 				rumboot_printf("len =0x%x\n",len);
 				rumboot_printf("nack =0x%x\n",i2c_is_nack(I2C_BASE));
 				rumboot_printf("bit_nack =0x%x\n",(len && i2c_is_nack(I2C_BASE)));
-				goto bailout;
+				//rumboot_printf("bit2_nack =0x%x\n", (ioread32(I2C_BASE + I2C_STATUS) & (1<<9)));
+				
+				//goto bailout;
 			}
 
 		if (i2c_is_arblost(I2C_BASE)) {
@@ -276,7 +307,7 @@ int main()
 	bailout:
 	i2c_stat_rst(I2C_BASE);
 	i2c_reset(I2C_BASE);
-	rumboot_printf( "error  \n");
+	rumboot_printf( "TEST_ERROR  \n");
 	return 1;	
 	end:
 	i2c_reset(I2C_BASE);
