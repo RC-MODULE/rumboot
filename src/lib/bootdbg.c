cf3842c7f85f9aa76eaf33507586ed39921b8600
Andrew Andrianov <andrew@ncrmnt.org>
2019-05-08 13:12:01 +0300

    boot: Add missing bootgbg
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ src/lib/bootdbg.c
@@ -0,0 +1,14 @@
+#include <rumboot/boot.h>
+#include <rumboot/printf.h>
+#include <rumboot/platform.h>
+#include <algo/crc32.h>
+#include <platform/bootheader.h>
+
+void dbg_boot(const struct rumboot_bootsource * src, const char *fmt, ...)
+{
+	va_list args;
+	rumboot_printf("boot: %s: ", src ? src->name : "host");
+	va_start(args, fmt);
+	rumboot_vprintf(fmt, args);
+	va_end(args);
+}
