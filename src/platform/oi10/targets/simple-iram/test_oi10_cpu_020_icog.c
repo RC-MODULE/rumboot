96d378967a9c2b2ccfe7ab7513d8b5a27cac4a37
Chernousov Stepan <s.chernousov@modulew.ru>
2019-05-30 13:44:00 +0300

    oi10_rumboot: test_oi10_cpu_020_icog upd

--- src/platform/oi10/targets/simple-iram/test_oi10_cpu_020_icog.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_cpu_020_icog.c
@@ -60,7 +60,8 @@ static void check_func (uint32_t code_ev, uint32_t r_addr, uint32_t w_addr, bool
     iowrite32(CODE_BLR, w_addr);
 
     check_addr_in_l2c (w_addr, data_l2c_expect);
-    ici(2);
+    dcbf((void*)w_addr);
+    dci(2);
 }
 
 int main()
