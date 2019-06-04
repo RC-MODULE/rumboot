1a0adab86457df2101f45b3d838276a4a5c23814
Andrew Andrianov <andrew@ncrmnt.org>
2019-06-04 15:05:36 +0300

    basis: Fix spi1 redirector, that was broken due to API change
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/basis/targets/simple-iram/boot-spi1-redirect.c
+++ src/platform/basis/targets/simple-iram/boot-spi1-redirect.c
@@ -10,6 +10,19 @@
 #include <platform/devices.h>
 #include <regs/regs_sdio.h>
 #include <rumboot/io.h>
+#include <string.h>
+
+ struct rom_rumboot_bootsource {
+   const char *name;
+   uintptr_t base;
+   uint64_t offset;
+   uint32_t freq_khz;
+   uint32_t slave_addr;
+   const struct rumboot_bootmodule *plugin;
+   bool (*enable)   (const struct rumboot_bootsource* src, void* pdata);
+   void (*disable) (const struct rumboot_bootsource* src, void* pdata);
+   void (*chipselect)(const struct rumboot_bootsource* src, void* pdata, int select);
+ };
 
 int (*rom_bootsource_try_single)(const struct rumboot_bootsource *src, void *pdata, struct rumboot_bootheader *dst, size_t maxsize, size_t *offset);
 const struct rumboot_bootsource *(*rom_rumboot_platform_get_bootsources)();
@@ -19,29 +32,28 @@ void *(*rom_rumboot_platform_get_spl_area)(size_t *size);
 int boot_redirect()
 {
 	char pdata[128];
-        char buf[512];
         char redirname[64];
         strcpy(redirname, "SPI1 (CS: internal) [Redirection hack]");
         size_t splsize;
         size_t offset = 0;
 
-
-
-        rom_bootsource_try_single = 0x2ca8;
-        rom_rumboot_platform_get_bootsources = 0xc08;        
-        rom_rumboot_platform_get_spl_area = 0xbe8;
+        rom_bootsource_try_single = (void *) 0x2ca8;
+        rom_rumboot_platform_get_bootsources = (void *) 0xc08;        
+        rom_rumboot_platform_get_spl_area = (void *) 0xbe8; 
 
         void *dst = rom_rumboot_platform_get_spl_area(&splsize);
 	rumboot_printf("SPL from ROM: %x size %d bytes\n", dst, splsize);
-        struct rumboot_bootsource *romsrc = rom_rumboot_platform_get_bootsources();
-        struct rumboot_bootsource redir = romsrc[1];
+        struct rom_rumboot_bootsource *romsrc = (void *) rom_rumboot_platform_get_bootsources();
+        struct rom_rumboot_bootsource redir = romsrc[1];
         redir.name = redirname;
         redir.base = GSPI1_BASE;
-        return rom_bootsource_try_single(&redir, (void *) pdata, dst, splsize, &offset);       
+        return rom_bootsource_try_single((void *)&redir, (void *) pdata, dst, splsize, &offset);       
+
 
 }
 
 int main()
 {
+        rumboot_platform_runtime_info->silent = 0;
         return boot_redirect();        
 }
