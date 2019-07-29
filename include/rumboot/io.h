ca517e476cc4d54a30be607cde66f916d03acb99
Andrew Andrianov <andrew@ncrmnt.org>
2019-07-29 18:43:13 +0300

    nmc3: Add arch/io.h
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/rumboot/io.h
+++ include/rumboot/io.h
@@ -98,7 +98,7 @@ static inline uint8_t ioread8(uint32_t const base_addr);
  */
 static inline void iowrite8(uint8_t const value, uint32_t const base_addr);
 
-#ifdef __PPC__
+#if defined(__PPC__) || defined(__NM__)
     #include <arch/io.h>
 #endif
 
