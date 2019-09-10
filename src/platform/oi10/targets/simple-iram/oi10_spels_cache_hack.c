65b3c318139853a50cd2645da49c6bd3fb9f5522
Andrew Andrianov <andrew@ncrmnt.org>
2019-09-10 16:52:33 +0300

    oi10: Add hacky spels cache test
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ src/platform/oi10/targets/simple-iram/oi10_spels_cache_hack.c
@@ -0,0 +1,28 @@
+#include <rumboot/printf.h>
+#include <rumboot/macros.h>
+#include <platform/arch/ppc/ppc_476fp_mmu.h>
+#include <unistd.h>
+#include <platform/devices.h>
+#include <rumboot/memtest.h>
+
+int main()
+{
+    rumboot_printf("Writing TLB entry for SRAM0\n");
+    #ifndef TEST_NO_CACHE
+    static const tlb_entry sram0_tlb_entry[] = {
+//       MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
+        {MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b0,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )}
+    };
+    #else
+    static const tlb_entry sram0_tlb_entry[] = {
+//       MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
+        {MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )}
+    };
+    #endif
+
+    write_tlb_entries(sram0_tlb_entry, ARRAY_SIZE(sram0_tlb_entry));
+    rumboot_printf("Testing cached access\n");    
+    int err = memtest(SRAM0_BASE, 256 * 1024);
+    rumboot_printf("%d errors detected\n", err);    
+    return err ? 1 : 0;
+}
