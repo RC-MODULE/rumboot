a8b7010c36dcc6eed4fe55430fc051b6adcce5aa
Rishat Galiulin <r.galiulin@modulew.ru>
2019-09-16 17:52:22 +0300

    oi10_rumboot: Fix FPR MC checking

--- src/platform/oi10/targets/simple-iram/test_oi10_sys_011.c
+++ src/platform/oi10/targets/simple-iram/test_oi10_sys_011.c
@@ -36,173 +36,158 @@ enum {
 } test_event_code;
 
 enum {
-   	TEST_DATA_PROGG     = 0xFFFFFFFF,
-   	TEST_DATA_OK        = 0x12345678,
-	TEST_DATA_EVENT     = 0x00000002,
-   	TEST_DATA_ERROR     = 0x8E5917E8,
-   	TEST_DATA_UNDEF     = 0x8E591794,
+       TEST_DATA_PROGG     = 0xFFFFFFFF,
+       TEST_DATA_OK        = 0x12345678,
+       TEST_DATA_EVENT     = 0x00000002,
+       TEST_DATA_ERROR     = 0x8E5917E8,
+       TEST_DATA_UNDEF     = 0x8E591794,
 } test_data;
 
-int event_get()
+static int event_get(void)
 {
-	enum rumboot_simulation_event rumboot_event;
-	volatile uint32_t *data;
-	rumboot_event = rumboot_platform_event_get(&data);
-	if (rumboot_event != EVENT_TESTEVENT ){
-		rumboot_printf("Error event!\n");
-		return 1;
-	}
-	if (data [0] != TEST_DATA_EVENT){
-		rumboot_printf("Error event!\n");
-		return 1;
-	}
-	return 0;
+    enum rumboot_simulation_event rumboot_event;
+    volatile uint32_t *data;
+    rumboot_event = rumboot_platform_event_get(&data);
+    if (rumboot_event != EVENT_TESTEVENT ){
+        rumboot_printf("Error event!\n");
+        return 1;
+    }
+    if (data [0] != TEST_DATA_EVENT){
+        rumboot_printf("Error event!\n");
+        return 1;
+    }
+    return 0;
 }
 
-int check_systemstatus ()
+static int check_systemstatus(void)
 {
-	rumboot_printf("DEBUG_SYSTEMSTATUS check start!\n");
+    rumboot_printf("DEBUG_SYSTEMSTATUS check start!\n");
 
-	rumboot_platform_event_clear();
-	rumboot_printf("send TEC_CHECK_DEBUG_SYSTEMSTATUS\n");
-	test_event(TEC_CHECK_DEBUG_SYSTEMSTATUS );
-	if(event_get()) return 1;
+    rumboot_platform_event_clear();
+    rumboot_printf("send TEC_CHECK_DEBUG_SYSTEMSTATUS\n");
+    test_event(TEC_CHECK_DEBUG_SYSTEMSTATUS );
+    if(event_get()) return 1;
 
-	rumboot_printf("DEBUG_SYSTEMSTATUS check done!\n");
-	return 0;
+    rumboot_printf("DEBUG_SYSTEMSTATUS check done!\n");
+    return 0;
 }
 
-int check_uncondevent ()
+static int check_uncondevent(void)
 {
-	uint32_t dbsr;
-	rumboot_printf("DEBUG_UNCONDEVENT check start!\n");
+    uint32_t dbsr;
+    rumboot_printf("DEBUG_UNCONDEVENT check start!\n");
 
-	spr_write(SPR_DBSR_RC,0xFFFFFFFF); //clear dbsr
-	rumboot_printf("send TEC_CHECK_DEBUG_UNCONDEVENT\n");
-	test_event(TEC_CHECK_DEBUG_UNCONDEVENT );
+    spr_write(SPR_DBSR_RC,0xFFFFFFFF); //clear dbsr
+    rumboot_printf("send TEC_CHECK_DEBUG_UNCONDEVENT\n");
+    test_event(TEC_CHECK_DEBUG_UNCONDEVENT );
 
-	dbsr = spr_read(SPR_DBSR_RC);
-	spr_write(SPR_DBSR_RC,0xFFFFFFFF); //clear dbsr
-	if (!(dbsr & (1 << DEBUG_DBSR_UDE_i)) ){
-		rumboot_printf("DBSR UDE is not set!\n");
-		return 1;
-	}
+    dbsr = spr_read(SPR_DBSR_RC);
+    spr_write(SPR_DBSR_RC,0xFFFFFFFF); //clear dbsr
+    if (!(dbsr & (1 << DEBUG_DBSR_UDE_i)) ){
+        rumboot_printf("DBSR UDE is not set!\n");
+        return 1;
+    }
 
-	rumboot_printf("DEBUG_UNCONDEVENT check done!\n");
-	return 0;
+    rumboot_printf("DEBUG_UNCONDEVENT check done!\n");
+    return 0;
 }
 
-int check_halt ()
+static int check_halt(void)
 {
-	uint32_t dbdr;
+    uint32_t dbdr;
 
-	rumboot_printf("DEBUG_HALT check start!\n");
-	spr_write(SPR_DBDR, TEST_DATA_ERROR);
+    rumboot_printf("DEBUG_HALT check start!\n");
+    spr_write(SPR_DBDR, TEST_DATA_ERROR);
 
-	rumboot_platform_event_clear();
-	rumboot_printf("send TEC_CHECK_DEBUG_HALT\n");
-	test_event(TEC_CHECK_DEBUG_HALT );
+    rumboot_platform_event_clear();
+    rumboot_printf("send TEC_CHECK_DEBUG_HALT\n");
+    test_event(TEC_CHECK_DEBUG_HALT );
 
-	dbdr = spr_read(SPR_DBDR);
-	if (dbdr != TEST_DATA_OK ){
-		rumboot_printf("DEBUG_HALT check failed!\n");
-		return 1;
-	}
-	rumboot_printf("DEBUG_HALT check done!\n");
-	return 0;
+    dbdr = spr_read(SPR_DBDR);
+    if (dbdr != TEST_DATA_OK ){
+        rumboot_printf("DEBUG_HALT check failed!\n");
+        return 1;
+    }
+    rumboot_printf("DEBUG_HALT check done!\n");
+    return 0;
 }
 
-volatile static uint32_t mc_cnt = 0;
+static const float test_float    = 1.2345678f;
+static volatile uint32_t fpr_machine_check_handled = 0;
 
 static void exception_handler(int id, const char *name)
 {
-    rumboot_printf("\nWE GOT AN EXCEPTION: %d: %s\n", id, name);
-    if(id == RUMBOOT_IRQ_MACHINE_CHECK)
+    if( (id == RUMBOOT_IRQ_MACHINE_CHECK)
+     && (spr_read(SPR_MCSR_RW) & (1<<ITRPT_MCSR_FPR_i)) )
     {
-        rumboot_printf("It is OK!\n");
-        rumboot_printf("MSR:  0x%x\n", msr_read());
-        rumboot_printf("MCSR: 0x%x\n", spr_read(SPR_MCSR_C));
-        rumboot_printf("MCSRR0: 0x%x\n", spr_read(SPR_MCSRR0));
-        rumboot_printf("MCSRR1: 0x%x\n", spr_read(SPR_MCSRR1));
-	    mc_cnt += 1;
-	    spr_write(SPR_MCSR_C,0xFFFFFFFF);
+        rumboot_printf("Got MACHINE_CHECK on FPR. It is OK!\n");
+        fpr_machine_check_handled = 1;
+
+        msr_write( msr_read() | (1<<ITRPT_XSR_FP_i));
+
+        asm volatile   (
+            "lfs 1, 0(%0)\n\t"
+            "lfs 2, 0(%0)\n\t"
+            "msync\n\t"
+            "fcmpu 0, 1, 2\n\t"
+            ::"r"(&test_float)
+        );
+
+        spr_write(SPR_MCSR_C,0xFFFFFFFF);
     }
-    else
-    {
-        rumboot_printf("--- Guru Meditation ---\n");
-        rumboot_printf("MSR:  0x%x\n", msr_read());
-        rumboot_printf("SRR0: 0x%x\n", spr_read(SPR_SRR0));
-        rumboot_printf("SRR1: 0x%x\n", spr_read(SPR_SRR1));
-        rumboot_printf("CSRR0: 0x%x\n", spr_read(SPR_CSRR0));
-        rumboot_printf("CSRR1: 0x%x\n", spr_read(SPR_CSRR1));
-        rumboot_printf("MCSRR0: 0x%x\n", spr_read(SPR_MCSRR0));
-        rumboot_printf("MCSRR1: 0x%x\n", spr_read(SPR_MCSRR1));
-        rumboot_printf("---       ---       ---\n");
-      	rumboot_platform_panic("Please reset or power-cycle the board\n");
-    }
+    else rumboot_arch_exception(id, name);
 }
 
-long for_test = TEST_DATA_UNDEF;
-
-int check_machinecheck ()
+static int check_machinecheck(void)
 {
-	uint32_t const msr_old_value = msr_read();
-	uint32_t const ccr1_old_value = spr_read(SPR_CCR1);
-	uint32_t addr = (uint32_t)&for_test;
+    uint32_t const msr_old_value = msr_read();
+    uint32_t const ccr1_old_value = spr_read(SPR_CCR1);
 
-	rumboot_printf("DEBUG_MACHINECHECK check start!\n");
+    rumboot_printf("DEBUG_MACHINECHECK check start!\n");
 
-	spr_write(SPR_MCSR_C,0xFFFFFFFF);
-	msr_write(msr_old_value | (0b1 << ITRPT_XSR_FP_i)   /* MSR[FP] - Floating point available. */
+    rumboot_irq_set_exception_handler(exception_handler);
+    spr_write(SPR_MCSR_C,0xFFFFFFFF);
+    msr_write(msr_old_value | (0b1 << ITRPT_XSR_FP_i)   /* MSR[FP] - Floating point available. */
                             | (0b1 << ITRPT_XSR_ME_i)); /* MSR[ME] - Machine check enable.     */
 
-	/* Set our own handler */
-    rumboot_irq_set_exception_handler(exception_handler);
 
-	rumboot_printf("send TEC_CHECK_DEBUG_MACHINECHECK\n");
-	test_event(TEC_CHECK_DEBUG_MACHINECHECK );
+    rumboot_printf("send TEC_CHECK_DEBUG_MACHINECHECK\n");
+    test_event(TEC_CHECK_DEBUG_MACHINECHECK );
 
-	//Check C470S_DBGMACHINECHECK via ESR[ISMC]
-	spr_write(SPR_ESR,0x80000000);
-	spr_write(SPR_ESR,0x00000000);
-	spr_write(SPR_MCSR_C,0xFFFFFFFF);
+    //Check C470S_DBGMACHINECHECK via ESR[ISMC]
+    spr_write(SPR_ESR,0x80000000);
+    spr_write(SPR_ESR,0x00000000);
+    spr_write(SPR_MCSR_C,0xFFFFFFFF);
 
     //Other check C470S_DBGMACHINECHECK
-	asm volatile (
-			"lfs  1, 0(%0)\n\t"
-			"lfs  2, 0(%0)\n\t"
-			::"r"(addr)
-			: "memory"
-	);
-	msync();
+    spr_write( SPR_CCR1, ccr1_old_value | (0b11 << CTRL_CCR1_FPRPEI_i)); //Floating-Point Register (FPR) parity error insert.
+    isync();
 
-	spr_write( SPR_CCR1, ccr1_old_value | (0b11 << CTRL_CCR1_FPRPEI_i)); //Floating-Point Register (FPR) parity error insert.
-	isync();
+    asm volatile (
+            "lfs  1, 0(%0)\n\t"
+            "lfs  2, 0(%0)\n\t"
+            ::"r"(&test_float)
+            : "memory"
+    );
+    msync();
 
-	asm volatile (
-			"lfs  1, 0(%0)\n\t"
-			"lfs  2, 0(%0)\n\t"
-			::"r"(addr)
-			: "memory"
-	);
-	msync();
+    spr_write( SPR_CCR1, ccr1_old_value);
+    isync();
 
-	spr_write( SPR_CCR1, ccr1_old_value);
-	isync();
+    asm volatile ("fcmpu 0, 1, 2\n\t");
 
-	asm volatile ("fcmpu 0, 1, 2\n\t");
+    //Restore default exception handler
+    rumboot_irq_set_exception_handler(rumboot_arch_exception);
+    msr_write(msr_old_value);
 
-	spr_write(SPR_MCSR_C,0xFFFFFFFF);
-	msr_write(msr_old_value);
+    if(!fpr_machine_check_handled) return 1;
 
-	if(mc_cnt != 1) return 1;
+    rumboot_printf("DEBUG_MACHINECHECK check done!\n");
 
-	rumboot_printf("DEBUG_MACHINECHECK check done!\n");
-
-	return 0;
+    return 0;
 }
 
-int check_dbomask ()
+static int check_dbomask(void)
 {
     uint32_t dbdr;
 
@@ -226,7 +211,7 @@ int check_dbomask ()
     return 0;
 }
 
-int check_systemcontrol ()
+static int check_systemcontrol (void)
 {
     rumboot_printf("SYSTEMCONTROL check start!\n");
     spr_write(SPR_DBDR, TEST_DATA_ERROR);
@@ -244,24 +229,24 @@ int check_systemcontrol ()
 
 int main()
 {
-	test_event_send_test_id("test_oi10_sys_011");
+    test_event_send_test_id("test_oi10_sys_011");
 
-	if(check_systemstatus()) {
-		rumboot_printf("DEBUG_SYSTEMSTATUS test failed!");
-		return 1;
-	}
-	if(check_uncondevent()) {
-		rumboot_printf("DEBUG_UNCONDEVENT test failed!");
-		return 1;
-	}
-	if(check_halt()) {
-		rumboot_printf("DEBUG_HALT test failed!");
-		return 1;
-	}
-	if(check_machinecheck()) {
-		rumboot_printf("DEBUG_MACHINECHECK test failed!");
-		return 1;
-	}
+    if(check_systemstatus()) {
+        rumboot_printf("DEBUG_SYSTEMSTATUS test failed!");
+        return 1;
+    }
+    if(check_uncondevent()) {
+        rumboot_printf("DEBUG_UNCONDEVENT test failed!");
+        return 1;
+    }
+    if(check_halt()) {
+        rumboot_printf("DEBUG_HALT test failed!");
+        return 1;
+    }
+    if(check_machinecheck()) {
+        rumboot_printf("DEBUG_MACHINECHECK test failed!");
+        return 1;
+    }
     if(check_dbomask()) {
         rumboot_printf("DBOMask test failed!");
         return 1;
@@ -271,6 +256,6 @@ int main()
         return 1;
     }
 
-	rumboot_printf("TEST OK\n");
+    rumboot_printf("TEST OK\n");
     return 0;
 }
