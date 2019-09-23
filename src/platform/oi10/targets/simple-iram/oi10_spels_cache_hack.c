63ce73d869eb4c64df5dd61bae126e146ead89de
Andrew Andrianov <andrew@ncrmnt.org>
2019-09-23 15:33:17 +0300

    oi10: spels cache hack: init emi
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/oi10/targets/simple-iram/oi10_spels_cache_hack.c
+++ src/platform/oi10/targets/simple-iram/oi10_spels_cache_hack.c
@@ -4,26 +4,63 @@
 #include <unistd.h>
 #include <platform/devices.h>
 #include <rumboot/memtest.h>
+#include <platform/devices/emi.h>
+#include <platform/devices/l2c.h>
+
 
 //#define TEST_NO_CACHE
 int main()
 {
-    rumboot_printf("Writing TLB entry for SRAM0\n");
-    #ifndef TEST_NO_CACHE
+
+
+
+    rumboot_printf( "Init ext_clk \n");
+
+    uint32_t read_data;
+
+    dcr_write(0x8008003c,0x1ACCE551);
+
+    read_data = dcr_read(0x8008003c);
+
+    rumboot_printf( "read_data 0x80080130 = %x\n", read_data);
+
+    dcr_write(0x80080130,0x7);
+
+    read_data = dcr_read(0x80080130);
+
+    rumboot_printf( "read_data 0x80080130 = %x\n", read_data);
+
+    dcr_write(0x80080060,0x1);
+
+    read_data = dcr_read(0x80080060);
+
+    rumboot_printf( "read_data 0x80080060= %x\n", read_data);
+
+    read_data = dcr_read(0x80080130);
+
+    rumboot_printf( "read_data 0x80080130 = %x\n", read_data);
+
+    rumboot_printf("Writing TLB entry for SSRAM\n");
+
+#ifndef TEST_NO_CACHE
     static const tlb_entry sram0_tlb_entry[] = {
 //       MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
-        {MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b0,    0b0,    0b0,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )}
+        {MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b0,    0b1,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )}
     };
-    #else
+#else
     static const tlb_entry sram0_tlb_entry[] = {
 //       MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )
         {MMU_TLB_ENTRY(  0x000,  0x00000,    0x00000,    MMU_TLBE_DSIZ_1GB,      0b1,    0b1,    0b0,    0b1,    0b0,    0b0,    MMU_TLBE_E_BIG_END,     0b0,0b0,0b0,    0b1,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_0,       MMU_TLBWE_WAY_3,    MMU_TLBWE_BE_UND,   0b1 )}
     };
-    #endif
+#endif
 
     write_tlb_entries(sram0_tlb_entry, ARRAY_SIZE(sram0_tlb_entry));
-    rumboot_printf("Testing cached access\n");    
+    emi_init(DCR_EM2_EMI_BASE);
+    rumboot_printf("Testing cached access\n");
+
     int err = memtest(SRAM0_BASE, 256 * 1024);
-    rumboot_printf("%d errors detected\n", err);    
+    rumboot_printf("%d errors detected\n", err);
+
+
     return err ? 1 : 0;
 }
