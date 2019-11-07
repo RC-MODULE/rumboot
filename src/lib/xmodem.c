3fadf3c4606edbf2bc12c27396303cce7be6cb95
Andrew Andrianov <andrew@ncrmnt.org>
2019-11-07 16:23:48 +0300

    xmodem: Better error handling on a shitty serial line
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/xmodem.c
+++ src/lib/xmodem.c
@@ -57,7 +57,7 @@ int xmodem_chksum(int crcflag, const unsigned char *buffer, int size)
 
 void xmodem_flush_input(void)
 {
-	while(rumboot_getchar(XMODEM_TIMEOUT_DELAY) >= 0);
+	while(rumboot_getchar(XMODEM_TIMEOUT_DELAY / 2) >= 0);
 }
 
 static void memcpy_cb(size_t curpos, void *data, size_t length, void *arg)
@@ -81,7 +81,8 @@ size_t xmodem_get_async(size_t maxszs, void (*recv_cb)(size_t curpos, void *ptr,
 
 	while(retry > 0)
 	{
-		rumboot_platform_putchar(response);
+		if (response)
+			rumboot_platform_putchar(response);
 		if( (c = rumboot_getchar(XMODEM_TIMEOUT_DELAY)) >= 0)
 		{
 			switch(c)
@@ -112,6 +113,8 @@ size_t xmodem_get_async(size_t maxszs, void (*recv_cb)(size_t curpos, void *ptr,
 		else
 		{
 			retry--;
+			if (response != 'C')
+				response = 0;
 			continue;
 		}
 
@@ -182,3 +185,117 @@ size_t xmodem_get( char *to, size_t maxszs)
 {
 	return xmodem_get_async(maxszs, memcpy_cb, to);
 }
+
+#if 0
+void (*transmit_cb)(size_t curpos, void *ptr, size_t length, void *arg)
+{
+	
+}
+
+
+int xmodem_tx_async(void (*transmit_cb)(size_t curpos, void *ptr, size_t length, void *arg), int srcsz, void *arg)
+{
+	unsigned char xbuff[1030]; /* 1024 for XModem 1k + 3 head chars + 2 crc + nul */
+	int bufsz, crc = -1;
+	unsigned char packetno = 1;
+	int i, c, len = 0;
+	int retry;
+
+	for(;;) {
+		for( retry = 0; retry < 16; ++retry) {
+			if ((c = _inbyte((DLY_1S)<<1)) >= 0) {
+				switch (c) {
+				case 'C':
+					crc = 1;
+					goto start_trans;
+				case NAK:
+					crc = 0;
+					goto start_trans;
+				case CAN:
+					if ((c = _inbyte(DLY_1S)) == CAN) {
+						_outbyte(ACK);
+						flushinput();
+						return -1; /* canceled by remote */
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		_outbyte(CAN);
+		_outbyte(CAN);
+		_outbyte(CAN);
+		flushinput();
+		return -2; /* no sync */
+
+		for(;;) {
+		start_trans:
+			xbuff[0] = SOH; bufsz = 128;
+			xbuff[1] = packetno;
+			xbuff[2] = ~packetno;
+			c = srcsz - len;
+			if (c > bufsz) c = bufsz;
+			if (c >= 0) {
+				memset (&xbuff[3], 0, bufsz);
+				if (c == 0) {
+					xbuff[3] = CTRLZ;
+				}
+				else {
+					//memcpy (&xbuff[3], &src[len], c);
+					transmit_cb()
+					if (c < bufsz) xbuff[3+c] = CTRLZ;
+				}
+				if (crc) {
+					unsigned short ccrc = crc16_ccitt(&xbuff[3], bufsz);
+					xbuff[bufsz+3] = (ccrc>>8) & 0xFF;
+					xbuff[bufsz+4] = ccrc & 0xFF;
+				}
+				else {
+					unsigned char ccks = 0;
+					for (i = 3; i < bufsz+3; ++i) {
+						ccks += xbuff[i];
+					}
+					xbuff[bufsz+3] = ccks;
+				}
+				for (retry = 0; retry < MAXRETRANS; ++retry) {
+					for (i = 0; i < bufsz+4+(crc?1:0); ++i) {
+						_outbyte(xbuff[i]);
+					}
+					if ((c = _inbyte(DLY_1S)) >= 0 ) {
+						switch (c) {
+						case ACK:
+							++packetno;
+							len += bufsz;
+							goto start_trans;
+						case CAN:
+							if ((c = _inbyte(DLY_1S)) == CAN) {
+								_outbyte(ACK);
+								flushinput();
+								return -1; /* canceled by remote */
+							}
+							break;
+						case NAK:
+						default:
+							break;
+						}
+					}
+				}
+				_outbyte(CAN);
+				_outbyte(CAN);
+				_outbyte(CAN);
+				flushinput();
+				return -4; /* xmit error */
+			}
+			else {
+				for (retry = 0; retry < 10; ++retry) {
+					_outbyte(EOT);
+					if ((c = _inbyte((DLY_1S)<<1)) == ACK) break;
+				}
+				flushinput();
+				return (c == ACK)?len:-5;
+			}
+		}
+	}
+}
+#endif
