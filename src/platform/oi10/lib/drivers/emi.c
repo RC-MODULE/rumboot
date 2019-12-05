9d3db553cea66853a67f140c88537a064709c9f0
Alexander Chelyishev <a.chelyishev@module.ru>
2019-12-05 17:59:49 +0300

    fix timings emi for NOR
    Signed-off-by: Alexander Chelyishev <a.chelyishev@module.ru>

--- src/platform/oi10/lib/drivers/emi.c
+++ src/platform/oi10/lib/drivers/emi.c
@@ -455,7 +455,7 @@ void emi_init_impl (uint32_t const emi_dcr_base, uint32_t const plb6mcif2_dcr_ba
             SST_Flow_Through,
             TSSOE_1,
             TSOE_1,
-            TCYC_8,
+            TCYC_12,
             0, //T_RDY
             TDEL_0
         },
