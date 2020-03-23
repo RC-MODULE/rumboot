031bae024d3578301cd020179c420c41246908c5
v.koshlyak <v.koshlyak@module.ru>
2020-03-23 14:07:49 +0300

    averaging

--- src/platform/mm7705/targets/tests/tvsens.c
+++ src/platform/mm7705/targets/tests/tvsens.c
@@ -5,6 +5,8 @@
 #include <platform/regs_tvsens.h>
 #include <platform/devices.h>
 
+#define OUT_OF_RANGE    0xE7707
+
 static const int temp_table[] = {
 	650	,	-40	,
 	642	,	-35	,
@@ -42,35 +44,29 @@ static const int temp_table[] = {
 	390	,	125	,
 };
 
-int main() {
-    volatile uint32_t t0, t1, t2, i;
+uint32_t get_tC(volatile uint32_t t0) {
+    uint32_t t1, t2, i;
     double tC;
 
-    rumboot_printf("tvsens: start\n");
+    t2 = (t0 >> 20) & 0x3FF;
+    t1 = (t0 >> 10) & 0x3FF;
+    t0 = t0 & 0x3FF;
 
-    while (1) {
-        for (i = 0; i<800000;) { i++; }
+    //rumboot_printf("%d %d %d; ", t0, t1, t2);
 
-        t0 = ioread32 (TVSENS_BASE + TAVG);
+    // t0 - min value, max temperature
+    t0 = t0 < t1 ? t0 : t1;
+    t0 = t0 < t2 ? t0 : t2;
 
-        t2 = (t0 >> 20) & 0x3FF;
-        t1 = (t0 >> 10) & 0x3FF;
-        t0 = t0 & 0x3FF;
+    // t1 - max value, min temperature
+    t1 = t1 > t2 ? t1 : t2;
 
-        //rumboot_printf("%d %d %d; ", t0, t1, t2);
+    if ((t0 < 390) || (t1 > 650)) {
+        rumboot_printf("ERROR: out of range min %d max %d\n", t0, t1);
 
-        // t0 - min value, max temperature
-        t0 = t0 < t1 ? t0 : t1;
-        t0 = t0 < t2 ? t0 : t2;
-
-        // t1 - max value, min temperature
-        t1 = t1 > t2 ? t1 : t2;
-
-        if ((t0 < 390) || (t1 > 650)) {
-            rumboot_printf("ERROR: out of range min %d max %d\n", t0, t1);
-            break;
-        }
-
+        t0 = OUT_OF_RANGE;
+    }
+    else {
         for (i=0; i<68; i+=2) { if (t0 >= temp_table[i]) break; }
 
         // 5 = temp_table[i+1] - temp_table[i-1]
@@ -78,18 +74,41 @@ int main() {
             tC = temp_table[i-1] + (temp_table[i-2] - t0)*5.0/(temp_table[i-2]-temp_table[i]);
 
             t0 = (int)tC;   // int
-            //tC = (tC - (int)tC)*100;
-            //t1 = (int)tC;   // decimal
         }
-        else {
-            t0 = temp_table[i+1];
-            //t1 = 0;
-        }
+        else { t0 = temp_table[i+1]; }
+    }
 
-        // t real <= tC
-        //rumboot_printf ("tC = %d.%d\n", t0, t1);
+    return t0;
+}
+
+void tvsens_tavg() {
+    volatile uint32_t t0, i;
+
+    rumboot_printf("tvsens_tavg: start\n");
+
+    while (1) {
+        for (i = 0; i<800000;) { i++; }
+
+        t0 = ioread32 (TVSENS_BASE + TAVG);
+
+        t0 = get_tC(t0);
+        if (t0 == OUT_OF_RANGE) continue;
+
         rumboot_printf ("tC = %d\n", t0);
     }
+}
+
+int main() {
+    volatile uint32_t tmp;
+
+    rumboot_printf("tvsens: start\n");
+
+    tmp = (0x10<<5) ^ 0x10;
+    iowrite32 (tmp, TVSENS_BASE+TCTRL_AVG);
+    //tmp = ioread32 (TVSENS_BASE+TCTRL_AVG);
+    //rumboot_printf ("TCTRL_AVG: 0x%x\n", tmp);
+
+    tvsens_tavg();
 
     return 0;
 }
