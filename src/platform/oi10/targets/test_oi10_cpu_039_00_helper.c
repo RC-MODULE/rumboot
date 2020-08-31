2e24966d279193918776ace40ffadae1710d2925
Dmitry Dryagalkin <didryaga@module.ru>
2020-08-31 17:20:18 +0300

    OI10-457: Fixed test_oi10_cpu_039_00_helper.c

--- src/platform/oi10/targets/test_oi10_cpu_039_00_helper.c
+++ src/platform/oi10/targets/test_oi10_cpu_039_00_helper.c
@@ -134,6 +134,53 @@ int __attribute__((section(".text.test"))) check_cpu_ppc_039(void)
         ".set r25, 25               \n\t" // CR copy
         ".set r26, 26               \n\t" // 1 - Direct values , 2 - Inverse Values
         "    addi r26, r0, 0x0    \n\t" // 1 - direct values error, 2 - inverse values errors, 0 - no errors
+// clear only conflict lines
+        "    addis r4, r0, 0x0    \n\t" // number of inerrations
+        "    ori   r4, r4, 0x4    \n\t" // 
+        "    mtctr r4             \n\t" // ctr - number of iterations, 4x8=32, cache line=128*8=1024, 1024/32=32, ctr=ctr/32
+        "    mr r4, (%3)          \n\t" // addr
+        "    addis r5, r0, 0x0    \n\t" //
+        "l0_0: dcbz r5, r4        \n\t" //
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t"
+        "    dcbz r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5, 0x80    \n\t" 
+        "    addis r5, r5, 0x1    \n\t" // r5 = r5 + 0x0001_0000
+        "    bdnz+ l0_0           \n\t"
+        "    addis r4, r0, 0x0    \n\t" // number of interations
+        "    ori   r4, r4, 0x4    \n\t" // number of interations = number of interations / 4 / 8
+        "    mtctr r4             \n\t" // ctr - number of iterations, 4x8=32, cache line=128*8=1024, 1024/32=32, ctr=ctr/32
+        "    mr r4, (%3)          \n\t" // addr
+        "    addis r5, r0, 0x0    \n\t" //
+        "l0_1: dcbf r5, r4        \n\t" //
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t"
+        "    dcbf r5, r4          \n\t" // two commands in the store loop
+        "    addi r5, r5,  0x80   \n\t" 
+        "    addis r5, r5, 0x1    \n\t" // r5 = r5 + 0x0001_0000
+        "    bdnz+ l0_1           \n\t"
 // store loop 1
         "    mtctr (%4)           \n\t" // ctr - number of iterations
         "    mr r4, (%3)          \n\t" // addr
