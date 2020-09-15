ec48de0c9f5f7bb56b5fff52937b95c2ca3b1b31
Nina Kutsak <n.kutsak@module.ru>
2020-09-15 12:07:11 +0300

    add demonstrator regs names

--- include/platform/scr1/platform/devices.h
+++ include/platform/scr1/platform/devices.h
@@ -1,15 +1,16 @@
 #ifndef DEVICES_H
 #define DEVICES_H
 
-#define SCR1_TIMER_BASE     0x00490000
-#define DUT_BASE            0x00498000
-#define EXT_MEMORY_BASE     0x004A0000
+#define SCR1_TIMER_BASE         0x00490000
+#define DUT_BASE                0x00498000
+#define EXT_MEMORY_BASE         0x004A0000
 
-#define SPI_CONTROL_BASE    DUT_BASE
-#define SPI_FIFO_BASE       (DUT_BASE + 0x00002000)
+#define SPI_CONTROL_BASE        DUT_BASE
+#define SPI_FIFO_BASE           (DUT_BASE + 0x00002000)
 
-#define MDMA_BASE           DUT_BASE
+#define MDMA_BASE               DUT_BASE
 
-#define DEMONSTRATOR_BASE   (DUT_BASE + 0x00002000)
+#define DEMONSTRATOR_APB_BASE   DUT_BASE
+#define DEMONSTRATOR_MDMA_BASE  (DUT_BASE + 0x00002000)
 
 #endif /* end of include guard: DEVICES_H */
--- include/regs/regs_demonstrator.h
+++ include/regs/regs_demonstrator.h
@@ -1,21 +1,23 @@
-// !!! copy from toplevel/units/demonstrator/test/v_test/env/config.v
-
-// ================ Address definition ===============
-// -------- System --------
-// #define M_ID                0x00 // Read only
-// #define PUMP                0x04 // Read only
-// #define CNTT                0x08 // Read only
-// #define ENAB                0x0C
-// #define DATA                0x10
-// #define CRST                0x14
-// #define PMEN                0x18
-// #define WCNT                0x1C
-// #define DCNT                0x20
-// #define RCNT                0x24
-// #define COMP                0x28
-
-
 #define DATA_BASE           0xFFF00000
+#define COEFFICIENT_BASE    0xFFF40000
 #define WEIGHT_BASE         0xFFF80000
 #define RESULT_BASE         0xFFFC0000
 #define EXT_MEMORY_BASE     0x004A0000
+
+#define NA_MXID             0x000
+#define NA_PUMP             0x004
+#define NA_CNTT             0x008
+#define NA_ENAB             0x00C
+#define NA_FADR             0x010
+#define NA_CRST             0x014
+#define NA_PMEN             0x018
+#define NA_WCNT             0x01C
+#define NA_DCNT             0x020
+#define NA_CCNT             0x024
+#define NA_RCNT             0x028
+#define NA_COMP             0x02C
+#define NA_STGS             0x030
+#define NA_ACTS             0x034
+#define NA_WCHG             0x038
+
+ 
