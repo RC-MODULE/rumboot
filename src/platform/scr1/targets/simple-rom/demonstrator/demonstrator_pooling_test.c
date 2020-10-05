379500c61e3610f7a1937eb4be94484c8021be12
Lyudmila Alexandrova <laalexan.module.ru>
2020-10-05 12:35:45 +0300

    change pointer16 t0 32

--- src/platform/scr1/targets/simple-rom/demonstrator/demonstrator_pooling_test.c
+++ src/platform/scr1/targets/simple-rom/demonstrator/demonstrator_pooling_test.c
@@ -19,28 +19,28 @@
   #define dbg(x, ...)
 #endif
 
-int load_data_for_pooling_array (  uint32_t len, int16_t* dump_ptr[]) {
+int load_data_for_pooling_array (  uint32_t len, int32_t* dump_ptr[]) {
 
-	uint16_t i;
-	uint16_t data;
+	uint32_t i;
+	uint32_t data;
 	
 	for (i = 0; i< len ; i++){
-	data = i; //rand;	
+	
+		data =(( 2*i +1)<< 16) + 2*i;  ;//[i]= (2*i + (2*i + 1) <<16);//i; //rand;	
 	dump_ptr[i] =  data ;	
 	
 	//rumboot_printf(" address_data=0x%x\n", &dump_ptr[i]);
 	rumboot_printf(" input data=0x%x\n", (dump_ptr[i]));
 	
-	
 	}
 	return 0;		
 }
 
-int load_etalon_array (  uint32_t len, int16_t* dump_ptr[]) {  
+int load_etalon_array (  uint32_t len, int32_t* dump_ptr[]) {  
 
 	uint32_t i;
-	uint16_t etalon;
-	uint16_t data[len-1];
+	uint32_t etalon;
+	uint32_t data[len-1];
     uint32_t pooling_mode;
 	
 	pooling_mode = 0x00000100 & ioread32(DEMONSTRATOR_APB_BASE + NA_ACTS);
@@ -48,41 +48,26 @@ int load_etalon_array (  uint32_t len, int16_t* dump_ptr[]) {
   if (pooling_mode ==0){
 	for (i = 0; i< len ; i++){
 		
-		data[i] =i;	//rand;
-		data[i+1] =i+1;	//rand;
-		data[i+2] =i+2;	//rand;
-		data[i+3] =i+3;	//rand;
+		data[i] = 2*i + (2*i + 1) + (2*i+2) + (2*i +3);//i + ((i +1) << 16);	//rand;			
 		
-		if ((i % 4) ==0) 
-		etalon = (data[i] + data[i+1] +  data[i+2] + data[i+3]) /2;
-		if	((i % 4) !=0)		
+		if ((i % 2) ==0)		
+		etalon = data[i]/4;
+		if ((i % 2) !=0)
 		etalon = 0;	
 		dump_ptr[i] = etalon;
 		rumboot_printf(" i=0x%x\n", i);
 		rumboot_printf(" etalon=0x%x\n", etalon);
 	}
   }	
- else if	(pooling_mode !=0) {
+ else if (pooling_mode !=0) {
 	for (i = 0; i< len ; i++){
-	
-		data[i] = i;	//rand;
-		data[i+1] =i+1;	//rand;
-		data[i+2] =i+2; //rand;
-		data[i+3] =i+3; //rand;
-				
-		if  (((i % 4) ==0)&(data[i]>= data[i+1]) & (data[i] >= data[i+2]) & (data[i] >= data[i+3]))
-		etalon = i;		
-		if (((i % 4) ==0)&(data[i+1] >=data[i]) & (data[i+1] >= data[i+2]) & (data[i+1] >= data[i+3]))
-		etalon = i+1;		
-		if ( ((i % 4) ==0)&(data[i+2] >=data[i+1]) & (data[i+2] >=data[i+3]) & (data[i+2] >=data[i]))		
-		etalon = i+2;
-		if (( (i % 4) ==0) &(data[i+3] >=data[i]) & (data[i+3] >= data[i+2]) & (data[i+3] >= data[i+1]))
-	    etalon =i+3;
-		
-if	((i % 4) !=0)		
-	etalon = 0;	
 
-	dump_ptr[i] = etalon;	//data[i];
+		if  ((i % 2) ==0)
+		etalon = 2*i+3;
+		if	((i % 2) !=0)		
+		etalon = 0;	
+
+		dump_ptr[i] = etalon;	//data[i];
 
 	rumboot_printf(" i=0x%x\n", i);
 	rumboot_printf(" etalon=0x%x\n", etalon);
@@ -92,46 +77,37 @@ if	((i % 4) !=0)
 } 
 
 
-int pooling_compare_data(int16_t* res, int16_t* src_etalon, uint32_t size)
+int pooling_compare_data(int32_t* res, int32_t* src_etalon, uint32_t size)
 {
   uint32_t pooling_mode;
   pooling_mode = 0x00000100 & ioread32(DEMONSTRATOR_APB_BASE + NA_ACTS);
   
   for (int i=0; i<size; i++)
   {
-  if	(((res[i] != (src_etalon[2*i] >> 3))& ( pooling_mode ==0)) |
-		((res[i] != (src_etalon[2*i] >> 1))& ( pooling_mode !=0)))
-    {
+
+   if	(res[i] != src_etalon[i])  
+	{
     rumboot_printf("read_in_data 0x%x\n",   res[i]);
 	rumboot_printf("read_in_etal 0x%x\n",   src_etalon[i]);
-	
-	rumboot_printf("read_etalon1 0x%x\n", (src_etalon[2*i] >> 1)); //to fix
-	rumboot_printf("read_etalon2 0x%x\n", (src_etalon[2*i] >> 3) ); //to fix
-	
-	rumboot_printf("adr_data 0x%x\n",  &res[2*i]);
-	rumboot_printf("adr_etal 0x%x\n",  &src_etalon[2*i]);
-	//rumboot_printf("adr_etal 0x%x\n",  &src_etalon[i]);
+		
     rumboot_printf("i=0x%x\n", i);
     rumboot_printf("POOLING test ERROR!\n");
- return 1;
+return 1;
     }
 
   }
   return 0;
 }
 
-
-
-
 int main() {
   uint32_t src_vectors = 2;//32;
   uint32_t src_data_array_size = src_vectors * 64 * 2; // Each Vector Has 64 int16
-  //uint32_t dst_data_array_size = src_vectors * 64 * 2; // Each Vector Has 16 int64
+  //uint32_t dst_data_array_size = src_vectors * 64 * 2; // Each Vector Has 64 int16
   int ret;
-  uint32_t pooling_mode =0x1;
-  int16_t* src_data;
-  int16_t* src_etalon;
-  int16_t* dst_data;
+  uint32_t pooling_mode =0x0;
+  int32_t* src_data;
+  int32_t* src_etalon;
+  int32_t* dst_data;
 
  
   rumboot_printf("TEST pooling start\n");
@@ -151,7 +127,7 @@ int main() {
     return 1;
   } 
   
-  ret=load_data_for_pooling_array(src_data_array_size/2,  src_data);
+  ret=load_data_for_pooling_array(src_data_array_size/4,  src_data);
     if(ret!=0){
     rumboot_printf("ERROR loading  source data\n");
     return 1;
@@ -176,14 +152,14 @@ int main() {
 
 	rumboot_printf(" demonstrator result array was loaded by DMA\n");
    
-	ret = load_etalon_array(src_data_array_size/2, src_etalon);
+	ret = load_etalon_array(src_data_array_size/4, src_etalon);
 	if(ret!=0){
     rumboot_printf("ERROR loading etalon\n");
 		return 1;
   }
  
 	rumboot_printf(" compare demonstrator results\n");	
-	ret = pooling_compare_data((int16_t*)dst_data,(int16_t*)src_etalon,(src_data_array_size/2));	
+	ret = pooling_compare_data((int32_t*)dst_data,(int32_t*)src_etalon,(src_data_array_size/4));	
 	if (ret) {
 	rumboot_printf(" compare pooling result ERROR\n");
 	rumboot_printf("TEST pooling operation FAILED\n");
