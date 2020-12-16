c818805c5b5b54b28a2923b802a92a57efe1d1ff
Daniel Kaltakhchyan <d.kaltakhchyan@module.ru>
2020-12-16 18:26:05 +0300

    Rewrite interconnect test test_oi10_sys_012.c to correspon with new interconnect structure of o32t, change regs_nic400.h for the same reason; Create devices.h that includes it version from oi10 and changes addresses of interconnect config registers, ad utlb_entries.S from oi10 with addition of additional region 0xC0400000 to it

---
+++ include/platform/o32t/devices.h
@@ -0,0 +1,7 @@
+#include <platform/oi10/platform/devices.h>
+#undef AXI32HSIFS_CTRL_BASE
+#undef AXI64HSIFS_CTRL_BASE
+#define AXI32_LEFT_CTRL_BASE    0xC0100000
+#define AXI32_RIGHT_CTRL_BASE   0xC0200000
+#define AXI64_CTRL_BASE         0xC0400000
+
