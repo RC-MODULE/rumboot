38ba2a79cb5b9bc56705adc91847425b3ef508dd
Andrew Andrianov <andrew@ncrmnt.org>
2021-01-15 15:40:25 +0300

    o32t: Add easter egg quote
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/platform/o32t/platform/bootheader.h
+++ include/platform/o32t/platform/bootheader.h
@@ -5,5 +5,9 @@
 #define RUMBOOT_PLATFORM_CHIPREV  2
 #define RUMBOOT_PLATFORM_NUMCORES 1
 
+#define RUMBOOT_PLATFORM_EASTER_EGG "I am a part of you, little mortal. I am a shadow in your subconscious, a \n"\
+                                    "blemish on your fragile little psyche. You know me. You just don't know it. \n"\
+                                    "-- Sheogorath"
+
 
 #endif /* end of include guard: PLATFORM_BOOTHEADER_h */
