17289d41bf6cf1793e14f137e43c86c2f22ffdda
Lyudmila Alexandrova <laalexan.module.ru>
2021-02-01 15:32:27 +0300

    add test commport

--- include/platform/o32t/platform/interrupts.h
+++ include/platform/o32t/platform/interrupts.h
@@ -4,6 +4,9 @@
 /* Place all O32-specific interrupts here. 
    Use #undef carefully where needed
 */
-
+#define CP0_RCV_INT                    0x4E
+#define CP0_TRM_INT                    0x4F
+#define CP1_RCV_INT                    0x50
+#define CP1_TRM_INT                    0x51
 #endif // !O32T_INTERRUPTS_H
 
