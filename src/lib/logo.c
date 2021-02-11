ab541d6116774440351d22ecb5fa678f108fa9f2
Andrew Andrianov <andrew@ncrmnt.org>
2021-02-11 14:32:19 +0300

    logo: Update logo for V3
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/logo.c
+++ src/lib/logo.c
@@ -2,11 +2,19 @@
 #include <rumboot/version.h>
 static const char rumboot_logo[] =
 "\n\n\n"
-"    RC Module's          __                __ \n"
-"   _______  ______ ___  / /_  ____  ____  / /_\n"
-"  / ___/ / / / __ `__ \\/ __ \\/ __ \\/ __ \\/ __/\n"
-" / /  / /_/ / / / / / / /_/ / /_/ / /_/ / /_  \n"
-"/_/   \\__,_/_/ /_/ /_/_.___/\\____/\\____/\\__/  \n";
+" ,ggggggggggg,                               ,ggggggggggg,\n"
+"dP\"\"\"88\"\"\"\"\"\"Y8,                            dP\"\"\"88\"\"\"\"\"\"Y8,                        I8\n"
+"Yb,  88      `8b                            Yb,  88      `8b                        I8   \n"
+" `\"  88      ,8P                             `\"  88      ,8P                     88888888\n"
+"     88aaaad8P\"                                  88aaaad8P\"                         I8   \n"
+"     88\"\"\"\"Yb,    gg      gg   ,ggg,,ggg,,ggg,   88\"\"\"\"Y8ba    ,ggggg,    ,ggggg,   I8\n"
+"     88     \"8b   I8      8I  ,8\" \"8P\" \"8P\" \"8,  88      `8b  dP\"  \"Y8gggdP\"  \"Y8gggI8\n"
+"     88      `8i  I8,    ,8I  I8   8I   8I   8I  88      ,8P i8'    ,8I i8'    ,8I ,I8,  \n"
+"     88       Yb,,d8b,  ,d8b,,dP   8I   8I   Yb, 88_____,d8',d8,   ,d8',d8,   ,d8',d88b, \n"
+"     88        Y88P'\"Y88P\"`Y88P'   8I   8I   `Y888888888P\"  P\"Y8888P\"  P\"Y8888P\"  8P\"\"Y8\n";
+                                                                                         
+                                                                                         
+            
 
 static const char rumboot_host_logo[] = ""
 "\n";
@@ -17,7 +25,8 @@ void rumboot_print_logo()
 {
     rumboot_printf(rumboot_logo);
     const struct rumboot_version_info *inf = rumboot_version_info();
-    rumboot_printf("%s | %s | %s \n", inf->platform, CMAKE_BUILD_TYPE, inf->gitversion);
+    rumboot_printf("     The Third Generation ROM Loader\n");
+    rumboot_printf("     %s | %s | %s \n\n", inf->platform, CMAKE_BUILD_TYPE, inf->gitversion);
 }
 
 void rumboot_print_host_logo()
