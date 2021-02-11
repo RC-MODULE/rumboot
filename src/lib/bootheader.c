78240d7bed50432240bcb3e06068bd2aa8a5f8e3
Andrew Andrianov <andrew@ncrmnt.org>
2021-02-11 14:28:39 +0300

    bootheader: Make sure it is packed
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/bootheader.c
+++ src/lib/bootheader.c
@@ -5,16 +5,18 @@
 extern int main(int argc, const char *argv[]);
 
 static const __attribute__((used)) __attribute__((section(".header")))
-struct rumboot_bootheader hdr = {
+struct rumboot_bootheader hdr __attribute__((packed)) = {
 	.magic		= RUMBOOT_HEADER_MAGIC,
 	.version	= RUMBOOT_HEADER_VERSION,
 	.chip_id	= RUMBOOT_PLATFORM_CHIPID,
 	.chip_rev	= RUMBOOT_PLATFORM_CHIPREV,
 	.entry_point	= {
-#ifndef RUMBOOT_NOINIT
-		(uint32_t)&rumboot_main,
-#else
+#if defined(RUMBOOT_NOINIT)
 		(uint32_t)&main,
+#elif defined(RUMBOOT_NOENTRY)
+		0x0,
+#else
+		(uint32_t)&rumboot_main,
 #endif
 	}
 };
