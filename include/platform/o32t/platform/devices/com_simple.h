cd0cd655656db9964cf422b8686d95f8765118ef
Lyudmila Alexandrova <laalexan.module.ru>
2021-02-22 15:37:56 +0300

    comport func change

--- include/platform/o32t/platform/devices/com_simple.h
+++ include/platform/o32t/platform/devices/com_simple.h
@@ -8,6 +8,7 @@
 #define COM_CONTROL_EN  0x1
 #define COM_CONTROL_CPL 0x2
 
+//#define COM_DO_DELAY	0x0
 #define COM_COUNT 		  0x40
 /*
 typedef struct com_cfg {
@@ -25,6 +26,8 @@ typedef struct com_cfg {
   uint8_t   WR_InterruptMask;
 } com_cfg_t;
 */
+int comp_dma_run_tr(uint32_t addr, uint32_t base, uint32_t mask);
+int comp_dma_run_rcv(uint32_t addr, uint32_t base, uint32_t mask);
 int comp_dma_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1 );
 int comp_dma_irq_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1,uint32_t COM0_Cpl,uint32_t COM1_Cpl  );  
 int com_simple_wait_complete(uintptr_t base, uintptr_t ctrl);
