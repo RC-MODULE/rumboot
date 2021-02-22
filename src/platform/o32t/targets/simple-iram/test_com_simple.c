5be1753f47501a1646b4b00d1a61f2d430e182c0
Lyudmila Alexandrova <laalexan.module.ru>
2021-02-22 16:03:17 +0300

    change commport func for test lib

--- src/platform/o32t/targets/simple-iram/test_com_simple.c
+++ src/platform/o32t/targets/simple-iram/test_com_simple.c
@@ -15,26 +15,7 @@
 #include <arch/ppc/arch/ppc_476fp_mmu_fields.h>
 #include <arch/ppc/arch/ppc_476fp_mmu.h>
 
-/*
-com_cfg_t cfg = {
-  .RD_MainCounter  =0x0,
-  .RD_Address      =(uint32_t)0x0,
-  .RD_Bias         =0x0,
-  .RD_RowCounter   =0x0,
-  .RD_AddressMode  =ONE_DIMENSION,
-  .RD_InterruptMask=COM_RD_CONTROL_EN|COM_RD_CONTROL_CPL,
-  .RD_IH_EN_tr         =0x0,
-  .WR_MainCounter     =0x0,
-  .WR_Address      =(uint32_t)0x0,
-  .WR_Bias         =0x0,
-  .WR_RowCounter   =0x0,
-  .WR_AddressMode  =ONE_DIMENSION,
-  .WR_InterruptMask   =COM_WR_CONTROL_EN|COM_WR_CONTROL_CPL,
-  .WR_PHYConfig         =0x0,
-  .WR_Err1CNT_rcv         =0x0,
-  .WR_Err2CNT_rcv         =0x0 
-};
-*/
+
 #define ARR_SIZE 128
 
 #ifndef COM_SRC_HEAP
@@ -63,7 +44,7 @@ static struct tlb_entry const em2_nospeculative_tlb_entries[] =
 
 int main()
 {
-	uint32_t result; 
+	uint32_t result =0;
 	uint32_t * src0;
 	uint32_t * dst0;
 	uint32_t * src1;
@@ -74,15 +55,18 @@ int main()
     prepare_arrays( &src0, &dst0 );
     prepare_arrays( &src1, &dst1 );
     if( (comp_dma_run( rumboot_virt_to_dma(src0), rumboot_virt_to_dma(dst0),COM0_BASE,COM1_BASE ) != 0)
-    || (memcmp( src0, dst0, sizeof(uint32_t)*ARR_SIZE  ) != 0) ) {
+ ||	memcmp( src0, dst0, sizeof(uint32_t)*ARR_SIZE  ) != 0){
         result = 1;
-	 }  
+	 } 
+
+	 
  	 rumboot_printf("COMMPORT0 to COMMPORT1 direction checked\n"); 
 	if( (comp_dma_run( rumboot_virt_to_dma(src1), rumboot_virt_to_dma(dst1),COM1_BASE,COM0_BASE ) != 0)
-     || (memcmp( src1, dst1, sizeof(uint32_t)*ARR_SIZE  ) != 0) ) {
+	||	(memcmp( src1, dst1, sizeof(uint32_t)*ARR_SIZE  ) != 0)){
         return 1;
 	 }
-   rumboot_printf("COMMPORT1 to COMMPORT0 direction checked\n"); 	 
-  return 0;
+
+   rumboot_printf("COMMPORT1 to COMMPORT0 direction checked\n"); 
+  return result;
 }
 
