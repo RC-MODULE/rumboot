3be059c70e51191b64dc7b804ec741f532461409
Lyudmila Alexandrova <laalexan.module.ru>
2021-02-22 15:56:36 +0300

    change commport func for test lib

--- src/platform/o32t/lib/drivers/com_simple.c
+++ src/platform/o32t/lib/drivers/com_simple.c
@@ -11,28 +11,56 @@
 #include <platform/devices.h>
 #include <platform/devices/com_simple.h>
 
- int comp_dma_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1 ) {
+
+int comp_dma_run_tr( uint32_t addr, uint32_t base, uint32_t mask ) {
     //write data
- iowrite32( COM_COUNT, base0 + MainCounter_tr ); //set dma total data 512 byte
-	iowrite32( src_addr, base0 + Address_tr ); //dma source atart address
-	iowrite32( 0x0, base0 + Bias_tr );
-	iowrite32( 0x0, base0 + RowCounter_tr );
-	iowrite32( 0x0, base0 + AddressMode_tr );	
-	iowrite32( 0x3, base0 + InterruptMask_tr );
+	iowrite32( COM_COUNT, base + MainCounter_tr ); //set dma total data 512 byte
+	iowrite32( addr, base + Address_tr ); //dma source atart address
+	iowrite32( 0x0, base + Bias_tr );
+	iowrite32( 0x0, base + RowCounter_tr );
+	iowrite32( 0x0, base + AddressMode_tr );	
+	iowrite32( mask, base + InterruptMask_tr );
 	
-	iowrite32( COM_COUNT, base1 + MainCounter_rcv ); //set dma total data 512 byte
-	iowrite32( dst_addr, base1 + Address_rcv ); //dma destination atart address	
-	iowrite32( 0x0, base1 + Bias_rcv );
-	iowrite32( 0x0, base1 + RowCounter_rcv );
-	iowrite32( 0x0, base1 + AddressMode_rcv );	
-	iowrite32( 0x3, base1 + InterruptMask_rcv );
+//iowrite32(COM_CONTROL_EN,base + CSR_tr);
+return 0;
+   }
+
+int comp_dma_run_rcv( uint32_t addr, uint32_t base, uint32_t mask) {
+    //write data
+	iowrite32( COM_COUNT, base + MainCounter_rcv ); //set dma total data 512 byte
+	iowrite32( addr, base + Address_rcv ); //dma destination atart address
+	iowrite32( 0x0, base + Bias_rcv );
+	iowrite32( 0x0, base + RowCounter_rcv );
+	iowrite32( 0x0, base + AddressMode_rcv );	
+	iowrite32( mask, base + InterruptMask_rcv );
 	
-	iowrite32(COM_CONTROL_EN,base0 + CSR_tr);
-	// comp_delay (50);
-	iowrite32(COM_CONTROL_EN,base1 + CSR_rcv);
-    rumboot_printf( "check_run\n" );
-
-
+	//iowrite32(COM_CONTROL_EN,base + CSR_rcv);
+return 0;
+   }
+   
+void comp_delay( uint32_t timeout){
+uint32_t count ;
+do { count = timeout;
+ rumboot_printf( "count=%d\n",count );
+} while ( --timeout );	
+} 
+  
+int comp_dma_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1 ) {
+	uint32_t result;
+	
+	result =comp_dma_run_tr(src_addr,base0,0x3);
+	result =comp_dma_run_rcv(dst_addr,base1,0x3);
+	
+	iowrite32(COM_CONTROL_EN,base0+ CSR_tr);
+	
+	#ifdef COM_DO_DELAY
+	comp_delay (25);
+	#endif
+	
+	iowrite32(COM_CONTROL_EN,base1	+ CSR_rcv);
+	
+	rumboot_printf( "check_run\n" );
+	
 	if (com_simple_wait_complete(base0, CSR_tr)) {
         rumboot_printf( "COM0_Cpl  timeout_1\n" );
         return 1;
@@ -41,21 +69,15 @@
         rumboot_printf( "COM1_Cpl  timeout_1\n" );
         return 1;
     }
-	return 0;
-   }
+return result;
+   }   
 
-void comp_delay( uint32_t timeout){
-uint32_t count ;
-do { count = timeout;
- rumboot_printf( "count=%d\n",count );
-} while ( --timeout );	
-}
+
+
 static inline __attribute__((always_inline)) uint32_t wait_com_int_handled( uint32_t timeout, volatile uint32_t * const  flag ) {
     do {
         rumboot_printf( "COMPORT  wait interrupt Cpl\n" ); 
-		if( *flag ) {
-			// rumboot_printf( "*flag=%d\n",(*flag) );
-			//  rumboot_printf( "flag=%d\n",(&flag) );
+		if( *flag ) {		
             *flag = 0;
             msync();
 
@@ -65,49 +87,31 @@ static inline __attribute__((always_inline)) uint32_t wait_com_int_handled( uint
 
     return 0;
 }
-
-
 int comp_dma_irq_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1, uint32_t COM0_Cpl,uint32_t COM1_Cpl ) {
-
- 
-	iowrite32( COM_COUNT,  base0 + MainCounter_tr ); //set dma total data 512 byte
-	iowrite32( src_addr,  base0 + Address_tr ); //dma source atart address
-	iowrite32( 0x0, base0 + Bias_tr );
-	iowrite32( 0x0, base0 + RowCounter_tr );
-	iowrite32( 0x0, base0 + AddressMode_tr );	
-	iowrite32( 0x0, base0 + InterruptMask_tr );
+	uint32_t result;
 	
+	result = comp_dma_run_tr(src_addr,base0,0x0);	
+	result = comp_dma_run_rcv(dst_addr,base1,0x0);
 	
-	iowrite32( COM_COUNT, base1 + MainCounter_rcv ); //set dma total data 512 byte
-	iowrite32( dst_addr, base1 + Address_rcv ); //dma destination atart address	
-	iowrite32( 0x0, base1 + Bias_rcv );
-	iowrite32( 0x0, base1 + RowCounter_rcv );
-	iowrite32( 0x0, base1 + AddressMode_rcv );	
-	iowrite32( 0x0, base1 + InterruptMask_rcv );
-	
-
 	iowrite32(COM_CONTROL_EN,base0 + CSR_tr);
-	iowrite32(COM_CONTROL_EN,base1 + CSR_rcv);
-    rumboot_printf( "check_irq_run\n" );
- 
+	iowrite32(COM_CONTROL_EN,base1	+ CSR_rcv);	
+	
+	rumboot_printf( "check_run\n" );
 
 	if( !wait_com_int_handled( 1000, &COM0_Cpl) )
-		{
-			
+		{			
     rumboot_printf( "COM0_Cpl interrupt timeout_1\n" );
         return 1;
     }
 	 rumboot_printf( "COM0_Cpl_trm interrupt  is ended\n" );
-
 	if( !wait_com_int_handled( 1000, &COM1_Cpl) ) {	
         rumboot_printf( "COM1_Cpl interrupt timeout_1\n" );
         return 1;
     }
 	rumboot_printf( "COM1_Cpl_rcv interrupt is ended\n" );
+	return result;
+   }   
 
-    return 0;
-} 
- 
 
 int com_simple_wait_complete(uintptr_t base, uintptr_t cntrl )
 {
