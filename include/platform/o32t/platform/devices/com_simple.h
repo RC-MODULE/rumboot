a3cd7eab31374e8139070cef875b652dab0d8958
Lyudmila Alexandrova <laalexan.module.ru>
2021-02-23 18:47:13 +0300

    change commport func for test lib

--- include/platform/o32t/platform/devices/com_simple.h
+++ include/platform/o32t/platform/devices/com_simple.h
@@ -8,7 +8,7 @@
 #define COM_CONTROL_EN  0x1
 #define COM_CONTROL_CPL 0x2
 
-#define COM_DO_DELAY	0x1
+//#define COM_DO_DELAY	0x1
 #define COM_COUNT 		  0x40
 /*
 typedef struct com_cfg {
@@ -30,4 +30,5 @@ int comp_dma_run_tr(uint32_t addr, uint32_t base, uint32_t mask);
 int comp_dma_run_rcv(uint32_t addr, uint32_t base, uint32_t mask);
 int comp_dma_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1 );
 int comp_dma_irq_run( uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1,uint32_t COM0_Cpl,uint32_t COM1_Cpl  );  
+int comp_dma_run_xrdy(uint32_t src_addr, uint32_t dst_addr,uint32_t base0, uint32_t base1 );
 int com_simple_wait_complete(uintptr_t base, uintptr_t ctrl);
