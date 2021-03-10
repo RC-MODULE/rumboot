91f34d410e8a22f58c753a41f3ffd73586ecfbbb
Leonid Shchigorev <l.shchigorev@module.ru>
2021-03-10 14:57:51 +0300

    test_ndma_apb was added

--- include/platform/o32t/platform/devices/ndma_simple.h
+++ include/platform/o32t/platform/devices/ndma_simple.h
@@ -5,8 +5,9 @@ typedef enum ndma_ams {ONE_DIMENSION, TWO_DIMENSION} ndma_am_t;
 #define NDMA_IRQ_MASK_COMPLETE 0x1
 #define NDMA_IRQ_MASK_ERROR    0x2
 
-#define NDMA_CONTROL_EN  0x1
-#define NDMA_CONTROL_CPL 0x2
+#define NDMA_CONTROL_DFLT  0x0
+#define NDMA_CONTROL_EN    0x1
+#define NDMA_CONTROL_CPL   0x2
 
 typedef struct ndma_cfg {
   uint32_t  RD_Address      ;
@@ -25,3 +26,5 @@ typedef struct ndma_cfg {
 int ndma_simple_run(uintptr_t base, ndma_cfg_t * cfg);
 int ndma_simple_wait_complete(uintptr_t base);
 int ndma_simple_memcpy(uintptr_t base, ndma_cfg_t * cfg);
+int ndma_apb_check(uintptr_t base, ndma_cfg_t * cfg);
+int ndma_apb_running_one(uintptr_t base);
--- src/platform/o32t/lib/drivers/ndma_simple.c
+++ src/platform/o32t/lib/drivers/ndma_simple.c
@@ -32,6 +32,7 @@ int ndma_simple_wait_complete(uintptr_t base)
   return 0;
 }
 
+
 int ndma_simple_memcpy(uintptr_t base, ndma_cfg_t * cfg)
 {
   int res;
@@ -40,4 +41,94 @@ int ndma_simple_memcpy(uintptr_t base, ndma_cfg_t * cfg)
   if(res) return res;
   res = ndma_simple_wait_complete(base);
   return res;
+}
+
+
+int ndma_apb_check(uintptr_t base, ndma_cfg_t * cfg)
+{
+  iowrite32(cfg->RD_Address    ,base+NDMARD_Address     );
+  iowrite32(cfg->RD_Bias       ,base+NDMARD_Bias        );
+  iowrite32(cfg->RD_RowCounter ,base+NDMARD_RowCounter  );
+  iowrite32(cfg->RD_AddressMode,base+NDMARD_AddressMode );
+  iowrite32(cfg->WR_Address    ,base+NDMAWR_Address     );
+  iowrite32(cfg->WR_Bias       ,base+NDMAWR_Bias        );
+  iowrite32(cfg->WR_RowCounter ,base+NDMAWR_RowCounter  );
+  iowrite32(cfg->WR_AddressMode,base+NDMAWR_AddressMode );
+  iowrite32(cfg->MainCounter   ,base+NDMA_MainCounter   );
+  iowrite32(cfg->InterruptMask ,base+NDMA_InterruptMask );  
+  iowrite32(NDMA_CONTROL_DFLT  ,base+NDMA_Control);
+  
+  if (ioread32(base+NDMARD_Address) != cfg->RD_Address)
+  {
+      rumboot_printf("NDMARD_Address read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMARD_Bias) != cfg->RD_Bias)
+  {
+      rumboot_printf("NDMARD_Bias read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMARD_RowCounter) != cfg->RD_RowCounter)
+  {
+      rumboot_printf("NDMARD_RowCounter read fail\n");
+      return 1;   
+  }      
+  if (ioread32(base+NDMARD_AddressMode) != cfg->RD_AddressMode)
+  {
+      rumboot_printf("NDMARD_AddressMode read fail\n");
+      return 1;
+  }
+
+  if (ioread32(base+NDMAWR_Address) != cfg->WR_Address)
+  {
+      rumboot_printf("NDMAWR_Address read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMAWR_Bias) != cfg->WR_Bias)
+  {
+      rumboot_printf("NDMAWR_Bias read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMAWR_RowCounter) != cfg->WR_RowCounter)
+  {
+      rumboot_printf("NDMAWR_RowCounter read fail\n");
+      return 1;    
+  }
+  if (ioread32(base+NDMAWR_AddressMode) != cfg->WR_AddressMode)
+  {
+      rumboot_printf("NDMAWR_AddressMode read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMA_MainCounter) != cfg->MainCounter)
+  {
+      rumboot_printf("NDMA_MainCounter read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMA_InterruptMask) != cfg->InterruptMask)
+  {
+      rumboot_printf("NDMA_InterruptMask read fail\n");
+      return 1;
+  }
+  if (ioread32(base+NDMA_Control) != NDMA_CONTROL_DFLT)
+  {
+      rumboot_printf("NDMA_Control read fail\n");
+      return 1;
+  }
+  return 0;
+}
+
+int ndma_apb_running_one(uintptr_t base)
+{
+    uint32_t data = 1;
+    for (int i=0; i<32; i++)
+    {
+        iowrite32(data    ,base+NDMARD_Address     );
+        if (ioread32(base+NDMARD_Address) != data)
+        {
+            rumboot_printf("NDMAC apb running 1 fail, data %x", data);
+            return 1;
+        }
+        data = data << 1;
+    }
+    return 0;
 }
