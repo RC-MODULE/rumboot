c7c6c89b53bf9ada72ded27d20e44e567f9fe03b
Leonid Shchigorev <l.shchigorev@module.ru>
2021-03-12 14:28:19 +0300

    test_ndma_mem was added

--- cmake/platforms/o32t.cmake
+++ cmake/platforms/o32t.cmake
@@ -138,6 +138,34 @@ macro(RUMBOOT_PLATFORM_ADD_COMPONENTS)
   #  CONFIGURATION IRAM_IM0
   #  PREFIX iram-im0
   #)
+  
+  add_rumboot_target(
+      CONFIGURATION IRAM_IM1
+      FILES simple-iram/test_ndma_mem.c
+      CFLAGS -DRD_TEST_BANK="IM1" -DWR_TEST_BANK="IM1"
+      PREFIX rd-im1-wr-im1
+    )
+    
+  add_rumboot_target(
+      CONFIGURATION IRAM_IM1
+      FILES simple-iram/test_ndma_mem.c
+      CFLAGS -DRD_TEST_BANK="IM2" -DWR_TEST_BANK="IM2"
+      PREFIX rd-im2-wr-im2
+    )
+    
+    add_rumboot_target(
+      CONFIGURATION IRAM_IM1
+      FILES simple-iram/test_ndma_mem.c
+      CFLAGS -DRD_TEST_BANK="IM3" -DWR_TEST_BANK="IM3"
+      PREFIX rd-im3-wr-im3
+    )
+    
+    add_rumboot_target(
+      CONFIGURATION IRAM_IM1
+      FILES simple-iram/test_ndma_mem.c
+      CFLAGS -DRD_TEST_BANK="SRAM0" -DWR_TEST_BANK="SRAM0"
+      PREFIX rd-sram0-wr-sram0
+    )
 
   
     #  The one test, checking access to closed space
