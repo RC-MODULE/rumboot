0b358540628b3cd11e9a4123757f4a5efaae6de9
Andrew Andrianov <andrew@ncrmnt.org>
2021-03-18 19:13:40 +0300

    nmc: Use nfifo for all communications
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/nmc/glue.c
+++ src/platform/nmc/glue.c
@@ -8,33 +8,52 @@
 #include <rumboot/rumboot.h>
 #include <rumboot/printf.h>
 #include <devices/irq-nmc.h> 
+#include <devices/nfifo.h>
 
-#ifndef CMAKE_BUILD_TYPE_DEBUG
-void  rumboot_platform_putchar(uint8_t c)
-{
-
-}
-
-int rumboot_platform_getchar(uint32_t timeout_us)
-{
-
-}
-#endif
 
 uint32_t rumboot_platform_get_uptime()
 {
         return (ioread32(TIMERS_BASE) / 100);
 }
 
+int64_t rumboot_virt_to_phys(volatile void *addr)
+{
+    return ((uintptr_t) addr << 2);
+}
+
+uint32_t rumboot_virt_to_dma(volatile void *addr) 
+{
+    return ((uintptr_t) addr << 2);
+}
+
 extern char rumboot_IRAM_heap_start;
 extern char rumboot_IRAM_heap_end;
+static struct nfifo_instance g_nfifo;
 
 void rumboot_platform_setup()
 {
         iowrite32(0x0, TIMERS_BASE); 
         iowrite32(0x3, TIMERS_BASE + 8);
+        nfifo_instance_init(&g_nfifo, FIFO_BASE);
+        nfifo_enable(&g_nfifo, 1);
         rumboot_irq_register_nmc_intc_controller();
         rumboot_malloc_register_heap("IM",
                                      &rumboot_IRAM_heap_start, &rumboot_IRAM_heap_end);
-
 }
+
+void rumboot_platform_exit(int status) {
+        nfifo_write(&g_nfifo, (1<<31) | status);
+}
+
+#ifndef CMAKE_BUILD_TYPE_DEBUG
+void  rumboot_platform_putchar(uint8_t c)
+{
+        nfifo_write(&g_nfifo, c);
+}
+
+int rumboot_platform_getchar(uint32_t timeout_us)
+{
+        return nfifo_read(&g_nfifo, timeout_us);
+}
+#endif
+
