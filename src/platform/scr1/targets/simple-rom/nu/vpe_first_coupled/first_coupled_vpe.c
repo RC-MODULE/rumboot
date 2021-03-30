1b69f45a9388c8032a1522906e58f72b3fa77850
Ilya Nasonov <nasonov@module.ru>
2021-03-30 16:57:46 +0300

    NU VPE test: get heap_id from main

--- src/platform/scr1/targets/simple-rom/nu/vpe_first_coupled/first_coupled_vpe.c
+++ src/platform/scr1/targets/simple-rom/nu/vpe_first_coupled/first_coupled_vpe.c
@@ -42,7 +42,7 @@ void nu_vpe_decide_dma_config (ConfigVPE* cfg, CubeMetrics* metrics, ConfigDMAVP
 }
 
 
-void* nu_vpe_load_op01(ConfigOp01* cfg, int index) {
+void* nu_vpe_load_op01(int heap_id, ConfigOp01* cfg, int index) {
   void* op;
   CubeMetrics* cube_metrics;
   VectorMetrics* vec_metrics;
@@ -50,9 +50,9 @@ void* nu_vpe_load_op01(ConfigOp01* cfg, int index) {
     // Try If OP0 Is A Cube
   if((cfg->alu_en==Enable_En && cfg->alu_mode==Mode_Element) || 
      (cfg->mux_en==Enable_En && cfg->mux_mode==Mode_Element) ) {
-    cube_metrics = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,sizeof(CubeMetrics),sizeof(int32_t));
+    cube_metrics = rumboot_malloc_from_heap_aligned(heap_id,sizeof(CubeMetrics),sizeof(int32_t));
     rumboot_platform_request_file(index?"metrics_op1_cube_tag":"metrics_op0_cube_tag",(uintptr_t)cube_metrics);
-    op = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,cube_metrics->s,64);
+    op = rumboot_malloc_from_heap_aligned(heap_id,cube_metrics->s,64);
     rumboot_platform_request_file(index?"op1_cube_file_tag":"op0_cube_file_tag",(uintptr_t)op);
     return op;
   } 
@@ -60,16 +60,16 @@ void* nu_vpe_load_op01(ConfigOp01* cfg, int index) {
     // Try If Op0 Is A Vector
   if((cfg->alu_en==Enable_En && cfg->alu_mode==Mode_Channel) || 
      (cfg->mux_en==Enable_En && cfg->mux_mode==Mode_Channel) ) {
-    vec_metrics = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,sizeof(VectorMetrics),sizeof(int32_t));
+    vec_metrics = rumboot_malloc_from_heap_aligned(heap_id,sizeof(VectorMetrics),sizeof(int32_t));
     rumboot_platform_request_file(index?"metrics_op1_vec_tag":"metrics_op0_vec_tag",(uintptr_t)vec_metrics);
-    op = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,vec_metrics->s,64);
+    op = rumboot_malloc_from_heap_aligned(heap_id,vec_metrics->s,64);
     rumboot_platform_request_file(index?"op1_vec_file_tag":"op0_vec_file_tag",(uintptr_t)op);
     return op;
   } 
   
   return NULL;
 }
-void* nu_vpe_load_op2(ConfigOp2* cfg) {
+void* nu_vpe_load_op2(int heap_id, ConfigOp2* cfg) {
   void* op;
   CubeMetrics* cube_metrics;
   VectorMetrics* vec_metrics;
@@ -77,9 +77,9 @@ void* nu_vpe_load_op2(ConfigOp2* cfg) {
     // Try If OP2 Is A Cube
   if((cfg->alu_en==Enable_En && cfg->alu_mode==Mode_Element) || 
      (cfg->mux_en==Enable_En && cfg->mux_mode==Mode_Element) ) {
-    cube_metrics = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,sizeof(CubeMetrics),sizeof(int32_t));
+    cube_metrics = rumboot_malloc_from_heap_aligned(heap_id,sizeof(CubeMetrics),sizeof(int32_t));
     rumboot_platform_request_file("metrics_op2_cube_tag",(uintptr_t)cube_metrics);
-    op = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,cube_metrics->s,64);
+    op = rumboot_malloc_from_heap_aligned(heap_id,cube_metrics->s,64);
     rumboot_platform_request_file("op2_cube_file_tag",(uintptr_t)op);
     return op;
   } 
@@ -87,9 +87,9 @@ void* nu_vpe_load_op2(ConfigOp2* cfg) {
     // Try If Op2 Is A Vector
   if((cfg->alu_en==Enable_En && cfg->alu_mode==Mode_Channel) || 
      (cfg->mux_en==Enable_En && cfg->mux_mode==Mode_Channel) ) {
-    vec_metrics = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,sizeof(VectorMetrics),sizeof(int32_t));
+    vec_metrics = rumboot_malloc_from_heap_aligned(heap_id,sizeof(VectorMetrics),sizeof(int32_t));
     rumboot_platform_request_file("metrics_op2_vec_tag",(uintptr_t)vec_metrics);
-    op = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,vec_metrics->s,64);
+    op = rumboot_malloc_from_heap_aligned(heap_id,vec_metrics->s,64);
     rumboot_platform_request_file("op2_vec_file_tag",(uintptr_t)op);
     return op;
   } 
@@ -126,13 +126,13 @@ int main() {
   
     // Load OP0-OP2 Operands If Needed
   if(cfg.op0_en==Enable_En) {
-    op0 = nu_vpe_load_op01(&cfg.op0_config,0);
+    op0 = nu_vpe_load_op01(NU_VPE_HEAPID,&cfg.op0_config,0);
   }
   if(cfg.op1_en==Enable_En) {
-    op1 = nu_vpe_load_op01(&cfg.op1_config,1);
+    op1 = nu_vpe_load_op01(NU_VPE_HEAPID,&cfg.op1_config,1);
   }
   if(cfg.op2_en==Enable_En) {
-    op2 = nu_vpe_load_op2(&cfg.op2_config);
+    op2 = nu_vpe_load_op2(NU_VPE_HEAPID,&cfg.op2_config);
   }
   
   etalon = rumboot_malloc_from_heap_aligned(NU_VPE_HEAPID,res_metrics->s, 32);
