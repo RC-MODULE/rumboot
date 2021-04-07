b9237bd4e1d9ed7d613dfa39a22089c9fcd3e71a
Andrew Andrianov <andrew@ncrmnt.org>
2021-04-07 15:55:09 +0300

    logo: Switch to a more compact V3 logo
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/logo.c
+++ src/lib/logo.c
@@ -1,25 +1,11 @@
 #include <rumboot/printf.h>
 #include <rumboot/version.h>
 static const char rumboot_logo[] =
-"\n\n\n"
-" ,ggggggggggg,                               ,ggggggggggg,\n"
-"dP\"\"\"88\"\"\"\"\"\"Y8,                            dP\"\"\"88\"\"\"\"\"\"Y8,                        I8\n"
-"Yb,  88      `8b                            Yb,  88      `8b                        I8   \n"
-" `\"  88      ,8P                             `\"  88      ,8P                     88888888\n"
-"     88aaaad8P\"                                  88aaaad8P\"                         I8   \n"
-"     88\"\"\"\"Yb,    gg      gg   ,ggg,,ggg,,ggg,   88\"\"\"\"Y8ba    ,ggggg,    ,ggggg,   I8\n"
-"     88     \"8b   I8      8I  ,8\" \"8P\" \"8P\" \"8,  88      `8b  dP\"  \"Y8gggdP\"  \"Y8g  I8\n"
-"     88      `8i  I8,    ,8I  I8   8I   8I   8I  88      ,8P i8'    ,8I i8'    ,8I  I8,  \n"
-"     88       Yb,,d8b,  ,d8b,,dP   8I   8I   Yb, 88_____,d8',d8,   ,d8',d8,   ,d8'  88b, \n"
-"     88        Y88P'\"Y88P\"`Y88P'   8I   8I   `Y888888888P\"  P\"Y8888P\"  P\"Y8888P\"     \"Y888\n";
-                                                                                      
-                                                                                         
-            
-
-static const char rumboot_host_logo[] = ""
-"\n";
-
-//static const char version[] = RUMBOOT_PLATFORM " | " CMAKE_BUILD_TYPE " | " RUMBOOT_VERSION "\n";
+"▄▄▄  ▄• ▄▌• ▌ ▄ ·. ▄▄▄▄·             ▄▄▄▄▄\n"
+"▀▄ █·█  █▌· █ ▐███▪▐█ ▀█▪▪     ▪     •██  \n"
+"▐▀▀▄ █▌ █▌ █ ▌▐▌ █·▐█▀▀█▄ ▄█▀▄  ▄█▀▄  ▐█.▪\n"
+"▐█•█▌▐█▄█▌ █  █▌ █▌██▄▪▐█▐█▌.▐▌▐█▌.▐▌ ▐█▌·\n"
+".▀  ▀ ▀▀▀  ▀  █  ▀▀·▀▀▀▀  ▀█▄▀▪ ▀█▄▀▪ ▀▀▀ \n";
 
 void rumboot_print_logo()
 {
@@ -29,8 +15,3 @@ void rumboot_print_logo()
     rumboot_printf("     %s | %s | %s \n\n", inf->platform, CMAKE_BUILD_TYPE, inf->gitversion);
 }
 
-void rumboot_print_host_logo()
-{
-    rumboot_printf(rumboot_host_logo);
-    rumboot_printf("Host mode\n");
-}
