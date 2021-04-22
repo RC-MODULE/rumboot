a1a9f1bf4adb7345db36a20942ceb06aa34a747f
Lyudmila Alexandrova <laalexan.module.ru>
2021-04-22 16:49:13 +0300

    nmc update test for risc

--- src/platform/o32t/targets/simple-iram/test_com_xrdy.c
+++ src/platform/o32t/targets/simple-iram/test_com_xrdy.c
@@ -11,9 +11,10 @@
 #include <platform/devices.h>
 #include <platform/devices/com_simple.h>
 
-
+#ifdef __PPC__
 #include <arch/ppc/arch/ppc_476fp_mmu_fields.h>
 #include <arch/ppc/arch/ppc_476fp_mmu.h>
+#endif
 
 #define COM_DO_DELAY	0x1
 #define ARR_SIZE 128
@@ -67,11 +68,13 @@ void prepare_arrays( uint32_t ** src, uint32_t ** dst ) {
 }
 
 // Crutch Until This Will Be Made For All Tests Accurately
+#ifdef __PPC__
 static struct tlb_entry const em2_nospeculative_tlb_entries[] =
 {
 /*   MMU_TLB_ENTRY(  ERPN,   RPN,        EPN,        DSIZ,                   IL1I,   IL1D,   W,      I,      M,      G,      E,                      UX, UW, UR,     SX, SW, SR      DULXE,  IULXE,      TS,     TID,                WAY,                BID,                V   )*/
     {MMU_TLB_ENTRY(  0x020,  0xC0304,    0xC0304,    MMU_TLBE_DSIZ_16KB,     0b1,    0b1,    0b0,    0b1,    0b0,    0b1,    MMU_TLBE_E_LITTLE_END,  0b0,0b0,0b0,    0b0,0b1,0b1,    0b0,    0b0,        0b0,    MEM_WINDOW_SHARED,  MMU_TLBWE_WAY_1,    MMU_TLBWE_BE_UND,   0b1 )}
 };
+#endif
 
 int main()
 {
@@ -81,8 +84,9 @@ int main()
 	uint32_t * src1;
 	uint32_t * dst1;
   rumboot_printf("Start COMMPORT\n");
-  
+#ifdef __PPC__  
   write_tlb_entries(em2_nospeculative_tlb_entries, ARRAY_SIZE(em2_nospeculative_tlb_entries));
+#endif
     prepare_arrays( &src0, &dst0 );
     prepare_arrays( &src1, &dst1 );
     if( (comp_dma_run_xrdy(rumboot_virt_to_dma(src0), rumboot_virt_to_dma(dst0),COM0_BASE,COM1_BASE,(ARR_SIZE>>1) ) != 0)
