3e95029e7f8d56ff1c96fb6177dd30280653fc86
v.koshlyak <v.koshlyak@module.ru>
2021-05-05 22:08:25 +0300

    PPE_14; npe_rm update

--- src/platform/scr1/targets/simple-rom/nu/coupled_with_rm/coupled_loop_ppe_new.c
+++ src/platform/scr1/targets/simple-rom/nu/coupled_with_rm/coupled_loop_ppe_new.c
@@ -36,39 +36,27 @@ int main() {
   for (i=0; i<it_nmb && !res; i++) {
     rumboot_malloc_update_heaps(1);
 
+    nu_ppe_load_cfg_by_tag(heap_id, &cfg, cfg_file_tag[i]);
+
+    #ifdef MEMtoMEM
     in_metrics  = nu_load_cube_metrics(heap_id, metrics_in_tag[i]);
-
-    //in_metrics  = nu_load_cube_metrics(heap_id, metrics_in_ameba_tag[i]);
-    //in_metrics  = nu_load_cube_metrics(heap_id, metrics_in_with_unused_tag[i]);
-
     res_metrics = nu_load_cube_metrics (heap_id, metrics_etalon_tag[i]);
-    //res_metrics = nu_load_cube_metrics (heap_id, metrics_etalon_ameba_tag[i]);
-    //res_metrics = nu_load_cube_metrics (heap_id, metrics_etalon_with_unused_tag[i]);
 
     if (in_metrics == NULL || res_metrics == NULL) res = 1;
 
     if (!res) {
       in_data     = nu_load_cube(heap_id, in_file_tag[i], in_metrics);
-      //in_data     = nu_load_cube(heap_id, in_ameba_file_tag[i], in_metrics);
-      //in_data     = nu_load_cube(heap_id, in_with_unused_file_tag[i], in_metrics);
-
       etalon      = nu_load_cube (heap_id, etalon_file_tag[i], res_metrics);
-      //etalon      = nu_load_cube (heap_id, etalon_ameba_file_tag[i], res_metrics);
-      //etalon      = nu_load_cube (heap_id, etalon_with_unused_file_tag[i], res_metrics);
-
       res_data    = nu_ppe_malloc_res(heap_id, res_metrics);
     }
 
     if (in_data == NULL || etalon == NULL || res_data == NULL) res = 1;
-
-    if (!res) res = nu_ppe_load_cfg_by_tag(heap_id, &cfg, cfg_file_tag[i]);
 
     if (!res) {
       cfg_reg.rBALs = (uintptr_t)in_data;
       cfg_reg.wBALd = (uintptr_t)res_data;
 
       cfg_reg.rOpEn = 0x1;  // is needed to set memory linear all-in-one
-
       res = nu_ppe_decide_dma_config_trivial(&cfg, in_metrics, res_metrics, &cfg_reg);
     }
 
@@ -91,7 +79,6 @@ int main() {
 
       // Wait RDMA then PPE+WDMA
       nu_ppe_rdma_wait_complete(NU_PPE_RDMA_BASE);
-
       nu_ppe_wait_complete(NU_PPE_STANDALONE_BASE);
 
       #ifdef ShowPerf
@@ -101,6 +88,42 @@ int main() {
       clk_cnt = (in_metrics->H * in_metrics->W * in_metrics->C * dtB)/(clk_cnt*100);
       #endif
     }
+    #endif
+
+    #ifdef VPEtoPPE
+    in_metrics  = nu_load_cube_metrics(heap_id, metrics_in_ameba_tag[i]);
+    res_metrics = nu_load_cube_metrics (heap_id, metrics_etalon_tag[i]);
+
+    if (in_metrics == NULL || res_metrics == NULL) res = 1;
+
+    if (!res) {
+      in_data     = nu_load_cube(heap_id, in_ameba_file_tag[i], in_metrics);
+      etalon      = nu_load_cube (heap_id, etalon_file_tag[i], res_metrics);
+      res_data    = nu_ppe_malloc_res(heap_id, res_metrics);
+    }
+
+    if (in_data == NULL || etalon == NULL || res_data == NULL) res = 1;
+
+    if (!res) {
+      cfg_reg.wBALd = (uintptr_t)res_data;
+
+      cfg_reg.rOpEn = 0x0; // to set wOpM to VPE Linear mode
+      res = nu_ppe_decide_dma_config_trivial(&cfg, in_metrics, res_metrics, &cfg_reg);
+    }
+
+    if(!res){
+      nu_ppe_setup_reg(NU_PPE_RDMA_BASE, NU_PPE_STANDALONE_BASE, &cfg_reg);
+
+      cfg_reg.wOpEn  = 0x1; // Set start of PPE+WDMA field to active value
+      nu_ppe_run(NU_PPE_STANDALONE_BASE, &cfg_reg);
+
+      nu_cpdmac_trn256_config(NU_CPDMAC_ASM_BASE,in_data,in_metrics->s);
+      nu_cpdmac_trn256_run(NU_CPDMAC_ASM_BASE);
+
+      nu_cpdmac_trn256_wait_complete(NU_CPDMAC_ASM_BASE);
+      nu_ppe_wait_complete(NU_PPE_STANDALONE_BASE);
+    }
+    #endif
 
     if (!res) {
       rumboot_printf("Comparing...\n");
