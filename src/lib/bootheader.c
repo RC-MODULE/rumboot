d01d80fe07b3b3db670f0df6f9120a06128f86c9
Andrew Andrianov <andrew@ncrmnt.org>
2021-06-18 17:05:25 +0300

    nmc: Adjust bootheader and entry points
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/lib/bootheader.c
+++ src/lib/bootheader.c
@@ -2,7 +2,7 @@
 #include <rumboot/boot.h>
 #include <rumboot/platform.h>
 
-extern int main(int argc, const char *argv[]);
+int main();
 
 /* TODO: Handle 64-bits here */
 #if defined(RUMBOOT_NOINIT)
@@ -21,15 +21,18 @@ struct rumboot_bootheader hdr  = {
 	.magic		= RUMBOOT_HEADER_MAGIC,
 #ifdef __NM__
 	/* NeuroMatrix images always need decapsulation */
-	.flags      = RUMBOOT_FLAG_DECAPS,
+	.flags      = RUMBOOT_FLAG_DECAPS | RUMBOOT_FLAG_SYNC | RUMBOOT_FLAG_KILL,
 #endif
 	.version	= RUMBOOT_HEADER_VERSION,
 	.chip_id	= RUMBOOT_PLATFORM_CHIPID,
 	.chip_rev	= RUMBOOT_PLATFORM_CHIPREV,
+
+	.entry_point	= { THE_ENTRY_POINT, 0x0 },
+
 #if defined (__NM__) && !defined(RUMBOOT_TARGET_CPU_CLUSTER)
 	.target_cpu_cluster = 1,
 #elif defined (RUMBOOT_TARGET_CPU_CLUSTER)
 	.target_cpu_cluster = RUMBOOT_TARGET_CPU_CLUSTER,
 #endif
-	.entry_point	= { THE_ENTRY_POINT, 0x0 },
+
 };
