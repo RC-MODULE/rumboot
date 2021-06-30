756a46a325f2b38f50aa59a9305193f439c0e59c
Andrew Andrianov <andrew@ncrmnt.org>
2021-06-30 20:27:17 +0300

    nmc: Update cp_boot stuff
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/nmc/targets/stubs/cp_boot_stub.c
+++ src/platform/nmc/targets/stubs/cp_boot_stub.c
@@ -24,7 +24,7 @@
 
 
 #ifndef XFER_SIZE_WORDS
-#define XFER_SIZE_WORDS 256
+#define XFER_SIZE_WORDS 2048
 #endif
 
 /* No printf's in production, since bootrom won't be listening to the fifo */
@@ -34,20 +34,23 @@
 
 /* This shit receives data at the start of nmc memory and jumps there after getting 256 words */
 static void (*spl_main)() = 0; 
+char staging_buffer[XFER_SIZE_WORDS * 8] __attribute__ ((aligned (16)));
+
 int main()
 {
-    char staging_buffer[XFER_SIZE_WORDS * 8];
     struct rcm_cp_instance cp; 
     cp_instance_init(&cp, COM_BASE, BASE_FREQ);
     cp_set_speed(&cp, TARGET_FREQ);
-    rumboot_printf("nmstub: Initialized cp at 0x%x, receiving to 0x%x\n", 
-        COM_BASE, staging_buffer, rumboot_virt_to_dma(staging_buffer));
-    udelay(100);
-    cp_start_rx(&cp, rumboot_virt_to_dma(staging_buffer), XFER_SIZE_WORDS * 8);
+    rumboot_printf("nmstub: Initialized cp at 0x%x, receiving to virt: 0x%x (dma: 0x%x) total len: %d bytes\n", 
+        COM_BASE, staging_buffer, rumboot_virt_to_dma(staging_buffer), XFER_SIZE_WORDS * 8);
+    udelay(10);
+    cp_start_rx(&cp, staging_buffer, XFER_SIZE_WORDS * 8);
     cp_wait(&cp, 1, 1, XFER_SIZE_WORDS * 8 * 50);
     rumboot_printf("nmstub: Payload received, moving it to the start\n");
     rumboot_printf("staging: %x %x %x\n", staging_buffer[0], staging_buffer[1], staging_buffer[2]);
     memmove(0x0, staging_buffer, XFER_SIZE_WORDS * 8);
-    rumboot_printf("nmstub: Executing it\n"),
+    rumboot_printf("nmstub: Executing it\n");
     spl_main();
+    rumboot_printf("nmstub: Back from spl, WTF?\n");
+    return 1;
 }
--- src/platform/nmc/targets/stubs/cp_spl.c
+++ src/platform/nmc/targets/stubs/cp_spl.c
@@ -1,6 +1,32 @@
 #include <rumboot/printf.h>
 #include <rumboot/platform.h>
 #include <platform/devices.h>
+#include <devices/rcm_cp.h>
+#include <rumboot/macros.h>
+#include <rumboot/timer.h>
+#include <rumboot/io.h>
+
+#ifndef COM_BASE
+    #error Please provide COM_BASE
+#endif
+
+#ifndef TARGET_FREQ
+    #define TARGET_FREQ 10000
+#endif
+
+#ifndef BASE_FREQ
+    #define BASE_FREQ 100000
+#endif
+
+#ifdef CMAKE_BUILD_TYPE_DEBUG
+#define debug_printf(fmt, ...) rumboot_printf(fmt, ##__VA_ARGS__)
+#else
+#define debug_printf(fmt, ...) 
+
+#endif
+
+uint64_t __attribute__ ((aligned (16))) txbuf[16];
+uint64_t __attribute__ ((aligned (16))) rxbuf[16];
 
 int main()
 {
@@ -11,7 +37,43 @@ int main()
     rumboot_platform_runtime_info->in.magic  = RUMBOOT_SYNC_MAGIC_IN;
     rumboot_platform_runtime_info->out.magic = RUMBOOT_SYNC_MAGIC_OUT;
     /* TODO: Send a char via cp */
+    debug_printf("SPL: Started, cp 0x%x txbuf %x rxbuf %x\n", COM_BASE, rxbuf, txbuf);
+    struct rcm_cp_instance cp; 
+
+    txbuf[0] = 'H';
+    txbuf[1] = 'e';
+    txbuf[2] = 'l';
+    txbuf[3] = 'l';
+    txbuf[4] = 'o';
+    txbuf[5] = '\n';
+    txbuf[6] = 1UL<<63;
+
+    //iowrite32(0x0, TIMERS_BASE); 
+    //iowrite32(0x3, TIMERS_BASE + 8);
+
+    cp_instance_init(&cp, COM_BASE, BASE_FREQ);
+    cp_set_speed(&cp, TARGET_FREQ);
+    debug_printf("SPL: Sending message\n");
+    cp_start_tx(&cp, txbuf, 8 * 8);
+    debug_printf("SPL: Sending message\n");
+    cp_wait_tx(&cp, ARRAY_SIZE(txbuf) * 8 * 50);
+    debug_printf("SPL: Message sent\n");
+    while(1);
+    return 0;
+
+/*
+    cp_start_rx(&cp, tmp, 7 * 8);
+    tmp[7] = 1UL << 63;
+    cp_wait(&cp, 1, 1, ARRAY_SIZE(tmp) * 8 * 50);
+    debug_printf("SPL: Message received\n");
+
+    cp_start_tx(&cp, tmp, 8 * 8);
+    cp_wait(&cp, 1, 1, ARRAY_SIZE(tmp) * 8 * 50);
+    debug_printf("SPL: Message sent\n");
+    udelay(1000);
+    debug_printf("SPL: Still running? Doing emergency exit\n");
 	uint32_t c = 8;
     rumboot_platform_event_raise(EVENT_TERM, &c, 1);
-    return 0;
+    return 1;
+    */
 }
