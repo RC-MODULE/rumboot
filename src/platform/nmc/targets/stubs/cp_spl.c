213eb2432a4e95c8890eb653aa0c72a178034acd
Andrew Andrianov <andrew@ncrmnt.org>
2021-07-01 18:21:19 +0300

    nmc: Adjust cp_spl
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/nmc/targets/stubs/cp_spl.c
+++ src/platform/nmc/targets/stubs/cp_spl.c
@@ -46,10 +46,7 @@ int main()
     txbuf[3] = 'l';
     txbuf[4] = 'o';
     txbuf[5] = '\n';
-    txbuf[6] = 1UL<<63;
-
-    //iowrite32(0x0, TIMERS_BASE); 
-    //iowrite32(0x3, TIMERS_BASE + 8);
+    txbuf[6] = '\n';
 
     cp_instance_init(&cp, COM_BASE, BASE_FREQ);
     cp_set_speed(&cp, TARGET_FREQ);
@@ -57,7 +54,20 @@ int main()
     cp_start_tx(&cp, txbuf, 8 * 8);
     debug_printf("SPL: Sending message\n");
     cp_wait_tx(&cp, ARRAY_SIZE(txbuf) * 8 * 50);
-    debug_printf("SPL: Message sent\n");
+
+    #ifndef CMAKE_BUILD_TYPE_DEBUG
+    debug_printf("SPL: Message sent, receiving\n");
+    cp_start_rx(&cp, rxbuf, 3 * 8);
+    cp_wait_rx(&cp, 3 * 8 * 50);
+    debug_printf("SPL: Done, sending back\n");
+    cp_start_tx(&cp, rxbuf, 3 * 8);
+    cp_wait_tx(&cp, 3 * 8 * 50);
+    #endif // !CMAKE_BUILD_TYPE_DEBUG
+
+    debug_printf("SPL: Done, requesting termination\n");
+    txbuf[0] = 1ULL << 63;
+    cp_start_tx(&cp, rxbuf, 3 * 8);
+    cp_wait_tx(&cp, 3 * 8 * 50);
     while(1);
     return 0;
 
