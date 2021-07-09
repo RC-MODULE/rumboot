03e85576cd81b1a5f9ee96c5493efb52560f45e0
Lyudmila Alexandrova <laalexan.module.ru>
2021-07-09 12:52:44 +0300

    change interrupts number

--- include/platform/nmc/platform/interrupts.h
+++ include/platform/nmc/platform/interrupts.h
@@ -5,11 +5,16 @@
 #define IRQ_ITIMER0  0
 #define IRQ_ITIMER1  1
 #define IRQ_HP       2
-
+/*
 #define CP0_RCV_INT  8
 #define CP0_TRM_INT  9
 #define CP1_RCV_INT  10
 #define CP1_TRM_INT  11
+*/
+#define CP0_RCV_INT  16
+#define CP0_TRM_INT  17
+#define CP1_RCV_INT  18
+#define CP1_TRM_INT  19
 
 
 #endif
