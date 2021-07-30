625bf15612f42bfd1aad13c8bf0b8ef38e51ec3c
Mikhail Chelyshev <mch87@module.ru>
2021-07-30 18:01:33 +0300

    Fix typo in async-demo-ppc.c
    Signed-off-by: Mikhail Chelyshev <mch87@module.ru>

--- src/platform/o32t/targets/async-demo-ppc.c
+++ src/platform/o32t/targets/async-demo-ppc.c
@@ -7,9 +7,10 @@ int main()
 {
     rumboot_printf("Hello, printing something every 100 us\n");
     int i = 0 ;
-    int k = 5
+    int k = 5 ;
     while (k--) {
         rumboot_printf("Hello, my uptime is %d\n", rumboot_platform_get_uptime());
         udelay(300);
-    }    
+    }
+    return 0;
 }
