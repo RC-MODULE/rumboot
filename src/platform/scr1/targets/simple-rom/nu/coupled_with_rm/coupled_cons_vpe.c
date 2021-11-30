e0c6931fc11125c7b20c6929446d57e74cc5ed05
Lyudmila Alexandrova <laalexan.module.ru>
2021-11-30 14:03:22 +0300

    add fun fn_base_it_nmb

--- src/platform/scr1/targets/simple-rom/nu/coupled_with_rm/coupled_cons_vpe.c
+++ src/platform/scr1/targets/simple-rom/nu/coupled_with_rm/coupled_cons_vpe.c
@@ -42,6 +42,11 @@ void nu_vpe_decide_dma_config (
   cfg->wdma_config.dma_data_mode     = Mode_Element; // Copypaste From src_rdma_config in nu_vpe_decide_dma_config_trivial
   cfg->wdma_config.dma_data_use      = DmaDUse_Off;
   
+  cfg->src_rdma_config.dma_bsize=0;
+  cfg->src_rdma_config.dma_bstride=0;
+  cfg->wdma_config.dma_bsize=0;
+  cfg->wdma_config.dma_bstride=0;
+ 
 #ifdef AXI_LEN
     axi_len = AxiLen;
 #else 
@@ -93,6 +98,62 @@ int main() {
   uint32_t productivity_x100;
   uint32_t productivity_frac;  
   
+  char* fn_in_base          			= "in_file_tag_"        ;
+  char* fn_cfg_base         			= "cfg_file_tag_"   	;
+  char* fn_in_metrics_base  			= "metrics_in_tag_"     ;
+  char* fn_res_metrics_base 			= "metrics_etalon_tag_" ;
+  char* fn_etalon_base      			= "etalon_file_tag_"    ;
+  
+  char* fn_metrics_op0_cube_base    	= "metrics_op0_cube_tag_" ;
+  char* fn_metrics_op0_vec_base     	= "metrics_op0_vec_tag_" ;
+  char* fn_op0_cube_file_base   		= "op0_cube_file_tag_" ;
+  char* fn_op0_vec_file_base    		= "op0_vec_file_tag_" ;
+  
+  char* fn_metrics_op1_cube_base    	= "metrics_op1_cube_tag_" ;
+  char* fn_metrics_op1_vec_base     	= "metrics_op1_vec_tag_" ;
+  char* fn_op1_cube_file_base   		= "op1_cube_file_tag_" ;
+  char* fn_op1_vec_file_base    		= "op1_vec_file_tag_" ;
+  
+  char* fn_metrics_op2_cube_base    	= "metrics_op2_cube_tag_" ;
+  char* fn_metrics_op2_vec_base     	= "metrics_op2_vec_tag_" ;
+  char* fn_op2_cube_file_base   		= "op2_cube_file_tag_" ;
+  char* fn_op2_vec_file_base    		= "op2_vec_file_tag_" ;
+  
+  char* fn_metrics_lut1_file_base   	= "metrics_lut1_file_tag_" ; 
+  char* fn_metrics_lut2_file_base   	= "metrics_lut2_file_tag_" ;
+  char* fn_lut1_file_base   			= "lut1_file_tag_" ; 
+  char* fn_lut2_file_base   			= "lut2_file_tag_" ;
+ 
+  char* fn_status_regs_file_base   		= "status_regs_file_tag_" ;
+ 
+  char  fn_in[32];
+  char  fn_cfg[32];
+  char  fn_in_metrics[32];
+  char  fn_res_metrics[32];
+  char  fn_etalon[32];
+   
+  char  fn_metrics_op0_cube[32]; 
+  char  fn_metrics_op0_vec[32];
+  char  fn_op0_cube_file[32]; 
+  char  fn_op0_vec_file[32];
+
+  char  fn_metrics_op1_cube[32]; 
+  char  fn_metrics_op1_vec[32];
+  char  fn_op1_cube_file[32]; 
+  char  fn_op1_vec_file[32];
+
+  char  fn_metrics_op2_cube[32]; 
+  char  fn_metrics_op2_vec[32];
+  char  fn_op2_cube_file[32]; 
+  char  fn_op2_vec_file[32];
+
+  char  fn_metrics_lut1_file[32]; 
+  char  fn_metrics_lut2_file[32];
+  char  fn_lut1_file[32]; 
+  char  fn_lut2_file[32];
+
+  char  fn_status_regs_file[32]; 
+
   rumboot_printf("Hello\n");
   
   heap_id = nu_get_heap_id();
@@ -106,45 +167,73 @@ int main() {
   
   for(i=0;i<iterations;i++) {
     rumboot_printf("Starting iteration %d\n",i);
+	
+	fn_base_it_nmb(fn_in, fn_in_base, i);
+    fn_base_it_nmb(fn_cfg, fn_cfg_base, i);
+    fn_base_it_nmb(fn_in_metrics, fn_in_metrics_base, i);
+    fn_base_it_nmb(fn_res_metrics, fn_res_metrics_base, i);
+    fn_base_it_nmb(fn_etalon, fn_etalon_base, i);
+	
+	fn_base_it_nmb(fn_metrics_op0_cube, fn_metrics_op0_cube_base, i);
+	fn_base_it_nmb(fn_metrics_op0_vec, fn_metrics_op0_vec_base, i);
+	fn_base_it_nmb(fn_op0_cube_file, fn_op0_cube_file_base, i);
+	fn_base_it_nmb(fn_op0_vec_file, fn_op0_vec_file_base, i);	
+	
+	
+	fn_base_it_nmb(fn_metrics_op1_cube, fn_metrics_op1_cube_base, i);
+	fn_base_it_nmb(fn_metrics_op1_vec, fn_metrics_op1_vec_base, i);
+	fn_base_it_nmb(fn_op1_cube_file, fn_op1_cube_file_base, i);
+	fn_base_it_nmb(fn_op1_vec_file, fn_op1_vec_file_base, i);	
+
+	fn_base_it_nmb(fn_metrics_op2_cube, fn_metrics_op2_cube_base, i);
+	fn_base_it_nmb(fn_metrics_op2_vec, fn_metrics_op2_vec_base, i);
+	fn_base_it_nmb(fn_op2_cube_file, fn_op2_cube_file_base, i);
+	fn_base_it_nmb(fn_op2_vec_file, fn_op2_vec_file_base, i);	
+	
+	fn_base_it_nmb(fn_metrics_lut1_file, fn_metrics_lut1_file_base, i);
+	fn_base_it_nmb(fn_metrics_lut2_file, fn_metrics_lut2_file_base, i);
+	fn_base_it_nmb(fn_lut1_file, fn_lut1_file_base, i);
+	fn_base_it_nmb(fn_lut2_file, fn_lut2_file_base, i);
+	
+	fn_base_it_nmb(fn_status_regs_file, fn_status_regs_file_base, i);    
+	
+    if(nu_vpe_load_cfg_by_tag(heap_id, &cfg, fn_cfg) != 0) return -1;
     
-    if(nu_vpe_load_cfg_by_tag(heap_id, &cfg, cfg_file_tag[i]) != 0) return -1;
-    
-    in_metrics = nu_load_cube_metrics(heap_id,metrics_in_tag[i]);
+    in_metrics = nu_load_cube_metrics(heap_id,fn_in_metrics);
     if(in_metrics == NULL) return -1;
     
-    res_metrics= nu_load_cube_metrics(heap_id,metrics_etalon_tag[i]);
+    res_metrics= nu_load_cube_metrics(heap_id,fn_res_metrics);
     if(res_metrics == NULL) return -1;
     
-    in_data = nu_load_cube(heap_id,in_file_tag[i],in_metrics);
+    in_data = nu_load_cube(heap_id,fn_in,in_metrics);
     if(in_data == NULL) return -1;
-    
     res_data = nu_vpe_malloc_res(heap_id, res_metrics);
     if(res_data == NULL) return -1;
     
       // Load OP0-OP2 Operands If Needed
     if(cfg.op0_en==Enable_En) {
-      op0 = nu_vpe_load_op01_by_tags(heap_id,&cfg.op0_config,metrics_op0_cube_tag[i],metrics_op0_vec_tag[i],op0_cube_file_tag[i],op0_vec_file_tag[i]);
+   op0 = nu_vpe_load_op01_by_tags(heap_id,&cfg.op0_config,fn_metrics_op0_cube,fn_metrics_op0_vec,fn_op0_cube_file,fn_op0_vec_file);
     }
     else op0 = NULL;
     if(cfg.op1_en==Enable_En) {
-      op1 = nu_vpe_load_op01_by_tags(heap_id,&cfg.op1_config,metrics_op1_cube_tag[i],metrics_op1_vec_tag[i],op1_cube_file_tag[i],op1_vec_file_tag[i]);
+ 	   op1 = nu_vpe_load_op01_by_tags(heap_id,&cfg.op1_config,fn_metrics_op1_cube,fn_metrics_op1_vec,fn_op1_cube_file,fn_op1_vec_file);
     }
     else op1 = NULL;
     if(cfg.op2_en==Enable_En) {
-      op2 = nu_vpe_load_op2_by_tags(heap_id,&cfg.op2_config,metrics_op2_cube_tag[i],metrics_op2_vec_tag[i],op2_cube_file_tag[i],op2_vec_file_tag[i]);
+ 	   op2 = nu_vpe_load_op2_by_tags(heap_id,&cfg.op2_config,fn_metrics_op2_cube,fn_metrics_op2_vec,fn_op2_cube_file,fn_op2_vec_file);
     }
     else op2 = NULL;
     
       // Load LUTs If Needed
     if(cfg.op2_config.lut_en == Enable_En) {
-      lut1_metrics = nu_load_vec_metrics(heap_id,metrics_lut1_file_tag[i]);
-      lut2_metrics = nu_load_vec_metrics(heap_id,metrics_lut2_file_tag[i]);
-      lut1 = nu_load_vec(heap_id,lut1_file_tag[i],lut1_metrics);
-      lut2 = nu_load_vec(heap_id,lut2_file_tag[i],lut2_metrics);
+	  lut1_metrics = nu_load_vec_metrics(heap_id,fn_metrics_lut1_file);
+      lut2_metrics = nu_load_vec_metrics(heap_id,fn_metrics_lut2_file);
+      lut1 = nu_load_vec(heap_id,fn_lut1_file,lut1_metrics);
+      lut2 = nu_load_vec(heap_id,fn_lut2_file,lut2_metrics);
       nu_vpe_load_lut(MY_VPE_REGS_BASE,lut1,lut2);
     }
     
-    etalon = nu_load_cube(heap_id,etalon_file_tag[i],res_metrics);
+    etalon = nu_load_cube(heap_id,fn_etalon,res_metrics);
     if(etalon == NULL) return -1;
     
     //print_in_data(in_data,in_size);
