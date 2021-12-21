d7d1632cac0c48617d3c69b70e8e2e8a1bd7fd20
Andrew Andrianov <andrew@ncrmnt.org>
2021-12-21 15:24:54 +0300

    native: Reorder tests, add cross-compilation support
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- cmake/platforms/native.cmake
+++ cmake/platforms/native.cmake
@@ -11,8 +11,17 @@ set(RUMBOOT_HAS_V3_BOOTROM Yes)
 
 macro(RUMBOOT_PLATFORM_SET_COMPILER_FLAGS)
     SET(CMAKE_DUMP_FLAGS     "-EL")
-    SET(CMAKE_C_FLAGS     "-std=gnu99 -m32 -g")
-    SET(CMAKE_EXE_LINKER_FLAGS     "-g")
+    SET(CMAKE_C_FLAGS     "-std=gnu99 -g")
+    if (NOT CROSS_COMPILE)
+      SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32") #x86
+      SET(CMAKE_EXE_LINKER_FLAGS     "-m32")
+    elseif (${CROSS_COMPILE} STREQUAL "arm-rcm-linux-gnueabihf")
+      SET(XILINX_FPGA_PROTO YES)
+      SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -DDUT=NPE -DRUMBOOT_ENABLE_NATIVE_PCIE") #FixMe: This shouldn't be here
+    else()
+      message(FATAL_ERROR "Check flags before cross-compiling native for a weird arch")
+    endif()
+    SET(CMAKE_EXE_LINKER_FLAGS     "${CMAKE_EXE_LINKER_FLAGS} -g")
 endmacro()
 
 rumboot_add_configuration(
@@ -59,6 +68,24 @@ endfunction()
 include(${CMAKE_SOURCE_DIR}/cmake/bootrom.cmake)
 
 macro(RUMBOOT_PLATFORM_ADD_COMPONENTS)
+
+  add_rumboot_target_dir(tests/ 
+    CONFIGURATION NATIVE
+    PREFIX test
+  )
+
+  add_rumboot_target(
+    CONFIGURATION NATIVE
+    FILES common/tools/print-heaps.c
+  )
+
+  add_rumboot_target(
+    CONFIGURATION NATIVE
+    FILES scr1/targets/load_bin_example/hello_load_bin.c
+    IRUN_FLAGS +myfile=${CMAKE_SOURCE_DIR}/src/platform/scr1/targets/load_bin_example/data
+  )
+
+  ############# BOOTROM UNIT TESTS #############
   file(GLOB RUMBOOT_TARGETS ${CMAKE_SOURCE_DIR}/src/platform/${RUMBOOT_PLATFORM}/targets/*.c)
   foreach(target ${RUMBOOT_TARGETS})
     add_rumboot_target(
