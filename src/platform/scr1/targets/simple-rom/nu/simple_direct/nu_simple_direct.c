cc1ed4f3ae00887090ab88a482ebe4156853a8a9
Ilya Nasonov <nasonov@module.ru>
2022-02-28 12:11:31 +0300

    fix simple test after nu_lib batch mode work edition

--- src/platform/scr1/targets/simple-rom/nu/simple_direct/nu_simple_direct.c
+++ src/platform/scr1/targets/simple-rom/nu/simple_direct/nu_simple_direct.c
@@ -502,6 +502,12 @@ cfg_mpe->dma_w_config.rdma.   BFCA = nu_virt_to_dma(warr);    //   To A Physical
   
 cfg_vpe->src_rdma_config.dma_bsize=0;   // Batch Parameters Are Default In This Program (Do Not Use Batch)
 cfg_vpe->src_rdma_config.dma_bstride=0;
+cfg_vpe->op0_rdma_config.dma_bsize=0;
+cfg_vpe->op0_rdma_config.dma_bstride=0;
+cfg_vpe->op1_rdma_config.dma_bsize=0;
+cfg_vpe->op1_rdma_config.dma_bstride=0;
+cfg_vpe->op2_rdma_config.dma_bsize=0;
+cfg_vpe->op2_rdma_config.dma_bstride=0;
 cfg_vpe->wdma_config.dma_bsize=0;
 cfg_vpe->wdma_config.dma_bstride=0;
 
