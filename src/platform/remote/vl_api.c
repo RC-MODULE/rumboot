551d1f94a38106ae5630a2a29b285b3216864885
Andrew Andrianov <andrew@ncrmnt.org>
2022-04-19 19:43:33 +0300

    vl_api: Ditch reference function
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/platform/remote/vl_api.c
+++ src/platform/remote/vl_api.c
@@ -186,30 +186,6 @@ void *vl_shmem_map(struct vl_shmem* shmem){
 }
 
 
-int hostname_to_ip(char * hostname , char* ip)
-{
-	struct hostent *he;
-	struct in_addr **addr_list;
-	int i;
-		
-	if ( (he = gethostbyname( hostname ) ) == NULL) 
-	{
-		// get the host info
-		herror("gethostbyname");
-		return 1;
-	}
-
-	addr_list = (struct in_addr **) he->h_addr_list;
-	
-	for(i = 0; addr_list[i] != NULL; i++) 
-	{
-		//Return the first one;
-		strcpy(ip , inet_ntoa(*addr_list[i]) );
-		return 0;
-	}
-	
-	return 1;
-}
 struct vl_instance *vl_create(const char *host, uint16_t port){
 	struct sockaddr_in addr;
 	struct in_addr **addr_list;
