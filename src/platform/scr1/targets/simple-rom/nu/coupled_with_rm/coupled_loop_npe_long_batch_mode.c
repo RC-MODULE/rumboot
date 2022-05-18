734c9a121ddfa742217f2422f0bcdc2f9cfb34bc
Leonid Shchigorev <l.shchigorev@module.ru>
2022-05-18 17:25:31 +0300

    add npe_batch test_cases

--- src/platform/scr1/targets/simple-rom/nu/coupled_with_rm/coupled_loop_npe_long_batch_mode.c
+++ src/platform/scr1/targets/simple-rom/nu/coupled_with_rm/coupled_loop_npe_long_batch_mode.c
@@ -275,7 +275,10 @@ int main() {
       nu_ppe_wdma_run(MY_PPE_REGS_BASE, iteration_desc.cfg_reg_ppe);
     }
     
-    //nu_mpe_print_config(iteration_desc.cfg_mpe);
+    nu_mpe_print_config(iteration_desc.cfg_mpe);
+    //nu_vpe_print_config(iteration_desc.cfg_vpe);
+    //if(iteration_desc.PPE_ENABLED==Enable_En)
+    //  nu_ppe_print_config(iteration_desc.cfg_ppe);
     
     nu_vpe_run(MY_VPE_REGS_BASE, iteration_desc.cfg_vpe);
     nu_mpe_run(MY_MPE_REGS_BASE, iteration_desc.cfg_mpe);
@@ -293,10 +296,7 @@ int main() {
     if(NU_COMPARE_FUNCTION(iteration_desc.res_data, iteration_desc.etalon, (iteration_desc.res_metrics->s*(iteration_desc.cfg_vpe->wdma_config.dma_bsize+1))) == 0)
       rumboot_printf("Iteration %d PASSED\n",i);
     else {
-      //nu_mpe_print_config(iteration_desc.cfg_mpe);
-      //nu_vpe_print_config(iteration_desc.cfg_vpe);
-      //if(iteration_desc.PPE_ENABLED==Enable_En)
-      //  nu_ppe_print_config(iteration_desc.cfg_ppe);
+      
       
       rumboot_printf("Test FAILED at invocation %d\n",i);
 
